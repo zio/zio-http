6c8208b29f90e1851689d6eb84335b2bb00da5b3
Dejan Mijic <dejan.mijic@zalando.de>
2019-11-03 22:00:39 +0100

    Reformat the sources

--- core/src/main/scala/zio/http/Method.scala
+++ core/src/main/scala/zio/http/Method.scala
@@ -21,13 +21,13 @@ sealed abstract class Method(val name: String) {
 }
 
 object Method {
-  final case object GET  extends Method("GET")
-  final case object HEAD extends Method("HEAD")
-  final case object POST extends Method("POST")
-  final case object PUT extends Method("PUT")
-  final case object DELETE extends Method("DELETE")
+  final case object GET     extends Method("GET")
+  final case object HEAD    extends Method("HEAD")
+  final case object POST    extends Method("POST")
+  final case object PUT     extends Method("PUT")
+  final case object DELETE  extends Method("DELETE")
   final case object CONNECT extends Method("CONNECT")
   final case object OPTIONS extends Method("OPTIONS")
-  final case object TRACE extends Method("TRACE")
-  final case object PATCH extends Method("PATCH")
+  final case object TRACE   extends Method("TRACE")
+  final case object PATCH   extends Method("PATCH")
 }
