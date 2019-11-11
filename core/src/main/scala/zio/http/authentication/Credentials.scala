d5e9e2095042f1b1ef0bb286074d749423ac8e9e
Mikail Khan <co.category@gmail.com>
2019-11-11 18:42:40 +0000

    remove semi-colons

--- core/src/main/scala/zio/http/authentication/Credentials.scala
+++ core/src/main/scala/zio/http/authentication/Credentials.scala
@@ -1,8 +1,8 @@
 package zio.http.authentication
 
-import java.util.Base64;
+import java.util.Base64
 
 final case class Credentials(value: String) extends AnyVal {
   override def toString =
-    Base64.getEncoder().encodeToString(value.getBytes());
+    Base64.getEncoder().encodeToString(value.getBytes())
 }
