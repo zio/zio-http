72ec5f676984e545f330242ca4501b4de15837cb
pushk4r <vitaly.pushkar@gmail.com>
2019-11-12 21:02:26 +0100

    Fix formatting

--- core/src/main/scala/zio/http/ContentType.scala
+++ core/src/main/scala/zio/http/ContentType.scala
@@ -21,11 +21,11 @@ final case class ContentType(value: String) extends AnyVal {
 }
 
 object ContentType {
-  val Plain = ContentType("text/plain")
-  val HTML = ContentType("text/html")
-  val CSV = ContentType("text/csv")
-  val XML = ContentType("text/xml")
-  val JSON = ContentType("application/json")
+  val Plain       = ContentType("text/plain")
+  val HTML        = ContentType("text/html")
+  val CSV         = ContentType("text/csv")
+  val XML         = ContentType("text/xml")
+  val JSON        = ContentType("application/json")
   val OctetStream = ContentType("application/octet-stream")
-  val Form = ContentType("application/x-www-form-urlencoded")
+  val Form        = ContentType("application/x-www-form-urlencoded")
 }
