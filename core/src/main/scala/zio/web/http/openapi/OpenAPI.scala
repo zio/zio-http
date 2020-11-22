e8398218fd4fdd5d6e4ef9f70c15c76f5d302056
Bendix Saeltz <bendix@saeltz.de>
2020-11-22 21:28:12 +0100

    #91: Link only with operationRef

--- core/src/main/scala/zio/web/http/openapi/OpenAPI.scala
+++ core/src/main/scala/zio/web/http/openapi/OpenAPI.scala
@@ -454,27 +454,19 @@ object OpenAPI {
    *
    * For computing links, and providing instructions to execute them, a runtime expression is used for accessing values in an operation and using them as parameters while invoking the linked operation.
    *
-   * @param operationRef A relative or absolute URI reference to an OAS operation. This field is mutually exclusive of the operationId field, and MUST point to an Operation Object. Relative operationRef values MAY be used to locate an existing Operation Object in the OpenAPI definition.
-   * @param operationId The name of an existing, resolvable OAS operation, as defined with a unique operationId. This field is mutually exclusive of the operationRef field.
-   * @param parameters A map representing parameters to pass to an operation as specified with operationId or identified via operationRef. The key is the parameter name to be used, whereas the value can be a constant or an expression to be evaluated and passed to the linked operation. The parameter name can be qualified using the parameter location [{in}.]{name} for operations that use the same parameter name in different locations (e.g. path.id).
+   * @param operationRef A relative or absolute URI reference to an OAS operation. This field MUST point to an Operation Object. Relative operationRef values MAY be used to locate an existing Operation Object in the OpenAPI definition.
+   * @param parameters A map representing parameters to pass to an operation as identified via operationRef. The key is the parameter name to be used, whereas the value can be a constant or an expression to be evaluated and passed to the linked operation. The parameter name can be qualified using the parameter location [{in}.]{name} for operations that use the same parameter name in different locations (e.g. path.id).
    * @param requestBody A literal value or {expression} to use as a request body when calling the target operation.
    * @param description A description of the link.
    * @param server A server object to be used by the target operation.
    */
   final case class Link(
-    operationRef: Option[URI],
-    operationId: Option[String],
+    operationRef: URI,
     parameters: Map[String, Any],
     requestBody: Any,
     description: Doc,
     server: Option[Server]
-  ) {
-
-    /**
-     * A linked operation MUST be identified using either an operationRef or operationId. In the case of an operationId, it MUST be unique and resolved in the scope of the OAS document. Because of the potential for name clashes, the operationRef syntax is preferred for specifications with external references.
-     */
-    require((operationRef.isDefined && operationId.isEmpty) || (operationRef.isEmpty && operationRef.isDefined))
-  }
+  )
 
   /**
    * Adds metadata to a single tag that is used by the Operation Object. It is not mandatory to have a Tag Object per tag defined in the Operation Object instances.
