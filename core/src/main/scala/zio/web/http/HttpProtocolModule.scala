54abf5d847bbd2d08d1c9dfe48682c835d2519e5
John A. De Goes <john@degoes.net>
2021-01-11 19:14:03 -0500

    Revert "[WIP] Initial makeClient sketch (#95)"
    This reverts commit b49c3a801fe26dbedb7cbe2f6d8b2fa0fbe9fe6b.

--- core/src/main/scala/zio/web/http/HttpProtocolModule.scala
+++ core/src/main/scala/zio/web/http/HttpProtocolModule.scala
@@ -3,8 +3,8 @@ package zio.web.http
 import zio._
 import zio.web.http.model._
 import zio.web._
-import java.io.{ IOException, InputStream }
-import java.net.{ HttpURLConnection, URL }
+
+import java.io.IOException
 
 trait HttpProtocolModule extends ProtocolModule {
   type ServerConfig       = HttpServerConfig
@@ -28,63 +28,5 @@ trait HttpProtocolModule extends ProtocolModule {
 
   override def makeClient[M >: MaxMetadata <: MinMetadata, A](
     endpoints: Endpoints[M, A]
-  ): ZLayer[Has[ClientConfig], IOException, Has[ClientService[A]]] =
-    ZLayer.service[ClientConfig].map { hasClientConfig =>
-      val clientService: ClientService[A] = new ClientService[A] {
-        override def invoke[M, Request, Response, H](endpoint: Endpoint[M, Request, Response, H], request: Request)(
-          implicit ev: A <:< Endpoint[M, Request, Response, H]
-        ): Task[Response] = {
-
-          val requestToJson: Request => Option[String] = ???
-
-          val responseFromJson: InputStream => Task[Response] = ???
-
-          val endpointUrlPart = {
-            def inAnnotation(annotations: Annotations[_]): Option[String] =
-              annotations match {
-                case Annotations.None              => None
-                case Annotations.Cons(Route(r), _) => Some(r)
-                case Annotations.Cons(_, tail)     => inAnnotation(tail)
-              }
-
-            inAnnotation(endpoint.annotations).getOrElse("")
-          }
-
-          val con = new URL(
-            s"${hasClientConfig.get.host}:${hasClientConfig.get.host}/${endpointUrlPart.stripPrefix("/")}"
-          ).openConnection
-            .asInstanceOf[HttpURLConnection]
-
-          requestToJson(request).foreach { strEncodedPayload =>
-            con.setDoOutput(true);
-            val os = con.getOutputStream();
-            os.write(strEncodedPayload.getBytes());
-            os.flush();
-            os.close();
-          }
-
-          val requestMethodStr = {
-            def inAnnotation(annotations: Annotations[_]): Option[Method] =
-              annotations match {
-                case Annotations.None                    => None
-                case Annotations.Cons(method: Method, _) => Some(method)
-                case Annotations.Cons(_, tail)           => inAnnotation(tail)
-              }
-
-            inAnnotation(endpoint.annotations).getOrElse(Method.GET).toString
-          }
-
-          con.setRequestMethod(requestMethodStr)
-          if (con.getResponseCode == HttpURLConnection.HTTP_OK) {
-            ZManaged.fromAutoCloseable(ZIO(con.getInputStream)).use { inputStream =>
-              responseFromJson(inputStream)
-            }
-          } else {
-            ZIO.fail(new Exception(s"Server responded with ${con.getResponseCode}"))
-          }
-        }
-      }
-
-      Has(clientService)
-    }
+  ): ZLayer[Has[ClientConfig], IOException, Has[ClientService[A]]] = ???
 }
