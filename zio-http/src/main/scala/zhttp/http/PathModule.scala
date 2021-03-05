001cf0a6d6a69df5fbdbfeb947f70952d4ff4ea0
Tushar Mathur <tusharmath@gmail.com>
2021-03-05 18:25:23 +0530

    refactor(path): flatten Path

---
+++ zio-http/src/main/scala/zhttp/http/PathModule.scala
@@ -0,0 +1,35 @@
+package zhttp.http
+
+trait PathModule { module =>
+  sealed trait Path {
+    self =>
+    def asString: String
+
+    def /(name: String): Path = append(name)
+
+    def append(name: String): Path = if (name.isEmpty) this else module./(this, name)
+
+    def toList: List[String]
+
+    override def toString: String = this.asString
+  }
+
+  object Path {
+    def apply(): Path                               = Root
+    def apply(string: String): Path                 = if (string.isBlank) Root else Path(string.split("/").toList)
+    def apply(seqString: String*): Path             = Path(seqString.toList)
+    def apply(list: List[String]): Path             = list.foldLeft[Path](Root)((a, s) => a.append(s))
+    def unapplySeq(arg: Path): Option[List[String]] = Option(arg.toList)
+    def empty: Path                                 = Root
+  }
+
+  case class /(path: Path, name: String) extends Path {
+    override lazy val asString: String = s"${path.asString}/${name}"
+    override def toList: List[String]  = path.toList ::: List(name)
+  }
+
+  case object Root extends Path {
+    override lazy val asString: String = ""
+    override def toList: List[String]  = Nil
+  }
+}
--- zio-http/src/main/scala/zhttp/http/package.scala
+++ zio-http/src/main/scala/zhttp/http/package.scala
@@ -4,7 +4,7 @@ import io.netty.util.CharsetUtil
 
 import java.nio.charset.Charset
 
-package object http {
+package object http extends PathModule {
   type HttpApp[-R, +E] = Http[R, E, Request, Response]
   type Endpoint        = (Method, URL)
   type Route           = (Method, Path)
