1ff641b1735e1c62cc70bea7b7ce3e2ef742efc3
Tushar Mathur <tusharmath@gmail.com>
2021-03-18 15:46:40 +0530

    fix(http): improve CanBeSilenced
    Handle Throwable and HttpError separately

--- zio-http/src/main/scala/zhttp/http/CanBeSilenced.scala
+++ zio-http/src/main/scala/zhttp/http/CanBeSilenced.scala
@@ -5,7 +5,10 @@ trait CanBeSilenced[-E, +A] {
 }
 
 object CanBeSilenced {
-  implicit object SilenceHttpError extends CanBeSilenced[HttpError, Response] {
-    override def silent(e: HttpError): Response = e.toResponse
+  implicit object SilenceHttpError extends CanBeSilenced[Throwable, Response] {
+    override def silent(e: Throwable): Response = e match {
+      case m: HttpError => m.toResponse
+      case m            => Response.fromHttpError(HttpError.InternalServerError("Internal Server Error", Option(m)))
+    }
   }
 }
