0ec926643a592545ff37ad2dcf97083f78fb5ca3
Tushar Mathur <tusharmath@gmail.com>
2021-03-22 17:02:01 +0530

    refactor(example):add SocketEchoServer example

---
+++ example/src/main/scala/SocketEchoServer.scala
@@ -0,0 +1,20 @@
+import zhttp.http._
+import zhttp.service._
+import zhttp.socket._
+import zio._
+import zio.duration._
+import zio.stream.ZStream
+
+object SocketEchoServer extends App {
+  private val socket =
+    Socket.forall[WebSocketFrame](msg => ZStream.repeat(msg).schedule(Schedule.spaced(1 second)).take(10))
+
+  private val app =
+    Http.collectM[Request] {
+      case Method.GET -> Root / "greet" / name  => UIO(Response.text(s"Greetings {$name}!"))
+      case Method.GET -> Root / "subscriptions" => socket.asResponse(None)
+    }
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    Server.start(8090, app).exitCode
+}
