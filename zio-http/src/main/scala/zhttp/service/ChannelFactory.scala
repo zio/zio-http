b7b78c07a9c2ff7488c357eaf0e99c2e20af78e6
Tushar Mathur <tushar@dream11.com>
2021-03-26 17:54:03 +0530

    refactor: remove unnecessary changes (#76)

--- zio-http/src/main/scala/zhttp/service/ChannelFactory.scala
+++ zio-http/src/main/scala/zhttp/service/ChannelFactory.scala
@@ -6,36 +6,21 @@ import zhttp.core._
 import zio.{UIO, ZLayer}
 
 object ChannelFactory {
-  def nio: ZLayer[Any, Nothing, ChannelFactory] = Live.nio.toLayer
-
-  def epoll: ZLayer[Any, Nothing, ChannelFactory] = Live.epoll.toLayer
-
+  def nio: ZLayer[Any, Nothing, ChannelFactory]      = Live.nio.toLayer
+  def epoll: ZLayer[Any, Nothing, ChannelFactory]    = Live.epoll.toLayer
   def embedded: ZLayer[Any, Nothing, ChannelFactory] = Live.embedded.toLayer
+  def auto: ZLayer[Any, Nothing, ChannelFactory]     = Live.auto.toLayer
 
-  def auto: ZLayer[Any, Nothing, ChannelFactory] = Live.auto.toLayer
+  def make[A <: JChannel](fn: () => A): UIO[JChannelFactory[A]] = UIO(new JChannelFactory[A] {
+    override def newChannel(): A = fn()
+  })
 
   object Live {
-    def nio: UIO[JChannelFactory[JChannel]] = {
-      val sam: JChannelFactory[JChannel] = () => new JNioSocketChannel()
-      UIO(sam)
-    }
-
-    def epoll: UIO[JChannelFactory[JChannel]] = {
-      val sam: JChannelFactory[JChannel] = () => new JEpollSocketChannel()
-      UIO(sam)
-    }
-
-    def kQueue: UIO[JChannelFactory[JChannel]] = {
-      val sam: JChannelFactory[JChannel] = () => new JKQueueSocketChannel()
-      UIO(sam)
-    }
-
-    def embedded: UIO[JChannelFactory[JChannel]] = {
-      val sam: JChannelFactory[JChannel] = () => new JEmbeddedChannel(false, false)
-      UIO(sam)
-    }
-
-    def auto: UIO[JChannelFactory[JChannel]] =
+    def nio: UIO[JChannelFactory[JChannel]]      = make(() => new JNioSocketChannel())
+    def epoll: UIO[JChannelFactory[JChannel]]    = make(() => new JEpollSocketChannel())
+    def kQueue: UIO[JChannelFactory[JChannel]]   = make(() => new JKQueueSocketChannel())
+    def embedded: UIO[JChannelFactory[JChannel]] = make(() => new JEmbeddedChannel(false, false))
+    def auto: UIO[JChannelFactory[JChannel]]     =
       if (JEpoll.isAvailable) epoll
       else if (JKQueue.isAvailable) kQueue
       else nio
--- zio-http/src/main/scala/zhttp/service/server/ServerChannelFactory.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerChannelFactory.scala
@@ -3,33 +3,19 @@ package zhttp.service.server
 import io.netty.channel.epoll.{Epoll => JEpoll}
 import io.netty.channel.kqueue.{KQueue => JKQueue}
 import zhttp.core._
-import zhttp.service.ServerChannelFactory
+import zhttp.service.{ChannelFactory, ServerChannelFactory}
 import zio.{UIO, ZLayer}
 
 object ServerChannelFactory {
-  def nio: ZLayer[Any, Nothing, ServerChannelFactory] = Live.nio.toLayer
-
+  def nio: ZLayer[Any, Nothing, ServerChannelFactory]   = Live.nio.toLayer
   def epoll: ZLayer[Any, Nothing, ServerChannelFactory] = Live.epoll.toLayer
-
-  def auto: ZLayer[Any, Nothing, ServerChannelFactory] = Live.auto.toLayer
+  def auto: ZLayer[Any, Nothing, ServerChannelFactory]  = Live.auto.toLayer
 
   object Live {
-    def nio: UIO[JChannelFactory[JServerChannel]] = {
-      val sam: JChannelFactory[JServerChannel] = () => new JNioServerSocketChannel()
-      UIO(sam)
-    }
-
-    def epoll: UIO[JChannelFactory[JServerChannel]] = {
-      val sam: JChannelFactory[JServerChannel] = () => new JEpollServerSocketChannel()
-      UIO(sam)
-    }
-
-    def kQueue: UIO[JChannelFactory[JServerChannel]] = {
-      val sam: JChannelFactory[JServerChannel] = () => new JKQueueServerSocketChannel()
-      UIO(sam)
-    }
-
-    def auto: UIO[JChannelFactory[JServerChannel]] =
+    def nio: UIO[JChannelFactory[JServerChannel]]    = ChannelFactory.make(() => new JNioServerSocketChannel())
+    def epoll: UIO[JChannelFactory[JServerChannel]]  = ChannelFactory.make(() => new JEpollServerSocketChannel())
+    def kQueue: UIO[JChannelFactory[JServerChannel]] = ChannelFactory.make(() => new JKQueueServerSocketChannel())
+    def auto: UIO[JChannelFactory[JServerChannel]]   =
       if (JEpoll.isAvailable) epoll
       else if (JKQueue.isAvailable) kQueue
       else nio
