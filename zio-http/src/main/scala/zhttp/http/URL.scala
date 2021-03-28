5cc9388ac1d0bff8aed48ad4cf277bd9d95c2a83
David Hoyt <davidhoyt@users.noreply.github.com>
2021-03-28 12:27:35 -0700

    Feature: Basic Query String Support.
    * Cleanup scaladoc reference.
    
    * Enhance URL to include the query string.
    
    * Format code.
    
    Co-authored-by: David Hoyt <david.hoyt@creditkarma.com>

--- zio-http/src/main/scala/zhttp/http/URL.scala
+++ zio-http/src/main/scala/zhttp/http/URL.scala
@@ -3,20 +3,20 @@ package zhttp.http
 import java.net.{URI, URISyntaxException}
 import scala.util.Try
 
-case class URL(path: Path, kind: URL.Location = URL.Location.Relative) { self =>
+case class URL(path: Path, kind: URL.Location = URL.Location.Relative, query: String = "") { self =>
   val host: Option[String] = kind match {
-    case URL.Location.Relative             => None
-    case URL.Location.Absolute(_, host, _) => Option(host)
+    case URL.Location.Relative      => None
+    case abs: URL.Location.Absolute => Option(abs.host)
   }
 
   val port: Option[Int] = kind match {
-    case URL.Location.Relative             => None
-    case URL.Location.Absolute(_, _, port) => Option(port)
+    case URL.Location.Relative      => None
+    case abs: URL.Location.Absolute => Option(abs.port)
   }
 
   def asString: String = URL.asString(self)
 }
-object URL                                                             {
+object URL                                                                                 {
   sealed trait Location
   object Location {
     case object Relative                                         extends Location
@@ -28,12 +28,14 @@ object URL                                                             {
     host   <- Option(uri.getHost)
     port   <- Option(uri.getPort)
     path   <- Option(uri.getPath)
+    query      = Option(uri.getRawQuery).getOrElse("")
     connection = URL.Location.Absolute(scheme, host, port)
-  } yield URL(Path(path), connection)
+  } yield URL(Path(path), connection, query)
 
   private def fromRelativeURI(uri: URI): Option[URL] = for {
     path <- Option(uri.getPath)
-  } yield URL(Path(path), Location.Relative)
+    query = Option(uri.getRawQuery).getOrElse("")
+  } yield URL(Path(path), Location.Relative, query)
 
   def fromString(string: String): Either[Throwable, URL] =
     for {
@@ -45,12 +47,22 @@ object URL                                                             {
 
     } yield url
 
-  def asString(url: URL): String = url.kind match {
-    case Location.Relative                     => url.path.asString
-    case Location.Absolute(scheme, host, port) =>
-      port match {
-        case 80   => s"${scheme.asString}://${host}${url.path.asString}"
-        case port => s"${scheme.asString}://${host}:${port}${url.path.asString}"
+  def asString(url: URL): String = {
+    def withQuery(u: String): String =
+      if (url.query.nonEmpty) {
+        s"$u?${url.query}"
+      } else {
+        u
       }
+
+    url.kind match {
+      case Location.Relative                     => withQuery(url.path.asString)
+      case Location.Absolute(scheme, host, port) =>
+        (scheme, port) match {
+          case (Scheme.HTTP, 80)   => withQuery(s"${scheme.asString}://${host}${url.path.asString}")
+          case (Scheme.HTTPS, 443) => withQuery(s"${scheme.asString}://${host}${url.path.asString}")
+          case (_, port)           => withQuery(s"${scheme.asString}://${host}:${port}${url.path.asString}")
+        }
+    }
   }
 }
