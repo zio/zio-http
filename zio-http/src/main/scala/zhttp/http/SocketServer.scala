38e3bfc98f09ce4ef1d10504af64ef08171619e1
Amit Kumar Singh <amit.singh@dream11.com>
2021-04-06 22:18:51 +0530

    feat(http): add socket server implementation (#124)

--- zio-http/src/main/scala/zhttp/http/SocketServer.scala
+++ zio-http/src/main/scala/zhttp/http/SocketServer.scala
@@ -1,7 +1,7 @@
 package zhttp.http
 
-import zio._
 import zhttp.socket.WebSocketFrame
+import zio._
 import zio.stream.ZStream
 
 sealed trait SocketServer[-R, +E] { self =>
@@ -18,14 +18,14 @@ object SocketServer {
     subProtocol: Option[String] = None,
     // Triggered when the socket is upgraded
     // there is a failure, ctx.close() is called
-    onOpen: Connection => ZIO[R, E, Unit],
+    onOpen: Connection => ZIO[R, E, Unit] = (_: Connection) => ZIO.unit,
     // There is a failure, ctx.close() is called.
-    onMessage: WebSocketFrame => ZStream[R, E, WebSocketFrame],
+    onMessage: WebSocketFrame => ZStream[R, E, WebSocketFrame] = (_: WebSocketFrame) => ZStream.empty,
     // No error channel because there is nothing left to handle it.
     // Channel should get closed after execution.
-    onError: Throwable => ZIO[R, Nothing, Unit],
+    onError: Throwable => ZIO[R, Nothing, Unit] = (_: Throwable) => ZIO.unit,
     // Last thing in the pipeline, so it can't fail.
-    onClose: (Connection, Cause) => ZIO[R, Nothing, Unit],
+    onClose: (Connection, Cause) => ZIO[R, Nothing, Unit] = (_: Connection, _: Cause) => ZIO.unit,
   )
 
   private case class SubProtocol(name: String)                                         extends SocketServer[Any, Nothing]
@@ -36,12 +36,20 @@ object SocketServer {
   private case class OnClose[R](onClose: (Connection, Cause) => ZIO[R, Nothing, Unit]) extends SocketServer[R, Nothing]
   private case class Concat[R, E](a: SocketServer[R, E], b: SocketServer[R, E])        extends SocketServer[R, E]
 
-  def close: SocketServer[Any, Nothing]                                                             = ???
-  def subProtocol(name: String): SocketServer[Any, Nothing]                                         = ???
-  def open[R, E](onOpen: Connection => ZIO[R, E, Unit]): SocketServer[R, E]                         = ???
-  def message[R, E](onMessage: WebSocketFrame => ZStream[R, E, WebSocketFrame]): SocketServer[R, E] = ???
-  def error[R](onError: Throwable => ZIO[R, Nothing, Unit]): SocketServer[R, Nothing]               = ???
-  def close[R](onClose: (Connection, Cause) => ZIO[R, Nothing, Unit]): SocketServer[R, Nothing]     = ???
+  def subProtocol(name: String): SocketServer[Any, Nothing]                                         = SubProtocol(name)
+  def open[R, E](onOpen: Connection => ZIO[R, E, Unit]): SocketServer[R, E]                         = OnOpen(onOpen)
+  def message[R, E](onMessage: WebSocketFrame => ZStream[R, E, WebSocketFrame]): SocketServer[R, E] = OnMessage(
+    onMessage,
+  )
+  def error[R](onError: Throwable => ZIO[R, Nothing, Unit]): SocketServer[R, Nothing]               = OnError(onError)
+  def close[R](onClose: (Connection, Cause) => ZIO[R, Nothing, Unit]): SocketServer[R, Nothing]     = OnClose(onClose)
 
-  def settings[R, E](ss: SocketServer[R, E]): Settings[R, E] = ???
+  def settings[R, E](ss: SocketServer[R, E], s: Settings[R, E] = Settings()): Settings[R, E] = ss match {
+    case SubProtocol(name)    => s.copy(Option(name))
+    case OnOpen(onOpen)       => s.copy(onOpen = onOpen)
+    case OnMessage(onMessage) => s.copy(onMessage = onMessage)
+    case OnError(onError)     => s.copy(onError = onError)
+    case OnClose(onClose)     => s.copy(onClose = onClose)
+    case Concat(a, b)         => settings(b, settings(a, s))
+  }
 }
