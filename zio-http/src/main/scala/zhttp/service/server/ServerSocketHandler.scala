6941cf3908a078466e92e6d0b4e8f2d5ac8bcf9b
Tushar Mathur <tusharmath@gmail.com>
2021-04-07 12:18:51 +0530

    refactor(server): handle channel exceptions uninterruptibly

--- zio-http/src/main/scala/zhttp/service/server/ServerSocketHandler.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerSocketHandler.scala
@@ -3,13 +3,14 @@ package zhttp.service.server
 import zhttp.core.{JChannelHandlerContext, JSimpleChannelInboundHandler, JWebSocketFrame}
 import zhttp.service.{ChannelFuture, UnsafeChannelExecutor}
 import zhttp.socket.{SocketBuilder, WebSocketFrame}
+import zio.{Exit, ZIO}
 
 /**
  * Creates a new websocket handler
  */
-final case class ServerSocketHandler[R, E](
+final case class ServerSocketHandler[R](
   zExec: UnsafeChannelExecutor[R],
-  ss: SocketBuilder.Settings[R, E],
+  ss: SocketBuilder.Settings[R, Throwable],
 ) extends JSimpleChannelInboundHandler[JWebSocketFrame] {
 
   /**
@@ -19,19 +20,33 @@ final case class ServerSocketHandler[R, E](
   override def channelRead0(ctx: JChannelHandlerContext, msg: JWebSocketFrame): Unit = {
     WebSocketFrame.fromJFrame(msg) match {
       case Some(frame) =>
-        zExec.unsafeExecute_(ctx) {
+        executeAsync(
+          ctx,
           ss.onMessage(frame)
             .mapM(frame => ChannelFuture.unit(ctx.writeAndFlush(frame.toJWebSocketFrame)))
-            .runDrain
-        }
+            .runDrain,
+        )
 
       case _ => ()
     }
   }
 
+  def executeAsync(ctx: JChannelHandlerContext, program: ZIO[R, Throwable, Unit]): Unit = {
+    zExec.unsafeExecute(ctx, program) {
+      case Exit.Success(_)     => ()
+      case Exit.Failure(cause) =>
+        cause.failureOption match {
+          case Some(error: Throwable) => ctx.fireExceptionCaught(error)
+          case _                      => ()
+        }
+        ctx.close()
+        ()
+    }
+  }
+
   override def exceptionCaught(ctx: JChannelHandlerContext, x: Throwable): Unit =
-    zExec.unsafeExecute_(ctx)(ss.onError(x).uninterruptible)
+    executeAsync(ctx, ss.onError(x).uninterruptible)
 
   override def channelUnregistered(ctx: JChannelHandlerContext): Unit =
-    zExec.unsafeExecute_(ctx)(ss.onClose(ctx.channel().remoteAddress(), None).uninterruptible)
+    executeAsync(ctx, ss.onClose(ctx.channel().remoteAddress(), None).uninterruptible)
 }
