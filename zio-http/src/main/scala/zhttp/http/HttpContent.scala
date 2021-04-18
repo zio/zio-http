842bf6959acd4c6fa0d7c4dbf3087b121532213c
Amit Kumar Singh <amit.singh@dream11.com>
2021-04-18 15:50:58 +0530

    Extend support for various content encodings (#144)

--- zio-http/src/main/scala/zhttp/http/HttpContent.scala
+++ zio-http/src/main/scala/zhttp/http/HttpContent.scala
@@ -1,5 +1,7 @@
 package zhttp.http
 
+import io.netty.buffer.ByteBuf
+import zio.Chunk
 import zio.stream.ZStream
 
 /**
@@ -8,6 +10,12 @@ import zio.stream.ZStream
 sealed trait HttpContent[-R, +A] extends Product with Serializable
 
 object HttpContent {
-  final case class Complete[A](data: A)                        extends HttpContent[Any, A]
-  final case class Chunked[R, A](data: ZStream[R, Nothing, A]) extends HttpContent[R, A]
+  final case class Complete[A](data: Chunk[A])                        extends HttpContent[Any, A]
+  final case class Chunked[R, A](data: ZStream[R, Nothing, Chunk[A]]) extends HttpContent[R, A]
+
+  def complete(byteBuf: ByteBuf): Complete[Byte] = {
+    val bytes = new Array[Byte](byteBuf.readableBytes)
+    byteBuf.readBytes(bytes)
+    HttpContent.Complete(Chunk.fromArray(bytes))
+  }
 }
