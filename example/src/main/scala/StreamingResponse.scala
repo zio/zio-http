04220a7c7562cb7e9bbd80a7beeadf8c2c7054dd
Tushar Mathur <tusharmath@gmail.com>
2021-04-19 11:04:50 +0530

    doc: update StreamExample

--- example/src/main/scala/StreamingResponse.scala
+++ example/src/main/scala/StreamingResponse.scala
@@ -4,26 +4,30 @@ import zio._
 import zio.stream.ZStream
 
 /**
- * In this example we send Response content as, a Stream of Chunk[Byte]
+ * Example to encode content using a ZStream
  */
 object StreamingResponse extends App {
-  // message as Chunk[Byte]
+  // Create a message as a Chunk[Byte]
   val message = Chunk.fromArray("Hello world !\r\n".getBytes(HTTP_CHARSET))
-  val app     = Http.collect {
+
+  // Use `Http.collect` to match on route
+  val app = Http.collect {
+
+    // Simple (non-stream) based route
     case Method.GET -> Root / "health" => Response.ok
+
+    // ZStream powered response
     case Method.GET -> Root / "stream" =>
       Response.http(
         status = Status.OK,
         headers = List(Header.contentLength(message.length.toLong)),
-        content = HttpData.fromStream(ZStream.succeed(message)), // creates Content as Stream
+        content = HttpData.fromStream(ZStream.succeed(message)), // Encoding content using a ZStream
       )
 
   }
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
 
-    /**
-     * To configure the server refer example [[HelloWorldAdvanced]]
-     */
+    // Starting the server (for more advanced startup configuration checkout `HelloWorldAdvanced`)
     Server.start(8090, app.silent).exitCode
   }
 }
