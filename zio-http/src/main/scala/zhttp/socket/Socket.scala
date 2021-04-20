4205c58fc198bf97ca9b2e3d0708aefd71369180
Tushar Mathur <tusharmath@gmail.com>
2021-04-21 01:02:48 +0530

    refactor(socket): rename methods

--- zio-http/src/main/scala/zhttp/socket/Socket.scala
+++ zio-http/src/main/scala/zhttp/socket/Socket.scala
@@ -8,24 +8,25 @@ sealed trait Socket[-R, +E] { self =>
 }
 
 object Socket {
-  private case class Concat[R, E](a: Socket[R, E], b: Socket[R, E])  extends Socket[R, E]
-  private case class SocketConfig[R, E](socket: SocketChannel[R, E]) extends Socket[R, E]
-  private case class Protocol(config: SocketProtocol)                extends Socket[Any, Nothing]
-  private case class Decoder(config: SocketDecoder)                  extends Socket[Any, Nothing]
+  private case class Concat[R, E](a: Socket[R, E], b: Socket[R, E]) extends Socket[R, E]
+  private case class Channel[R, E](socket: SocketChannel[R, E])     extends Socket[R, E]
+  private case class Protocol(config: SocketProtocol)               extends Socket[Any, Nothing]
+  private case class Decoder(config: SocketDecoder)                 extends Socket[Any, Nothing]
 
-  def config[R, E](config: SocketChannel[R, E]): Socket[R, E]  = SocketConfig(config)
+  def channel[R, E](config: SocketChannel[R, E]): Socket[R, E] = Channel(config)
   def protocol(protocol: SocketProtocol): Socket[Any, Nothing] = Protocol(protocol)
   def decoder(decoder: SocketDecoder): Socket[Any, Nothing]    = Decoder(decoder)
 
   def asResponse[R, E](self: Socket[R, E]): SocketResponse[R, E] = {
     def loop(socketB: Socket[R, E], res: SocketResponse[R, E]): SocketResponse[R, E] = {
       socketB match {
-        case Concat(a, b)         => loop(b, loop(a, res))
-        case SocketConfig(socket) => res.copy(socket = res.socket ++ socket)
-        case Protocol(config)     => res.copy(protocol = res.protocol ++ config)
-        case Decoder(config)      => res.copy(decoder = res.decoder ++ config)
+        case Concat(a, b)     => loop(b, loop(a, res))
+        case Channel(socket)  => res.copy(socket = res.socket ++ socket)
+        case Protocol(config) => res.copy(protocol = res.protocol ++ config)
+        case Decoder(config)  => res.copy(decoder = res.decoder ++ config)
       }
     }
     loop(self, SocketResponse())
   }
+
 }
