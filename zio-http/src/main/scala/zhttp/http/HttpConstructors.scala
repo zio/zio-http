110ccd9cbf41a0307f6a3cc6fc6514d2acb7e828
Tushar Mathur <tusharmath@gmail.com>
2021-04-23 01:12:00 +0530

    refactor(http): rename HttpChannel to Http

--- zio-http/src/main/scala/zhttp/http/HttpConstructors.scala
+++ zio-http/src/main/scala/zhttp/http/HttpConstructors.scala
@@ -7,52 +7,52 @@ trait HttpConstructors {
   /**
    * Creates an Http that always returns the same response and never fails.
    */
-  def succeed[B](b: B): HttpChannel[Any, Nothing, Any, B] = HttpChannel.Succeed(b)
+  def succeed[B](b: B): Http[Any, Nothing, Any, B] = Http.Succeed(b)
 
   /**
    * Creates an Http app from a function that returns a ZIO
    */
-  def fromEffectFunction[A]: HttpChannel.MakeFromEffectFunction[A] = HttpChannel.MakeFromEffectFunction(())
+  def fromEffectFunction[A]: Http.MakeFromEffectFunction[A] = Http.MakeFromEffectFunction(())
 
   /**
    * Converts a ZIO to an Http type
    */
-  def fromEffect[R, E, B](effect: ZIO[R, E, B]): HttpChannel[R, E, Any, B] = HttpChannel.fromEffectFunction(_ => effect)
+  def fromEffect[R, E, B](effect: ZIO[R, E, B]): Http[R, E, Any, B] = Http.fromEffectFunction(_ => effect)
 
   /**
    * Creates an Http that always fails
    */
-  def fail[E](e: E): HttpChannel[Any, E, Any, Nothing] = HttpChannel.Fail(e)
+  def fail[E](e: E): Http[Any, E, Any, Nothing] = Http.Fail(e)
 
   /**
    * Creates a pass thru Http instances
    */
-  def identity[A]: HttpChannel[Any, Nothing, A, A] = HttpChannel.Identity
+  def identity[A]: Http[Any, Nothing, A, A] = Http.Identity
 
   /**
    * Creates an HTTP app which accepts a request and produces response.
    */
-  def collect[A]: HttpChannel.MakeCollect[A] = HttpChannel.MakeCollect(())
+  def collect[A]: Http.MakeCollect[A] = Http.MakeCollect(())
 
   /**
    * Creates an HTTP app which accepts a request and produces response effectfully.
    */
-  def collectM[A]: HttpChannel.MakeCollectM[A] = HttpChannel.MakeCollectM(())
+  def collectM[A]: Http.MakeCollectM[A] = Http.MakeCollectM(())
 
   /**
    * Creates an HTTP app which for any request produces a response.
    */
-  def succeedM[R, E, B](zio: ZIO[R, E, B]): HttpChannel[R, E, Any, B] = HttpChannel.fromEffectFunction(_ => zio)
+  def succeedM[R, E, B](zio: ZIO[R, E, B]): Http[R, E, Any, B] = Http.fromEffectFunction(_ => zio)
 
   /**
    * Flattens an Http app of an Http app
    */
-  def flatten[R, E, A, B](http: HttpChannel[R, E, A, HttpChannel[R, E, A, B]]): HttpChannel[R, E, A, B] =
+  def flatten[R, E, A, B](http: Http[R, E, A, Http[R, E, A, B]]): Http[R, E, A, B] =
     http.flatten
 
   /**
    * Flattens an Http app of an effectful response
    */
-  def flattenM[R, E, A, B](http: HttpChannel[R, E, A, ZIO[R, E, B]]): HttpChannel[R, E, A, B] =
-    http.flatMap(HttpChannel.fromEffect)
+  def flattenM[R, E, A, B](http: Http[R, E, A, ZIO[R, E, B]]): Http[R, E, A, B] =
+    http.flatMap(Http.fromEffect)
 }
