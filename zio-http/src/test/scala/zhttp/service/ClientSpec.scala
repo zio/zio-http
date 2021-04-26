7089e4427273801c1efb90a96152a378e13c4236
Amit Kumar Singh <amit.singh@dream11.com>
2021-04-26 20:07:27 +0530

    fix(test): ClientSpec issue (#164)

--- zio-http/src/test/scala/zhttp/service/ClientSpec.scala
+++ zio-http/src/test/scala/zhttp/service/ClientSpec.scala
@@ -7,7 +7,7 @@ object ClientSpec extends HttpRunnableSpec(8082) {
   val env           = ChannelFactory.auto ++ EventLoopGroup.auto()
   override def spec = suite("Client")(
     testM("respond Ok") {
-      val actual = Client.request("https://api.github.com/users/zio/repos")
+      val actual = Client.request("http://api.github.com/users/zio/repos")
       assertM(actual)(anything)
     },
   ).provideCustomLayer(env)
