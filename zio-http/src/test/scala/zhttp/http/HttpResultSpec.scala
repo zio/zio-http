2af494a8529e2500b1c05c26eca8f8bf64ab91df
Tushar Mathur <tusharmath@gmail.com>
2021-04-28 18:59:53 +0530

    test(http): update Http tests
    - update HttpResultSpec with a failing test
    - update HttpSpec with a failing test

--- zio-http/src/test/scala/zhttp/http/HttpResultSpec.scala
+++ zio-http/src/test/scala/zhttp/http/HttpResultSpec.scala
@@ -1,16 +1,16 @@
 package zhttp.http
 
 import zio.test.Assertion.{equalTo, isLeft, isRight, isSome}
-import zio.test.{DefaultRunnableSpec, ZSpec, assert, assertM}
+import zio.test.{assert, assertM, DefaultRunnableSpec, ZSpec}
 
-object HttpResultSpec extends DefaultRunnableSpec {
+object HttpResultSpec extends DefaultRunnableSpec with HttpResultAssertion {
   def spec: ZSpec[Environment, Failure] =
     suite("HttpResult")(
       suite("catchAll") {
         test("should handle error") {
           val actual =
             HttpResult.failure(1).flatMapError(e => HttpResult.success(e + 1)).asOut
-          assert(actual)(equalTo(HttpResult.success(2)))
+          assert(actual)(isSuccess(equalTo(2)))
         }
       },
       suite("catchAll") {
@@ -21,7 +21,7 @@ object HttpResultSpec extends DefaultRunnableSpec {
             .flatMapError(e => HttpResult.failure("FOLD_" + e))
 
           val actual = a.asOut
-          assert(actual)(equalTo(HttpResult.failure("FOLD_FAIL")))
+          assert(actual)(isFailure(equalTo("FOLD_FAIL")))
         }
       },
       suite("asEffect")(
@@ -33,44 +33,50 @@ object HttpResultSpec extends DefaultRunnableSpec {
           val actual = HttpResult.failure(1).asOut.asEffect.either
           assertM(actual)(isLeft(isSome(equalTo(1))))
         },
-      ),
-      suite("flatMap")(
-        testM("should succeed") {
-          val res    = HttpResult.success(1) *> HttpResult.success(2)
-          val actual = res.asOut.asEffect.either
-          assertM(actual)(isRight(equalTo(2)))
-        },
-        testM("should fail") {
-          val res    = HttpResult.failure(1) *> HttpResult.success(2)
-          val actual = res.asOut.asEffect.either
-          assertM(actual)(isLeft(isSome(equalTo(1))))
-        },
+        suite("flatMap")(
+          testM("should succeed") {
+            val res    = HttpResult.success(1) *> HttpResult.success(2)
+            val actual = res.asOut.asEffect.either
+            assertM(actual)(isRight(equalTo(2)))
+          },
+          testM("should fail") {
+            val res    = HttpResult.failure(1) *> HttpResult.success(2)
+            val actual = res.asOut.asEffect.either
+            assertM(actual)(isLeft(isSome(equalTo(1))))
+          },
+        ),
       ),
       suite("defaultWith")(
         test("should succeed") {
           val res    = HttpResult.success(1).defaultWith(HttpResult.success(2))
           val actual = res.asOut
-          assert(actual)(equalTo(HttpResult.success(1)))
+          assert(actual)(isSuccess(equalTo(1)))
         },
         test("should fail") {
           val res    = HttpResult.failure(1).defaultWith(HttpResult.success(2))
           val actual = res.asOut
-          assert(actual)(equalTo(HttpResult.failure(1)))
+          assert(actual)(isFailure(equalTo(1)))
         },
         test("should succeed with default") {
           val res    = HttpResult.empty.defaultWith(HttpResult.success(2))
           val actual = res.asOut
-          assert(actual)(equalTo(HttpResult.success(2)))
+          assert(actual)(isSuccess(equalTo(2)))
         },
         test("should succeed with default") {
           val res    = (HttpResult.success(1) *> HttpResult.empty).defaultWith(HttpResult.success(2))
           val actual = res.asOut
-          assert(actual)(equalTo(HttpResult.success(2)))
+          assert(actual)(isSuccess(equalTo(2)))
         },
         test("should fail with default") {
           val res    = (HttpResult.success(1) *> HttpResult.empty).defaultWith(HttpResult.failure(2))
           val actual = res.asOut
-          assert(actual)(equalTo(HttpResult.failure(2)))
+          assert(actual)(isFailure(equalTo(2)))
+        },
+        test("should succeed with default") {
+          val a      = HttpResult.empty.flatten.flatten.flatten
+          val b      = HttpResult.success("B")
+          val actual = (a defaultWith b).asOut
+          assert(actual)(isSuccess(equalTo("B")))
         },
       ),
     )
