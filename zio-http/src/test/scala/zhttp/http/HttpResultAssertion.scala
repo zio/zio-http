7f33dcbe818aed80fe211a7a87103ea060602fbe
Tushar Mathur <tusharmath@gmail.com>
2021-04-28 17:12:50 +0530

    test(http): add HttpResultAssertion

---
+++ zio-http/src/test/scala/zhttp/http/HttpResultAssertion.scala
@@ -0,0 +1,29 @@
+package zhttp.http
+
+import zio.test.Assertion
+
+trait HttpResultAssertion {
+  def isEffect[R, E, A]: Assertion[HttpResult[R, E, A]] =
+    Assertion.assertion("isEffect")()({
+      case HttpResult.Effect(_) => true
+      case _                    => false
+    })
+
+  def isEmpty[R, E, A]: Assertion[HttpResult[R, E, A]] =
+    Assertion.assertion("isEmpty")()({
+      case HttpResult.Empty => true
+      case _                => false
+    })
+
+  def isSuccess[R, E, A](ass: Assertion[A]): Assertion[HttpResult[R, E, A]] =
+    Assertion.assertion("isSuccess")()({
+      case HttpResult.Success(a) => ass.test(a)
+      case _                     => false
+    })
+
+  def isFailure[R, E, A](ass: Assertion[E]): Assertion[HttpResult[R, E, A]] =
+    Assertion.assertion("isFailure")()({
+      case HttpResult.Failure(e) => ass.test(e)
+      case _                     => false
+    })
+}
