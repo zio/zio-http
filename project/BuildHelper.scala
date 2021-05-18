018abe3d07fcfa1e439de6cd74c3f44e5e0d53b1
Scala Steward <me@scala-steward.org>
2021-05-18 14:18:42 +0200

    Update scala-library to 2.13.6

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -5,7 +5,7 @@ import xerial.sbt.Sonatype.autoImport._
 
 object BuildHelper extends ScalaSettings {
   val Scala212   = "2.12.13"
-  val Scala213   = "2.13.5"
+  val Scala213   = "2.13.6"
   val ScalaDotty = "3.0.0"
 
   private val stdOptions = Seq(
