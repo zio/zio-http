40d85f00f73366a44d7e7722d5a1681d027a77dd
Tushar Mathur <tusharmath@gmail.com>
2021-05-28 19:47:13 +0530

    refactor(http): add helpers to create HttpData

--- zio-http/src/main/scala/zhttp/http/HttpData.scala
+++ zio-http/src/main/scala/zhttp/http/HttpData.scala
@@ -1,8 +1,11 @@
 package zhttp.http
 
-import io.netty.buffer.{ByteBuf, ByteBufUtil}
-import zio.Chunk
+import zhttp.core.{Direction, HBuf, HBuf1, Nat}
 import zio.stream.ZStream
+import zio.{Chunk, ZEnqueue}
+import zio.blocking.Blocking
+
+import java.nio.file.{Path => JPath}
 
 /**
  * Content holder for Requests and Responses
@@ -10,21 +13,38 @@ import zio.stream.ZStream
 sealed trait HttpData[-R, +E] extends Product with Serializable
 
 object HttpData {
-  case object Empty                                            extends HttpData[Any, Nothing]
-  final case class CompleteData(data: Chunk[Byte])             extends HttpData[Any, Nothing]
-  final case class StreamData[R, E](data: ZStream[R, E, Byte]) extends HttpData[R, E]
+  private type Buffer = HBuf1[Direction.Out]
+
+  case object Empty                                                extends HttpData[Any, Nothing]
+  final case class CompleteData(data: Buffer)                      extends HttpData[Any, Nothing]
+  final case class StreamData[R, E](stream: ZStream[R, E, Buffer]) extends HttpData[R, E]
+  final case class HBufQueue[R, E](queue: ZEnqueue[R, E, Buffer])  extends HttpData[R, E]
+
+  def apply(text: String): HttpData[Any, Nothing] = HttpData.fromString(text)
 
   /**
    * Helper to create CompleteData from ByteBuf
    */
-  def fromByteBuf(byteBuf: ByteBuf): HttpData[Any, Nothing] = {
-    HttpData.CompleteData(Chunk.fromArray(ByteBufUtil.getBytes(byteBuf)))
-  }
+  def fromBuf(buf: Buffer): HttpData[Any, Nothing] = HttpData.CompleteData(buf)
 
   /**
    * Helper to create StreamData from Stream of Chunks
    */
-  def fromStream[R, E](data: ZStream[R, E, Byte]): HttpData[R, E] = HttpData.StreamData(data)
+  def fromStream[R, E](data: ZStream[R, E, HBuf[Nat.One, Direction.Out]]): HttpData[R, E] = HttpData.StreamData(data)
+
+  /**
+   * Creates HttpData from a stream of bytes
+   */
+  def fromByteStream[R, E](data: ZStream[R, E, Byte]): HttpData[R, E] =
+    fromStream(data.mapChunks(chunks => Chunk(HBuf.fromChunk(chunks))))
+
+  /**
+   * Creates HttpData by reading a file
+   */
+  def fromFile(path: JPath): HttpData[Blocking, Throwable] =
+    HttpData.fromStream(ZStream.fromFile(path).mapChunks(chunks => Chunk(HBuf.fromChunk(chunks))))
+
+  def fromQueue[R, E](queue: ZEnqueue[R, E, Buffer]): HttpData[R, E] = HttpData.HBufQueue(queue)
 
   /**
    * Helper to create Empty HttpData
@@ -35,5 +55,5 @@ object HttpData {
    * Creates HttpData from string
    */
   def fromString(text: String): HttpData[Any, Nothing] =
-    HttpData.CompleteData(Chunk.fromArray(text.getBytes(HTTP_CHARSET)))
+    HttpData.CompleteData(HBuf.fromString(text, HTTP_CHARSET))
 }
