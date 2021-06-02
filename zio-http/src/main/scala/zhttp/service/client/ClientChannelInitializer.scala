f1586830eb5472880c7d4619c72a3a91d9bdb0e3
shrutiverma97 <shruti@dream11.com>
2021-06-02 19:07:30 +0530

    fix(trustmanager): added trustmanager for ssl handler

--- zio-http/src/main/scala/zhttp/service/client/ClientChannelInitializer.scala
+++ zio-http/src/main/scala/zhttp/service/client/ClientChannelInitializer.scala
@@ -1,25 +1,62 @@
 package zhttp.service.client
 
+import java.io.FileInputStream
+import java.security.KeyStore
 import io.netty.channel.ChannelPipeline
 import io.netty.handler.codec.http.{HttpClientCodec => JHttpClientCodec}
 import io.netty.handler.ssl.SslContext
+import javax.net.ssl.TrustManagerFactory
 import zhttp.core.{JChannel, JChannelHandler, JChannelInitializer, JHttpObjectAggregator}
 
+import scala.util.{Failure, Success, Try}
 final case class ClientChannelInitializer[R](channelHandler: JChannelHandler, port: Int)
     extends JChannelInitializer[JChannel]() {
   override def initChannel(ch: JChannel): Unit = {
     import io.netty.handler.ssl.SslContextBuilder
-    import io.netty.handler.ssl.util.InsecureTrustManagerFactory
-    val sslCtx: SslContext =
-      if (port == 443) SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build()
-      else null
-    val p: ChannelPipeline = ch
+    // truststore
+    val trustStore: Option[KeyStore]       = Option(KeyStore.getInstance("JKS"))
+    val trustStorePath: Option[String]     = Option(System.getProperty("javax.net.ssl.trustStore"))
+    val trustStorePassword: Option[String] = Option(System.getProperty("javax.net.ssl.trustStorePassword"))
+    val trustManagerFactory                = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+
+    val file: Try[FileInputStream] = Try(new FileInputStream(trustStorePath match {
+      case Some(value) => value
+      case None        => throw new Exception
+    }))
+
+    val ssl: SslContext            = trustStore match {
+      case Some(ts) =>
+        Try(
+          ts.load(
+            file match {
+              case Failure(exception) => throw exception
+              case Success(value)     => value
+            },
+            trustStorePassword match {
+              case Some(value) => value.toCharArray
+              case None        => throw new Exception
+            },
+          ),
+        ).toEither match {
+          case Left(_)  => SslContextBuilder.forClient().build()
+          case Right(_) => {
+            trustManagerFactory.init(ts)
+            SslContextBuilder.forClient().trustManager(trustManagerFactory).build()
+          }
+        }
+
+      case None => SslContextBuilder.forClient().build()
+    }
+    val sslCtx: Option[SslContext] = if (port == 443) Some(ssl) else None
+    val p: ChannelPipeline         = ch
       .pipeline()
       .addLast(new JHttpClientCodec)
       .addLast(new JHttpObjectAggregator(Int.MaxValue))
       .addLast(channelHandler)
-    if (sslCtx != null)
-      p.addFirst(sslCtx.newHandler(ch.alloc))
+    sslCtx match {
+      case Some(value) => p.addFirst(value.newHandler(ch.alloc))
+      case None        => ()
+    }
     ()
   }
 }
--- zio-http/src/main/scala/zhttp/service/client/ClientHttpChannelReader.scala
+++ zio-http/src/main/scala/zhttp/service/client/ClientHttpChannelReader.scala
@@ -1,11 +1,26 @@
 package zhttp.service.client
 
+import io.netty.handler.codec.DecoderException
+import javax.net.ssl.SSLHandshakeException
 import zhttp.core.JChannelHandlerContext
 import zhttp.service.ChannelFuture
 import zio.{Promise, Task, UIO}
 
+final case class SSLHandshake(message: String) extends Throwable
 final case class ClientHttpChannelReader[E, A](msg: AnyRef, promise: Promise[E, A]) {
   def onChannelRead(a: A): UIO[Unit]                    = promise.succeed(a).unit
-  def onExceptionCaught(e: E): UIO[Unit]                = promise.fail(e).unit
+  def onExceptionCaught(e: E): UIO[Unit] = {
+    e match {
+      case a: DecoderException =>
+        a.getCause match {
+          case _: SSLHandshakeException =>
+            promise
+              .fail(new Exception("SSL certificate problem: unable to get local issuer certificate").asInstanceOf[E])
+              .unit
+          case _                        => promise.fail(e).unit
+        }
+      case _                   => promise.fail(e).unit
+    }
+  }
   def onActive(ctx: JChannelHandlerContext): Task[Unit] = ChannelFuture.unit(ctx.writeAndFlush(msg))
 }
