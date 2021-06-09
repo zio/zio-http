bed9c2f9d90c282254bd564965d96790c08608d8
amitsingh <amitksingh1490@gmail.com>
2021-06-09 14:01:33 +0530

    Refactor: StreamingResponse

--- example/src/main/scala/StreamingResponse.scala
+++ example/src/main/scala/StreamingResponse.scala
@@ -18,7 +18,7 @@ object StreamingResponse extends App {
 
     // ZStream powered response
     case Method.GET -> Root / "stream" =>
-      Response.Default(
+      Response(
         status = Status.OK,
         headers = List(Header.contentLength(message.length.toLong)),
         content = HttpData.fromStream(ZStream.fromChunk(message)), // Encoding content using a ZStream
