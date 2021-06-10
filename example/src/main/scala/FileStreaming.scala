69d829d1f475a9d850e99733912cdb68683ac3be
dinojohn <66246799+dinojohn@users.noreply.github.com>
2021-06-10 11:40:44 +0530

    fix: added sampleFile to resources and using it in fileStreaming example

--- example/src/main/scala/FileStreaming.scala
+++ example/src/main/scala/FileStreaming.scala
@@ -3,12 +3,10 @@ import zhttp.service._
 import zio._
 import zio.stream._
 
-import java.nio.file.{Paths => JPaths}
-
 object FileStreaming extends App {
   // Read the file as ZStream
   val content = HttpData.fromStream {
-    ZStream.fromFile(JPaths.get("README.md"))
+    ZStream.fromInputStream(getClass.getClassLoader.getResourceAsStream("SampleFile.txt"))
   }
 
   // Create HTTP route
