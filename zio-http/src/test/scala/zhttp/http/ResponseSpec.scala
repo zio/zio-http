2c9dda4ee888f2ef1e3f38499e3e4d4673b198b7
Tushar Mathur <tusharmath@gmail.com>
2021-06-14 01:58:59 +0530

    test(http): add specs for Request and Response

---
+++ zio-http/src/test/scala/zhttp/http/ResponseSpec.scala
@@ -0,0 +1,36 @@
+package zhttp.http
+
+import zio.test._
+import zio.test.Assertion._
+
+object ResponseSpec extends DefaultRunnableSpec {
+  trait R; trait E
+
+  def spec = suite("Response")(
+    suite("content")(
+      testM("streaming content") {
+        assertM(typeCheck("Mock[Response[R, E, Buffered]].map(_.content).is[Content[R, E, Buffered]]"))(
+          isRight(anything),
+        )
+      },
+      testM("complete content") {
+        assertM(typeCheck("Mock[Response[R, E, Complete]].map(_.content).is[Content[R, E, Complete]]"))(
+          isRight(anything),
+        )
+      },
+      testM("opaque content") {
+        assertM(typeCheck("Mock[Response[R, E, Opaque]].map(_.content).is[Content[R, E, Opaque]]"))(isRight(anything))
+      },
+      testM("nothing content") {
+        assertM(typeCheck("Mock[Response[R, E, Nothing]].map(_.content)"))(
+          isLeft(equalTo("content unavailable")),
+        )
+      },
+      testM("any content") {
+        assertM(typeCheck("Mock[Response[R, E, Any]].map(_.content)"))(
+          isLeft(equalTo("content unavailable")),
+        )
+      },
+    ),
+  )
+}
