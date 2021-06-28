7485bc83c83cc497b4576122450c502fe14aa8cd
dinojohn <66246799+dinojohn@users.noreply.github.com>
2021-06-28 10:03:53 +0530

    refactor: added named handlers

--- zio-http/src/main/scala/zhttp/service/server/Http2OrHttpHandler.scala
+++ zio-http/src/main/scala/zhttp/service/server/Http2OrHttpHandler.scala
@@ -1,10 +1,11 @@
 package zhttp.service.server
 import io.netty.channel.ChannelHandlerContext
-import io.netty.handler.codec.http.{HttpObjectAggregator, HttpServerCodec, HttpServerKeepAliveHandler}
+import io.netty.handler.codec.http.{HttpServerKeepAliveHandler => JHttpServerKeepAliveHandler}
 import io.netty.handler.codec.http2.Http2FrameCodecBuilder
 import io.netty.handler.ssl.{ApplicationProtocolNames, ApplicationProtocolNegotiationHandler}
-import zhttp.core.JChannelHandler
+import zhttp.core.{JChannelHandler, JHttpObjectAggregator, JHttpServerCodec}
 import zhttp.service.Server.Settings
+import zhttp.service.{HTTP_KEEPALIVE_HANDLER, HTTP_REQUEST_HANDLER, OBJECT_AGGREGATOR, SERVER_CODEC_HANDLER}
 final case class Http2OrHttpHandler[R](
   httpH: JChannelHandler,
   http2H: JChannelHandler,
@@ -16,12 +17,10 @@ final case class Http2OrHttpHandler[R](
       ctx.pipeline.addLast(Http2FrameCodecBuilder.forServer().build(), http2H)
       ()
     } else if (ApplicationProtocolNames.HTTP_1_1 == protocol) {
-      ctx.pipeline.addLast(
-        new HttpServerCodec,
-        new HttpServerKeepAliveHandler,
-        new HttpObjectAggregator(settings.maxRequestSize),
-        httpH,
-      )
+      ctx.pipeline.addLast(SERVER_CODEC_HANDLER, new JHttpServerCodec)
+        .addLast(HTTP_KEEPALIVE_HANDLER, new JHttpServerKeepAliveHandler)
+        .addLast(OBJECT_AGGREGATOR, new JHttpObjectAggregator(settings.maxRequestSize))
+        .addLast(HTTP_REQUEST_HANDLER, httpH)
       ()
     } else
       throw new IllegalStateException("unknown protocol: " + protocol)
