7a88b5c771e15b9678f15eca9b12833c70cac936
shrutiverma97 <shruti@dream11.com>
2021-07-03 21:39:21 +0530

    fix(JWSExample): added badResponse for UserResponse

--- example/src/main/scala/JsonWebService.scala
+++ example/src/main/scala/JsonWebService.scala
@@ -1,3 +1,4 @@
+import io.circe
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
@@ -20,24 +21,23 @@ object JsonWebService extends App {
   object UserResponse {
     case class Show(users: List[User]) extends UserResponse
     case class Create(user: User)      extends UserResponse
+    case object BadResponse            extends UserResponse
   }
   final case class User(id: Long, name: String)
 
-  def getUser(jsonString: String): User = decode[User](jsonString).getOrElse(null)
+  def getUser(jsonString: String): Either[circe.Error, User] = decode[User](jsonString)
 
   def userApp(ref: Ref[List[User]]): Http[Any, Nothing, UserRequest, UserResponse] =
     Http.collectM[UserRequest]({
-      case UserRequest.Show         => {
+      case UserRequest.Show         =>
         for {
           users <- ref.get
         } yield UserResponse.Show(users)
-      }
-      case UserRequest.Create(user) => {
+      case UserRequest.Create(user) =>
         for {
-          users <- ref.get
-          _     <- ref.set(users :+ user)
+          _ <- ref.update(_ :+ user)
         } yield UserResponse.Create(user)
-      }
+      case UserRequest.BadRequest   => ZIO.succeed(UserResponse.BadResponse)
     })
 
   // Create HTTP route
@@ -45,13 +45,17 @@ object JsonWebService extends App {
     .contramap[Request] {
       case Method.GET -> Root / "show"          => UserRequest.Show
       case req @ Method.POST -> Root / "create" => {
-        UserRequest.Create(getUser(req.getBodyAsString.get))
+        getUser(req.getBodyAsString.get) match {
+          case Left(_)     => UserRequest.BadRequest
+          case Right(user) => UserRequest.Create(user)
+        }
       }
       case _                                    => UserRequest.BadRequest
     }
     .map {
       case UserResponse.Show(users)  => Response.text(users.asJson.noSpaces)
       case UserResponse.Create(user) => Response.text(user.asJson.noSpaces)
+      case UserResponse.BadResponse  => Response.status(Status.BAD_REQUEST)
     }
 
   // Run it like any simple app
