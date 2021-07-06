b458d787b1a03ad27904e33e5716abd6a38de2e0
Tushar Mathur <tusharmath@gmail.com>
2021-07-06 18:49:53 +0530

    refactor(channel): add `map` operator in Event

--- zio-http/src/main/scala/zhttp/channel/Event.scala
+++ zio-http/src/main/scala/zhttp/channel/Event.scala
@@ -3,10 +3,21 @@ package zhttp.channel
 /**
  * Represents all the incoming events on a channel
  */
-sealed trait Event[+A]
+sealed trait Event[+A] { self =>
+  import Event._
+  def map[B](ab: A => B): Event[B] =
+    self match {
+      case Read(data)    => Read(ab(data))
+      case msg: TypeLess => msg
+    }
+}
+
 object Event {
-  case object Register                 extends Event[Nothing]
-  case class Read[A](data: A)          extends Event[A]
-  case class Failure(cause: Throwable) extends Event[Nothing]
-  case object Complete                 extends Event[Nothing]
+  case class Read[A](data: A) extends Event[A]
+
+  sealed trait TypeLess                extends Event[Nothing]
+  case object Register                 extends TypeLess
+  case class Failure(cause: Throwable) extends TypeLess
+  case object Complete                 extends TypeLess
+
 }
