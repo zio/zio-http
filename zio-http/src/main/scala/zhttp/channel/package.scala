64022c1a8d95ed6de17ee4d0c7dc82685a1925f7
Tushar Mathur <tusharmath@gmail.com>
2021-07-06 18:50:40 +0530

    refactor(http): update typealias

--- zio-http/src/main/scala/zhttp/channel/package.scala
+++ zio-http/src/main/scala/zhttp/channel/package.scala
@@ -3,7 +3,7 @@ package zhttp
 import zhttp.http._
 
 package object channel {
-  type UOperation[+A]                    = Operation[Any, Nothing, A]
+  type UOperation[+A]                    = Operation[A]
   type HttpDataChannel[-R, +E, S]        = HttpChannel[R, E, S, S]
   type HttpChannelHandler[-R, +E, -A, B] = Http[R, E, A, HttpDataChannel[R, E, B]]
   type UHttpChannelHandler[-A, B]        = HttpChannelHandler[Any, Nothing, A, B]
