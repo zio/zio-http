f554482c4302e0f6cd3258ac18826bf7d99a2e1e
Scala Steward <me@scala-steward.org>
2021-07-09 14:41:09 +0200

    Update scala3-library to 3.0.1

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import xerial.sbt.Sonatype.autoImport._
 object BuildHelper extends ScalaSettings {
   val Scala212   = "2.12.14"
   val Scala213   = "2.13.6"
-  val ScalaDotty = "3.0.0"
+  val ScalaDotty = "3.0.1"
 
   private val stdOptions = Seq(
     "-deprecation",
