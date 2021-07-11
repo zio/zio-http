ef4405caea838722855af1a885ce71b5f321845b
Tushar Mathur <tusharmath@gmail.com>
2021-07-11 18:22:05 +0530

    refactor(channel): add support to maintain states via operation

--- zio-http/src/main/scala/zhttp/channel/Operation.scala
+++ zio-http/src/main/scala/zhttp/channel/Operation.scala
@@ -9,42 +9,32 @@ import io.netty.channel.{ChannelHandlerContext => JChannelHandlerContext}
  *
  * TODO: Add Benchmarks and possibly add stack-safety.
  */
-sealed trait Operation[+A] { self =>
+sealed trait Operation[+A, +S] { self =>
   import Operation._
 
-  def combine[A1 >: A](other: Operation[A1]): Operation[A1] = Operation.Combine(self, other)
+  def combine[A1 >: A, S1 >: S](other: Operation[A1, S1]): Operation[A1, S1] = Operation.Combine(self, other)
 
-  def ++[A1 >: A](other: Operation[A1]): Operation[A1] = self combine other
+  def ++[A1 >: A, S1 >: S](other: Operation[A1, S1]): Operation[A1, S1] = self combine other
 
-  def map[B](aa: A => B): Operation[B] = FMap(self, aa)
-
-  private[zhttp] def execute(ctx: JChannelHandlerContext): Unit =
-    self match {
-      case Write(data)          => ctx.write(data): Unit
-      case Combine(self, other) => self.execute(ctx); other.execute(ctx);
-      case Modify(cb)           => cb(ctx): Unit
-      case FMap(self, ab)       =>
-        (self match {
-          case Write(data)          => Write(ab(data))
-          case Combine(self, other) => Combine(self.map(ab), other.map(ab))
-          case FMap(self, bc)       => self.map(bc.andThen(ab))
-          case m @ Modify(_)        => m
-        }).execute(ctx)
-    }
+  def map[B](aa: A => B): Operation[B, S] = FMap(self, aa)
 }
 
 object Operation {
-  val close: Operation[Nothing]                                     = modify(_.close())
-  val read: Operation[Nothing]                                      = modify(_.read())
-  val flush: Operation[Nothing]                                     = modify(_.flush)
-  val empty: Operation[Nothing]                                     = modify(_ => ())
-  val startAutoRead: Operation[Nothing]                             = modify(_.channel().config().setAutoRead(true))
-  val stopAutoRead: Operation[Nothing]                              = modify(_.channel().config().setAutoRead(false))
-  def write[A](a: A): Operation[A]                                  = Write(a)
-  def modify(cb: JChannelHandlerContext => Any): Operation[Nothing] = Modify(cb)
+  val close: Operation[Nothing, Nothing]                                  = run(_.close())
+  val read: Operation[Nothing, Nothing]                                   = run(_.read())
+  val flush: Operation[Nothing, Nothing]                                  = run(_.flush())
+  val empty: Operation[Nothing, Nothing]                                  = run(_ => ())
+  val startAutoRead: Operation[Nothing, Nothing]                          = run(_.channel().config().setAutoRead(true))
+  val stopAutoRead: Operation[Nothing, Nothing]                           = run(_.channel().config().setAutoRead(false))
+  def write[A](a: A): Operation[A, Nothing]                               = Write(a)
+  def save[S](s: S): Operation[Nothing, S]                                = Save(s)
+  def run(cb: JChannelHandlerContext => Any): Operation[Nothing, Nothing] = Run(cb)
+  def combine[A, S](list: Iterable[Operation[A, S]]): Operation[A, S]     =
+    list.fold(Operation.empty)((a, b) => a ++ b)
 
-  case class Write[A](data: A)                                   extends Operation[A]
-  case class Combine[A](self: Operation[A], other: Operation[A]) extends Operation[A]
-  case class FMap[A, B](self: Operation[A], ab: A => B)          extends Operation[B]
-  case class Modify(cb: JChannelHandlerContext => Any)           extends Operation[Nothing]
+  private[zhttp] final case class Write[A](data: A)                                            extends Operation[A, Nothing]
+  private[zhttp] final case class Combine[A, S](self: Operation[A, S], other: Operation[A, S]) extends Operation[A, S]
+  private[zhttp] final case class FMap[A, B, S](self: Operation[A, S], ab: A => B)             extends Operation[B, S]
+  private[zhttp] final case class Run(cb: JChannelHandlerContext => Any)                       extends Operation[Nothing, Nothing]
+  private[zhttp] final case class Save[S](cb: S)                                               extends Operation[Nothing, S]
 }
