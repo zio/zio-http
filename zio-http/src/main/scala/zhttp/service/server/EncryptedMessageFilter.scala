e26296d1796d63d921915c99f2334911cc9bdb5f
dinojohn <66246799+dinojohn@users.noreply.github.com>
2021-07-12 19:53:15 +0530

    feat: added a filter for encrypted message that may be end on an unencrypted server

---
+++ zio-http/src/main/scala/zhttp/service/server/EncryptedMessageFilter.scala
@@ -0,0 +1,31 @@
+package zhttp.service.server
+
+import io.netty.handler.codec.http.{HttpServerKeepAliveHandler => JHttpServerKeepAliveHandler}
+import io.netty.handler.codec.{ByteToMessageDecoder => JByteToMessageDecoder}
+import io.netty.handler.ssl.{SslHandler => JSslHandler}
+import zhttp.core.{JByteBuf, JChannelHandler, JChannelHandlerContext, JHttpObjectAggregator}
+import zhttp.service.Server.Settings
+import zhttp.service._
+
+import java.util
+
+case class EncryptedMessageFilter[R](httpH: JChannelHandler, settings: Settings[R, Throwable])
+  extends JByteToMessageDecoder {
+  override def decode(context: JChannelHandlerContext, in: JByteBuf, out: util.List[AnyRef]): Unit = {
+    if (JSslHandler.isEncrypted(in)) {
+      context.channel().pipeline().remove(CLEAR_TEXT_HTTP2_HANDLER)
+      context.channel().pipeline().remove(CLEAR_TEXT_HTTP2_FALLBACK_HANDLER)
+      context
+        .channel()
+        .pipeline()
+        .addLast(HTTP_KEEPALIVE_HANDLER, new JHttpServerKeepAliveHandler)
+        .addLast(OBJECT_AGGREGATOR, new JHttpObjectAggregator(settings.maxRequestSize))
+        .addLast(HTTP_REQUEST_HANDLER, httpH)
+        .remove(this)
+      ()
+    } else {
+      context.pipeline().remove(this)
+      ()
+    }
+  }
+}
