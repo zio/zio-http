41938553dc45b9cb7a5dd74fd36d60614650c3a4
shrutiverma97 <shruti@dream11.com>
2021-07-25 16:34:11 +0530

    fix(GetBodyAsStringSpec): simplified test case

--- zio-http/src/test/scala/zhttp/http/GetBodyAsStringSpec.scala
+++ zio-http/src/test/scala/zhttp/http/GetBodyAsStringSpec.scala
@@ -1,45 +1,26 @@
 package zhttp.http
 
-import zio.random.Random
+import zio.Chunk
 import zio.test.Assertion._
 import zio.test._
-import zio.test.magnolia.DeriveGen
-import zio.{Chunk, ZIO}
 
-import java.nio.charset.StandardCharsets
+import java.nio.charset.StandardCharsets._
 
 object GetBodyAsStringSpec extends DefaultRunnableSpec {
-  sealed trait Charset
-  case object UTF_8      extends Charset
-  case object UTF_16     extends Charset
-  case object UTF_16BE   extends Charset
-  case object UTF_16LE   extends Charset
-  case object US_ASCII   extends Charset
-  case object ISO_8859_1 extends Charset
-
-  val genPoint: Gen[Random with Sized, Charset] = DeriveGen[Charset]
-  def getCharset(a: Charset)                    = a match {
-    case UTF_8      => StandardCharsets.UTF_8
-    case UTF_16     => StandardCharsets.UTF_16
-    case UTF_16BE   => StandardCharsets.UTF_16BE
-    case UTF_16LE   => StandardCharsets.UTF_16LE
-    case US_ASCII   => StandardCharsets.US_ASCII
-    case ISO_8859_1 => StandardCharsets.ISO_8859_1
-  }
 
   def spec = suite("getBodyAsString")(
     testM("should map bytes according to charset given") {
-      val actual: ZIO[Random with Sized, Option[Nothing], Boolean] =
+      val actual: Gen[Any, Boolean] =
         for {
-          a <- genPoint.runHead.get
+          a <- Gen.fromIterable(List(UTF_8, UTF_16, UTF_16BE, UTF_16LE, US_ASCII, ISO_8859_1))
           b = Request(
             endpoint = Method.GET -> URL(Path("/")),
-            content = HttpData.CompleteData(Chunk.fromArray("abc".getBytes(getCharset(a)))),
-          ).getBodyAsString(getCharset(a))
-          c = Option(new String(Chunk.fromArray("abc".getBytes(getCharset(a))).toArray, getCharset(a)))
+            content = HttpData.CompleteData(Chunk.fromArray("abc".getBytes(a))),
+          ).getBodyAsString(a)
+          c = Option(new String(Chunk.fromArray("abc".getBytes(a)).toArray, a))
         } yield b.equals(c)
 
-      assertM(actual)(isTrue)
+      check(actual) { actual => assert(actual)(equalTo(true)) }
     },
     test("should map bytes to default utf-8 if no charset given") {
       val data                            = Chunk.fromArray("abc".getBytes())
