f360cab539ca7641c12cb0717512757209fde253
Shea Hawkins <shea@engagio.com>
2021-07-25 22:53:03 -0600

    Wrap the resulting HttpResult in a zio.

--- zio-http-test/src/main/scala/zhttp/test/test.scala
+++ zio-http-test/src/main/scala/zhttp/test/test.scala
@@ -1,10 +1,11 @@
 package zhttp
 
 import zhttp.http.{Http, HttpResult}
+import zio.ZIO
 
 package object test {
   implicit class HttpWithTest[R, E, A, B](http: Http[R, E, A, B]) {
-    def apply(req: A): HttpResult[R, E, B] = http.execute(req)
+    def apply(req: A): zio.UIO[HttpResult[R, E, B]] = ZIO.succeed(http.execute(req))
   }
 }
 
