d38c1eb5ecb9b73804383f7e3b652cf1a60d07b8
shrutiverma97 <shruti@dream11.com>
2021-07-27 17:45:58 +0530

    fix(GetBodyAsStringSpec): fixed zio-gen test

--- zio-http/src/test/scala/zhttp/http/GetBodyAsStringSpec.scala
+++ zio-http/src/test/scala/zhttp/http/GetBodyAsStringSpec.scala
@@ -4,23 +4,25 @@ import zio.Chunk
 import zio.test.Assertion._
 import zio.test._
 
+import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets._
 
 object GetBodyAsStringSpec extends DefaultRunnableSpec {
 
   def spec = suite("getBodyAsString")(
     testM("should map bytes according to charset given") {
-      val actual: Gen[Any, Boolean] =
-        for {
-          a <- Gen.fromIterable(List(UTF_8, UTF_16, UTF_16BE, UTF_16LE, US_ASCII, ISO_8859_1))
-          b = Request(
-            endpoint = Method.GET -> URL(Path("/")),
-            content = HttpData.CompleteData(Chunk.fromArray("abc".getBytes(a))),
-          ).getBodyAsString(a)
-          c = Option(new String(Chunk.fromArray("abc".getBytes(a)).toArray, a))
-        } yield b.equals(c)
+      val charsetGen: Gen[Any, Charset] =
+        Gen.fromIterable(List(UTF_8, UTF_16, UTF_16BE, UTF_16LE, US_ASCII, ISO_8859_1))
 
-      check(actual) { actual => assert(actual)(equalTo(true)) }
+      check(charsetGen) { charset =>
+        val encoded = Request(
+          endpoint = Method.GET -> URL(Path("/")),
+          content = HttpData.CompleteData(Chunk.fromArray("abc".getBytes(charset))),
+        ).getBodyAsString(charset)
+        val actual  = Option(new String(Chunk.fromArray("abc".getBytes(charset)).toArray, charset))
+
+        assert(actual)(equalTo(encoded))
+      }
     },
     test("should map bytes to default utf-8 if no charset given") {
       val data                            = Chunk.fromArray("abc".getBytes())
