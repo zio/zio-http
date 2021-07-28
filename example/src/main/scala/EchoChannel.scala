c10a0030f314b2df45949361ba334e2b23a554f4
Tushar Mathur <tusharmath@gmail.com>
2021-07-28 15:35:31 +0530

    refactor(example): update echo channel example

--- example/src/main/scala/EchoChannel.scala
+++ example/src/main/scala/EchoChannel.scala
@@ -3,7 +3,6 @@ import io.netty.handler.codec.http.{
   DefaultFullHttpResponse => JDefaultFullHttpResponse,
   DefaultHttpHeaders => JDefaultHttpHeaders,
   DefaultHttpResponse => JDefaultHttpResponse,
-  EmptyHttpHeaders => JEmptyHttpHeaders,
   HttpContent => JHttpContent,
   HttpHeaderNames => JHttpHeaderNames,
   HttpHeaderValues => JHttpHeaderValues,
@@ -18,72 +17,85 @@ import zhttp.http._
 import zhttp.service.Server
 import zio._
 
-import java.nio.charset.StandardCharsets
-
 object EchoChannel extends App {
   def chunkedHeaders = new JDefaultHttpHeaders().set(JHttpHeaderNames.TRANSFER_ENCODING, JHttpHeaderValues.CHUNKED)
 
-  val health = Http.collect[JHttpRequest] {
-    case req if req.uri() == "/health" =>
-      HttpChannel.collect[JHttpObject] {
-        case Event.Complete => Operation.flush
-        case Event.Read(_)  =>
-          Operation.write(new JDefaultHttpResponse(req.protocolVersion(), JHttpResponseStatus.OK)) ++
-            Operation.write(JLastHttpContent.EMPTY_LAST_CONTENT)
-      }
+  val health: Http[Any, Throwable, JHttpRequest, HttpChannel[Any, Nothing, JHttpContent, JHttpObject]] =
+    Http.collect[JHttpRequest] {
+      case req if req.uri() == "/health" =>
+        HttpChannel.make[JHttpContent, JHttpObject] { case (Event.Register, context) =>
+          ZIO.fail(new Error("What up?"))
+          context.write(new JDefaultHttpResponse(req.protocolVersion(), JHttpResponseStatus.OK)) *>
+            context.write(JLastHttpContent.EMPTY_LAST_CONTENT) *>
+            context.flush
+        }
+    }
 
-  }
+  val serverError: Http[Any, Throwable, JHttpRequest, HttpChannel[Any, Nothing, JHttpContent, JHttpObject]] =
+    Http.collectM[JHttpRequest] {
+      case req if req.uri() == "/server-error" => ZIO.fail(new Error("What up?"))
+    }
 
   val upload = Http.collect[JHttpRequest] {
     case req if req.uri() == "/upload" && req.method == JHttpMethod.POST =>
-      HttpChannel.collect[JHttpObject] {
-        case Event.Read(data) => Operation.write(data)
-        case Event.Complete   => Operation.flush ++ Operation.read
-        case Event.Register   =>
-          Operation.write(new JDefaultHttpResponse(req.protocolVersion(), JHttpResponseStatus.OK, chunkedHeaders))
+      HttpChannel.make[JHttpContent, JHttpObject] {
+        case (Event.Read(data: JLastHttpContent), context) => context.write(data) *> context.flush
+        case (Event.Read(data: JHttpContent), context)     => context.write(data) *> context.flush *> context.read
+        case (Event.Register, context)                     =>
+          context.write(
+            new JDefaultFullHttpResponse(req.protocolVersion(), JHttpResponseStatus.CONTINUE, JUnpooled.EMPTY_BUFFER),
+          )
+          context.flush *>
+            context.write(
+              new JDefaultHttpResponse(req.protocolVersion(), JHttpResponseStatus.OK, chunkedHeaders),
+            ) *> context.flush *>
+            context.read
       }
   }
 
-  case class State[A](content: A, done: Boolean = false)
+//  val fullRequest = Http.collect[JHttpRequest] {}
 
-  val reverseText = Http.collect[JHttpRequest] {
-    case req if req.uri() == "/reverse" && req.method == JHttpMethod.POST =>
-      val contentLength = req.headers().get(JHttpHeaderNames.CONTENT_LENGTH)
-      val headers       = new JDefaultHttpHeaders().set(JHttpHeaderNames.CONTENT_LENGTH, contentLength)
-
-      HttpChannel.collectWith[JHttpContent](State[String]("")) {
-        case (State(_, false), Event.Complete) => Operation.read
-
-        case (s, Event.Read(data)) =>
-          val done   = data.isInstanceOf[JLastHttpContent]
-          val string = data.content().toString(StandardCharsets.UTF_8)
-
-          Operation.save(State(s.content ++ string, done))
-
-        case (State(content, true), Event.Complete) =>
-          val buffer   = JUnpooled.copiedBuffer(content.reverse, StandardCharsets.UTF_8)
-          val response =
-            new JDefaultFullHttpResponse(
-              req.protocolVersion(),
-              JHttpResponseStatus.OK,
-              buffer,
-              headers,
-              JEmptyHttpHeaders.INSTANCE,
-            )
-
-          Operation.write(response) ++ Operation.flush
-      }
-  }
+//  case class State[A](content: A, done: Boolean = false)
+//
+//  val reverseText = Http.collect[JHttpRequest] {
+//    case req if req.uri() == "/reverse" && req.method == JHttpMethod.POST =>
+//      val contentLength = req.headers().get(JHttpHeaderNames.CONTENT_LENGTH)
+//      val headers       = new JDefaultHttpHeaders().set(JHttpHeaderNames.CONTENT_LENGTH, contentLength)
+//
+//      HttpChannel.collectWith[JHttpContent](State[String]("")) {
+//        case (State(_, false), Event.Complete) => Operation.read
+//
+//        case (s, Event.Read(data)) =>
+//          val done   = data.isInstanceOf[JLastHttpContent]
+//          val string = data.content().toString(StandardCharsets.UTF_8)
+//
+//          Operation.save(State(s.content ++ string, done))
+//
+//        case (State(content, true), Event.Complete) =>
+//          val buffer   = JUnpooled.copiedBuffer(content.reverse, StandardCharsets.UTF_8)
+//          val response =
+//            new JDefaultFullHttpResponse(
+//              req.protocolVersion(),
+//              JHttpResponseStatus.OK,
+//              buffer,
+//              headers,
+//              JEmptyHttpHeaders.INSTANCE,
+//            )
+//
+//          Operation.write(response) ++ Operation.flush
+//      }
+//  }
 
   val notFound = Http.collect[JHttpRequest] { case req =>
-    HttpChannel.collect[JHttpObject] { case Event.Complete =>
-      Operation.write(new JDefaultHttpResponse(req.protocolVersion(), JHttpResponseStatus.NOT_FOUND)) ++
-        Operation.write(JLastHttpContent.EMPTY_LAST_CONTENT) ++
-        Operation.flush
+    HttpChannel.make[JHttpContent, JHttpObject] { case (_, context) =>
+      context.write(new JDefaultHttpResponse(req.protocolVersion(), JHttpResponseStatus.NOT_FOUND)) *>
+        context.write(JLastHttpContent.EMPTY_LAST_CONTENT) *>
+        context.flush
     }
+
   }
 
-  val app = health +++ upload +++ reverseText +++ notFound
+  val app = health +++ upload +++ serverError
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
     Server.start0(8090, app).exitCode
