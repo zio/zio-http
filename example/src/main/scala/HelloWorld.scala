bcb92a14edaa59580202ac4075343e9999e7d26a
Tushar Mathur <tusharmath@gmail.com>
2021-07-28 10:41:08 +0530

    refactor: add example back

---
+++ example/src/main/scala/HelloWorld.scala
@@ -0,0 +1,16 @@
+import zhttp.http._
+import zhttp.service.Server
+import zio._
+
+object HelloWorld extends App {
+
+  // Create HTTP route
+  val app: HttpApp[Any, Nothing] = HttpApp.collect {
+    case Method.GET -> Root / "text" => Response.text("Hello World!")
+    case Method.GET -> Root / "json" => Response.jsonString("""{"greetings": "Hello World!"}""")
+  }
+
+  // Run it like any simple app
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    Server.start(8090, app.silent).exitCode
+}
