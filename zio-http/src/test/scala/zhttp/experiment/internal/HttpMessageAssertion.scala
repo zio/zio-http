4a9587ab85a27abe1d7af8ada867dacfcd775042
Tushar Mathur <tusharmath@gmail.com>
2021-08-02 18:57:13 +0530

    test(http): add HttpMessageAssertion

---
+++ zio-http/src/test/scala/zhttp/experiment/internal/HttpMessageAssertion.scala
@@ -0,0 +1,24 @@
+package zhttp.experiment.internal
+
+import io.netty.handler.codec.http.{HttpMessage, HttpResponse}
+import zio.test.Assertion
+import zio.test.Assertion.anything
+import zio.test.AssertionM.Render.param
+
+trait HttpMessageAssertion {
+  implicit final class HttpMessageSyntax(m: HttpMessage) {
+    def asString: String = m.toString.dropWhile(_ != '\n')
+  }
+
+  def isResponse[A](assertion: Assertion[HttpResponse]): Assertion[A] =
+    Assertion.assertion("isResponse")(param(assertion))(_.isInstanceOf[HttpResponse])
+
+  def statusIs[A](code: Int): Assertion[HttpResponse] =
+    Assertion.assertion("statusIs")()(_.status.code == code)
+
+  def hasHeader[A](name: String, value: String, ignoreCase: Boolean = true): Assertion[HttpResponse] =
+    Assertion.assertion("statusIs")()(_.headers().contains(name, value, ignoreCase))
+
+  def isAnyResponse: Assertion[Any] = isResponse(anything)
+
+}
