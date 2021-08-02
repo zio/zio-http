dc1bee3bc6b89d40d0a28096dd0742d1669f78a6
Tushar Mathur <tusharmath@gmail.com>
2021-08-02 18:57:03 +0530

    test(http): add HttpQueue

---
+++ zio-http/src/test/scala/zhttp/experiment/internal/HttpQueue.scala
@@ -0,0 +1,31 @@
+package zhttp.experiment.internal
+
+import io.netty.handler.codec.http.HttpMessage
+import zhttp.experiment.HApp
+import zio.{Queue, UIO, ZIO}
+import io.netty.channel.embedded.EmbeddedChannel
+import zhttp.service.{EventLoopGroup, UnsafeChannelExecutor}
+
+case class HttpQueue(rtm: zio.Runtime[Any], channel: EmbeddedChannel, queue: Queue[HttpMessage]) {
+  def offer(a: HttpMessage): UIO[Boolean] = UIO(channel.writeInbound(a))
+  def take: UIO[HttpMessage]              = queue.take
+}
+
+object HttpQueue {
+  def make[R](
+    app: HApp[R, Throwable],
+  ): ZIO[R with EventLoopGroup, Nothing, HttpQueue] =
+    for {
+      zExec <- UnsafeChannelExecutor.make[R]
+      rtm   <- ZIO.runtime[Any]
+      queue <- Queue.unbounded[HttpMessage]
+    } yield {
+      val embeddedChannel = new EmbeddedChannel(app.compile(zExec)) {
+        override def handleOutboundMessage(msg: AnyRef): Unit = {
+          rtm.unsafeRunAsync_(queue.offer(msg.asInstanceOf[HttpMessage]))
+          super.handleOutboundMessage(msg)
+        }
+      }
+      HttpQueue(rtm, embeddedChannel, queue)
+    }
+}
