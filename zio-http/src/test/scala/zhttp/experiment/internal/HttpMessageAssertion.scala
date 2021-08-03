adad4e38c139893ce013ab44d8656bc5f65122b3
Tushar Mathur <tusharmath@gmail.com>
2021-08-03 17:11:24 +0530

    test(http): add more test helpers for HttpMessage

--- zio-http/src/test/scala/zhttp/experiment/internal/HttpMessageAssertion.scala
+++ zio-http/src/test/scala/zhttp/experiment/internal/HttpMessageAssertion.scala
@@ -1,9 +1,11 @@
 package zhttp.experiment.internal
 
-import io.netty.handler.codec.http.{HttpMessage, HttpResponse}
+import io.netty.handler.codec.http.{HttpContent, HttpMessage, HttpResponse, LastHttpContent}
 import zio.test.Assertion
 import zio.test.Assertion.anything
 import zio.test.AssertionM.Render.param
+
+import java.nio.charset.Charset
 
 trait HttpMessageAssertion {
   implicit final class HttpMessageSyntax(m: HttpMessage) {
@@ -16,12 +18,26 @@ trait HttpMessageAssertion {
       case _                 => None
     })
 
-  def statusIs[A](code: Int): Assertion[HttpResponse] =
-    Assertion.assertion("statusIs")(param(code))(_.status().code() == code)
+  def isContent[A](assertion: Assertion[HttpContent]): Assertion[A] =
+    Assertion.assertionRec("isContent")(param(assertion))(assertion)({
+      case msg: HttpContent => Option(msg)
+      case _                => None
+    })
 
-  def hasHeader[A](name: String, value: String, ignoreCase: Boolean = true): Assertion[HttpResponse] =
-    Assertion.assertion("hasHeader")(param(s"$name: $value"))(_.headers().contains(name, value, ignoreCase))
+  def isLastContent[A](assertion: Assertion[LastHttpContent]): Assertion[A] =
+    Assertion.assertionRec("isLastContent")(param(assertion))(assertion)({
+      case msg: LastHttpContent => Option(msg)
+      case _                    => None
+    })
+
+  def status(code: Int): Assertion[HttpResponse] =
+    Assertion.assertion("status")(param(code))(_.status().code() == code)
+
+  def bodyText(data: String, charset: Charset = Charset.defaultCharset()): Assertion[HttpContent] =
+    Assertion.assertion("body")(param(data))(_.content().toString(charset) == data)
+
+  def header[A](name: String, value: String, ignoreCase: Boolean = true): Assertion[HttpResponse] =
+    Assertion.assertion("header")(param(s"$name: $value"))(_.headers().contains(name, value, ignoreCase))
 
   def isAnyResponse: Assertion[Any] = isResponse(anything)
-
 }
