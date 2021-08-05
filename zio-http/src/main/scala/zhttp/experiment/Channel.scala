2c4fdafc17b680a06b566bb0236b3cb17b4fcf02
Tushar Mathur <tusharmath@gmail.com>
2021-08-05 22:48:54 +0530

    refactor(http): make Channel polymorphic

--- zio-http/src/main/scala/zhttp/experiment/Channel.scala
+++ zio-http/src/main/scala/zhttp/experiment/Channel.scala
@@ -1,25 +1,21 @@
 package zhttp.experiment
 
-import io.netty.channel.ChannelHandler
 import zio.ZIO
 
 trait Channel[-R, +E, -A, +B] {
   def message(message: A, context: Context[B]): ZIO[R, E, Any]
   def error(cause: Throwable, context: Context[B]): ZIO[R, E, Any]
   def complete(context: Context[B]): ZIO[R, E, Any]
-
-  def compile: ChannelHandler = ???
 }
 
 object Channel {
-  def apply[R, E](
-    onRead: (CByte, Context[CByte]) => ZIO[R, E, Any] = (a: CByte, b: Context[CByte]) => b.fireChannelRead(a),
-    onError: (Throwable, Context[CByte]) => ZIO[R, E, Any] = (a: Throwable, b: Context[CByte]) =>
-      b.fireExceptionCaught(a),
-    onComplete: Context[CByte] => ZIO[R, E, Any] = (b: Context[CByte]) => b.fireChannelReadComplete(),
-  ): Channel[R, E, CByte, CByte] = new Channel[R, E, CByte, CByte] {
-    override def message(message: CByte, context: Context[CByte]): ZIO[R, E, Any] = onRead(message, context)
-    override def error(cause: Throwable, context: Context[CByte]): ZIO[R, E, Any] = onError(cause, context)
-    override def complete(context: Context[CByte]): ZIO[R, E, Any]                = onComplete(context)
+  def apply[R, E, A, B](
+    onRead: (A, Context[B]) => ZIO[R, E, Any] = (a: A, b: Context[B]) => b.fireChannelRead(a),
+    onError: (Throwable, Context[B]) => ZIO[R, E, Any] = (a: Throwable, b: Context[B]) => b.fireExceptionCaught(a),
+    onComplete: Context[B] => ZIO[R, E, Any] = (b: Context[B]) => b.fireChannelReadComplete(),
+  ): Channel[R, E, A, B] = new Channel[R, E, A, B] {
+    override def message(message: A, context: Context[B]): ZIO[R, E, Any]     = onRead(message, context)
+    override def error(cause: Throwable, context: Context[B]): ZIO[R, E, Any] = onError(cause, context)
+    override def complete(context: Context[B]): ZIO[R, E, Any]                = onComplete(context)
   }
 }
