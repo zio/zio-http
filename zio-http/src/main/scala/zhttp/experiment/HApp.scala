4bdb4b5f8da96795e0146663651d0d82a1d54dfa
Tushar Mathur <tusharmath@gmail.com>
2021-08-06 01:02:36 +0530

    refactor(happ): make it agnostic to data types viz ByteBuf and Chunk[Byte]

--- zio-http/src/main/scala/zhttp/experiment/HApp.scala
+++ zio-http/src/main/scala/zhttp/experiment/HApp.scala
@@ -1,145 +1,158 @@
 package zhttp.experiment
 
-import io.netty.buffer.Unpooled
+import io.netty.buffer.{ByteBuf, Unpooled}
 import io.netty.channel.{ChannelHandler, ChannelHandlerContext, ChannelInboundHandlerAdapter}
 import io.netty.handler.codec.http._
-import zhttp.experiment.Check.AutoCheck
-import zhttp.experiment.HttpMessage.HResponse
-import zhttp.http.{Header, HTTP_CHARSET}
+import zhttp.experiment.HttpMessage.{AnyRequest, CompleteRequest, HResponse}
+import zhttp.http.{HTTP_CHARSET, Header, Http}
 import zhttp.service.UnsafeChannelExecutor
-import zio.stream.ZStream
-import zio.{Chunk, UIO, ZQueue}
-
+import zio.{UIO, ZIO}
 import scala.annotation.unused
+import Params._
+import Codec._
 
 sealed trait HApp[-R, +E] { self =>
   def combine[R1 <: R, E1 >: E](other: HApp[R1, E1]): HApp[R1, E1] = HApp.Combine(self, other)
   def +++[R1 <: R, E1 >: E](other: HApp[R1, E1]): HApp[R1, E1]     = self combine other
 
-  private[zhttp] def compile[R1 <: R](
-    @unused zExec: UnsafeChannelExecutor[R1],
-    @unused config: HApp.Config = HApp.Config(),
-  )(implicit
-    ev: E <:< Throwable,
+  private[zhttp] def compile[R1 <: R](zExec: UnsafeChannelExecutor[R1])(implicit
+    evE: E <:< Throwable,
   ): ChannelHandler =
     new ChannelInboundHandlerAdapter { adapter =>
+      import HttpVersion._
+      import HttpResponseStatus._
+
+      val app                                                                                 = self.asInstanceOf[HApp[R, Throwable]]
+      var completeHttpApp: Http[R, Throwable, CompleteRequest[_], HResponse[R, Throwable, _]] = Http.empty
+      val completeBody: ByteBuf                                                               = Unpooled.compositeBuffer()
+      var jRequest: HttpRequest                                                               = _
+      @unused var encoder: Encoder[_]                                                         = _
+      var decoder: Decoder[_]                                                                 = _
+
       override def channelRead(ctx: ChannelHandlerContext, msg: Any): Unit = {
+        def eval(program: ZIO[R, Throwable, Any]): Unit = zExec.unsafeExecute_(ctx)(program)
+
         val void = ctx.channel().voidPromise()
         msg match {
-          case _: HttpRequest     =>
-            self match {
-              case HApp.Empty          =>
-                ctx.writeAndFlush(HApp.notFoundResponse, void): Unit
-              case HApp.Succeed(http)  =>
-                zExec.unsafeExecute_(ctx) {
+          case msg: HttpRequest     =>
+            adapter.jRequest = msg
+
+            app match {
+              case HApp.Fail(cause) =>
+                ctx.writeAndFlush(serverErrorResponse(cause), void): Unit
+
+              case HApp.Empty =>
+                ctx.writeAndFlush(notFoundResponse, void): Unit
+
+              case HApp.Response(_, http) =>
+                eval {
                   (for {
                     res <- http.executeAsZIO(())
                     _   <- UIO(ctx.writeAndFlush(decodeResponse(res), void))
                   } yield ()).catchAll({
-                    case Some(cause) => UIO(ctx.writeAndFlush(HApp.serverErrorResponse(cause), void): Unit)
-                    case None        => UIO(ctx.writeAndFlush(HApp.notFoundResponse, void): Unit)
+                    case Some(cause) => UIO(ctx.writeAndFlush(serverErrorResponse(cause), void): Unit)
+                    case None        => UIO(ctx.writeAndFlush(notFoundResponse, void): Unit)
                   })
                 }
-              case HApp.Combine(_, _)  => ???
-              case HApp.Complete(_, _) => ???
-              case HApp.Partial(_, _)  => ???
+
+              case HApp.Complete(decoder, encoder, http) =>
+                adapter.completeHttpApp = http
+                adapter.encoder = encoder
+                adapter.decoder = decoder
+
+              case HApp.Unknown(_, _) => ???
+
+              case HApp.Buffered(_, _, _) => ???
+
+              case HApp.Combine(_, _) => ???
             }
-          case _: LastHttpContent => ???
-          case _: HttpContent     => ???
-          case _                  => ???
+          case msg: LastHttpContent =>
+            adapter.completeBody.writeBytes(msg.content())
+            val request = AnyRequest.from(adapter.jRequest)
+            eval {
+              (for {
+                res <- adapter.completeHttpApp.executeAsZIO(
+                  CompleteRequest(request, adapter.decoder.decode(adapter.completeBody)),
+                )
+                _   <- UIO(ctx.writeAndFlush(decodeResponse(res), void))
+
+              } yield ()).catchAll({
+                case Some(_) => ???
+                case None    => ???
+              })
+            }
+          case msg: HttpContent     => completeBody.writeBytes(msg.content()): Unit
+          case _                    => ???
         }
       }
+      private def decodeResponse(res: HResponse[_, _, _]): HttpResponse = {
+        new DefaultHttpResponse(HttpVersion.HTTP_1_1, res.status.toJHttpStatus, Header.disassemble(res.headers))
+      }
+
+      private val notFoundResponse = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND, false)
+
+      private def serverErrorResponse(cause: Throwable): HttpResponse = {
+        val content  = cause.toString
+        val response = new DefaultFullHttpResponse(
+          HTTP_1_1,
+          INTERNAL_SERVER_ERROR,
+          Unpooled.copiedBuffer(content, HTTP_CHARSET),
+        )
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.length)
+        response
+      }
+
     }
 
-  private def decodeResponse(res: HResponse[_, _]): HttpResponse = {
-    new DefaultHttpResponse(
-      HttpVersion.HTTP_1_1,
-      res.status.toJHttpStatus,
-      Header.disassemble(res.headers),
-    )
-  }
 }
 
 object HApp {
 
-  import HttpVersion._
-  import HttpResponseStatus._
+  case object Empty extends HApp[Any, Nothing]
 
-  case object Empty                                                                       extends HApp[Any, Nothing]
-  case class Combine[R, E](self: HApp[R, E], other: HApp[R, E])                           extends HApp[R, E]
-  case class Complete[R, E](check: Check[AnyRequest], http: RHttp[R, E, CompleteRequest]) extends HApp[R, E]
-  case class Partial[R, E](check: Check[AnyRequest], http: RHttp[R, E, AnyRequest])       extends HApp[R, E]
-  case class Succeed[R, E](http: RHttp[R, E, Any])                                        extends HApp[R, E]
+  final case class Combine[R, E](self: HApp[R, E], other: HApp[R, E]) extends HApp[R, E]
 
-  case class Config(validateHeaders: Boolean = true)
+  final case class Complete[R, E, A, B](
+    decoder: Decoder[A],
+    encoder: Encoder[B],
+    http: Http[R, E, CompleteRequest[A], HResponse[R, E, B]],
+  ) extends HApp[R, E]
 
-  private val notFoundResponse = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND, false)
-  private def serverErrorResponse(cause: Throwable): HttpResponse = {
-    val content  = cause.toString
-    val response = new DefaultFullHttpResponse(
-      HTTP_1_1,
-      INTERNAL_SERVER_ERROR,
-      Unpooled.copiedBuffer(content, HTTP_CHARSET),
-    )
-    response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.length)
-    response
-  }
+  final case class Buffered[R, E, A, B](
+    decoder: Decoder[A],
+    encoder: Encoder[B],
+    http: Http[R, E, BufferedRequest[A], HResponse[R, E, B]],
+  ) extends HApp[R, E]
 
-  // App Generators
-  sealed trait HAppGen[A] {
-    def make[R, E](check: Check[AnyRequest], http: RHttp[R, E, A]): HApp[R, E]
-  }
+  final case class Unknown[R, E, B](encoder: Encoder[B], http: Http[R, E, AnyRequest, HResponse[R, E, B]])
+      extends HApp[R, E]
 
-  implicit object CompleteRequestHAppGen extends HAppGen[CompleteRequest] {
-    override def make[R, E](check: Check[AnyRequest], http: RHttp[R, E, CompleteRequest]): HApp[R, E] =
-      HApp.complete(check, http)
-  }
+  final case class Response[R, E, B](encoder: Encoder[B], http: Http[R, E, Any, HResponse[R, E, B]]) extends HApp[R, E]
 
-  implicit object PartialRequestHAppGen extends HAppGen[AnyRequest] {
-    override def make[R, E](check: Check[AnyRequest], http: RHttp[R, E, AnyRequest]): HApp[R, E] =
-      HApp.partial(check, http)
-  }
+  final case class Fail[E, A](cause: E) extends HApp[Any, E]
 
-  implicit object BufferedRequestHAppGen extends HAppGen[BufferedRequest] {
-    override def make[R, E](check: Check[AnyRequest], http: RHttp[R, E, BufferedRequest]): HApp[R, E] =
-      HApp.streaming(check, http)
-  }
+  def empty: HApp[Any, Nothing] =
+    HApp.Empty
 
-  implicit object AnyHAppGen extends HAppGen[Any] {
-    override def make[R, E](check: Check[AnyRequest], http: RHttp[R, E, Any]): HApp[R, E] =
-      HApp.streaming(check, http)
-  }
+  def from[R, E](
+    http: Http[R, E, Any, HResponse[R, E, ByteBuf]],
+  )(implicit encoder: Encoder[ByteBuf], ev: P1): HApp[R, E] =
+    Response(encoder, http)
 
-  // Constructor
-  def apply[R, E, A, C](c: Check[AnyRequest], http: RHttp[R, E, A])(implicit hl: HAppGen[A]): HApp[R, E] =
-    hl.make(c, http)
+  def from[R, E](
+    http: Http[R, E, CompleteRequest[ByteBuf], HResponse[R, E, ByteBuf]],
+  )(implicit decoder: Decoder[ByteBuf], encoder: Encoder[ByteBuf], ev: P2): HApp[R, E] =
+    Complete(decoder, encoder, http)
 
-  def apply[R, E, A, C](c: C)(http: RHttp[R, E, A])(implicit ch: AutoCheck[C, AnyRequest], hl: HAppGen[A]): HApp[R, E] =
-    HApp(ch.toCheck(c), http)
+  def from[R, E](
+    http: Http[R, E, BufferedRequest[ByteBuf], HResponse[R, E, ByteBuf]],
+  )(implicit decoder: Decoder[ByteBuf], encoder: Encoder[ByteBuf], ev: P3): HApp[R, E] =
+    Buffered(decoder, encoder, http)
 
-  def apply[R, E, A](http: RHttp[R, E, A])(implicit hl: HAppGen[A]): HApp[R, E] =
-    HApp(Check.isTrue, http)
+  def from[R, E](
+    http: Http[R, E, AnyRequest, HResponse[R, E, ByteBuf]],
+  )(implicit encoder: Encoder[ByteBuf], ev: P4): HApp[R, E] =
+    Unknown(encoder, http)
 
-  def empty: HApp[Any, Nothing] = HApp.Empty
-
-  def succeed[R, E](http: RHttp[R, E, Any]): HApp[R, E] = HApp.Succeed(http)
-
-  // Helper
-  private[zhttp] def partial[E, R](check: Check[AnyRequest], http: RHttp[R, E, AnyRequest]): HApp[R, E] =
-    Partial(check, http)
-
-  private[zhttp] def streaming[E, R](check: Check[AnyRequest], http: RHttp[R, E, BufferedRequest]): HApp[R, E] =
-    HApp.partial(
-      check,
-      http.contramapM { req: AnyRequest =>
-        ZQueue
-          .bounded[Chunk[Byte]](1)
-          .map(q => req.toBufferedRequest(ZStream.fromChunkQueueWithShutdown(q)))
-      },
-    )
-
-  private def complete[E, R](check: Check[AnyRequest], http: RHttp[R, E, CompleteRequest]) = {
-    HApp.Complete(check, http)
-  }
-
+  def fail[E](cause: E): HApp[Any, E] = HApp.Fail(cause)
 }
--- zio-http/src/test/scala/zhttp/experiment/HAppSpec.scala
+++ zio-http/src/test/scala/zhttp/experiment/HAppSpec.scala
@@ -1,13 +1,16 @@
 package zhttp.experiment
 
+import io.netty.buffer.ByteBuf
 import io.netty.handler.codec.http._
-import zhttp.experiment.internal.HttpMessageAssertion
 import zhttp.experiment.HttpMessage.HResponse
-import zhttp.http.{Header, Http}
+import zhttp.experiment.internal.{HttpMessageAssertion, HttpQueue}
+import zhttp.http.{HTTP_CHARSET, Header, Http}
 import zhttp.service.EventLoopGroup
+import zio.Promise
 import zio.duration._
-import zio.test.DefaultRunnableSpec
+import zio.test.Assertion.equalTo
 import zio.test.TestAspect.timeout
+import zio.test._
 
 /**
  * Be prepared for some real nasty runtime tests.
@@ -48,33 +51,68 @@ object HAppSpec extends DefaultRunnableSpec with HttpMessageAssertion {
     ),
     suite("succeed(ok)")(
       testM("status is 200") {
-        HApp.succeed(Http.succeed(HResponse())) === isResponse(status(200))
+        HApp.from(Http.succeed(HResponse())) === isResponse(status(200))
       },
       testM("headers are empty") {
-        HApp.succeed(Http.succeed(HResponse())) === isResponse(noHeader)
+        HApp.from(Http.succeed(HResponse())) === isResponse(noHeader)
       },
       testM("headers are set") {
-        HApp.succeed(Http.succeed(HResponse(headers = List(Header("key", "value"))))) === isResponse(
+        HApp.from(Http.succeed(HResponse(headers = List(Header("key", "value"))))) === isResponse(
           header("key", "value"),
         )
       },
       testM("version is 1.1") {
-        HApp.succeed(Http.succeed(HResponse())) === isResponse(version("HTTP/1.1"))
+        HApp.from(Http.succeed(HResponse())) === isResponse(version("HTTP/1.1"))
       },
       testM("version is 1.1") {
-        HApp.succeed(Http.succeed(HResponse())) === isResponse(version("HTTP/1.1"))
+        HApp.from(Http.succeed(HResponse())) === isResponse(version("HTTP/1.1"))
       },
     ),
     suite("succeed(fail)")(
       testM("status is 500") {
-        HApp.succeed(Http.fail(new Error("SERVER_ERROR"))) === isResponse(status(500))
+        HApp.from(Http.fail(new Error("SERVER_ERROR"))) === isResponse(status(500))
       },
       testM("content is SERVER_ERROR") {
-        HApp.succeed(Http.fail(new Error("SERVER_ERROR"))) === isResponse(isContent(bodyText("SERVER_ERROR")))
+        HApp.from(Http.fail(new Error("SERVER_ERROR"))) === isResponse(isContent(bodyText("SERVER_ERROR")))
       },
       testM("headers are set") {
-        HApp.succeed(Http.fail(new Error("SERVER_ERROR"))) === isResponse(header("content-length"))
+        HApp.from(Http.fail(new Error("SERVER_ERROR"))) === isResponse(header("content-length"))
+      },
+    ),
+    suite("fail(cause)")(
+      testM("status is 500") {
+        HApp.fail(new Error("SERVER_ERROR")) === isResponse(status(500))
+      },
+      testM("content is SERVER_ERROR") {
+        HApp.fail(new Error("SERVER_ERROR")) === isResponse(isContent(bodyText("SERVER_ERROR")))
+      },
+      testM("headers are set") {
+        HApp.fail(new Error("SERVER_ERROR")) === isResponse(header("content-length"))
+      },
+    ),
+    suite("succeed(CompleteRequest)")(
+      testM("status is 200") {
+        for {
+          proxy <- HttpQueue.make(HApp.from(Http.collect[CompleteRequest[ByteBuf]](req => HResponse())))
+          _     <- proxy.request(HttpVersion.HTTP_1_1, HttpMethod.POST, "/")
+          _     <- proxy.content("Hello")
+          _     <- proxy.content("World", true)
+          res   <- proxy.receive
+        } yield assert(res)(isResponse(status(200)))
+      },
+      testM("content is Hello World") {
+        for {
+          promise <- Promise.make[Nothing, ByteBuf]
+          proxy   <- HttpQueue.make(
+            HApp.from(Http.collectM[CompleteRequest[ByteBuf]](req => promise.succeed(req.content) as HResponse())),
+          )
+          _       <- proxy.request(HttpVersion.HTTP_1_1, HttpMethod.POST, "/")
+          _       <- proxy.content("Hello")
+          _       <- proxy.content("World", true)
+          res     <- promise.await.map(bytes => bytes.toString(HTTP_CHARSET))
+        } yield assert(res)(equalTo("HelloWorld"))
       },
     ),
   ).provideCustomLayer(env) @@ timeout(5 second)
+
 }
