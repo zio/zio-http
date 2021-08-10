bc5916bb3d44e80f09cbad678e7234e3536d8682
Tushar Mathur <tusharmath@gmail.com>
2021-08-10 22:38:49 +0530

    test(http): simplify ChannelProxy

--- zio-http/src/test/scala/zhttp/experiment/internal/ChannelProxy.scala
+++ zio-http/src/test/scala/zhttp/experiment/internal/ChannelProxy.scala
@@ -15,23 +15,28 @@ import scala.concurrent.ExecutionContext
 case class ChannelProxy(
   inbound: TQueue[HttpObject],
   outbound: TQueue[HttpObject],
-  channel: EmbeddedChannel,
   ec: ExecutionContext,
-) extends EmbeddedChannel {
+  rtm: zio.Runtime[Any],
+) extends EmbeddedChannel { self =>
+
+  private var pendingRead: Boolean = false
 
   /**
    * Schedules a `writeInbound` operation on the channel using the provided group. This is done to make sure that all
    * the execution of HEndpoint happens in the same thread.
    */
-  private def scheduleWrite(msg: => HttpObject): UIO[Unit] = (for {
-    canWrite <- UIO(channel.config().isAutoRead)
-    _        <-
-      if (canWrite) UIO { channel.writeInbound(msg) }
-      else
-        for {
-          _ <- inbound.offer(msg).commit
-        } yield ()
-  } yield ()).on(ec)
+  private def scheduleWrite(msg: => HttpObject): UIO[Unit] = UIO {
+    val autoRead = self.config().isAutoRead
+    if (autoRead) self.writeInbound(msg): Unit
+    else {
+      self.handleInboundMessage(msg): Unit
+      if (pendingRead) {
+        self.doBeginRead()
+        pendingRead = false
+      }
+    }
+  }
+    .on(ec)
 
   def receive: UIO[HttpObject] = outbound.take.commit
 
@@ -52,16 +57,38 @@ case class ChannelProxy(
   }
 
   def data(iter: Iterable[String]): UIO[Unit] = {
-    ZIO.foreach(iter)(a => data(a, isLast = false)) *> end
+    ZIO.foreach(iter)(a => data(a, isLast = false)).unit
   }
 
   def end: UIO[Unit] = scheduleWrite(new DefaultLastHttpContent(Unpooled.EMPTY_BUFFER))
+
+  def end(text: String): UIO[Unit] = data(text, true)
+
+  /**
+   * Handles all the outgoing messages ie all the `ctx.write()` and `ctx.writeAndFlush()` that happens inside of the
+   * HEndpoint.
+   */
+  override def handleOutboundMessage(msg: AnyRef): Unit = {
+    rtm.unsafeRunAsync_(outbound.offer(msg.asInstanceOf[HttpObject]).commit)
+  }
+
+  /**
+   * Called whenever `ctx.read()` is called from withing the HEndpoint
+   */
+  override def doBeginRead(): Unit = {
+    val msg = self.readInbound[HttpObject]()
+    if (msg == null) {
+      pendingRead = true
+    } else {
+      self.writeInbound(msg): Unit
+      pendingRead = false
+    }
+  }
 }
 
 object ChannelProxy {
-  def make[R](app: HEndpoint[R, Throwable]): ZIO[R with EventLoopGroup, Nothing, ChannelProxy] =
+  def make[R](app: HEndpoint[R, Throwable]): ZIO[R with EventLoopGroup, Nothing, ChannelProxy] = {
     for {
-
       group <- ZIO.access[EventLoopGroup](_.get)
       rtm   <- ZIO.runtime[Any]
       ec   = ExecutionContext.fromExecutor(group)
@@ -71,33 +98,10 @@ object ChannelProxy {
       outbound <- TQueue.unbounded[HttpObject].commit
       inbound  <- TQueue.unbounded[HttpObject].commit
       proxy    <- UIO {
-        val embeddedChannel = new EmbeddedChannel(app.compile(zExec)) { self =>
-          /**
-           * Handles all the outgoing messages ie all the `ctx.write()` and `ctx.writeAndFlush()` that happens inside of
-           * the HEndpoint.
-           */
-          override def handleOutboundMessage(msg: AnyRef): Unit = {
-            gRtm.unsafeRunAsync_(outbound.offer(msg.asInstanceOf[HttpObject]).commit)
-          }
-
-          /**
-           * Called whenever `ctx.read()` is called from withing the HEndpoint
-           */
-          override def doBeginRead(): Unit = {
-            gRtm
-              .unsafeRunAsync_(
-                inbound.take.commit
-                  .tap(msg =>
-                    UIO {
-                      self.writeInbound(msg)
-                    },
-                  )
-                  .on(ec),
-              ): Unit
-          }
-        }
-
-        ChannelProxy(inbound, outbound, embeddedChannel, ec)
+        val ch = ChannelProxy(inbound, outbound, ec, gRtm)
+        ch.pipeline().addLast(app.compile(zExec))
+        ch
       }
     } yield proxy
+  }
 }
