1cb09e12d835e5c0317200a1b66d89b74ea6243b
shrutiverma97 <shruti@dream11.com>
2021-08-13 01:44:31 +0530

    test(encoded-path): added test case

--- example/src/main/scala/HelloWorld.scala
+++ example/src/main/scala/HelloWorld.scala
@@ -2,15 +2,12 @@ import zhttp.http._
 import zhttp.service.Server
 import zio._
 
-import java.net.URLDecoder
-
 object HelloWorld extends App {
 
   // Create HTTP route
   val app: HttpApp[Any, Nothing] = HttpApp.collect {
-    case Method.GET -> Root / "text"         => Response.text("Hello World!")
-    case Method.GET -> Root / "json"         => Response.jsonString("""{"greetings": "Hello World!"}""")
-    case Method.GET -> Root / "greet" / name => Response.text(s"Greetings ${URLDecoder.decode(name, "UTF-8")}!")
+    case Method.GET -> Root / "text" => Response.text("Hello World!")
+    case Method.GET -> Root / "json" => Response.jsonString("""{"greetings": "Hello World!"}""")
   }
 
   // Run it like any simple app
