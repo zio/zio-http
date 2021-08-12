e361b4f3fcfca7f2c5c55942a824cbda0c15043f
Tushar Mathur <tusharmath@gmail.com>
2021-08-12 18:51:15 +0530

    refactor(http): simplify Check type

--- zio-http/src/main/scala/zhttp/experiment/Check.scala
+++ zio-http/src/main/scala/zhttp/experiment/Check.scala
@@ -1,30 +1,14 @@
 package zhttp.experiment
 
-import zhttp.http._
-
 case class Check[-A](is: A => Boolean) { self =>
-  def &&[A1 <: A](other: Check[A1]): Check[A1] = Check(a => self.is(a) && other.is(a))
-  def ||[A1 <: A](other: Check[A1]): Check[A1] = Check(a => self.is(a) || other.is(a))
+  def &&[A1 <: A](other: Check[A1]): Check[A1]     = Check(a => self.is(a) && other.is(a))
+  def ||[A1 <: A](other: Check[A1]): Check[A1]     = self orElse other
+  def orElse[A1 <: A](other: Check[A1]): Check[A1] = Check(a => self.is(a) || other.is(a))
 }
 
 object Check {
   def isTrue: Check[Any]  = Check(_ => true)
   def isFalse: Check[Any] = Check(_ => false)
 
-  sealed trait AutoCheck[-A, -B] {
-    def toCheck(a: A): Check[B]
-  }
-
-  implicit object PathChecker extends AutoCheck[Path, AnyRequest] {
-    override def toCheck(a: Path): Check[AnyRequest] = Check[AnyRequest](_.url.path == a)
-  }
-
-  implicit object MethodChecker extends AutoCheck[Method, AnyRequest] {
-    override def toCheck(a: Method): Check[AnyRequest] = Check[AnyRequest](_.method == a)
-  }
-
-  implicit object RouteChecker extends AutoCheck[(Method, Path), AnyRequest] {
-    override def toCheck(a: (Method, Path)): Check[AnyRequest] =
-      Check[AnyRequest](_.method == a._1) && Check[AnyRequest](_.url.path == a._2)
-  }
+  def startsWith(prefix: String): Check[AnyRequest] = Check(_.url.path.asString.startsWith(prefix))
 }
