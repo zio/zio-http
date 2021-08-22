17f2128082d8260aee137830edbe47072e166900
Tushar Mathur <tusharmath@gmail.com>
2021-08-22 22:48:21 +0530

    refactor(http): add `uuid` decoder

--- zio-http/src/main/scala/zhttp/http/RouteDecoderModule.scala
+++ zio-http/src/main/scala/zhttp/http/RouteDecoderModule.scala
@@ -1,15 +1,23 @@
 package zhttp.http
 
+import java.util.UUID
+
 trait RouteDecoderModule {
-  abstract class RouteDecode[A](f: String => Option[A]) {
-    def unapply(a: String): Option[A] = f(a)
+  abstract class RouteDecode[A](f: String => A) {
+    def unapply(a: String): Option[A] =
+      try {
+        Option(f(a))
+      } catch {
+        case _: Throwable => None
+      }
   }
 
-  object boolean extends RouteDecode(_.toBooleanOption)
-  object byte    extends RouteDecode(_.toByteOption)
-  object short   extends RouteDecode(_.toShortOption)
-  object int     extends RouteDecode(_.toIntOption)
-  object long    extends RouteDecode(_.toLongOption)
-  object float   extends RouteDecode(_.toFloatOption)
-  object double  extends RouteDecode(_.toDoubleOption)
+  object boolean extends RouteDecode(_.toBoolean)
+  object byte    extends RouteDecode(_.toByte)
+  object short   extends RouteDecode(_.toShort)
+  object int     extends RouteDecode(_.toInt)
+  object long    extends RouteDecode(_.toLong)
+  object float   extends RouteDecode(_.toFloat)
+  object double  extends RouteDecode(_.toDouble)
+  object uuid    extends RouteDecode(str => UUID.fromString(str))
 }
