cd53028933673365741e765a2e7e5687f0fa75d6
Tushar Mathur <tusharmath@gmail.com>
2021-08-23 16:03:15 +0530

    doc(http): add Scala Doc

--- zio-http/src/main/scala/zhttp/http/RouteDecoderModule.scala
+++ zio-http/src/main/scala/zhttp/http/RouteDecoderModule.scala
@@ -3,6 +3,22 @@ package zhttp.http
 import java.time.{LocalDate, LocalDateTime}
 import java.util.UUID
 
+/**
+ * Instead of using just `String` as path params, using the RouteDecoderModule we can extract and converted params into
+ * a specific type also.
+ *
+ * ```scala
+ * Http.collect[Request] {
+ *   case GET -> !! / "user" / int(id) => Response.text("User id requested: ${id}")
+ *   case GET -> !! / "user" / name    => Response.text("User name requested: ${name}")
+ * }
+ * ```
+ *
+ * If the request looks like `GET /user/100` then it would match the first case. This is because internally the `id`
+ * param can be decoded into an `Int`. If a request of the form `GET /user/zio` is made, in that case the second case is
+ * matched.
+ */
+
 trait RouteDecoderModule {
   abstract class RouteDecode[A](f: String => A) {
     def unapply(a: String): Option[A] =
