935bf6687e2e29d2c58767f642e0029704b6dbf7
dinojohn <66246799+dinojohn@users.noreply.github.com>
2021-09-05 11:45:56 +0530

    refactor(EncodeRequest): added http2 headers

--- zio-http/src/main/scala/zhttp/service/EncodeRequest.scala
+++ zio-http/src/main/scala/zhttp/service/EncodeRequest.scala
@@ -1,14 +1,16 @@
 package zhttp.service
 
 import io.netty.buffer.Unpooled
-import io.netty.handler.codec.http.{DefaultFullHttpRequest, FullHttpRequest, HttpHeaderNames, HttpVersion}
+import io.netty.handler.codec.http.{DefaultFullHttpRequest, FullHttpRequest, HttpHeaderNames, HttpHeaderValues, HttpVersion}
+import io.netty.handler.codec.http2.HttpConversionUtil
 import zhttp.http.{HTTP_CHARSET, Header, Path, Request}
 trait EncodeRequest {
 
   /**
    * Converts Request to JFullHttpRequest
+   * @param streamId : Incase of Clear text upgrade to http2, the first message will be sent from streamId 1
    */
-  def encodeRequest(jVersion: HttpVersion, req: Request): FullHttpRequest = {
+  def encodeRequest(jVersion: HttpVersion, req: Request, enableHttp2:Boolean=false, streamId:Int=1): FullHttpRequest = {
     val method      = req.method.asHttpMethod
     val uri         = req.url.path match {
       case Path.End => "/"
@@ -25,6 +27,14 @@ trait EncodeRequest {
     }
     val jReq        = new DefaultFullHttpRequest(jVersion, method, uri, content)
     jReq.headers().set(headers)
+    if (enableHttp2){
+      jReq
+        .headers()
+        .set(HttpConversionUtil.ExtensionHeaderNames.STREAM_ID.text(), streamId)
+        .add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(), "http")
+        .add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP)
+        .add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.DEFLATE)
+    }
 
     jReq
   }
