afbcb14e319366b6904436eaefa0ae711f05746a
dinojohn <66246799+dinojohn@users.noreply.github.com>
2021-09-05 14:26:39 +0530

    fix: adding scheme header for http2 request

--- zio-http/src/main/scala/zhttp/service/EncodeRequest.scala
+++ zio-http/src/main/scala/zhttp/service/EncodeRequest.scala
@@ -1,14 +1,9 @@
 package zhttp.service
 
 import io.netty.buffer.Unpooled
-import io.netty.handler.codec.http.{
-  DefaultFullHttpRequest,
-  FullHttpRequest,
-  HttpHeaderNames,
-  HttpHeaderValues,
-  HttpVersion,
-}
+import io.netty.handler.codec.http.{DefaultFullHttpRequest, FullHttpRequest, HttpHeaderNames, HttpHeaderValues, HttpVersion}
 import io.netty.handler.codec.http2.HttpConversionUtil
+import zhttp.http.URL.Location
 import zhttp.http.{HTTP_CHARSET, Header, Path, Request}
 trait EncodeRequest {
 
@@ -37,12 +32,17 @@ trait EncodeRequest {
     if (writerIndex != 0) {
       headers.set(HttpHeaderNames.CONTENT_LENGTH, writerIndex.toString())
     }
+    val scheme=req.url.kind match {
+      case Location.Relative               => ""
+      case Location.Absolute(scheme, _, _) => scheme.asString
+    }
     val jReq        = new DefaultFullHttpRequest(jVersion, method, uri, content)
     jReq.headers().set(headers)
     if (enableHttp2) {
       jReq
         .headers()
         .set(HttpConversionUtil.ExtensionHeaderNames.STREAM_ID.text(), streamId)
+        .add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(), scheme)
         .add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP)
         .add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.DEFLATE)
     }
