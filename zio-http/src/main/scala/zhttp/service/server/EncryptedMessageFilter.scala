0c72a8e917d09b5e30a1b6c41fb6aea3faedea1c
dinojohn <66246799+dinojohn@users.noreply.github.com>
2021-09-05 10:45:19 +0530

    feat: handler to check for encryption in non ssl servers

---
+++ zio-http/src/main/scala/zhttp/service/server/EncryptedMessageFilter.scala
@@ -0,0 +1,32 @@
+package zhttp.service.server
+
+import io.netty.buffer.ByteBuf
+import io.netty.channel.{ChannelHandler, ChannelHandlerContext}
+import io.netty.handler.codec.ByteToMessageDecoder
+import io.netty.handler.codec.http.{HttpObjectAggregator, HttpServerKeepAliveHandler => JHttpServerKeepAliveHandler}
+import io.netty.handler.ssl.SslHandler
+import zhttp.service.Server.Settings
+import zhttp.service._
+
+import java.util
+
+case class EncryptedMessageFilter[R](httpH: ChannelHandler, settings: Settings[R, Throwable])
+  extends ByteToMessageDecoder {
+  override def decode(context: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]): Unit = {
+    if (SslHandler.isEncrypted(in)) {
+      context.channel().pipeline().remove(CLEAR_TEXT_HTTP2_HANDLER)
+      context.channel().pipeline().remove(CLEAR_TEXT_HTTP2_FALLBACK_HANDLER)
+      context
+        .channel()
+        .pipeline()
+        .addLast(HTTP_KEEPALIVE_HANDLER, new JHttpServerKeepAliveHandler)
+        .addLast(OBJECT_AGGREGATOR, new HttpObjectAggregator(settings.maxRequestSize))
+        .addLast(HTTP_REQUEST_HANDLER, httpH)
+        .remove(this)
+      ()
+    } else {
+      context.pipeline().remove(this)
+      ()
+    }
+  }
+}
