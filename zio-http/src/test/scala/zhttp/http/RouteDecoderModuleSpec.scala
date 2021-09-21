be19587a91000d67a7fc9741dd2cf4fba07ebb90
Dino John <dinopanali@gmail.com>
2021-09-21 18:26:47 +0530

    test: boolean

--- zio-http/src/test/scala/zhttp/http/RouteDecoderModuleSpec.scala
+++ zio-http/src/test/scala/zhttp/http/RouteDecoderModuleSpec.scala
@@ -1,5 +1,15 @@
 package zhttp.http
 
-object RouteDecoderModuleSpec {
+import zio.test.Assertion.{equalTo, isNone}
+import zio.test._
+
+object RouteDecoderModuleSpec extends DefaultRunnableSpec {
+  def spec  = suite("RouteDecoderModule")(
+    suite("boolean")(
+test("true")(assert(boolean.unapply("true"))(equalTo(Some(true)))),
+      test("false")(assert(boolean.unapply("false"))(equalTo(Some(false)))),
+      test("something else")(assert(boolean.unapply("something"))(isNone)),
+      ),
+  )
 
 }
