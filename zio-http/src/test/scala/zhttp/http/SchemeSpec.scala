693fb0b9fc502e6c281120b2457dc6b1cd546368
Dino John <dinopanali@gmail.com>
2021-09-21 16:38:39 +0530

    style: apply scalafmt

--- zio-http/src/test/scala/zhttp/http/SchemeSpec.scala
+++ zio-http/src/test/scala/zhttp/http/SchemeSpec.scala
@@ -2,22 +2,21 @@ package zhttp.http
 
 import io.netty.handler.codec.http.HttpScheme
 import zio.test.Assertion._
-import zio.test._
-import zio.test.DefaultRunnableSpec
+import zio.test.{DefaultRunnableSpec, _}
 
-object SchemeSpec extends DefaultRunnableSpec{
-def spec = suite("Scheme")(
-  suite("asString")(
-    test("HTTP")(assert(Scheme.asString(Scheme.HTTP))(equalTo("http"))),
-    test("HTTPS")(assert(Scheme.asString(Scheme.HTTPS))(equalTo("https")))
-  ),
-  suite("fromJScheme")(
-    test("HTTP")(assert(Scheme.fromJScheme(HttpScheme.HTTP))(equalTo(Option(Scheme.HTTP)))),
-    test("HTTPS")(assert(Scheme.fromJScheme(HttpScheme.HTTPS))(equalTo(Option(Scheme.HTTPS)))),
-  ),
-  suite("fromString")(
-    test("http")(assert(Scheme.fromString("httP"))(equalTo(Option(Scheme.HTTP)))),
-    test("https")(assert(Scheme.fromString("httPs"))(equalTo(Option(Scheme.HTTPS)))),
-  ),
-)
+object SchemeSpec extends DefaultRunnableSpec {
+  def spec = suite("Scheme")(
+    suite("asString")(
+      test("HTTP")(assert(Scheme.asString(Scheme.HTTP))(equalTo("http"))),
+      test("HTTPS")(assert(Scheme.asString(Scheme.HTTPS))(equalTo("https"))),
+    ),
+    suite("fromJScheme")(
+      test("HTTP")(assert(Scheme.fromJScheme(HttpScheme.HTTP))(equalTo(Option(Scheme.HTTP)))),
+      test("HTTPS")(assert(Scheme.fromJScheme(HttpScheme.HTTPS))(equalTo(Option(Scheme.HTTPS)))),
+    ),
+    suite("fromString")(
+      test("http")(assert(Scheme.fromString("httP"))(equalTo(Option(Scheme.HTTP)))),
+      test("https")(assert(Scheme.fromString("httPs"))(equalTo(Option(Scheme.HTTPS)))),
+    ),
+  )
 }
