7684169506ff66f48716d62c45dbbfbe4495480a
Dino John <dinopanali@gmail.com>
2021-09-21 16:23:36 +0530

    test: fromJScheme

--- zio-http/src/test/scala/zhttp/http/SchemeSpec.scala
+++ zio-http/src/test/scala/zhttp/http/SchemeSpec.scala
@@ -1,5 +1,6 @@
 package zhttp.http
 
+import io.netty.handler.codec.http.HttpScheme
 import zio.test.Assertion._
 import zio.test._
 import zio.test.DefaultRunnableSpec
@@ -9,6 +10,9 @@ def spec = suite("Scheme")(
   suite("asString")(
     test("HTTP")(assert(Scheme.asString(Scheme.HTTP))(equalTo("http"))),
     test("HTTPS")(assert(Scheme.asString(Scheme.HTTPS))(equalTo("https")))
-  )
-)
+  ),
+  suite("fromJScheme")(
+    test("HTTP")(assert(Scheme.fromJScheme(HttpScheme.HTTP))(equalTo(Option(Scheme.HTTP)))),
+    test("HTTPS")(assert(Scheme.fromJScheme(HttpScheme.HTTPS))(equalTo(Option(Scheme.HTTPS)))),
+))
 }
