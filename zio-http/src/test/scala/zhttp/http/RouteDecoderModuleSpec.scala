b818f6c1ec2168adc135d67da95fa41e5aa34fd8
Dino John <dinopanali@gmail.com>
2021-09-22 15:13:44 +0530

    test: short

--- zio-http/src/test/scala/zhttp/http/RouteDecoderModuleSpec.scala
+++ zio-http/src/test/scala/zhttp/http/RouteDecoderModuleSpec.scala
@@ -16,6 +16,12 @@ object RouteDecoderModuleSpec extends DefaultRunnableSpec {
       test("negative above -129")(assert(byte.unapply("-127"))(equalTo(Some(-127.byteValue())))),
       test("number smaller than -128")(assert(byte.unapply("-129"))(isNone)),
     ),
+    suite("short")(
+      test("positive below 32768")(assert(short.unapply("32767"))(equalTo(Some(32767.shortValue())))),
+      test("number greater than 32767")(assert(short.unapply("32768"))(isNone)),
+      test("negative above -32769")(assert(short.unapply("-32768"))(equalTo(Some(-32768.shortValue())))),
+      test("number smaller than -32768")(assert(short.unapply("-32769"))(isNone)),
+    ),
   )
 
 }
