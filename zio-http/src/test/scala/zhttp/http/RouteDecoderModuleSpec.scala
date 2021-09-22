40c4882b332a5fb244ec7d50774946b088cb2b8b
Dino John <dinopanali@gmail.com>
2021-09-22 15:12:07 +0530

    test: byte

--- zio-http/src/test/scala/zhttp/http/RouteDecoderModuleSpec.scala
+++ zio-http/src/test/scala/zhttp/http/RouteDecoderModuleSpec.scala
@@ -6,10 +6,16 @@ import zio.test._
 object RouteDecoderModuleSpec extends DefaultRunnableSpec {
   def spec  = suite("RouteDecoderModule")(
     suite("boolean")(
-test("true")(assert(boolean.unapply("true"))(equalTo(Some(true)))),
+      test("true")(assert(boolean.unapply("true"))(equalTo(Some(true)))),
       test("false")(assert(boolean.unapply("false"))(equalTo(Some(false)))),
       test("something else")(assert(boolean.unapply("something"))(isNone)),
       ),
+    suite("byte")(
+      test("positive below 128")(assert(byte.unapply("127"))(equalTo(Some(127.byteValue())))),
+      test("number greater than 127")(assert(byte.unapply("128"))(isNone)),
+      test("negative above -129")(assert(byte.unapply("-127"))(equalTo(Some(-127.byteValue())))),
+      test("number smaller than -128")(assert(byte.unapply("-129"))(isNone)),
+    ),
   )
 
 }
