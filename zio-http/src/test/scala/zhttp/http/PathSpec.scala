8fa95ec37b89e809f2084bf8f1e6c6ab3d89d85b
shrutiverma97 <shruti@dream11.com>
2021-09-28 11:01:46 +0530

    refactor(tests): added plus in suites

--- zio-http/src/test/scala/zhttp/http/PathSpec.scala
+++ zio-http/src/test/scala/zhttp/http/PathSpec.scala
@@ -15,93 +15,93 @@ object PathSpec extends DefaultRunnableSpec with HExitAssertion {
           test("prefixed")(assert(Path("/A").toList)(equalTo(List("A")))) +
           test("nested paths")(assert(Path("A", "B", "C").toList)(equalTo(List("A", "B", "C")))) +
           test("encoding string")(assert(Path("A", "B%2FC").toList)(equalTo(List("A", "B%2FC")))),
-      ),
-      suite("apply()")(
-        test("empty")(assert(Path())(equalTo(!!))) +
-          test("empty string")(assert(Path(""))(equalTo(!!))) +
-          test("just /")(assert(Path("/"))(equalTo(!!))) +
-          test("prefixed path")(assert(Path("/A"))(equalTo(Path("A")))) +
-          test("encoded paths")(assert(Path("/A/B%2FC"))(equalTo(Path("A", "B%2FC")))) +
-          test("nested paths")(assert(Path("/A/B/C"))(equalTo(Path("A", "B", "C")))),
-      ),
-      suite("unapplySeq")(
-        test("a, b, c") {
-          val path = collect { case Path(a, b, c) => (a, b, c) }
-          assert(path(Path("a", "b", "c")))(isSome(equalTo(("a", "b", "c"))))
-        },
-      ),
-      suite("asString")(
-        test("a, b, c") {
-          val path = Path("a", "b", "c").asString
-          assert(path)(equalTo("/a/b/c"))
-        } +
-          test("Path()") {
-            val path = Path().asString
-            assert(path)(equalTo(""))
+      ) +
+        suite("apply()")(
+          test("empty")(assert(Path())(equalTo(!!))) +
+            test("empty string")(assert(Path(""))(equalTo(!!))) +
+            test("just /")(assert(Path("/"))(equalTo(!!))) +
+            test("prefixed path")(assert(Path("/A"))(equalTo(Path("A")))) +
+            test("encoded paths")(assert(Path("/A/B%2FC"))(equalTo(Path("A", "B%2FC")))) +
+            test("nested paths")(assert(Path("/A/B/C"))(equalTo(Path("A", "B", "C")))),
+        ) +
+        suite("unapplySeq")(
+          test("a, b, c") {
+            val path = collect { case Path(a, b, c) => (a, b, c) }
+            assert(path(Path("a", "b", "c")))(isSome(equalTo(("a", "b", "c"))))
           },
-      ),
-      suite("PathSyntax /")(
-        test("construction") {
-          val path = !! / "a" / "b" / "c"
-          assert(path)(equalTo(Path("a", "b", "c")))
-        } +
-          test("extract path / a / b / c") {
-            val path = collect { case !! / "a" / b / c => (b, c) }
-            assert(path(Path("a", "b", "c")))(isSome(equalTo(("b", "c"))))
+        ) +
+        suite("asString")(
+          test("a, b, c") {
+            val path = Path("a", "b", "c").asString
+            assert(path)(equalTo("/a/b/c"))
           } +
-          test("extract path / a / b / c") {
-            val path = collect { case !! / "a" / b => b }
-            assert(path(Path("a", "b", "c")))(isNone)
-          } +
-          test("extract path / a / b / c") {
-            val path = collect { case !! / "a" / b => b }
-            assert(path(Path("a", "b")))(isSome(equalTo("b")))
-          },
-      ),
-      suite("PathSyntax /:")(
-        test("construction") {
-          val path = "a" /: "b" /: "c" /: !!
-          assert(path)(equalTo(Path("a", "b", "c")))
-        },
-        suite("default")(
-          test("extract path 'name' /: name") {
-            val path = collect { case "name" /: name => name.asString }
-            assert(path(Path("name", "a", "b", "c")))(isSome(equalTo("/a/b/c")))
-          } +
-            test("extract paths 'name' /: a /: b /: 'c' /: !!") {
-              val path = collect { case "name" /: a /: b /: "c" /: !! => (a, b) }
-              assert(path(Path("name", "a", "b", "c")))(isSome(equalTo(("a", "b"))))
-            } +
-            test("extract paths 'name' /: a /: b /: _") {
-              val path = collect { case "name" /: a /: b /: _ => (a, b) }
-              assert(path(Path("name", "a", "b", "c")))(isSome(equalTo(("a", "b"))))
-            } +
-            test("extract paths 'name' /: name /: 'a' /: 'b' /: 'c' /: !!") {
-              val path = collect { case "name" /: name /: "a" /: "b" /: "c" /: !! => name.toString }
-              assert(path(Path("name", "Xyz", "a", "b", "c")))(isSome(equalTo("Xyz")))
+            test("Path()") {
+              val path = Path().asString
+              assert(path)(equalTo(""))
             },
         ) +
-          suite("int()")(
-            test("extract path 'user' /: int(1)") {
-              val path = collect { case "user" /: int(age) /: !! => age }
-              assert(path(Path("user", "1")))(isSome(equalTo(1)))
+        suite("PathSyntax /")(
+          test("construction") {
+            val path = !! / "a" / "b" / "c"
+            assert(path)(equalTo(Path("a", "b", "c")))
+          } +
+            test("extract path / a / b / c") {
+              val path = collect { case !! / "a" / b / c => (b, c) }
+              assert(path(Path("a", "b", "c")))(isSome(equalTo(("b", "c"))))
             } +
-              test("extract path 'user' /: int(Xyz)") {
+            test("extract path / a / b / c") {
+              val path = collect { case !! / "a" / b => b }
+              assert(path(Path("a", "b", "c")))(isNone)
+            } +
+            test("extract path / a / b / c") {
+              val path = collect { case !! / "a" / b => b }
+              assert(path(Path("a", "b")))(isSome(equalTo("b")))
+            },
+        ) +
+        suite("PathSyntax /:")(
+          test("construction") {
+            val path = "a" /: "b" /: "c" /: !!
+            assert(path)(equalTo(Path("a", "b", "c")))
+          } +
+            suite("default")(
+              test("extract path 'name' /: name") {
+                val path = collect { case "name" /: name => name.asString }
+                assert(path(Path("name", "a", "b", "c")))(isSome(equalTo("/a/b/c")))
+              } +
+                test("extract paths 'name' /: a /: b /: 'c' /: !!") {
+                  val path = collect { case "name" /: a /: b /: "c" /: !! => (a, b) }
+                  assert(path(Path("name", "a", "b", "c")))(isSome(equalTo(("a", "b"))))
+                } +
+                test("extract paths 'name' /: a /: b /: _") {
+                  val path = collect { case "name" /: a /: b /: _ => (a, b) }
+                  assert(path(Path("name", "a", "b", "c")))(isSome(equalTo(("a", "b"))))
+                } +
+                test("extract paths 'name' /: name /: 'a' /: 'b' /: 'c' /: !!") {
+                  val path = collect { case "name" /: name /: "a" /: "b" /: "c" /: !! => name.toString }
+                  assert(path(Path("name", "Xyz", "a", "b", "c")))(isSome(equalTo("Xyz")))
+                },
+            ) +
+            suite("int()")(
+              test("extract path 'user' /: int(1)") {
                 val path = collect { case "user" /: int(age) /: !! => age }
-                assert(path(Path("user", "Xyz")))(isNone)
-              },
-          ) +
-          suite("boolean()")(
-            test("extract path 'user' /: boolean(true)") {
-              val path = collect { case "user" /: boolean(ok) /: !! => ok }
-              assert(path(Path("user", "True")))(isSome(isTrue))
-            } +
-              test("extract path 'user' /: boolean(false)") {
+                assert(path(Path("user", "1")))(isSome(equalTo(1)))
+              } +
+                test("extract path 'user' /: int(Xyz)") {
+                  val path = collect { case "user" /: int(age) /: !! => age }
+                  assert(path(Path("user", "Xyz")))(isNone)
+                },
+            ) +
+            suite("boolean()")(
+              test("extract path 'user' /: boolean(true)") {
                 val path = collect { case "user" /: boolean(ok) /: !! => ok }
-                assert(path(Path("user", "false")))(isSome(isFalse))
-              },
-          ),
-      ) +
+                assert(path(Path("user", "True")))(isSome(isTrue))
+              } +
+                test("extract path 'user' /: boolean(false)") {
+                  val path = collect { case "user" /: boolean(ok) /: !! => ok }
+                  assert(path(Path("user", "false")))(isSome(isFalse))
+                },
+            ),
+        ) +
         suite("startsWith")(
           test("isTrue") {
             assert(!! / "a" / "b" / "c" / "d" startsWith !! / "a")(isTrue) &&
