32c0960c1d36f71a3599d98f1980b4403eb41d27
chengjunxie <bbboson@gmail.com>
2021-10-04 21:18:52 +0800

    Add IOUring support

--- build.sbt
+++ build.sbt
@@ -91,6 +91,7 @@ lazy val zhttp = (project in file("./zio-http"))
         "dev.zio"                %% "zio"                     % zioVersion,
         "dev.zio"                %% "zio-streams"             % zioVersion,
         "io.netty"                % "netty-all"               % "4.1.68.Final",
+        "io.netty.incubator"      % "netty-incubator-transport-native-io_uring" % "0.0.8.Final" classifier "linux-x86_64",
         "org.scala-lang.modules" %% "scala-collection-compat" % "2.5.0",
       ),
   )
--- zio-http/src/main/scala/zhttp/service/EventLoopGroup.scala
+++ zio-http/src/main/scala/zhttp/service/EventLoopGroup.scala
@@ -3,6 +3,7 @@ package zhttp.service
 import io.netty.channel
 import io.netty.channel.epoll.Epoll
 import io.netty.channel.kqueue.KQueue
+import io.netty.incubator.channel.uring.IOUringEventLoopGroup
 import zio._
 
 import java.util.concurrent.Executor
@@ -14,6 +15,8 @@ object EventLoopGroup {
   def nio(nThreads: Int): ZLayer[Any, Nothing, EventLoopGroup] = EventLoopGroup.Live.nio(nThreads).toLayer
 
   def epoll(nThreads: Int): ZLayer[Any, Nothing, EventLoopGroup] = EventLoopGroup.Live.epoll(nThreads).toLayer
+
+  def uring(nThreads: Int = 0): ZLayer[Any, Nothing, EventLoopGroup] = EventLoopGroup.Live.uring(nThreads).toLayer
 
   def auto(nThreads: Int = 0): ZLayer[Any, Nothing, EventLoopGroup] = EventLoopGroup.Live.auto(nThreads).toLayer
 
@@ -37,6 +40,12 @@ object EventLoopGroup {
 
     def epoll(nThreads: Int, executor: Executor): ZManaged[Any, Nothing, channel.EventLoopGroup] =
       make(UIO(new channel.epoll.EpollEventLoopGroup(nThreads, executor)))
+
+    def uring(nThread: Int): ZManaged[Any, Nothing, channel.EventLoopGroup] =
+      make(UIO(new IOUringEventLoopGroup(nThread)))
+
+    def uring(nThread: Int, executor: Executor): ZManaged[Any, Nothing, channel.EventLoopGroup] =
+      make(UIO(new IOUringEventLoopGroup(nThread, executor)))
 
     def auto(nThreads: Int): ZManaged[Any, Nothing, channel.EventLoopGroup] =
       if (Epoll.isAvailable)
