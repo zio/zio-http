4d5033562aa57eb2b59507ce60b2531bdc1b0cd0
Dino John <dinopanali@gmail.com>
2021-10-08 12:47:53 +0530

    fix: removed redundant final from requestContent

--- zio-http/src/main/scala/zhttp/http/RequestContent.scala
+++ zio-http/src/main/scala/zhttp/http/RequestContent.scala
@@ -9,19 +9,19 @@ import java.nio.charset.Charset
 sealed trait RequestContent[-R, +E]
 object RequestContent {
 
-  final case class BinaryStream[R, E](data: ZStream[R, E, Byte]) extends RequestContent[R, E]
+  case class BinaryStream[R, E](data: ZStream[R, E, Byte]) extends RequestContent[R, E]
   def fromStream[R, E](data: ZStream[R, E, Byte]): RequestContent[R, E] = BinaryStream(data)
 
-  final case class Binary(data: Chunk[Byte]) extends RequestContent[Any, Nothing]
+  case class Binary(data: Chunk[Byte]) extends RequestContent[Any, Nothing]
   def fromChunk(data: Chunk[Byte]): RequestContent[Any, Nothing] = Binary(data)
 
-  final case class BinaryN(data: ByteBuf) extends RequestContent[Any, Nothing]
+  case class BinaryN(data: ByteBuf) extends RequestContent[Any, Nothing]
   def fromByteBuf(byteBuf: ByteBuf): RequestContent[Any, Nothing] = BinaryN(byteBuf)
 
-  final case object Empty extends RequestContent[Any, Nothing]
+  case object Empty extends RequestContent[Any, Nothing]
   def empty: RequestContent[Any, Nothing] = Empty
 
-  final case class Text(text: String, charset: Charset) extends RequestContent[Any, Nothing]
+  case class Text(text: String, charset: Charset) extends RequestContent[Any, Nothing]
   def fromText(data: String, charset: Charset = HTTP_CHARSET): RequestContent[Any, Nothing] = Text(data, charset)
 
   def provide[R, E](r: R, content: RequestContent[R, E]): RequestContent[Any, E] = content match {
