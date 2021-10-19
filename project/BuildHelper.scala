64df20e2a6cf4d454be6576d68a00e16b241d858
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-19 07:08:22 +0200

    Update scala3-library to 3.1.0 (#404)
    * Update scala3-library to 3.1.0
    
    * Regenerate workflow with sbt-github-actions

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import xerial.sbt.Sonatype.autoImport._
 object BuildHelper extends ScalaSettings {
   val Scala212   = "2.12.15"
   val Scala213   = "2.13.6"
-  val ScalaDotty = "3.0.2"
+  val ScalaDotty = "3.1.0"
 
   private val stdOptions = Seq(
     "-deprecation",
