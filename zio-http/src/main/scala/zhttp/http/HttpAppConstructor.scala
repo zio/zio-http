f220a0569515ec946c40c1600f2b72f25e4fd4b9
kaushik <kaushik@dream11.com>
2021-10-20 14:21:32 +0530

    refactor(route): Move HTTPAppConstructor to separate file

---
+++ zio-http/src/main/scala/zhttp/http/HttpAppConstructor.scala
@@ -0,0 +1,47 @@
+package zhttp.http
+
+import zhttp.experiment.Route
+import zio.{UIO, ZIO}
+
+/**
+ * Domain to create HttpApp from Route[A] and a partial function from Request[A] => B
+ */
+sealed trait HttpAppConstructor[A, B] {
+  type ROut
+  type EOut
+  def make(route: Route[A], f: Request.Typed[A] => B): HttpApp[ROut, EOut]
+}
+
+object HttpAppConstructor {
+  type Aux[R, E, A, B] = HttpAppConstructor[A, B] {
+    type ROut = R
+    type EOut = E
+  }
+
+  implicit def f1[R, E, A]: Aux[R, E, A, Response[R, E]] = new HttpAppConstructor[A, Response[R, E]] {
+    override type ROut = R
+    override type EOut = E
+
+    override def make(route: Route[A], f: Request.Typed[A] => Response[R, E]): HttpApp[R, E] =
+      HttpApp.collect { case req =>
+        route.extract(req) match {
+          case Some(value) => f(Request.Typed(req, value))
+          case None        => Response.fromHttpError(HttpError.NotFound(req.url.path))
+        }
+      }
+  }
+
+  implicit def f2[R, E, A]: Aux[R, E, A, ZIO[R, E, Response[R, E]]] =
+    new HttpAppConstructor[A, ZIO[R, E, Response[R, E]]] {
+      override type ROut = R
+      override type EOut = E
+
+      override def make(route: Route[A], f: Request.Typed[A] => ZIO[R, E, Response[R, E]]): HttpApp[R, E] =
+        HttpApp.collectM { case req =>
+          route.extract(req) match {
+            case Some(value) => f(Request.Typed(req, value))
+            case None        => UIO(Response.fromHttpError(HttpError.NotFound(req.url.path)))
+          }
+        }
+    }
+}
