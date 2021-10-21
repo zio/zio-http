c01d609f2ab2339ff5b724de5703889313802595
amitsingh <amitksingh1490@gmail.com>
2021-10-21 16:45:39 +0530

    refactor(Part): fix netty imports

--- zio-http/src/main/scala/zhttp/experiment/Part.scala
+++ zio-http/src/main/scala/zhttp/experiment/Part.scala
@@ -1,21 +1,21 @@
 package zhttp.experiment
 
 import io.netty.buffer.ByteBufUtil
-import io.netty.handler.codec.http.multipart.{Attribute => JAttribute, FileUpload, HttpData, InterfaceHttpData}
+import io.netty.handler.codec.http.multipart
 import zio.Chunk
 
 sealed trait Part
 
 object Part {
-  def fromHTTPData(data: InterfaceHttpData): Part = data match {
-    case data: HttpData =>
+  def fromHTTPData(data: multipart.InterfaceHttpData): Part = data match {
+    case data: multipart.HttpData =>
       data match {
-        case attribute: JAttribute => Part.Attribute(attribute.getName, Option(attribute.getValue))
-        case upload: FileUpload    =>
+        case attribute: multipart.Attribute => Part.Attribute(attribute.getName, Option(attribute.getValue))
+        case upload: multipart.FileUpload   =>
           Part.FileData(Chunk.fromArray(ByteBufUtil.getBytes(upload.content())), Option(upload.getFilename))
-        case _                     => Part.Empty
+        case _                              => Part.Empty
       }
-    case _              => Part.Empty
+    case _                        => Part.Empty
   }
   case class FileData(content: Chunk[Byte], fileName: Option[String]) extends Part
   case class Attribute(name: String, value: Option[String]) extends Part
