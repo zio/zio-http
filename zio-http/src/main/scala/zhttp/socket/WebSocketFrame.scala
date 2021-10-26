b77ea2b5af23cfd738847ea912fc1ddcc74a6834
morozov11 <56236793+morozov11@users.noreply.github.com>
2021-10-26 19:39:54 +0300

    Feature: `finalFragment` flag for WebSocketFrame (#393)
    * Propagate  finalFragment flag for WebSocketFrame from/to Netty layer.
    
    * Refactoring WebSocketFrame, add custom unapply methods
    
    * Update zio-http/src/main/scala/zhttp/socket/WebSocketFrame.scala
    
    * Update zio-http/src/main/scala/zhttp/socket/WebSocketFrame.scala
    
    * Update zio-http/src/main/scala/zhttp/socket/WebSocketFrame.scala
    
    * run fmt command
    
    * run fmt command
    
    * fix WebSocketFrame classes for Scala 2.12 pattern matching compatibility
    
    * fix WebSocketFrame classes for Scala 2.12 pattern matching compatibility
    
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

--- zio-http/src/main/scala/zhttp/socket/WebSocketFrame.scala
+++ zio-http/src/main/scala/zhttp/socket/WebSocketFrame.scala
@@ -4,16 +4,43 @@ import io.netty.handler.codec.http.websocketx.{WebSocketFrame => JWebSocketFrame
 import zhttp.core.ByteBuf
 
 sealed trait WebSocketFrame extends Product with Serializable { self =>
-  def toWebSocketFrame: JWebSocketFrame = WebSocketFrame.toJFrame(self)
+  final def toWebSocketFrame: JWebSocketFrame = WebSocketFrame.toJFrame(self)
+  def isFinal: Boolean                        = true
 }
+
 object WebSocketFrame {
 
-  final case class Binary(buffer: ByteBuf)                    extends WebSocketFrame
-  final case class Text(text: String)                         extends WebSocketFrame
+  case class Binary(buffer: ByteBuf) extends WebSocketFrame { override val isFinal: Boolean = true }
+  object Binary {
+    def apply(buffer: ByteBuf, isFinal: Boolean): Binary       = {
+      val arg = isFinal
+      new Binary(buffer) { override val isFinal: Boolean = arg }
+    }
+    def unapply(frame: WebSocketFrame.Binary): Option[ByteBuf] = Some(frame.buffer)
+  }
+
+  case class Text(text: String) extends WebSocketFrame { override val isFinal: Boolean = true }
+  object Text {
+    def apply(text: String, isFinal: Boolean): Text         = {
+      val arg = isFinal
+      new Text(text) { override val isFinal: Boolean = arg }
+    }
+    def unapply(frame: WebSocketFrame.Text): Option[String] = Some(frame.text)
+  }
+
   final case class Close(status: Int, reason: Option[String]) extends WebSocketFrame
-  case object Ping                                            extends WebSocketFrame
-  case object Pong                                            extends WebSocketFrame
-  final case class Continuation(buffer: ByteBuf)              extends WebSocketFrame
+
+  case object Ping extends WebSocketFrame
+  case object Pong extends WebSocketFrame
+
+  case class Continuation(buffer: ByteBuf) extends WebSocketFrame { override val isFinal: Boolean = true }
+  object Continuation {
+    def apply(buffer: ByteBuf, isFinal: Boolean): Continuation       = {
+      val arg = isFinal
+      new Continuation(buffer) { override val isFinal: Boolean = arg }
+    }
+    def unapply(frame: WebSocketFrame.Continuation): Option[ByteBuf] = Some(frame.buffer)
+  }
 
   def text(string: String): WebSocketFrame =
     WebSocketFrame.Text(string)
@@ -36,23 +63,23 @@ object WebSocketFrame {
       case _: PongWebSocketFrame         =>
         Option(Pong)
       case m: BinaryWebSocketFrame       =>
-        Option(Binary(ByteBuf(m.content())))
+        Option(Binary(ByteBuf(m.content()), m.isFinalFragment))
       case m: TextWebSocketFrame         =>
-        Option(Text(m.text()))
+        Option(Text(m.text(), m.isFinalFragment))
       case m: CloseWebSocketFrame        =>
         Option(Close(m.statusCode(), Option(m.reasonText())))
       case m: ContinuationWebSocketFrame =>
-        Option(Continuation(ByteBuf(m.content())))
+        Option(Continuation(ByteBuf(m.content()), m.isFinalFragment))
 
       case _ => None
     }
 
   def toJFrame(frame: WebSocketFrame): JWebSocketFrame =
     frame match {
-      case Binary(buffer)            =>
-        new BinaryWebSocketFrame(buffer.asJava)
-      case Text(text)                =>
-        new TextWebSocketFrame(text)
+      case b: Binary                 =>
+        new BinaryWebSocketFrame(b.isFinal, 0, b.buffer.asJava)
+      case t: Text                   =>
+        new TextWebSocketFrame(t.isFinal, 0, t.text)
       case Close(status, Some(text)) =>
         new CloseWebSocketFrame(status, text)
       case Close(status, None)       =>
@@ -61,7 +88,7 @@ object WebSocketFrame {
         new PingWebSocketFrame()
       case Pong                      =>
         new PongWebSocketFrame()
-      case Continuation(buffer)      =>
-        new ContinuationWebSocketFrame(buffer.asJava)
+      case c: Continuation           =>
+        new ContinuationWebSocketFrame(c.isFinal, 0, c.buffer.asJava)
     }
 }
