e7df50c55dcd160e6391f8bbb5ce1d62fca4bdbb
kaushik143 <kaushik@dream11.com>
2021-10-26 15:58:18 +0530

    refactor(Route): Move RouteSpec -> zHttp/EndpointSpec

--- zio-http/src/test/scala/zhttp/http/EndpointSpec.scala
+++ zio-http/src/test/scala/zhttp/http/EndpointSpec.scala
@@ -1,48 +1,48 @@
-package zhttp.experiment
-import zhttp.http._
-import zio.test.Assertion._
-import zio.test._
+package zhttp.http
 
-object RouteSpec extends DefaultRunnableSpec {
+import zio.test.Assertion._
+import zio.test.{DefaultRunnableSpec, assert}
+
+object EndpointSpec extends DefaultRunnableSpec {
   def spec = suite("Route") {
     test("match method") {
-      val route   = Route.fromMethod(Method.GET)
+      val route   = Endpoint.fromMethod(Method.GET)
       val request = Request(Method.GET)
       assert(route.parse(request))(isSome(equalTo(())))
     }
     test("not match method") {
-      val route   = Route.fromMethod(Method.POST)
+      val route   = Endpoint.fromMethod(Method.POST)
       val request = Request(Method.GET)
       assert(route.parse(request))(isNone)
     } +
       test("match method and string") {
-        val route   = Route.fromMethod(Method.GET) / "a"
+        val route   = Endpoint.fromMethod(Method.GET) / "a"
         val request = Request(Method.GET, URL(Path("a")))
         assert(route.parse(request))(isSome(equalTo(())))
       } +
       test("match method and not string") {
-        val route   = Route.fromMethod(Method.GET) / "a"
+        val route   = Endpoint.fromMethod(Method.GET) / "a"
         val request = Request(Method.GET, URL(Path("b")))
         assert(route.parse(request))(isNone)
       }
   } + suite("Path") {
     test("Route[Int]") {
-      val route = Route.fromMethod(Method.GET) / Route[Int]
+      val route = Endpoint.fromMethod(Method.GET) / Endpoint[Int]
       assert(route.parse(!! / "1"))(isSome(equalTo(1))) && assert(route.parse(!! / "a"))(isNone)
     } +
       test("Route[String]") {
-        val route = Route.fromMethod(Method.GET) / Route[String]
+        val route = Endpoint.fromMethod(Method.GET) / Endpoint[String]
         assert(route.parse(!! / "a"))(isSome(equalTo("a")))
       } +
       test("Route[Boolean]") {
-        val route = Route.fromMethod(Method.GET) / Route[Boolean]
+        val route = Endpoint.fromMethod(Method.GET) / Endpoint[Boolean]
         assert(route.parse(!! / "True"))(isSome(isTrue)) &&
         assert(route.parse(!! / "False"))(isSome(isFalse)) &&
         assert(route.parse(!! / "a"))(isNone) &&
         assert(route.parse(!! / "1"))(isNone)
       } +
       test("Route[Int] / Route[Int]") {
-        val route = Route.fromMethod(Method.GET) / Route[Int] / Route[Int]
+        val route = Endpoint.fromMethod(Method.GET) / Endpoint[Int] / Endpoint[Int]
         assert(route.parse(!! / "1" / "2"))(isSome(equalTo((1, 2)))) &&
         assert(route.parse(!! / "1" / "b"))(isNone) &&
         assert(route.parse(!! / "b" / "1"))(isNone) &&
@@ -50,13 +50,13 @@ object RouteSpec extends DefaultRunnableSpec {
         assert(route.parse(!!))(isNone)
       } +
       test("Route[Int] / c") {
-        val route = Route.fromMethod(Method.GET) / Route[Int] / "c"
+        val route = Endpoint.fromMethod(Method.GET) / Endpoint[Int] / "c"
         assert(route.parse(!! / "1" / "c"))(isSome(equalTo(1))) &&
         assert(route.parse(!! / "1"))(isNone) &&
         assert(route.parse(!! / "c"))(isNone)
       } +
       test("Route[Int] / c") {
-        val route = Route.fromMethod(Method.GET) / Route[Int] / "c"
+        val route = Endpoint.fromMethod(Method.GET) / Endpoint[Int] / "c"
         assert(route.parse(!! / "1" / "c"))(isSome(equalTo(1))) &&
         assert(route.parse(!! / "1"))(isNone) &&
         assert(route.parse(!! / "c"))(isNone)
