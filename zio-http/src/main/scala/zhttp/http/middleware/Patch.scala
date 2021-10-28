71db9bdde72381a11592e0f025c0709a2284d3aa
Tushar Mathur <tusharmath@gmail.com>
2021-10-28 13:09:49 +0530

    refactor: make patch stack safe

--- zio-http/src/main/scala/zhttp/http/middleware/Patch.scala
+++ zio-http/src/main/scala/zhttp/http/middleware/Patch.scala
@@ -2,20 +2,26 @@ package zhttp.http.middleware
 
 import zhttp.http.{Header, Response, Status}
 
+import scala.annotation.tailrec
+
 /**
  * Models the set of operations that one would want to apply on a Response.
  */
 sealed trait Patch { self =>
   def ++(that: Patch): Patch = Patch.Combine(self, that)
   def apply[R, E](res: Response[R, E]): Response[R, E] = {
-    self match {
-      case Patch.Empty                  => res
-      case Patch.AddHeaders(headers)    => res.addHeaders(headers)
-      case Patch.RemoveHeaders(headers) =>
-        res.removeHeaders(headers)
-      case Patch.SetStatus(status)      => res.setStatus(status)
-      case Patch.Combine(self, other)   => other(self(res))
-    }
+
+    @tailrec
+    def loop[R1, E1](res: Response[R1, E1], patch: Patch): Response[R1, E1] =
+      patch match {
+        case Patch.Empty                  => res
+        case Patch.AddHeaders(headers)    => res.addHeaders(headers)
+        case Patch.RemoveHeaders(headers) => res.removeHeaders(headers)
+        case Patch.SetStatus(status)      => res.setStatus(status)
+        case Patch.Combine(self, other)   => loop[R1, E1](self(res), other)
+      }
+
+    loop(res, self)
   }
 }
 
