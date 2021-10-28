77b1fb296f7e234bc78daca94297d67576f5300d
kaushik143 <kaushik@dream11.com>
2021-10-28 17:34:18 +0530

    Test(Logger): Add spec for when

--- zio-http/src/test/scala/zhttp/http/HttpMiddlewareSpec.scala
+++ zio-http/src/test/scala/zhttp/http/HttpMiddlewareSpec.scala
@@ -10,15 +10,26 @@ object HttpMiddlewareSpec extends DefaultRunnableSpec {
     Response.ok
   }
 
-  def spec = suite("Debug")(testM("Log status, method") {
-    val debugApp = app @@ HttpMiddleware.debug
+  def updateApp[R, E](app: HttpApp[R, E]) = for {
+    _   <- app(req = Request(url = URL(!! / "health")))
+    out <- TestConsole.output
+  } yield out
 
-    val a = for {
-      _   <- debugApp(req = Request(url = URL(!! / "health")))
-      out <- TestConsole.output
-    } yield out
-
-    assertM(a)(equalTo(Vector("200 GET /health 0ms\n")))
-
-  })
+  def spec = suite("Debug")(
+    testM("log status method url and time") {
+      val debugApp = app @@ HttpMiddleware.debug
+      val a        = updateApp(debugApp)
+      assertM(a)(equalTo(Vector("200 GET /health 0ms\n")))
+    } +
+      testM("log when method is GET") {
+        val debugApp = app @@ HttpMiddleware.debug.when((method, _, _) => method == Method.GET)
+        val a        = updateApp(debugApp)
+        assertM(a)(equalTo(Vector("200 GET /health 0ms\n")))
+      } +
+      testM("fail log when method is POST") {
+        val debugApp = app @@ HttpMiddleware.debug.when((method, _, _) => method == Method.POST)
+        val a        = updateApp(debugApp)
+        assertM(a)(equalTo(Vector()))
+      },
+  )
 }
