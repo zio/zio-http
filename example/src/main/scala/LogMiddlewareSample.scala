6c77b5730bc642a8b6c7ba8dd5ac3d6e7e6ecc4c
Javier Goday <jgoday@gmail.com>
2021-10-31 07:07:14 +0100

    LogMiddlewareSample: use routes instead of different endpoints

--- example/src/main/scala/LogMiddlewareSample.scala
+++ example/src/main/scala/LogMiddlewareSample.scala
@@ -2,9 +2,7 @@ import zio._
 import zio.console._
 import zio.clock._
 import zio.logging.{LogFormat, LogLevel, Logging}
-import zhttp.endpoint._
 import zhttp.http._
-import zhttp.http.Method.GET
 import zhttp.http.middleware.LogMiddleware._
 import zhttp.service.Server
 
@@ -15,12 +13,11 @@ object LogMiddlewareSample extends App {
       format = LogFormat.ColoredLogFormat(),
     ) >>> Logging.withRootLoggerName("zio-http")
 
-  def echo = GET / *[String] to { a =>
-    Response.text(a.params + "\n")
-  }
-
-  def test = GET / "test" to { _ =>
-    Response.text("TEST")
+  def routes: HttpApp[Any, Nothing] = HttpApp.collect {
+    case Method.GET -> !! / "test" =>
+      Response.text("TEST")
+    case Method.GET -> !! / echo   =>
+      Response.text(echo + "\n")
   }
 
   val requestLogger  = RequestLogger(
@@ -35,7 +32,7 @@ object LogMiddlewareSample extends App {
   val options = Options.Skip { case ((_, url, _), _) => url.path.startsWith(Path("test")) }
 
   def app: HttpApp[Console with Clock with Logging, Throwable] =
-    (test +++ echo) @@ log(
+    routes @@ log(
       request = requestLogger,
       response = responseLogger,
       options = options,
