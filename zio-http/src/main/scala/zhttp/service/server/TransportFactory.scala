392697a1bc12cb7d63ec4cd7f97ba153859f279a
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2021-11-01 19:37:41 +0530

    SocketServerChannel for underlying Transport passed via config (settings) instead of dependency layer: UIO to Task

--- zio-http/src/main/scala/zhttp/service/server/TransportFactory.scala
+++ zio-http/src/main/scala/zhttp/service/server/TransportFactory.scala
@@ -5,8 +5,8 @@ import io.netty.channel.kqueue.{KQueue, KQueueServerSocketChannel}
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.channel.{ServerChannel, ChannelFactory => JChannelFactory}
 import io.netty.incubator.channel.uring.IOUringServerSocketChannel
-import zhttp.service.{ChannelFactory}
-import zio.{UIO}
+import zhttp.service.ChannelFactory
+import zio.{Task}
 
 sealed trait Transport
 object Transport {
@@ -26,11 +26,11 @@ object Transport {
     }
 }
 object TransportFactory {
-    def nio: UIO[JChannelFactory[ServerChannel]]    = ChannelFactory.make(() => new NioServerSocketChannel())
-    def epoll: UIO[JChannelFactory[ServerChannel]]  = ChannelFactory.make(() => new EpollServerSocketChannel())
-    def uring: UIO[JChannelFactory[ServerChannel]]  = ChannelFactory.make(() => new IOUringServerSocketChannel())
-    def kQueue: UIO[JChannelFactory[ServerChannel]] = ChannelFactory.make(() => new KQueueServerSocketChannel())
-    def auto: UIO[JChannelFactory[ServerChannel]]   =
+    def nio: Task[JChannelFactory[ServerChannel]]    = ChannelFactory.make(() => new NioServerSocketChannel())
+    def epoll: Task[JChannelFactory[ServerChannel]]  = ChannelFactory.make(() => new EpollServerSocketChannel())
+    def uring: Task[JChannelFactory[ServerChannel]]  = ChannelFactory.make(() => new IOUringServerSocketChannel())
+    def kQueue: Task[JChannelFactory[ServerChannel]] = ChannelFactory.make(() => new KQueueServerSocketChannel())
+    def auto: Task[JChannelFactory[ServerChannel]]   =
       if (Epoll.isAvailable) epoll
       else if (KQueue.isAvailable) kQueue
       else nio
