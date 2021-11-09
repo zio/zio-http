f568d2b65a6b8717892f8a362b1bebe82ea1e4f6
Tushar Mathur <tusharmath@gmail.com>
2021-11-09 20:13:12 +0530

    remove typecasting

--- example/src/main/scala/HelloWorldTwirl.scala
+++ example/src/main/scala/HelloWorldTwirl.scala
@@ -10,20 +10,14 @@ object HelloWorldTwirl extends zio.App {
     val (a, b)              = pathParams.params
     val response: UResponse = Response.text(advanced.html.index(a, b).toString())
     response
-      .asInstanceOf[UHttpResponse]
       .copy(headers = List(Header.custom("content-type", "text/html")))
   }
 
   val h1: HttpApp[Any, Nothing] = HttpApp.collect {
-    case Method.GET -> !!         => {
-      val response: UResponse = Response.text(html.index("John Doe").toString())
-
-      response
-        .asInstanceOf[UHttpResponse]
-        .copy(headers = List(Header.custom("content-type", "text/html")))
-    }
+    case Method.GET -> !!         =>
+      Response.text(html.index("John Doe").toString()).addHeader(Header.contentTypeHtml)
     case Method.GET -> !! / "nav" => {
-      val nav                 = Seq(
+      val nav = Seq(
         Navigation("link-1", "http://google.com", Some("bi-alarm-fill")),
         Navigation("link-2", "http://google.com", Some("bi-award")),
         Navigation("link-3", "http://google.com", Some("bi-bank")),
@@ -38,10 +32,7 @@ object HelloWorldTwirl extends zio.App {
           ),
         ),
       )
-      val response: UResponse = Response.text(advanced.html.template(1, false, nav).toString())
-      response
-        .asInstanceOf[UHttpResponse]
-        .copy(headers = List(Header.custom("content-type", "text/html")))
+      Response.text(advanced.html.template(1, false, nav).toString()).addHeader(Header.contentTypeHtml)
     }
   }
 
