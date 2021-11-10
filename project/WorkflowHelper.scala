9a26eca63e2d3cfaf91150dab8572c5215871db7
Shubham Girdhar <girdharshubham@hotmail.com>
2021-11-10 21:18:23 +0530

    scoverage(coveralls): Scoverage integration with coveralls

--- project/WorkflowHelper.scala
+++ project/WorkflowHelper.scala
@@ -4,8 +4,10 @@ object WorkflowHelper {
   object Scoverage {
     // TODO move plugins to plugins.sbt after scoverage's support for Scala 3
     val scoveragePlugin        = """addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.1")"""
+    val coverallsPlugin        = """addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.1")"""
     val coverageDirectivesBase = """(project in file("./zio-http"))"""
 
+    val plugins                                                              = List(scoveragePlugin, coverallsPlugin)
     def apply(statementTotal: Double, branchTotal: Double): Seq[WorkflowJob] = {
       val coverageDirectivesSettings =
         s"settings(coverageEnabled:=true,coverageFailOnMinimum:=true,coverageMinimumStmtTotal:=${statementTotal},coverageMinimumBranchTotal:=${branchTotal})"
@@ -16,7 +18,7 @@ object WorkflowHelper {
           steps = List(
             WorkflowStep.CheckoutFull,
             WorkflowStep.Run(
-              commands = List(s"sed -i -e '$$a${scoveragePlugin}' project/plugins.sbt"),
+              commands = plugins.map(plugin => s"sed -i -e '$$a${plugin}' project/plugins.sbt"),
               id = Some("add_plugin"),
               name = Some("Add Scoverage"),
             ),
@@ -28,9 +30,10 @@ object WorkflowHelper {
               name = Some("Update Build Definition"),
             ),
             WorkflowStep.Run(
-              commands = List(s"sbt ++$${{ matrix.scala }} coverage 'project zhttp;test' coverageReport"),
-              id = Some("run_coverage"),
+              commands = List(s"sbt ++$${{ matrix.scala }} coverage 'project zhttp;test' coverageReport coveralls"),
+              id = Some("run_coverage_and_push_coveralls "),
               name = Some("Run Coverage"),
+              env = Map("COVERALLS_REPO_TOKEN" -> "${{ secrets.GITHUB_TOKEN }}"),
             ),
           ),
         ),
