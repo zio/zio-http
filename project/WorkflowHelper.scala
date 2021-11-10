400727113f8c66e8f1f6282b58d26136d6353ecd
Shubham Girdhar <girdharshubham@hotmail.com>
2021-11-10 16:05:02 +0530

    CI Coveralls integration

--- project/WorkflowHelper.scala
+++ project/WorkflowHelper.scala
@@ -2,8 +2,12 @@ import sbtghactions.GenerativePlugin.autoImport.{WorkflowJob, WorkflowStep}
 
 object WorkflowHelper {
   object Scoverage {
+    // TODO move plugins to plugins.sbt after scoverage's support for Scala 3
     val scoveragePlugin        = """addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.1")"""
+    val coverallsPlugin        = """addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.1")"""
     val coverageDirectivesBase = """(project in file("./zio-http"))"""
+
+    val plugins = List(scoveragePlugin, coverageDirectivesBase)
     def apply(statementTotal: Double, branchTotal: Double): Seq[WorkflowJob] = {
       val coverageDirectivesSettings =
         s"settings(coverageEnabled:=true,coverageFailOnMinimum:=true,coverageMinimumStmtTotal:=${statementTotal},coverageMinimumBranchTotal:=${branchTotal})"
@@ -14,7 +18,7 @@ object WorkflowHelper {
           steps = List(
             WorkflowStep.CheckoutFull,
             WorkflowStep.Run(
-              commands = List(s"sed -i -e '$$a${scoveragePlugin}' project/plugins.sbt"),
+              commands = plugins.map(plugin => s"sed -i -e '$$a${plugin}' project/plugins.sbt"),
               id = Some("add_plugin"),
               name = Some("Add Scoverage"),
             ),
@@ -26,15 +30,21 @@ object WorkflowHelper {
               name = Some("Update Build Definition"),
             ),
             WorkflowStep.Run(
-              commands = List(s"sbt ++$${{ matrix.scala }} coverage 'project zhttp;test' coverageReport"),
+              commands = List(s"sbt ++$${{ matrix.scala }} coverage 'project zhttp;test'"),
               id = Some("run_coverage"),
               name = Some("Run Coverage"),
             ),
-            WorkflowStep.Run(
+            /*WorkflowStep.Run(
               commands = List("bash <(curl -s https://codecov.io/bash)"),
               id = Some("push_codecov"),
               name = Some("Push Codecov"),
-            ),
+            ),*/
+            WorkflowStep.Sbt(
+              commands = List("coverageReport coveralls"),
+              id = Some("push_coveralls"),
+              name = Some("Push Coveralls"),
+              env = Map("COVERALLS_REPO_TOKEN" -> "${{ secrets.GITHUB_TOKEN }}")
+            )
           ),
         ),
       )
