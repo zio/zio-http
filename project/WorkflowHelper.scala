42a6eda9ec7933c7bf8720c95ec560a3a39d6cb4
Shubham Girdhar <girdharshubham@hotmail.com>
2021-11-10 16:14:31 +0530

    test coveralls

--- project/WorkflowHelper.scala
+++ project/WorkflowHelper.scala
@@ -7,7 +7,7 @@ object WorkflowHelper {
     val coverallsPlugin        = """addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.1")"""
     val coverageDirectivesBase = """(project in file("./zio-http"))"""
 
-    val plugins = List(scoveragePlugin, coverallsPlugin)
+    val plugins                                                              = List(scoveragePlugin, coverallsPlugin)
     def apply(statementTotal: Double, branchTotal: Double): Seq[WorkflowJob] = {
       val coverageDirectivesSettings =
         s"settings(coverageEnabled:=true,coverageFailOnMinimum:=true,coverageMinimumStmtTotal:=${statementTotal},coverageMinimumBranchTotal:=${branchTotal})"
@@ -30,21 +30,11 @@ object WorkflowHelper {
               name = Some("Update Build Definition"),
             ),
             WorkflowStep.Run(
-              commands = List(s"sbt ++$${{ matrix.scala }} coverage 'project zhttp;test'"),
-              id = Some("run_coverage"),
-              name = Some("Run Coverage"),
+              commands = List(s"sbt ++$${{ matrix.scala }} coverage 'project zhttp;test' coverageReport coveralls"),
+              id = Some("run_coverage_and_push_coveralls"),
+              name = Some("Run Coverage and push coveralls"),
+              env = Map("COVERALLS_REPO_TOKEN" -> "${{ secrets.GITHUB_TOKEN }}"),
             ),
-            /*WorkflowStep.Run(
-              commands = List("bash <(curl -s https://codecov.io/bash)"),
-              id = Some("push_codecov"),
-              name = Some("Push Codecov"),
-            ),*/
-            WorkflowStep.Sbt(
-              commands = List("coverageReport coveralls"),
-              id = Some("push_coveralls"),
-              name = Some("Push Coveralls"),
-              env = Map("COVERALLS_REPO_TOKEN" -> "${{ secrets.GITHUB_TOKEN }}")
-            )
           ),
         ),
       )
