3d97f7a6005ca585cf802be69fe438b7e7ef6436
Tushar Mathur <tusharmath@gmail.com>
2021-11-13 20:01:10 +0530

    test: fix middleware spec

--- zio-http/src/test/scala/zhttp/http/HttpAppResponseSpec.scala
+++ zio-http/src/test/scala/zhttp/http/HttpAppResponseSpec.scala
@@ -6,7 +6,7 @@ import zhttp.service.{EventLoopGroup, Server}
 import zio.duration._
 import zio.test.Assertion._
 import zio.test.TestAspect.timeout
-import zio.test.{assertM, checkAllM, DefaultRunnableSpec, Gen}
+import zio.test.{DefaultRunnableSpec, Gen, assertM, checkAllM}
 import zio.{UIO, ZIO}
 
 object HttpAppResponseSpec extends DefaultRunnableSpec with HttpMessageAssertions {
--- zio-http/src/test/scala/zhttp/internal/HttpAppTestExtensions.scala
+++ zio-http/src/test/scala/zhttp/internal/HttpAppTestExtensions.scala
@@ -1,11 +1,17 @@
 package zhttp.internal
 
-import zhttp.http.{Http, HttpApp, Request, Status}
+import zhttp.http._
 
 trait HttpAppTestExtensions {
   implicit class HttpAppSyntax[R, E](app: HttpApp[R, E]) {
     def getHeader(name: String): Http[R, E, Request, Option[String]] =
       app.asHttp.map(res => res.getHeaderValue(name))
+
+    def getHeaders: Http[R, E, Request, List[Header]] =
+      app.asHttp.map(res => res.getHeaders)
+
+    def getHeaderValues: Http[R, E, Request, List[String]] =
+      app.asHttp.map(res => res.getHeaders.map(_.value.toString))
 
     def getStatus: Http[R, E, Request, Status] =
       app.asHttp.map(res => res.status)
