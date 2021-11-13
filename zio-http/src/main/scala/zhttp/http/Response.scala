0ac9c1974bd458c642552e499a40722effe5da7c
Tushar Mathur <tusharmath@gmail.com>
2021-11-13 19:07:13 +0530

    refactor: move Attribute to Response and general cleanup

--- zio-http/src/main/scala/zhttp/http/Response.scala
+++ zio-http/src/main/scala/zhttp/http/Response.scala
@@ -8,12 +8,26 @@ import zio.Chunk
 import java.io.{PrintWriter, StringWriter}
 import java.nio.charset.Charset
 
-case class Response[-R, +E] private (
-  status: Status,
-  getHeaders: List[Header],
-  data: HttpData[R, E],
-  private[zhttp] val attribute: HttpAttribute[R, E],
+final case class Response[-R, +E] private (
+  status: Status = Status.OK,
+  private val headers: List[Header] = Nil,
+  data: HttpData[R, E] = HttpData.empty,
+  attribute: Response.Attribute[R, E] = Response.Attribute.empty,
 ) extends HeaderExtension[Response[R, E]] { self =>
+
+  /**
+   * Adds cookies in the response headers
+   */
+  def addCookie(cookie: Cookie): Response[R, E] =
+    self.copy(headers = self.getHeaders ++ List(Header.custom(HttpHeaderNames.SET_COOKIE.toString, cookie.encode)))
+
+  override def getHeaders: List[Header] = headers
+
+  /**
+   * Sets the response attributes
+   */
+  def setAttribute[R1 <: R, E1 >: E](attribute: Response.Attribute[R1, E1]): Response[R1, E1] =
+    self.copy(attribute = attribute)
 
   /**
    * Sets the status of the response
@@ -22,44 +36,14 @@ case class Response[-R, +E] private (
     self.copy(status = status)
 
   /**
-   * Adds cookies in the response headers
-   */
-  def addCookie(cookie: Cookie): Response[R, E] =
-    self.copy(getHeaders = self.getHeaders ++ List(Header.custom(HttpHeaderNames.SET_COOKIE.toString, cookie.encode)))
-
-  /**
    * Updates the headers using the provided function
    */
-  final override def updateHeaders(f: List[Header] => List[Header]): Response[R, E] =
-    self.copy(getHeaders = f(self.getHeaders))
+  override def updateHeaders(f: List[Header] => List[Header]): Response[R, E] =
+    self.copy(headers = f(self.getHeaders))
+
 }
 
 object Response {
-  def apply[R, E](
-    status: Status = Status.OK,
-    headers: List[Header] = Nil,
-    data: HttpData[R, E] = HttpData.Empty,
-  ): Response[R, E] =
-    Response(status, headers, data, HttpAttribute.empty)
-
-  @deprecated("Use `Response(status, headers, data)` constructor instead.", "22-Sep-2021")
-  def http[R, E](
-    status: Status = Status.OK,
-    headers: List[Header] = Nil,
-    data: HttpData[R, E] = HttpData.empty,
-  ): Response[R, E] = Response(status, headers, data)
-
-  /**
-   * Creates a socket response using an app
-   */
-  def socket[R, E](ss: SocketApp[R, E]): Response[R, E] =
-    Response(Status.SWITCHING_PROTOCOLS, Nil, HttpData.empty, HttpAttribute.socket(ss))
-
-  /**
-   * Creates a new WebSocket Response
-   */
-  def socket[R, E](ss: Socket[R, E, WebSocketFrame, WebSocketFrame]): Response[R, E] =
-    SocketApp.message(ss).asResponse
 
   def fromHttpError(error: HttpError): UResponse = {
     error match {
@@ -79,25 +63,76 @@ object Response {
     }
   }
 
-  def ok: UResponse = Response(Status.OK)
+  @deprecated("Use `Response(status, headers, data)` constructor instead.", "22-Sep-2021")
+  def http[R, E](
+    status: Status = Status.OK,
+    headers: List[Header] = Nil,
+    data: HttpData[R, E] = HttpData.empty,
+  ): Response[R, E] = Response(status, headers, data)
 
-  def text(text: String, charset: Charset = HTTP_CHARSET): UResponse =
-    Response(
-      data = HttpData.fromText(text, charset),
-      headers = List(Header.contentTypeTextPlain),
-    )
-
+  /**
+   * Creates a response with content-type set to application/json
+   */
   def jsonString(data: String): UResponse =
     Response(
       data = HttpData.fromChunk(Chunk.fromArray(data.getBytes(HTTP_CHARSET))),
       headers = List(Header.contentTypeJson),
     )
 
+  /**
+   * Creates an empty response with status 200
+   */
+  def ok: UResponse = Response(Status.OK)
+
+  /**
+   * Creates an empty response with status 301 or 302 depending on if it's permanent or not.
+   */
+  def redirect(location: String, isPermanent: Boolean = false): Response[Any, Nothing] = {
+    val status = if (isPermanent) Status.PERMANENT_REDIRECT else Status.TEMPORARY_REDIRECT
+    Response(status, List(Header.location(location)))
+  }
+
+  /**
+   * Creates a socket response using an app
+   */
+  def socket[R, E](app: SocketApp[R, E]): Response[R, E] =
+    Response(Status.SWITCHING_PROTOCOLS, Nil, HttpData.empty, Attribute(socketApp = app))
+
+  /**
+   * Creates a new WebSocket Response
+   */
+  def socket[R, E](ss: Socket[R, E, WebSocketFrame, WebSocketFrame]): Response[R, E] =
+    SocketApp.message(ss).asResponse
+
+  /**
+   * Creates an empty response with the provided Status
+   */
   def status(status: Status): UResponse = Response(status)
 
-  def temporaryRedirect(location: String): Response[Any, Nothing] =
-    Response(Status.TEMPORARY_REDIRECT, List(Header.location(location)))
+  /**
+   * Creates a response with content-type set to plain/text
+   */
+  def text(text: String, charset: Charset = HTTP_CHARSET): UResponse =
+    Response(
+      data = HttpData.fromText(text, charset),
+      headers = List(Header.contentTypeTextPlain),
+    )
 
-  def permanentRedirect(location: String): Response[Any, Nothing] =
-    Response(Status.PERMANENT_REDIRECT, List(Header.location(location)))
+  /**
+   * Attribute holds meta data for the backend
+   */
+
+  final case class Attribute[-R, +E](socketApp: SocketApp[R, E] = SocketApp.empty) {
+    self =>
+
+    def withSocketApp[R1 <: R, E1 >: E](app: SocketApp[R1, E1]): Attribute[R1, E1] = self.copy(socketApp = app)
+  }
+
+  object Attribute {
+
+    /**
+     * Helper to create an empty HttpData
+     */
+    def empty: Attribute[Any, Nothing] = Attribute()
+  }
 }
