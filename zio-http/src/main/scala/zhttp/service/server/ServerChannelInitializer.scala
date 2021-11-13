dc579a4337e7de9939eaee3217ce19cc99b2c1e0
Tushar Mathur <tusharmath@gmail.com>
2021-11-13 13:37:21 +0530

    refactor: restructure initializer

--- zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
@@ -1,9 +1,8 @@
 package zhttp.service.server
 
 import io.netty.channel.ChannelHandler.Sharable
-import io.netty.channel.{Channel, ChannelInitializer, ChannelPipeline}
+import io.netty.channel.{Channel, ChannelInitializer}
 import io.netty.handler.codec.http.{HttpServerCodec, HttpServerExpectContinueHandler, HttpServerKeepAliveHandler}
-import io.netty.handler.flow.FlowControlHandler
 import zhttp.service.Server.Settings
 import zhttp.service._
 
@@ -14,27 +13,31 @@ import zhttp.service._
 final case class ServerChannelInitializer[R](zExec: HttpRuntime[R], settings: Settings[R, Throwable])
     extends ChannelInitializer[Channel] {
   override def initChannel(channel: Channel): Unit = {
+    // !! IMPORTANT !!
+    // Order of handlers are critical to make this work
+    val pipeline = channel.pipeline()
 
+    // SSL
+    // Add SSL Handler if CTX is available
     val sslctx = if (settings.sslOption == null) null else settings.sslOption.sslContext
-    if (sslctx != null) {
-      channel
-        .pipeline()
-        .addFirst(
-          SSL_HANDLER,
-          new OptionalSSLHandler(sslctx, settings.sslOption.httpBehaviour),
-        )
-      ()
-    }
+    if (sslctx != null) pipeline.addFirst(SSL_HANDLER, new OptionalSSLHandler(sslctx, settings.sslOption.httpBehaviour))
 
-    val ch: ChannelPipeline = channel
-      .pipeline()
-      .addLast(HTTP_SERVER_CODEC, new HttpServerCodec())               // TODO: See if server codec is really required
-      .addLast(HTTP_KEEPALIVE_HANDLER, new HttpServerKeepAliveHandler) // TODO: Make keep-alive configurable
-      .addLast(FLOW_CONTROL_HANDLER, new FlowControlHandler())
-      .addLast(HTTP_REQUEST_HANDLER, settings.app.compile(zExec))
+    // ServerCodec
+    // Always add ServerCodec
+    pipeline.addLast(HTTP_SERVER_CODEC, new HttpServerCodec()) // TODO: See if server codec is really required
 
-    if (settings.acceptContinue)
-      ch.addAfter(HTTP_SERVER_CODEC, HTTP_SERVER_EXPECT_CONTINUE, new HttpServerExpectContinueHandler())
+    // ExpectContinueHandler
+    // Add expect continue handler is settings is true
+    if (settings.acceptContinue) pipeline.addLast(HTTP_SERVER_EXPECT_CONTINUE, new HttpServerExpectContinueHandler())
+
+    // KeepAliveHandler
+    // Add Keep-Alive handler is settings is true
+    if (settings.keepAlive) pipeline.addLast(HTTP_KEEPALIVE_HANDLER, new HttpServerKeepAliveHandler)
+
+    // RequestHandler
+    // Always add ZIO Http Request Handler
+    pipeline.addLast(HTTP_REQUEST_HANDLER, settings.app.compile(zExec, settings))
+
     ()
   }
 
