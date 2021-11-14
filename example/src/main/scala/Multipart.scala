da2b68b56c8149899999e6c3002dc2f1c1631b3d
Shrey Mehta <shrey@dream11.com>
2021-11-14 22:28:21 +0530

    test(multipart): Update test with new functions API

--- example/src/main/scala/Multipart.scala
+++ example/src/main/scala/Multipart.scala
@@ -4,6 +4,11 @@ import zhttp.service.Server
 import zio.stream.ZStream
 import zio.{App, ExitCode, URIO}
 
+/*
+ * Current multipart decoder API gives you access to Queue[Message].
+ * Message is a sum type and can be "MetaInfo", "ChunkedData" or "BodyEnd".
+ * The following example is converting message to Bytes and returning in response.
+ * */
 object Multipart extends App {
   def app: HttpApp[Any, Throwable] = HttpApp.fromHttp {
     Http.collectM[Request] { case req =>
--- zio-http/src/test/scala/zhttp/multipart/Parser.scala
+++ zio-http/src/test/scala/zhttp/multipart/Parser.scala
@@ -1,105 +1,138 @@
 package zhttp.experiment.multipart
 
-import zio.Chunk
-object Util {
-  def getBytes(input: Chunk[Message]): Chunk[Byte] = input
-    .filter(_.isInstanceOf[ChunkedData])
-    .asInstanceOf[Chunk[ChunkedData]]
-    .map(_.chunkedData)
-    .flatten
+import zhttp.http.{Header, Request}
+import zio.test.Assertion.equalTo
+import zio.{Chunk, UIO, ZIO}
+import zio.test.{DefaultRunnableSpec, assertM}
+
+object ParserTest extends DefaultRunnableSpec {
+  def makeHttpString(str: String): String =
+    augmentString(str).flatMap {
+      case '\n' => "\r\n"
+      case c    => c.toString
+    }
+  val request: Request                    = Request(headers =
+    List(
+      Header("Content-Type", "multipart/form-data; boundary=_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI"),
+    ),
+  )
+  def spec                                = suite("Multipart Parser")(
+    suite("Request Parsing")(
+      testM("Should extract boundary from request") {
+        assertM(ZIO.fromEither(Parser.getBoundary(request)))(equalTo("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI"))
+      },
+      testM("Should store partial header data to parserState") {
+        val data          =
+          """|--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI
+             |Content-Disposition: form-data; name="upload"; filename="integration.txt"""".stripMargin
+        val processedData = makeHttpString(data)
+        val output        = Parser.getMessages(
+          Chunk.fromArray(processedData.getBytes()),
+          ParserState(Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes)),
+        ) match {
+          case Left(_)      => ZIO.fail("Parsing Failed")
+          case Right(value) => UIO(value)
+        }
+        assertM(output)(
+          equalTo(
+            (
+              Chunk.empty,
+              ParserState(
+                Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes),
+                state = PartHeader,
+                0,
+                0,
+                Chunk.fromArray(
+                  "\r\nContent-Disposition: form-data; name=\"upload\"; filename=\"integration.txt\"".getBytes,
+                ),
+              ),
+            ),
+          ),
+        )
+      },
+      testM("Should output header info and clears up temp data") {
+        val data          = """|
+                      |Content-Type: application/octet-stream
+                      |Content-Transfer-Encoding: binary
+                      |
+                      |""".stripMargin
+        val processedData = makeHttpString(data)
+        val output        = Parser.getMessages(
+          Chunk.fromArray(processedData.getBytes()),
+          ParserState(
+            Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes),
+            state = PartHeader,
+            0,
+            0,
+            Chunk.fromArray(
+              "\r\nContent-Disposition: form-data; name=\"upload\"; filename=\"integration.txt\"".getBytes,
+            ),
+          ),
+        ) match {
+          case Left(_)      => ZIO.fail("Parsing Failed")
+          case Right(value) => UIO(value)
+        }
+        assertM(output)(
+          equalTo(
+            (
+              Chunk(
+                MetaInfo(
+                  PartContentDisposition("upload", Some("integration.txt")),
+                  Some(PartContentType("application/octet-stream", None)),
+                  Some(PartContentTransferEncoding("binary")),
+                ),
+              ),
+              ParserState(
+                Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes),
+                state = PartData,
+              ),
+            ),
+          ),
+        )
+      },
+      testM("Should read data from the file upload and output in ChunkedData") {
+        val data                                                    =
+          """
+            |this is a test""".stripMargin
+        val processedData: String                                   = makeHttpString(data)
+        val output: ZIO[Any, String, (Chunk[Message], ParserState)] = Parser.getMessages(
+          Chunk.fromArray(processedData.getBytes),
+          ParserState(Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes), state = PartData),
+        ) match {
+          case Left(_)      => ZIO.fail("Parsing Failed")
+          case Right(value) => UIO(value)
+        }
+        assertM(output)(
+          equalTo(
+            (
+              Chunk(ChunkedData(Chunk.fromArray("\r\nthis is a test".getBytes))),
+              ParserState(Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes), state = PartData),
+            ),
+          ),
+        )
+      },
+      testM("Should read data from the file upload and output in ChunkedData and should identify end of the body") {
+        val data                                                    = """here's another test
+                     |catch me if you can!
+                     |
+                     |--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI--""".stripMargin
+        val processedData                                           = makeHttpString(data)
+        val output: ZIO[Any, String, (Chunk[Message], ParserState)] = Parser.getMessages(
+          Chunk.fromArray(processedData.getBytes),
+          ParserState(Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes), state = PartData),
+        ) match {
+          case Left(_)      => ZIO.fail("Parsing Failed")
+          case Right(value) => UIO(value)
+        }
+        assertM(output)(
+          equalTo(
+            (
+              Chunk(ChunkedData(Chunk.fromArray("here's another test\r\ncatch me if you can!\r\n".getBytes)), BodyEnd),
+              ParserState(Chunk.fromArray("_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI".getBytes), state = End),
+            ),
+          ),
+        )
+      },
+    ),
+  )
 }
-//object ParserTest extends DefaultRunnableSpec {
-//  def makeHttpString(str: String) =
-//    augmentString(str).flatMap {
-//      case '\n' => "\r\n"
-//      case c    => c.toString
-//    }
-//  val request                     = Request(headers =
-//    List(
-//      Header("Content-Type", "multipart/form-data; boundary=_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI"),
-//    ),
-//  )
-//  def spec                        = suite("Multipart Parser")(
-//    suite("Boundary")(
-//      test("Should parse boundary and header") {
-//        val data1          = """|--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI
-//                       |Content-Disposition: form-data; name="upload"; filename="integration.txt"
-//                     """.stripMargin
-//        val processedData1 = makeHttpString(data1)
-//        val parser         = new Parser(request)
-//        val output1        = parser.getMessages(Chunk.fromArray(processedData1.getBytes()), request)
-//        val data2          = """|
-//                       |Content-Type: application/octet-stream
-//                       |Content-Transfer-Encoding: binary
-//                       |
-//                       |""".stripMargin
-//        val processedData2 = makeHttpString(data2)
-//        val output2        = parser.getMessages(Chunk.fromArray(processedData2.getBytes()), request, )
-//        assert(output1)(equalTo(Chunk.empty)) && assert(output2)(
-//          equalTo(
-//            Chunk(
-//              MetaInfo(
-//                PartContentDisposition("upload", Some("integration.txt")),
-//                Some(PartContentType("application/octet-stream", None)),
-//                Some(PartContentTransferEncoding("binary")),
-//              ),
-//            ),
-//          ),
-//        )
-//      },
-//      test("Full Multipart body") {
-//        val data          = """|--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI
-//                      |Content-Disposition: form-data; name="upload"; filename="integration.txt"
-//                      |Content-Type: application/octet-stream
-//                      |Content-Transfer-Encoding: binary
-//                      |
-//                      |this is a test
-//                      |here's another test
-//                      |catch me if you can!
-//                      |--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI--""".stripMargin
-//        val processedData = makeHttpString(data)
-//        val parser        = new Parser(request)
-//        val outputBytes   = getBytes(
-//          parser
-//            .getMessages(Chunk.fromArray(processedData.getBytes())),
-//        )
-//
-//        val outputString = new String(outputBytes.toArray, StandardCharsets.UTF_8)
-//
-//        assert(outputString)(equalTo("this is a test\r\nhere's another test\r\ncatch me if you can!"))
-//      },
-//      test("Multiparts messages in parts") {
-//        val data           = """|--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI
-//                      |Content-Disposition: form-data; name="upload"; filename="integration.txt"
-//                      |Content-Type: application/octet-stream
-//                      |Content-Transfer-Encoding: binary
-//                      |
-//                      |this is a test""".stripMargin
-//        val processedData  = makeHttpString(data)
-//        val parser         = new Parser(request)
-//        val output         = new String(
-//          getBytes(parser.getMessages(Chunk.fromArray(processedData.getBytes()))).toArray,
-//          StandardCharsets.UTF_8,
-//        )
-//        val data2          = """here's another test
-//                      |catch me if you can!
-//                      |
-//                      |--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI
-//                      |Content-Disposition: form-data; name="foo"
-//                      |
-//                      |bar
-//                      |--_5PHqf8_Pl1FCzBuT5o_mVZg36k67UYI--""".stripMargin
-//        val processedData2 = makeHttpString(data2)
-//        val output2        =
-//          new String(
-//            getBytes(parser.getMessages(Chunk.fromArray(processedData2.getBytes()))).toArray,
-//            StandardCharsets.UTF_8,
-//          )
-//
-//        assert(output)(equalTo("this is a test")) && assert(output2)(
-//          equalTo("here's another test\r\ncatch me if you can!\r\nbar"),
-//        )
-//      },
-//    ),
-//  )
-// }
