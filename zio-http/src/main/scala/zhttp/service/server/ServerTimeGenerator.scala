46e11be7f8024f4adbe9677289aef0765c3838ec
Tushar Mathur <tusharmath@gmail.com>
2021-11-14 20:50:18 +0530

    fix: ServerTimeGenerator to generate header everytime

--- zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
@@ -6,16 +6,11 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 case class ServerTimeGenerator() {
-  private var last: Long = 0
-  private val formatter  = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z")
+  private val formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z")
 
   def update(headers: HttpHeaders): Boolean = {
-    val now = System.currentTimeMillis()
-    if (now - last >= 1000) {
-      last = now
-      headers.set(HttpHeaderNames.DATE, formatter.format(new Date(now)))
-      true
-    } else false
+    headers.set(HttpHeaderNames.DATE, formatter.format(new Date()))
+    true
   }
 
   def update(response: HttpResponse): HttpResponse = {
