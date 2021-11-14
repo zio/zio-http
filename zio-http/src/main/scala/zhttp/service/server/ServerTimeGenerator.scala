0597615e23b1925c1c52112f8d6d132fd2c54a6b
Tushar Mathur <tusharmath@gmail.com>
2021-11-14 19:37:26 +0530

    perf: initialize the last value to 0 in ServerTimeGenerator

--- zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
@@ -1,26 +1,18 @@
 package zhttp.service.server
 
 import io.netty.handler.codec.http.{HttpHeaderNames, HttpHeaders, HttpResponse}
-import io.netty.util.concurrent.FastThreadLocal
 
 import java.text.SimpleDateFormat
 import java.util.Date
 
 case class ServerTimeGenerator() {
-  private val iTime = System.currentTimeMillis()
-
-  private val last: FastThreadLocal[Long] = new FastThreadLocal[Long] {
-    override def initialValue(): Long = iTime
-  }
-
-  private val formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z")
+  private var last: Long = 0
+  private val formatter  = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z")
 
   def update(headers: HttpHeaders): Boolean = {
-
     val now = System.currentTimeMillis()
-
-    if (now - last.get() >= 1000) {
-      last.set(now)
+    if (now - last >= 1000) {
+      last = now
       headers.set(HttpHeaderNames.DATE, formatter.format(new Date(now)))
       true
     } else false
