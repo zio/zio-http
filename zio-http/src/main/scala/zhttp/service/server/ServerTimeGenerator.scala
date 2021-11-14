7548dc9cc07222aea63b6956c708d2df6f9e1037
Tushar Mathur <tusharmath@gmail.com>
2021-11-14 18:32:04 +0530

    perf: Improve performance of ServerTimeGenerator

--- zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerTimeGenerator.scala
@@ -6,10 +6,16 @@ import java.time.format.DateTimeFormatter
 import java.time.{Instant, ZoneOffset}
 
 case class ServerTimeGenerator() {
+  private var last: Instant = Instant.now()
   private val formatter = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneOffset.UTC)
 
-  def update(headers: HttpHeaders): HttpHeaders = {
-    headers.set(HttpHeaderNames.DATE, formatter.format(Instant.now()))
+  def update(headers: HttpHeaders): Boolean = {
+    val now = Instant.now()
+    if (now.toEpochMilli - last.toEpochMilli >= 1000) {
+      last = now
+      headers.set(HttpHeaderNames.DATE, formatter.format(now))
+      true
+    } else false
   }
 
   def update(response: HttpResponse): HttpResponse = {
