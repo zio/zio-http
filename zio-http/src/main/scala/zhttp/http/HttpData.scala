48bc13de2b6989c90986160faee1845c139b76fc
Tushar Mathur <tusharmath@gmail.com>
2021-11-15 21:51:27 +0530

    perf: add caching capability in HttpData

--- zio-http/src/main/scala/zhttp/http/HttpData.scala
+++ zio-http/src/main/scala/zhttp/http/HttpData.scala
@@ -1,6 +1,6 @@
 package zhttp.http
 
-import io.netty.buffer.ByteBuf
+import io.netty.buffer.{ByteBuf, Unpooled}
 import zio.stream.ZStream
 import zio.{Chunk, NeedsEnv}
 
@@ -10,6 +10,23 @@ import java.nio.charset.Charset
  * Holds HttpData that needs to be written on the HttpChannel
  */
 sealed trait HttpData[-R, +E] { self =>
+
+  /**
+   * Returns true if HttpData is a stream
+   */
+  def isChunked: Boolean = self match {
+    case HttpData.BinaryStream(_) => true
+    case _                        => false
+  }
+
+  /**
+   * Returns true if HttpData is empty
+   */
+  def isEmpty: Boolean = self match {
+    case HttpData.Empty => true
+    case _              => false
+  }
+
   def provide[R1 <: R](env: R)(implicit ev: NeedsEnv[R]): HttpData[Any, E] =
     self match {
       case HttpData.BinaryStream(data) => HttpData.BinaryStream(data.provide(env))
@@ -34,40 +51,9 @@ sealed trait HttpData[-R, +E] { self =>
     case HttpData.BinaryByteBuf(data) => data.readableBytes().toLong
     case HttpData.BinaryStream(_)     => -1L
   }
-
-  /**
-   * Returns true if HttpData is empty
-   */
-  def isEmpty: Boolean = self match {
-    case HttpData.Empty => true
-    case _              => false
-  }
-
-  /**
-   * Returns true if HttpData is a stream
-   */
-  def isChunked: Boolean = self match {
-    case HttpData.BinaryStream(_) => true
-    case _                        => false
-  }
 }
 
 object HttpData {
-  private[zhttp] case object Empty                                                extends HttpData[Any, Nothing]
-  private[zhttp] final case class Text(text: String, charset: Charset)            extends HttpData[Any, Nothing]
-  private[zhttp] final case class BinaryChunk(data: Chunk[Byte])                  extends HttpData[Any, Nothing]
-  private[zhttp] final case class BinaryByteBuf(data: ByteBuf)                    extends HttpData[Any, Nothing]
-  private[zhttp] final case class BinaryStream[R, E](stream: ZStream[R, E, Byte]) extends HttpData[R, E]
-
-  /**
-   * Helper to create HttpData from ByteBuf
-   */
-  def fromByteBuf(byteBuf: ByteBuf): HttpData[Any, Nothing] = HttpData.BinaryByteBuf(byteBuf)
-
-  /**
-   * Helper to create HttpData from Stream of Chunks
-   */
-  def fromStream[R, E](stream: ZStream[R, E, Byte]): HttpData[R, E] = HttpData.BinaryStream(stream)
 
   /**
    * Helper to create empty HttpData
@@ -75,12 +61,49 @@ object HttpData {
   def empty: HttpData[Any, Nothing] = Empty
 
   /**
-   * Helper to create HttpData from String
+   * Helper to create HttpData from ByteBuf
    */
-  def fromText(text: String, charset: Charset = HTTP_CHARSET): HttpData[Any, Nothing] = Text(text, charset)
+  def fromByteBuf(byteBuf: ByteBuf): HttpData[Any, Nothing] = HttpData.BinaryByteBuf(byteBuf)
 
   /**
    * Helper to create HttpData from chunk of bytes
    */
   def fromChunk(data: Chunk[Byte]): HttpData[Any, Nothing] = BinaryChunk(data)
+
+  /**
+   * Helper to create HttpData from Stream of Chunks
+   */
+  def fromStream[R, E](stream: ZStream[R, E, Byte]): HttpData[R, E] = HttpData.BinaryStream(stream)
+
+  /**
+   * Helper to create HttpData from String
+   */
+  def fromText(text: String, charset: Charset = HTTP_CHARSET): HttpData[Any, Nothing] = Text(text, charset)
+
+  private[zhttp] sealed trait Cached { self =>
+    def encode: ByteBuf = self match {
+      case Text(text, charset) => Unpooled.copiedBuffer(text, charset)
+      case BinaryChunk(data)   => Unpooled.copiedBuffer(data.toArray)
+    }
+
+    // TODO: Add Unit Tests
+    def encodeAndCache(cache: Boolean): ByteBuf = {
+      if (cache) {
+        if (self.cache == null) {
+          val buf = Unpooled.unreleasableBuffer(encode)
+          self.cache = buf
+          buf
+        } else self.cache
+      } else
+        encode
+    }
+
+    var cache: ByteBuf = null
+  }
+
+  private[zhttp] final case class Text(text: String, charset: Charset) extends HttpData[Any, Nothing] with Cached
+  private[zhttp] final case class BinaryChunk(data: Chunk[Byte])       extends HttpData[Any, Nothing] with Cached
+  private[zhttp] final case class BinaryByteBuf(data: ByteBuf)         extends HttpData[Any, Nothing]
+  private[zhttp] final case class BinaryStream[R, E](stream: ZStream[R, E, Byte]) extends HttpData[R, E]
+  private[zhttp] case object Empty                                                extends HttpData[Any, Nothing]
 }
