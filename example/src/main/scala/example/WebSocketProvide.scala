c72129b33524a1479d2a7908486227e79be054d9
Shubham Girdhar <girdharshubham@hotmail.com>
2021-11-15 14:19:07 +0530

    example(WebSocketProvide): Example for WebSocket#provide added

---
+++ example/src/main/scala/example/WebSocketProvide.scala
@@ -0,0 +1,25 @@
+package example
+
+import zhttp.http._
+import zhttp.service._
+import zhttp.socket.{Socket, WebSocketFrame}
+import zio.stream.ZStream
+import zio.{ExitCode, URIO}
+
+object WebSocketProvide extends zio.App {
+  private val socket =
+    Socket
+      .collect[WebSocketFrame] { case WebSocketFrame.Text("FOO") => ZStream.environment[WebSocketFrame] }
+      .provide(WebSocketFrame.text("BAR"))
+
+  private val app =
+    HttpApp.collect { case Method.GET -> !! / "ws" => Response.socket(socket) }
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    val port = 80
+    Server
+      .start(port, app)
+      .debug(s"Server Started on port ${port}")
+      .exitCode
+  }
+}
