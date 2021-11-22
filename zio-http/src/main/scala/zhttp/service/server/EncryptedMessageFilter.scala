afa0a7f664efaa9d4caf3e9edc056d0ac8313f82
Dino John <dinopanali@gmail.com>
2021-11-22 13:44:42 +0530

    feat: encrypted message filter

---
+++ zio-http/src/main/scala/zhttp/service/server/EncryptedMessageFilter.scala
@@ -0,0 +1,33 @@
+package zhttp.service.server
+
+import io.netty.buffer.ByteBuf
+import io.netty.channel.{ChannelHandler, ChannelHandlerContext}
+import io.netty.handler.codec.ByteToMessageDecoder
+import io.netty.handler.codec.http.{HttpServerKeepAliveHandler => JHttpServerKeepAliveHandler}
+import io.netty.handler.flow.FlowControlHandler
+import io.netty.handler.ssl.SslHandler
+import zhttp.service.Server.Settings
+import zhttp.service._
+
+import java.util
+
+case class EncryptedMessageFilter[R](httpH: ChannelHandler, settings: Settings[R, Throwable])
+  extends ByteToMessageDecoder {
+  override def decode(context: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]): Unit = {
+    if (SslHandler.isEncrypted(in)) {
+      context.channel().pipeline().remove(SERVER_CLEAR_TEXT_HTTP2_HANDLER)
+      context.channel().pipeline().remove(SERVER_CLEAR_TEXT_HTTP2_FALLBACK_HANDLER)
+      context
+        .channel()
+        .pipeline()
+        .addLast(HTTP_KEEPALIVE_HANDLER, new JHttpServerKeepAliveHandler)
+        .addLast(FLOW_CONTROL_HANDLER, new FlowControlHandler())
+        .addLast(HTTP_REQUEST_HANDLER, httpH)
+        .remove(this)
+      ()
+    } else {
+      context.pipeline().remove(this)
+      ()
+    }
+  }
+}
