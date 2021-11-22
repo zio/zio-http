23ccf31c0ae1e3bc659c8a0c7ad3e0c2d2baee49
Dino John <dinopanali@gmail.com>
2021-11-22 13:37:31 +0530

    refactor: server channel initializer

--- zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
@@ -1,48 +1,34 @@
 package zhttp.service.server
 
 import io.netty.channel.ChannelHandler.Sharable
-import io.netty.channel.{Channel, ChannelInitializer}
-import io.netty.handler.codec.http.{HttpServerCodec, HttpServerExpectContinueHandler, HttpServerKeepAliveHandler}
-import zhttp.http.Http.HttpAppSyntax
-import zhttp.service.Server.Config
+import io.netty.channel.{Channel, ChannelHandler, ChannelInitializer}
+import zhttp.service.Server.Settings
 import zhttp.service._
+import zhttp.service.server.ServerChannelInitializerUtil.configureClearText
 
 /**
  * Initializes the netty channel with default handlers
  */
 @Sharable
 final case class ServerChannelInitializer[R](
-  zExec: HttpRuntime[R],
-  cfg: Config[R, Throwable],
-  serverTime: ServerTimeGenerator,
-) extends ChannelInitializer[Channel] {
+                                              zExec: HttpRuntime[R],
+                                              settings: Settings[R, Throwable],
+                                              httpHandler: ChannelHandler,
+                                              http2Handler: ChannelHandler,
+                                            ) extends ChannelInitializer[Channel] {
   override def initChannel(channel: Channel): Unit = {
-    // !! IMPORTANT !!
-    // Order of handlers are critical to make this work
-    val pipeline = channel.pipeline()
 
-    // SSL
-    // Add SSL Handler if CTX is available
-    val sslctx = if (cfg.sslOption == null) null else cfg.sslOption.sslContext
-    if (sslctx != null) pipeline.addFirst(SSL_HANDLER, new OptionalSSLHandler(sslctx, cfg.sslOption.httpBehaviour, cfg))
-
-    // ServerCodec
-    // Always add ServerCodec
-    pipeline.addLast(HTTP_SERVER_CODEC, new HttpServerCodec()) // TODO: See if server codec is really required
-
-    // ExpectContinueHandler
-    // Add expect continue handler is settings is true
-    if (cfg.acceptContinue) pipeline.addLast(HTTP_SERVER_EXPECT_CONTINUE, new HttpServerExpectContinueHandler())
-
-    // KeepAliveHandler
-    // Add Keep-Alive handler is settings is true
-    if (cfg.keepAlive) pipeline.addLast(HTTP_KEEPALIVE_HANDLER, new HttpServerKeepAliveHandler)
-
-    // RequestHandler
-    // Always add ZIO Http Request Handler
-    pipeline.addLast(HTTP_REQUEST_HANDLER, cfg.app.compile(zExec, cfg, serverTime))
-
-    ()
+    val sslctx = ServerSSLHandler.build(settings.sslOption, settings.http2)
+    if (sslctx != null) {
+      channel
+        .pipeline()
+        .addLast(HTTP2_OR_HTTP_SERVER_HANDLER, Http2OrHttpServerHandler(httpHandler, http2Handler, settings))
+        .addFirst(
+          SSL_HANDLER,
+          new OptionalSSLHandler(httpHandler, http2Handler, sslctx, settings),
+        )
+      ()
+    } else configureClearText(httpHandler, http2Handler, channel, settings)
   }
 
 }
