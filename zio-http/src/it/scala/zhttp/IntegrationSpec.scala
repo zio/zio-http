69b46568ba4edacf45e76782f301866efbcb22c9
Shubham Girdhar <girdharshubham@hotmail.com>
2021-11-22 11:55:41 +0530

    test(403,500): http status codes covered

--- zio-http/src/it/scala/zhttp/IntegrationSpec.scala
+++ zio-http/src/it/scala/zhttp/IntegrationSpec.scala
@@ -1,24 +1,60 @@
 package zhttp
 
 import zhttp.http._
-import zhttp.service.server.ServerChannelFactory
-import zhttp.service.{ChannelFactory, EventLoopGroup, Server}
+import zhttp.http.middleware.HttpMiddleware.basicAuth
+import zhttp.service._
+import zhttp.service.server._
 import zio._
-import zio.test.DefaultRunnableSpec
+import zio.test.Assertion.equalTo
+import zio.test._
 
 object IntegrationSpec extends DefaultRunnableSpec {
-  implicit val addr = "localhost"
-  implicit val port = 80
-  def env           = EventLoopGroup.auto() ++ ChannelFactory.auto ++ ServerChannelFactory.auto
+  val addr     = "localhost"
+  val port     = 80
+  val baseAddr = s"http://${addr}:${port}"
+  def env      = EventLoopGroup.auto() ++ ChannelFactory.auto ++ ServerChannelFactory.auto
 
-  def app = HttpApp.collectM {
-    case Method.GET -> !!  => ZIO.succeed(Response.ok)
-    case Method.POST -> !! => ZIO.succeed(Response.status(Status.CREATED))
+  def api = HttpApp.collect {
+    case Method.GET -> !!          => Response.ok
+    case Method.POST -> !!         => Response.status(Status.CREATED)
+    case Method.GET -> !! / "boom" => Response.status(Status.INTERNAL_SERVER_ERROR)
   }
 
-  def spec = suite("IntegrationSpec")(
-    HttpSpec.testSuite,
-  ) provideCustomLayer env
+  def basicAuthApi = HttpApp.collect { case Method.GET -> !! / "auth" =>
+    Response.ok
+  } @@ basicAuth("root", "changeme")
 
-  Runtime.default.unsafeRun(Server.start(port, app).forkDaemon)
+  def spec = suite("IntegrationSpec")(
+    HttpSpec,
+  ).provideCustomLayer(env)
+
+  def HttpSpec = suite("HttpStatusSpec") {
+    testM("200 ok on /") {
+      val response = Client.request(baseAddr)
+
+      assertM(response.map(_.status))(equalTo(Status.OK))
+    } +
+      testM("201 created on /post") {
+        val url      = URL(Path.apply(), URL.Location.Absolute(Scheme.HTTP, addr, port))
+        val endpoint = (Method.POST, url)
+
+        val response = Client.request(Client.ClientParams(endpoint))
+
+        assertM(response.map(_.status))(equalTo(Status.CREATED))
+      } +
+      testM("403 forbidden ok on /auth") {
+        val response = Client.request(s"${baseAddr}/auth")
+
+        assertM(response.map(_.status)) {
+          equalTo(Status.FORBIDDEN)
+        }
+      } +
+      testM("500 internal server error on /boom") {
+        val response = Client.request(s"${baseAddr}/boom")
+
+        assertM(response.map(_.status))(equalTo(Status.INTERNAL_SERVER_ERROR))
+      }
+  }
+
+  Runtime.default.unsafeRun(Server.start(port, (api +++ basicAuthApi)).forkDaemon)
 }
