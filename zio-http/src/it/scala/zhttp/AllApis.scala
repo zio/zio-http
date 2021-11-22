692a8dca0e10f2d4504329d8b39fff03d70d02e7
Shubham Girdhar <girdharshubham@hotmail.com>
2021-11-22 17:21:31 +0530

    test: test cases for more status codes 1xx, 4xx

--- zio-http/src/it/scala/zhttp/AllApis.scala
+++ zio-http/src/it/scala/zhttp/AllApis.scala
@@ -1,24 +1,38 @@
 package zhttp
 
 import zhttp.http._
-import zhttp.http.middleware.HttpMiddleware.basicAuth
+import zhttp.http.middleware.HttpMiddleware.{basicAuth, timeout}
+import zhttp.socket.{Socket, WebSocketFrame}
+import zio.ZIO
+import zio.duration.durationInt
+import zio.stream.ZStream
 
 object AllApis {
-  def api = HttpApp.collect {
-    case Method.GET -> !!          => Response.ok
-    case Method.POST -> !!         => Response.status(Status.CREATED)
-    case Method.GET -> !! / "boom" => Response.status(Status.INTERNAL_SERVER_ERROR)
+
+  def socket = Socket.collect[WebSocketFrame] { case WebSocketFrame.Ping =>
+    ZStream.succeed(WebSocketFrame.pong)
   }
+
+  def api = HttpApp.collect {
+    case Method.GET -> !!                   => Response.ok
+    case Method.POST -> !!                  => Response.status(Status.CREATED)
+    case Method.GET -> !! / "continue"      => Response.ok
+    case Method.GET -> !! / "boom"          => Response.status(Status.INTERNAL_SERVER_ERROR)
+    case Method.GET -> !! / "subscriptions" => Response.socket(socket)
+  }
+
+  def apiM = HttpApp.collectM {
+    case Method.GET -> !! / "timeout"                       => ZIO.sleep(100 seconds).as(Response.ok)
+    case req @ Method.GET -> !! / "contentdecoder" / "text" =>
+      req.getBody(ContentDecoder.text).map { content =>
+        Response(status = Status.OK, data = HttpData.fromText(content))
+      }
+  } @@ timeout(2 seconds)
 
   def basicAuthApi = HttpApp.collect { case Method.GET -> !! / "auth" =>
     Response.ok
   } @@ basicAuth("root", "changeme")
 
-  def contentDecoderApi = HttpApp.collectM { case req @ Method.GET -> !! / "contentdecoder" / "text" =>
-    req.getBody(ContentDecoder.text).map { content =>
-      Response(status = Status.OK, data = HttpData.fromText(content))
-    }
-  }
-  def apply()           = api +++ basicAuthApi +++ contentDecoderApi
+  def apply() = api +++ apiM +++ basicAuthApi
 
 }
--- zio-http/src/it/scala/zhttp/IntegrationSpec.scala
+++ zio-http/src/it/scala/zhttp/IntegrationSpec.scala
@@ -4,9 +4,8 @@ import zhttp.http._
 import zhttp.service._
 import zhttp.service.server._
 import zio._
-import zio.duration.durationInt
-import zio.test.Assertion.equalTo
-import zio.test.TestAspect.{ignore, timeout}
+import zio.test.Assertion._
+import zio.test.TestAspect._
 import zio.test._
 
 object IntegrationSpec extends DefaultRunnableSpec {
@@ -14,18 +13,24 @@ object IntegrationSpec extends DefaultRunnableSpec {
   val port     = 80
   val baseAddr = s"http://${addr}:${port}"
   def env      = EventLoopGroup.auto() ++ ChannelFactory.auto ++ ServerChannelFactory.auto
+  val server   = Server.port(80) ++ Server.app(AllApis()) ++ Server.acceptContinue
 
   def spec = suite("IntegrationSpec")(
     ApiSpec,
     ContentDecoderSpec,
-  ).provideCustomLayer(env) @@ timeout(10 seconds)
+  ).provideCustomLayer(env)
 
   def ApiSpec = suite("ApiSpec") {
-    testM("200 ok on /") {
-      val response = Client.request(baseAddr)
+    testM("100 continue on /continue") {
+      val response = Client.request(s"${baseAddr}/continue", List(Header("Expect", "100-continue")))
 
-      assertM(response.map(_.status))(equalTo(Status.OK))
+      assertM(response.map(_.status))(equalTo(Status.CONTINUE))
     } +
+      testM("200 ok on /") {
+        val response = Client.request(baseAddr)
+
+        assertM(response.map(_.status))(equalTo(Status.OK))
+      } +
       testM("201 created on /post") {
         val url      = URL(Path.apply(), URL.Location.Absolute(Scheme.HTTP, addr, port))
         val endpoint = (Method.POST, url)
@@ -33,6 +38,15 @@ object IntegrationSpec extends DefaultRunnableSpec {
         val response = Client.request(Client.ClientParams(endpoint))
 
         assertM(response.map(_.status))(equalTo(Status.CREATED))
+      } +
+      testM("400 bad request on /subscriptions without the connection upgrade header") {
+        val response = Client.request(s"${baseAddr}/subscriptions")
+
+        assertM(response.map(_.status))(equalTo(Status.BAD_REQUEST))
+      } + testM("408 timeout on /timeout") {
+        val response = Client.request(s"${baseAddr}/timeout")
+
+        assertM(response.map(_.status))(equalTo(Status.REQUEST_TIMEOUT))
       } +
       testM("500 internal server error on /boom") {
         val response = Client.request(s"${baseAddr}/boom")
@@ -59,5 +73,5 @@ object IntegrationSpec extends DefaultRunnableSpec {
     } @@ ignore
   }
 
-  Runtime.default.unsafeRun(Server.start(port, AllApis()).forkDaemon)
+  Runtime.default.unsafeRun(server.make.useForever.provideSomeLayer(env ++ zio.clock.Clock.live).forkDaemon)
 }
