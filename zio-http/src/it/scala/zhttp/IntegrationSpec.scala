2880c1f922e811232cc96f4321355497102848e1
Shubham Girdhar <girdharshubham@hotmail.com>
2021-11-22 17:30:50 +0530

    refactor: newlines remnoved

--- zio-http/src/it/scala/zhttp/IntegrationSpec.scala
+++ zio-http/src/it/scala/zhttp/IntegrationSpec.scala
@@ -12,8 +12,8 @@ object IntegrationSpec extends DefaultRunnableSpec {
   val addr     = "localhost"
   val port     = 80
   val baseAddr = s"http://${addr}:${port}"
-  def env      = EventLoopGroup.auto() ++ ChannelFactory.auto ++ ServerChannelFactory.auto
   val server   = Server.port(80) ++ Server.app(AllApis()) ++ Server.acceptContinue
+  def env      = EventLoopGroup.auto() ++ ChannelFactory.auto ++ ServerChannelFactory.auto
 
   def spec = suite("IntegrationSpec")(
     ApiSpec,
@@ -23,34 +23,27 @@ object IntegrationSpec extends DefaultRunnableSpec {
   def ApiSpec = suite("ApiSpec") {
     testM("100 continue on /continue") {
       val response = Client.request(s"${baseAddr}/continue", List(Header("Expect", "100-continue")))
-
       assertM(response.map(_.status))(equalTo(Status.CONTINUE))
     } +
       testM("200 ok on /") {
         val response = Client.request(baseAddr)
-
         assertM(response.map(_.status))(equalTo(Status.OK))
       } +
       testM("201 created on /post") {
         val url      = URL(Path.apply(), URL.Location.Absolute(Scheme.HTTP, addr, port))
-        val endpoint = (Method.POST, url)
-
-        val response = Client.request(Client.ClientParams(endpoint))
-
+        val response = Client.request(Client.ClientParams((Method.POST, url)))
         assertM(response.map(_.status))(equalTo(Status.CREATED))
       } +
       testM("400 bad request on /subscriptions without the connection upgrade header") {
         val response = Client.request(s"${baseAddr}/subscriptions")
-
         assertM(response.map(_.status))(equalTo(Status.BAD_REQUEST))
-      } + testM("408 timeout on /timeout") {
+      } +
+      testM("408 timeout on /timeout") {
         val response = Client.request(s"${baseAddr}/timeout")
-
         assertM(response.map(_.status))(equalTo(Status.REQUEST_TIMEOUT))
       } +
       testM("500 internal server error on /boom") {
         val response = Client.request(s"${baseAddr}/boom")
-
         assertM(response.map(_.status))(equalTo(Status.INTERNAL_SERVER_ERROR))
       }
   }
@@ -58,17 +51,13 @@ object IntegrationSpec extends DefaultRunnableSpec {
   def AuthSpec = suite("AuthSpec") {
     testM("403 forbidden ok on /auth") {
       val response = Client.request(s"${baseAddr}/auth")
-
-      assertM(response.map(_.status)) {
-        equalTo(Status.FORBIDDEN)
-      }
+      assertM(response.map(_.status))(equalTo(Status.FORBIDDEN))
     }
   }
 
   def ContentDecoderSpec = suite("ContentDecoderSpec") {
     testM("ContentDecoder text identity") {
       val response = Client.request(s"${baseAddr}/contentdecoder/text")
-
       assertM(response.map(_.status))(equalTo(Status.OK))
     } @@ ignore
   }
