b824e296d6388024d4ad980cff11f7338747df30
Dino John <dinopanali@gmail.com>
2021-11-22 13:36:31 +0530

    refactor: updated optional sslHandler

--- zio-http/src/main/scala/zhttp/service/server/OptionalSSLHandler.scala
+++ zio-http/src/main/scala/zhttp/service/server/OptionalSSLHandler.scala
@@ -1,33 +1,48 @@
 package zhttp.service.server
 
 import io.netty.buffer.ByteBuf
-import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.{ChannelHandler, ChannelHandlerContext}
 import io.netty.handler.codec.ByteToMessageDecoder
+import io.netty.handler.codec.http.{HttpServerCodec, HttpServerExpectContinueHandler}
 import io.netty.handler.ssl.{SslContext, SslHandler}
+import zhttp.service.Server.Settings
 import zhttp.service._
+import zhttp.service.server.ServerChannelInitializerUtil.configureClearText
 import zhttp.service.server.ServerSSLHandler.SSLHttpBehaviour
 
 import java.util
 
-class OptionalSSLHandler(sslContext: SslContext, httpBehaviour: SSLHttpBehaviour, cfg: Server.Config[_, _])
-    extends ByteToMessageDecoder {
+class OptionalSSLHandler[R](
+                             httpH: ChannelHandler,
+                             http2H: ChannelHandler,
+                             sslContext: SslContext,
+                             settings: Settings[R, Throwable],
+                           ) extends ByteToMessageDecoder {
   override def decode(context: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]): Unit = {
-    val pipeline = context.channel().pipeline()
     if (in.readableBytes < 5)
       ()
     else if (SslHandler.isEncrypted(in)) {
-      pipeline.replace(this, SSL_HANDLER, sslContext.newHandler(context.alloc()))
+      context.pipeline().replace(this, SSL_HANDLER, sslContext.newHandler(context.alloc()))
       ()
     } else {
-      httpBehaviour match {
+      settings.sslOption.httpBehaviour match {
         case SSLHttpBehaviour.Accept =>
-          pipeline.remove(this)
+          context.channel().pipeline().remove(HTTP2_OR_HTTP_SERVER_HANDLER)
+          configureClearText(httpH, http2H, context.channel(), settings)
+          context.channel().pipeline().remove(this)
           ()
         case _                       =>
-          pipeline.remove(HTTP_REQUEST_HANDLER)
-          if (cfg.keepAlive) pipeline.remove(HTTP_KEEPALIVE_HANDLER)
-          pipeline.remove(this)
-          pipeline.addLast(new HttpOnHttpsHandler(httpBehaviour))
+          context.channel().pipeline().remove(HTTP2_OR_HTTP_SERVER_HANDLER)
+          context.channel().pipeline().replace(this, HTTP_SERVER_CODEC, new HttpServerCodec)
+          context
+            .channel()
+            .pipeline()
+            .addLast(HTTP_ON_HTTPS_HANDLER, new HttpOnHttpsHandler(settings.sslOption.httpBehaviour))
+          if (settings.acceptContinue)
+            context
+              .channel()
+              .pipeline()
+              .addAfter(HTTP_SERVER_CODEC, HTTP_SERVER_EXPECT_CONTINUE, new HttpServerExpectContinueHandler())
           ()
       }
     }
