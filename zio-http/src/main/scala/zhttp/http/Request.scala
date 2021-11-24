3caf0235d00bdd58d9fce8f2738a88c37220377d
Dino John <dinopanali@gmail.com>
2021-11-24 16:56:18 +0530

    refactor: removed contentDecoder from Request, made it a final case class

--- zio-http/src/main/scala/zhttp/http/Request.scala
+++ zio-http/src/main/scala/zhttp/http/Request.scala
@@ -1,45 +1,31 @@
 package zhttp.http
 
-import zio.{Chunk, ZIO}
+import io.netty.channel.ChannelHandlerContext
 
-import java.net.InetAddress
+import java.net.{InetAddress, InetSocketAddress}
 
-trait Request extends HeaderExtension[Request] { self =>
-  def copy(method: Method = self.method, url: URL = self.url, headers: List[Header] = self.getHeaders): Request = {
-    val m = method
-    val u = url
-    val h = headers
-    new Request {
-      override def method: Method = m
-
-      override def url: URL = u
-
-      override def getHeaders: List[Header] = h
-
-      override def remoteAddress: Option[InetAddress] =
-        self.remoteAddress
-
-      override def getBody[R, B](
-        decoder: ContentDecoder[R, Throwable, Chunk[Byte], B],
-      ): ZIO[R, Throwable, B] =
-        self.getBody(decoder)
-    }
-  }
+final case class Request(method: Method, url: URL, headers: List[Header], data: HttpData[Any, Nothing], private val channelContext: ChannelHandlerContext = null) extends HeaderExtension[Request] { self =>
 
   /**
    * Decodes the content of the request using the provided ContentDecoder
    */
-  def getBody[R, B](decoder: ContentDecoder[R, Throwable, Chunk[Byte], B]): ZIO[R, Throwable, B]
+  def getBody: HttpData[Any,Nothing] = data
 
   /**
    * Decodes the content of request as string
    */
-  def getBodyAsString: ZIO[Any, Throwable, String] = getBody(ContentDecoder.text)
+  def getBodyAsString: Option[String] = data match {
+    case HttpData.Text(text, _) => Option(text)
+    case HttpData.BinaryChunk(data) => Option(new String(data.toArray, getCharset.getOrElse(HTTP_CHARSET)))
+    case HttpData.BinaryByteBuf(data) => Option(data.toString(getCharset.getOrElse(HTTP_CHARSET)))
+    case HttpData.BinaryStream(_) =>None
+    case HttpData.Empty => None
+  }
 
   /**
    * Gets all the headers in the Request
    */
-  def getHeaders: List[Header]
+  def getHeaders: List[Header] = headers
 
   /**
    * Checks is the request is a pre-flight request or not
@@ -47,93 +33,47 @@ trait Request extends HeaderExtension[Request] { self =>
   def isPreflight: Boolean = method == Method.OPTIONS
 
   /**
-   * Gets the request's method
-   */
-  def method: Method
-
-  /**
    * Gets the request's path
    */
   def path: Path = url.path
 
-  /**
-   * Gets the remote address if available
-   */
-  def remoteAddress: Option[InetAddress]
-
-  /**
-   * Gets the complete url
-   */
-  def url: URL
 
   /**
    * Updates the headers using the provided function
    */
   final override def updateHeaders(f: List[Header] => List[Header]): Request = self.copy(headers = f(self.getHeaders))
+
+  /**
+   * Gets the remote address if available
+   */
+  def remoteAddress: Option[InetAddress] = {
+    if (channelContext != null && channelContext.channel().remoteAddress().isInstanceOf[InetSocketAddress])
+      Some(channelContext.channel().remoteAddress().asInstanceOf[InetSocketAddress].getAddress)
+    else
+      None
+  }
 }
 
 object Request {
 
-  /**
-   * Constructor for Request
-   */
-  def apply(
-    method: Method = Method.GET,
-    url: URL = URL.root,
-    headers: List[Header] = Nil,
-    remoteAddress: Option[InetAddress] = None,
-    data: HttpData[Any, Throwable] = HttpData.Empty,
-  ): Request = {
-    val m  = method
-    val u  = url
-    val h  = headers
-    val ra = remoteAddress
-    new Request {
-      override def method: Method                     = m
-      override def url: URL                           = u
-      override def getHeaders: List[Header]           = h
-      override def remoteAddress: Option[InetAddress] = ra
-      override def getBody[R, B](
-        decoder: ContentDecoder[R, Throwable, Chunk[Byte], B],
-      ): ZIO[R, Throwable, B] =
-        decoder.decode(data, method, url, headers)
-    }
-  }
-
-  /**
-   * Effectfully create a new Request object
-   */
-  def make[R, E <: Throwable](
-    method: Method = Method.GET,
-    url: URL = URL.root,
-    headers: List[Header] = Nil,
-    remoteAddress: Option[InetAddress],
-    content: HttpData[R, E] = HttpData.empty,
-  ): ZIO[R, Nothing, Request] =
-    for {
-      r <- ZIO.environment[R]
-      c = content.provide(r)
-    } yield Request(method, url, headers, remoteAddress, c)
 
   /**
    * Lift request to TypedRequest with option to extract params
    */
-  final class ParameterizedRequest[A](req: Request, val params: A) extends Request {
-    override def getBody[R, B](
-      decoder: ContentDecoder[R, Throwable, Chunk[Byte], B],
-    ): ZIO[R, Throwable, B] =
-      req.getBody(decoder)
+  final class ParameterizedRequest[A](req: Request, val params: A) {
+     def getBody[R, B]: HttpData[Any,Nothing] = req.getBody
 
-    override def getHeaders: List[Header] = req.getHeaders
+     def getHeaders: List[Header] = req.getHeaders
 
-    override def method: Method = req.method
+     def method: Method = req.method
 
-    override def remoteAddress: Option[InetAddress] = req.remoteAddress
+     def remoteAddress: Option[InetAddress] = req.remoteAddress
 
-    override def url: URL = req.url
+     def url: URL = req.url
   }
 
   object ParameterizedRequest {
     def apply[A](req: Request, params: A): ParameterizedRequest[A] = new ParameterizedRequest(req, params)
   }
+
 }
