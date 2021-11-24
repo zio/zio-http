705a5fd4af3c71181ebd41bdf080ab2760fbfe86
shrutiverma97 <shruti@dream11.com>
2021-11-24 16:09:23 +0530

    feat(url): added url

--- zio-http/src/test/scala/zhttp/http/URLSpec.scala
+++ zio-http/src/test/scala/zhttp/http/URLSpec.scala
@@ -9,23 +9,25 @@ object URLSpec extends DefaultRunnableSpec {
   val fromStringSpec = suite("fromString")(
     test("Should Handle invalid url String with restricted chars") {
       assert(URL.fromString("http://mw1.google.com/$[level]/r$[y]_c$[x].jpg"))(isLeft)
-    },
-    test("Should Handle empty query string") {
-      assert(URL.fromString("http://yourdomain.com/list/users").map(_.queryParams))(
-        isRight(equalTo(Map.empty[String, List[String]])),
-      )
-    },
-    test("Should Handle query string") {
-      assert(
-        URL
-          .fromString("http://yourdomain.com/list/users?user_id=1&user_id=2&order=ASC&text=zio-http%20is%20awesome%21")
-          .map(_.queryParams),
-      )(
-        isRight(
-          equalTo(Map("user_id" -> List("1", "2"), "order" -> List("ASC"), "text" -> List("zio-http is awesome!"))),
-        ),
-      )
-    },
+    } +
+      test("Should Handle empty query string") {
+        assert(URL.fromString("http://yourdomain.com/list/users").map(_.queryParams))(
+          isRight(equalTo(Map.empty[String, List[String]])),
+        )
+      } +
+      test("Should Handle query string") {
+        assert(
+          URL
+            .fromString(
+              "http://yourdomain.com/list/users?user_id=1&user_id=2&order=ASC&text=zio-http%20is%20awesome%21",
+            )
+            .map(_.queryParams),
+        )(
+          isRight(
+            equalTo(Map("user_id" -> List("1", "2"), "order" -> List("ASC"), "text" -> List("zio-http is awesome!"))),
+          ),
+        )
+      },
     test("Should handle uri fragment") {
       assert(
         URL
@@ -49,16 +51,16 @@ object URLSpec extends DefaultRunnableSpec {
     suite("asString")(
       test("relative with pathname only") {
         roundtrip("/users")
-      },
-      test("relative with query string") {
-        roundtrip("/users?user_id=1&user_id=2&order=ASC&text=zio-http%20is%20awesome%21")
-      },
-      test("absolute with pathname only") {
-        roundtrip("http://yourdomain.com/list")
-      },
-      test("absolute with query string") {
-        roundtrip("http://yourdomain.com/list/users?user_id=1&user_id=2&order=ASC&text=zio-http%20is%20awesome%21")
-      },
+      } +
+        test("relative with query string") {
+          roundtrip("/users?user_id=1&user_id=2&order=ASC&text=zio-http%20is%20awesome%21")
+        } +
+        test("absolute with pathname only") {
+          roundtrip("http://yourdomain.com/list")
+        } +
+        test("absolute with query string") {
+          roundtrip("http://yourdomain.com/list/users?user_id=1&user_id=2&order=ASC&text=zio-http%20is%20awesome%21")
+        },
       test("absolute with fragment") {
         roundtrip("http://yourdomain.com/list/users#the%20hash")
       },
