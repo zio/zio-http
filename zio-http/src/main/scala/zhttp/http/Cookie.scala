802900acef56a625bacaeaeb4e15a65efff8b847
shrutiverma97 <shruti@dream11.com>
2021-11-24 16:29:11 +0530

    feat(cookie): added cookie changes

---
+++ zio-http/src/main/scala/zhttp/http/Cookie.scala
@@ -0,0 +1,242 @@
+package zhttp.http
+
+import zio.duration._
+
+import java.time.Instant
+import scala.util.{Failure, Success, Try}
+
+final case class Cookie(
+  name: String,
+  content: String,
+  expires: Option[Instant] = None,
+  domain: Option[String] = None,
+  path: Option[Path] = None,
+  isSecure: Boolean = false,
+  isHttpOnly: Boolean = false,
+  maxAge: Option[Long] = None,
+  sameSite: Option[Cookie.SameSite] = None,
+) { self =>
+
+  /**
+   * Helper method to create cookies
+   */
+  def @@(update: Cookie.Update): Cookie = update.f(self)
+
+  /**
+   * Creates a new cookie that can be used to clear the original cookie on the client.
+   */
+  def clear: Cookie =
+    copy(content = "", expires = Some(Instant.ofEpochSecond(0)))
+
+  /**
+   * Sets content in cookie
+   */
+  def withContent(v: String): Cookie = copy(content = v)
+
+  /**
+   * Sets expiry in cookie
+   */
+  def withExpiry(v: Instant): Cookie = copy(expires = Some(v))
+
+  /**
+   * Sets max-age in cookie
+   */
+  def withMaxAge(v: Duration): Cookie = copy(maxAge = Some(v.asScala.toSeconds))
+
+  /**
+   * Sets max-age in seconds in cookie
+   */
+  def withMaxAge(v: Long): Cookie = copy(maxAge = Some(v))
+
+  /**
+   * Sets domain in cookie
+   */
+  def withDomain(v: String): Cookie = copy(domain = Some(v))
+
+  /**
+   * Sets path in cookie
+   */
+  def withPath(v: Path): Cookie = copy(path = Some(v))
+
+  /**
+   * Sets secure in cookie
+   */
+  def withSecure: Cookie = copy(isSecure = true)
+
+  /**
+   * Sets httpOnly in cookie
+   */
+  def withHttpOnly: Cookie = copy(isHttpOnly = true)
+
+  /**
+   * Sets same-site in cookie
+   */
+  def withSameSite(v: Cookie.SameSite): Cookie = copy(sameSite = Some(v))
+
+  /**
+   * Resets secure flag in the cookie
+   */
+  def withoutSecure: Cookie = copy(isSecure = false)
+
+  /**
+   * Resets httpOnly flag in the cookie
+   */
+  def withoutHttpOnly: Cookie = copy(isHttpOnly = false)
+
+  /**
+   * Removes expiry from the cookie
+   */
+  def withoutExpiry: Cookie = copy(expires = None)
+
+  /**
+   * Removes domain from the cookie
+   */
+  def withoutDomain: Cookie = copy(domain = None)
+
+  /**
+   * Removes path from the cookie
+   */
+  def withoutPath: Cookie = copy(path = None)
+
+  /**
+   * Removes max-age from the cookie
+   */
+  def withoutMaxAge: Cookie = copy(maxAge = None)
+
+  /**
+   * Removes same-site from the cookie
+   */
+  def withoutSameSite: Cookie = copy(sameSite = None)
+
+  /**
+   * Converts cookie into a string
+   */
+  def encode: String = {
+    val cookie = List(
+      Some(s"$name=$content"),
+      expires.map(e => s"Expires=$e"),
+      maxAge.map(a => s"Max-Age=${a.toString}"),
+      domain.map(d => s"Domain=$d"),
+      path.map(p => s"Path=${p.asString}"),
+      if (isSecure) Some("Secure") else None,
+      if (isHttpOnly) Some("HttpOnly") else None,
+      sameSite.map(s => s"SameSite=${s.asString}"),
+    )
+    cookie.flatten.mkString("; ")
+  }
+
+}
+
+object Cookie {
+
+  sealed trait SameSite {
+    def asString: String
+  }
+  object SameSite       {
+    case object Lax    extends SameSite { def asString = "Lax"    }
+    case object Strict extends SameSite { def asString = "Strict" }
+    case object None   extends SameSite { def asString = "None"   }
+  }
+  case class Update(f: Cookie => Cookie)
+
+  /**
+   * Decodes from Set-Cookie header value inside of Response into a cookie
+   */
+  def decodeResponseCookie(headerValue: String): Either[Throwable, Cookie] = {
+    val cookieWithoutMeta = headerValue.split(";").map(_.trim)
+    val (first, other)    = (cookieWithoutMeta.head, cookieWithoutMeta.tail)
+    val (name, content)   = splitNameContent(first)
+    var cookie            =
+      if (name.trim == "" && content.isEmpty) Left(new IllegalArgumentException("Cookie can't be parsed"))
+      else Right(Cookie(name, content.getOrElse("")))
+
+    other.map(splitNameContent).map(t => (t._1.toLowerCase, t._2)).foreach {
+      case ("expires", Some(v))  =>
+        parseDate(v) match {
+          case Left(_)      => cookie = Left(new IllegalArgumentException("expiry date cannot be parsed"))
+          case Right(value) => cookie = cookie.map(_.withExpiry(value))
+        }
+      case ("max-age", Some(v))  =>
+        Try(v.toLong) match {
+          case Success(age) => cookie = cookie.map(x => x.withMaxAge(age))
+          case Failure(_)   => cookie = Left(new IllegalArgumentException("max-age cannot be parsed"))
+        }
+      case ("domain", v)         => cookie = cookie.map(_.withDomain(v.getOrElse("")))
+      case ("path", v)           => cookie = cookie.map(_.withPath(Path(v.getOrElse(""))))
+      case ("secure", _)         => cookie = cookie.map(_.withSecure)
+      case ("httponly", _)       => cookie = cookie.map(_.withHttpOnly)
+      case ("samesite", Some(v)) =>
+        v.trim.toLowerCase match {
+          case "lax"    => cookie = cookie.map(_.withSameSite(SameSite.Lax))
+          case "strict" => cookie = cookie.map(_.withSameSite(SameSite.Strict))
+          case "none"   => cookie = cookie.map(_.withSameSite(SameSite.None))
+          case _        => None
+        }
+      case (_, _)                => cookie
+    }
+    cookie
+  }
+
+  /**
+   * Decodes from `Cookie` header value inside of Request into a cookie
+   */
+  def decodeRequestCookie(headerValue: String): Either[Throwable, List[Cookie]] = {
+    val cookies: Array[String]  = headerValue.split(";").map(_.trim)
+    val x: List[Option[Cookie]] = cookies.toList.map(a => {
+      val (name, content) = splitNameContent(a)
+      if (name.trim == "" && content.isEmpty) None
+      else Some(Cookie(name, content.getOrElse("")))
+    })
+    if (x.contains(None))
+      Left(new IllegalArgumentException("Cookie can't be parsed"))
+    else Right(x.map(_.get))
+  }
+
+  private def parseDate(v: String): Either[String, Instant] =
+    Try(Instant.parse(v)) match {
+      case Success(r) => Right(r)
+      case Failure(e) => Left(s"Invalid http date: $v (${e.getMessage})")
+    }
+
+  private def splitNameContent(kv: String): (String, Option[String]) =
+    kv.split("=", 2).map(_.trim) match {
+      case Array(v1)     => (v1, None)
+      case Array(v1, v2) => (v1, Some(v2))
+      case _             => ("", None)
+    }
+
+  /**
+   * Updates maxAge in cookie
+   */
+  def maxAge(maxAge: Duration): Update = Update(_.withMaxAge(maxAge))
+
+  /**
+   * Updates domain in cookie
+   */
+  def domain(domain: String): Update = Update(_.withDomain(domain))
+
+  /**
+   * Updates expiry in cookie
+   */
+  def expiry(expires: Instant): Update = Update(_.withExpiry(expires))
+
+  /**
+   * Updates path in cookie
+   */
+  def path(path: Path): Update = Update(_.withPath(path))
+
+  /**
+   * Updates secure in cookie
+   */
+  def secure: Update = Update(_.withSecure)
+
+  /**
+   * Updates httpOnly in cookie
+   */
+  def httpOnly: Update = Update(_.withHttpOnly)
+
+  /**
+   * Updates sameSite in cookie
+   */
+  def sameSite(sameSite: SameSite): Update = Update(_.withSameSite(sameSite))
+}
---
+++ zio-http/src/test/scala/zhttp/internal/HttpGen.scala
@@ -0,0 +1,140 @@
+package zhttp.internal
+
+import io.netty.buffer.Unpooled
+import zhttp.http._
+import zio.Chunk
+import zio.random.Random
+import zio.stream.ZStream
+import zio.test.{Gen, Sized}
+
+object HttpGen {
+  val status: Gen[Any, Status] = Gen.fromIterable(
+    List(
+      Status.CONTINUE,
+      Status.SWITCHING_PROTOCOLS,
+      Status.PROCESSING,
+      Status.OK,
+      Status.CREATED,
+      Status.ACCEPTED,
+      Status.NON_AUTHORITATIVE_INFORMATION,
+      Status.NO_CONTENT,
+      Status.RESET_CONTENT,
+      Status.PARTIAL_CONTENT,
+      Status.MULTI_STATUS,
+      Status.MULTIPLE_CHOICES,
+      Status.MOVED_PERMANENTLY,
+      Status.FOUND,
+      Status.SEE_OTHER,
+      Status.NOT_MODIFIED,
+      Status.USE_PROXY,
+      Status.TEMPORARY_REDIRECT,
+      Status.PERMANENT_REDIRECT,
+      Status.BAD_REQUEST,
+      Status.UNAUTHORIZED,
+      Status.PAYMENT_REQUIRED,
+      Status.FORBIDDEN,
+      Status.NOT_FOUND,
+      Status.METHOD_NOT_ALLOWED,
+      Status.NOT_ACCEPTABLE,
+      Status.PROXY_AUTHENTICATION_REQUIRED,
+      Status.REQUEST_TIMEOUT,
+      Status.CONFLICT,
+      Status.GONE,
+      Status.LENGTH_REQUIRED,
+      Status.PRECONDITION_FAILED,
+      Status.REQUEST_ENTITY_TOO_LARGE,
+      Status.REQUEST_URI_TOO_LONG,
+      Status.UNSUPPORTED_MEDIA_TYPE,
+      Status.REQUESTED_RANGE_NOT_SATISFIABLE,
+      Status.EXPECTATION_FAILED,
+      Status.MISDIRECTED_REQUEST,
+      Status.UNPROCESSABLE_ENTITY,
+      Status.LOCKED,
+      Status.FAILED_DEPENDENCY,
+      Status.UNORDERED_COLLECTION,
+      Status.UPGRADE_REQUIRED,
+      Status.PRECONDITION_REQUIRED,
+      Status.TOO_MANY_REQUESTS,
+      Status.REQUEST_HEADER_FIELDS_TOO_LARGE,
+      Status.INTERNAL_SERVER_ERROR,
+      Status.NOT_IMPLEMENTED,
+      Status.BAD_GATEWAY,
+      Status.SERVICE_UNAVAILABLE,
+      Status.GATEWAY_TIMEOUT,
+      Status.HTTP_VERSION_NOT_SUPPORTED,
+      Status.VARIANT_ALSO_NEGOTIATES,
+      Status.INSUFFICIENT_STORAGE,
+      Status.NOT_EXTENDED,
+      Status.NETWORK_AUTHENTICATION_REQUIRED,
+    ),
+  )
+
+  val method: Gen[Any, Method] = Gen.fromIterable(
+    List(
+      Method.OPTIONS,
+      Method.GET,
+      Method.HEAD,
+      Method.POST,
+      Method.PUT,
+      Method.PATCH,
+      Method.DELETE,
+      Method.TRACE,
+      Method.CONNECT,
+    ),
+  )
+
+  def httpData[R](gen: Gen[R, List[String]]): Gen[R, HttpData[Any, Nothing]] =
+    for {
+      list <- gen
+      cnt  <- Gen
+        .fromIterable(
+          List(
+            HttpData.fromStream(ZStream.fromIterable(list).map(b => Chunk.fromArray(b.getBytes())).flattenChunks),
+            HttpData.fromText(list.mkString("")),
+            HttpData.fromChunk(Chunk.fromArray(list.mkString("").getBytes())),
+            HttpData.fromByteBuf(Unpooled.copiedBuffer(list.mkString(""), HTTP_CHARSET)),
+            HttpData.empty,
+          ),
+        )
+    } yield cnt
+
+  def nonEmptyHttpData[R](gen: Gen[R, List[String]]): Gen[R, HttpData[Any, Nothing]] =
+    for {
+      list <- gen
+      cnt  <- Gen
+        .fromIterable(
+          List(
+            HttpData.fromStream(ZStream.fromIterable(list).map(b => Chunk.fromArray(b.getBytes())).flattenChunks),
+            HttpData.fromText(list.mkString("")),
+            HttpData.fromChunk(Chunk.fromArray(list.mkString("").getBytes())),
+            HttpData.fromByteBuf(Unpooled.copiedBuffer(list.mkString(""), HTTP_CHARSET)),
+          ),
+        )
+    } yield cnt
+
+  def header: Gen[Random with Sized, Header] = for {
+    key   <- Gen.alphaNumericStringBounded(1, 4)
+    value <- Gen.alphaNumericStringBounded(1, 4)
+  } yield Header(key, value)
+
+  def cookies: Gen[Random with Sized, Cookie] = for {
+    name     <- Gen.anyString
+    content  <- Gen.anyString
+    expires  <- Gen.option(Gen.anyInstant)
+    domain   <- Gen.option(Gen.anyString)
+    path     <- Gen.option(path)
+    secure   <- Gen.boolean
+    httpOnly <- Gen.boolean
+    maxAge   <- Gen.option(Gen.anyLong)
+    sameSite <- Gen.option(Gen.fromIterable(List(Cookie.SameSite.Strict, Cookie.SameSite.Lax)))
+  } yield Cookie(name, content, expires, domain, path, secure, httpOnly, maxAge, sameSite)
+
+  def path: Gen[Random with Sized, Path] = {
+    for {
+      l <- Gen.listOf(Gen.alphaNumericString)
+      p <- Gen.const(Path(l))
+    } yield p
+  }
+
+  def urlGen: Gen[Random with Sized, URL] = path.map(URL(_))
+}
