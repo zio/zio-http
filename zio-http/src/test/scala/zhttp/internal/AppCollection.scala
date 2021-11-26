378c57baf3852dcf921024d2b97cea277d2b2719
Tushar Mathur <tusharmath@gmail.com>
2021-11-27 00:04:57 +0530

    scala fmt on app collection

--- zio-http/src/test/scala/zhttp/internal/AppCollection.scala
+++ zio-http/src/test/scala/zhttp/internal/AppCollection.scala
@@ -3,40 +3,13 @@ package zhttp.internal
 import zhttp.http._
 
 import java.util.UUID
-import zio.{console, Ref, UIO, ZIO, ZLayer}
+import zio.{Ref, UIO, ZIO, ZLayer}
 import zio.console.Console
 
 object AppCollection {
 
-  sealed trait Service {
-    def add(app: HttpApp[HttpEnv, Throwable]): UIO[Id]
-    def get(id: Id): UIO[Option[HttpApp[HttpEnv, Throwable]]]
-  }
-
-  type Id          = String
-  type HttpEnv     = HttpAppCollection with Console
-  type HttpAppTest = HttpApp[HttpEnv, Throwable]
-
-  final class Live(ref: Ref[Map[Id, HttpApp[HttpEnv, Throwable]]]) extends Service {
-    def add(app: HttpApp[HttpEnv, Throwable]): UIO[Id]        = for {
-      id <- UIO(UUID.randomUUID().toString)
-      _  <- ref.update(map => map + (id -> app))
-    } yield id
-    def get(id: Id): UIO[Option[HttpApp[HttpEnv, Throwable]]] = ref.get.map(_.get(id))
-  }
-
-  def live: ZLayer[Any, Nothing, HttpAppCollection] = Ref
-    .make(Map.empty[Id, HttpApp[HttpEnv, Throwable]])
-    .map(ref => new Live(ref))
-    .toLayer
-
   def add(app: HttpApp[HttpEnv, Throwable]): ZIO[HttpAppCollection, Nothing, String] =
     ZIO.accessM[HttpAppCollection](_.get.add(app))
-
-  def get(id: Id): ZIO[HttpAppCollection, Nothing, Option[HttpApp[HttpEnv, Throwable]]] =
-    ZIO.accessM[HttpAppCollection](_.get.get(id))
-
-  val APP_ID = "X-APP_ID"
 
   def app: HttpApp[HttpEnv, Throwable] = Http
     .fromPartialFunction[Request] { case req =>
@@ -52,4 +25,30 @@ object AppCollection {
         }
       } yield res
     }
+
+  def get(id: Id): ZIO[HttpAppCollection, Nothing, Option[HttpApp[HttpEnv, Throwable]]] =
+    ZIO.accessM[HttpAppCollection](_.get.get(id))
+
+  def live: ZLayer[Any, Nothing, HttpAppCollection] = Ref
+    .make(Map.empty[Id, HttpApp[HttpEnv, Throwable]])
+    .map(ref => new Live(ref))
+    .toLayer
+
+  type Id          = String
+  type HttpEnv     = HttpAppCollection with Console
+  type HttpAppTest = HttpApp[HttpEnv, Throwable]
+  val APP_ID = "X-APP_ID"
+
+  sealed trait Service {
+    def add(app: HttpApp[HttpEnv, Throwable]): UIO[Id]
+    def get(id: Id): UIO[Option[HttpApp[HttpEnv, Throwable]]]
+  }
+
+  final class Live(ref: Ref[Map[Id, HttpApp[HttpEnv, Throwable]]]) extends Service {
+    def add(app: HttpApp[HttpEnv, Throwable]): UIO[Id]        = for {
+      id <- UIO(UUID.randomUUID().toString)
+      _  <- ref.update(map => map + (id -> app))
+    } yield id
+    def get(id: Id): UIO[Option[HttpApp[HttpEnv, Throwable]]] = ref.get.map(_.get(id))
+  }
 }
