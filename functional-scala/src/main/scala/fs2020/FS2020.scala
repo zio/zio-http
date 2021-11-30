406accea6c2757d692fe0d481f80eeaea483d301
Tushar Mathur <tusharmath@gmail.com>
2021-11-30 06:41:14 +0000

    stylize fs template

--- functional-scala/src/main/scala/fs2020/FS2020.scala
+++ functional-scala/src/main/scala/fs2020/FS2020.scala
@@ -1,6 +1,12 @@
 package fs2020
 
-import zio.{App, ExitCode, URIO}
+import zio._
+
+//    _______ ___    _  _ _   _
+//   |_  /_ _/ _ \  | || | |_| |_ _ __
+//    / / | | (_) | | __ |  _|  _| '_ \
+//   /___|___\___/  |_||_|\__|\__| .__/
+//                               |_|
 
 object FS2020 extends App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = ???
