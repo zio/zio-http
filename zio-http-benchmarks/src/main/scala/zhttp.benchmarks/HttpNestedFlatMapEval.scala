d586474e9743947b02f9a759968372af5b99a19d
Tushar Mathur <tusharmath@gmail.com>
2021-12-14 04:09:58 +0530

    chore: reduce benchmark stack-size for HttpNestedFlatMapEval

--- zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpNestedFlatMapEval.scala
+++ zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpNestedFlatMapEval.scala
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit
 @OutputTimeUnit(TimeUnit.SECONDS)
 class HttpNestedFlatMapEval {
 
-  private val MAX = 10000
+  private val MAX = 1000
 
   val programFlatMap: Http[Any, Nothing, Int, Int] =
     (0 to MAX).foldLeft(Http.identity[Int])((a, _) => a.flatMap(i => Http.succeed(i + 1)))
