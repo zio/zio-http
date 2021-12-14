f3207d40ca452ebc98f965d2e66cff39f188437e
Scala Steward <me@scala-steward.org>
2021-12-14 12:23:57 +0100

    Update netty-incubator-transport-native-io_uring to 0.0.11.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val JwtCoreVersion                = "9.0.2"
   val NettyVersion                  = "4.1.71.Final"
-  val NettyIncubatorVersion         = "0.0.10.Final"
+  val NettyIncubatorVersion         = "0.0.11.Final"
   val ScalaCompactCollectionVersion = "2.6.0"
   val ZioVersion                    = "1.0.12"
 
