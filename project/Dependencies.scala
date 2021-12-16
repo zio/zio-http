493580394c24e33664c0df9cde434f05b3367bec
Amit Kumar Singh <amit.singh@dream11.com>
2021-12-16 16:19:58 +0530

    Update zio, zio-streams, zio-test, ... to 1.0.13 (#637)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val NettyVersion                  = "4.1.71.Final"
   val NettyIncubatorVersion         = "0.0.11.Final"
   val ScalaCompactCollectionVersion = "2.6.0"
-  val ZioVersion                    = "1.0.12"
+  val ZioVersion                    = "1.0.13"
 
   val `scala-compact-collection` = "org.scala-lang.modules" %% "scala-collection-compat" % ScalaCompactCollectionVersion
   val netty                      = "io.netty"                % "netty-all"               % NettyVersion
