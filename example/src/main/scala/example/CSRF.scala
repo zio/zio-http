5a824b711be8b3eba9fe7fbfb0bc414e6b4470a3
Shrey Mehta <shrey@dream11.com>
2021-12-18 18:12:06 +0530

    refactor(middleware): update code to work with HTTP changes

--- example/src/main/scala/example/CSRF.scala
+++ example/src/main/scala/example/CSRF.scala
@@ -1,11 +1,12 @@
 package example
 
+import zhttp.http.Middleware.{addCookie, csrf}
 import zhttp.http._
-import zhttp.http.middleware.HttpMiddleware.{addCookie, csrf}
-import zio.UIO
+import zhttp.service.Server
+import zio.{App, ExitCode, UIO, URIO}
 
 object CSRF extends App {
-  val app: HttpApp[Any, Nothing] = HttpApp.collectM {
+  val app: HttpApp[Any, Nothing] = Http.collectM {
     case Method.GET -> !! / "safeEndpoint"    =>
       UIO(Response.ok)
     case Method.POST -> !! / "unsafeEndpoint" => UIO(Response.ok)
@@ -13,4 +14,6 @@ object CSRF extends App {
   app @@ csrf("x-token", "token").when((method, _, _) => method == Method.POST) @@
     addCookie(Cookie("token", "randomToken")).when((method, _, _) => method == Method.GET)
 
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    Server.start(8091, app).exitCode
 }
