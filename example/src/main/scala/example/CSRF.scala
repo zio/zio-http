916d0ac7abe761adac5254b3f61c4c2c0b2b171c
Shrey Mehta <shrey@dream11.com>
2021-12-20 15:18:09 +0530

    doc(CSRF): Simplify example

--- example/src/main/scala/example/CSRF.scala
+++ example/src/main/scala/example/CSRF.scala
@@ -1,20 +1,16 @@
 package example
 
-import zhttp.http.Middleware.{addCookie, csrf}
+import zhttp.http.Middleware.csrf
 import zhttp.http._
 import zhttp.service.Server
 import zio.{App, ExitCode, URIO}
 
 object CSRF extends App {
-  val app: HttpApp[Any, Nothing] =
-    Http.collect[Request] {
-      case Method.GET -> !! / "safeEndpoint"    => Response.text("Hello World!")
-      case Method.POST -> !! / "unSafeEndpoint" => Response.jsonString("""{"greetings": "Hello World!"}""")
-    } @@ addCookie(Cookie("token", "randomToken")).when((method, _, _) => method == Method.GET) @@ csrf(
-      "x-token",
-      "token",
-    ).when((method, _, _) => method == Method.POST)
+  // CSRF middleware
+  // To prevent Cross-site request forgery attacks. This middleware is modeled after the double submit cookie pattern.
+  // https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#double-submit-cookie
+  val app: HttpApp[Any, Nothing] = Http.ok @@ csrf("x-token", "token")
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    Server.start(8091, app).exitCode
+    Server.start(8090, app).exitCode
 }
