d75a72ccc44733a2f2f15c3a9431733bb718b1e0
Shrey Mehta <shrey@dream11.com>
2021-12-20 15:53:30 +0530

    doc(CSRF): update doc comments and simplify tests

--- example/src/main/scala/example/CSRF.scala
+++ example/src/main/scala/example/CSRF.scala
@@ -6,9 +6,7 @@ import zhttp.service.Server
 import zio.{App, ExitCode, URIO}
 
 object CSRF extends App {
-  // CSRF middleware
-  // To prevent Cross-site request forgery attacks. This middleware is modeled after the double submit cookie pattern.
-  // https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#double-submit-cookie
+  // Will give FORBIDDEN when CSRF token in header doesn't match CSRF token set in cookies
   val app: HttpApp[Any, Nothing] = Http.ok @@ csrf("x-token", "token")
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
