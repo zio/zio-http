b8b1522e6ccacf14dffb8b8cbdd80215c1f3ec7d
Gabriel Ciuloaica <con_gabriel.ciuloaica@dream11.com>
2021-12-22 15:14:56 +0200

    refactored client spec

--- zio-http/src/test/scala/zhttp/service/ClientSpec.scala
+++ zio-http/src/test/scala/zhttp/service/ClientSpec.scala
@@ -3,63 +3,46 @@ package zhttp.service
 import zhttp.http._
 import zhttp.internal.{AppCollection, HttpRunnableSpec}
 import zhttp.service.server._
-import zio.ZIO
 import zio.duration.durationInt
-import zio.test.Assertion.{anything, containsString, isEmpty, isNonEmpty}
-import zio.test.TestAspect.{flaky, sequential, timeout}
-import zio.test.assertM
+import zio.test.Assertion._
+import zio.test.TestAspect._
+import zio.test._
 
 object ClientSpec extends HttpRunnableSpec(8082) {
 
   override def spec = {
     suiteM("Client") {
-      app.as(List(clientSpec)).useNow
-    }.provideCustomLayerShared(env) @@ timeout(30 seconds) @@ sequential
+      serve(AppCollection.app).as(List(clientSpec)).useNow
+    }.provideCustomLayerShared(env) @@ timeout(5 seconds) @@ sequential
   }
-
-  def clientSpec = suite("Client")(
+  def clientSpec    = suite("ClientSpec") {
     testM("respond Ok") {
-      val actual = request(!! / "users" / "zio" / "repos")
+      val actual = Http.ok.requestBodyAsString()
       assertM(actual)(anything)
     } +
       testM("non empty content") {
-        val actual          = request(!! / "users" / "zio")
-        val responseContent = actual.flatMap(_.getBody)
+        val actual          = Http.response(Response(status = Status.OK, data = HttpData.fromText("abc")))
+        val responseContent = actual.requestBody()
         assertM(responseContent)(isNonEmpty)
       } +
       testM("POST request expect non empty response content") {
-        val headers         = Headers.userAgent("zio-http test")
-        val response        = request(!! / "users" / "zio", method = Method.POST, headers = headers, content = "test")
-        val responseContent = response.flatMap(_.getBody)
+        val headers  = Headers.userAgent("zio-http test")
+        val response = Http.response(Response(status = Status.OK, data = HttpData.fromText("abc"), headers = headers))
+        val responseContent = response.requestBody()
         assertM(responseContent)(isNonEmpty)
       } +
       testM("empty content") {
-        val actual          = request(!! / "users" / "zio" / "repos")
-        val responseContent = actual.flatMap(_.getBody)
+        val actual          = Http.empty
+        val responseContent = actual.requestBody()
         assertM(responseContent)(isEmpty)
       } +
       testM("text content") {
-        val actual          = request(!! / "users" / "zio")
-        val responseContent = actual.flatMap(_.getBodyAsString)
+        val actual = Http.response(Response(status = Status.OK, data = HttpData.fromText("zio user does not exist")))
+        val responseContent = actual.requestBodyAsString()
         assertM(responseContent)(containsString("user"))
-      },
-  ).provideCustomLayer(env) @@ flaky @@ timeout(5 second)
-
-  private val env = EventLoopGroup.nio() ++ ChannelFactory.nio ++ ServerChannelFactory.nio ++ AppCollection.live
-
-  private val clientApp: HttpApp[Any, Nothing] = Http.collectM[Request] {
-    case Method.GET -> !! / "users" / "zio" / "repos" =>
-      ZIO.succeed(Response.ok)
-
-    case Method.GET -> !! / "users" / "zio" =>
-      ZIO.succeed(Response(status = Status.OK, data = HttpData.fromText("a zio user")))
-
-    case data @ Method.POST -> !! / "users" / "zio" =>
-      data.getBodyAsString
-        .flatMap(content => ZIO.succeed(Response(status = Status.OK, data = HttpData.fromText(content))))
-        .orDie
+      }
 
   }
 
-  private val app = serve { clientApp ++ AppCollection.app }
+  private val env = EventLoopGroup.nio() ++ ChannelFactory.nio ++ ServerChannelFactory.nio ++ AppCollection.live
 }
