61abd6ac407dce7ee1bb5f11c467930a6eebed78
Gabriel Ciuloaica <con_gabriel.ciuloaica@dream11.com>
2021-12-22 20:28:43 +0200

    updated ClientSpec implementation for better naming and proper Http internal API usage.

--- zio-http/src/test/scala/zhttp/service/ClientSpec.scala
+++ zio-http/src/test/scala/zhttp/service/ClientSpec.scala
@@ -3,12 +3,15 @@ package zhttp.service
 import zhttp.http._
 import zhttp.internal.{AppCollection, HttpRunnableSpec}
 import zhttp.service.server._
+import zio.Task
 import zio.duration.durationInt
 import zio.test.Assertion._
 import zio.test.TestAspect._
 import zio.test._
 
 object ClientSpec extends HttpRunnableSpec(8082) {
+
+  private val ClientUserAgentValue = "ZIO HTTP Client"
 
   override def spec = {
     suiteM("Client") {
@@ -17,28 +20,39 @@ object ClientSpec extends HttpRunnableSpec(8082) {
   }
   def clientSpec    = suite("ClientSpec") {
     testM("respond Ok") {
-      val actual = Http.ok.requestBodyAsString()
-      assertM(actual)(anything)
+      val app = Http.ok.requestBodyAsString()
+      assertM(app)(anything)
     } +
       testM("non empty content") {
-        val actual          = Http.response(Response(status = Status.OK, data = HttpData.fromText("abc")))
-        val responseContent = actual.requestBody()
+        val app             = Http.text("abc")
+        val responseContent = app.requestBody()
         assertM(responseContent)(isNonEmpty)
       } +
-      testM("POST request expect non empty response content") {
-        val headers  = Headers.userAgent("zio-http test")
-        val response = Http.response(Response(status = Status.OK, data = HttpData.fromText("abc"), headers = headers))
+      testM(
+        "POST request expect non empty response content from a server that requires a mandatory user agent header.",
+      ) {
+        val app = Http.text("test data") ++ Http.collectM[Request] { case req =>
+          val userAgentHeader       = req.getHeaderValue(Headers.Literals.Name.UserAgent)
+          val isExpectedHeaderValue = userAgentHeader.contains(ClientUserAgentValue)
+          if (isExpectedHeaderValue) req.getBodyAsString.map(text => Response.text(text))
+          else
+            Task.succeed(Response.status(Status.FORBIDDEN))
+
+        }
+
+        val userAgentHeader = Headers.userAgent(ClientUserAgentValue)
+        val response        = app.setMethod(Method.POST).addHeader(userAgentHeader)
         val responseContent = response.requestBody()
         assertM(responseContent)(isNonEmpty)
       } +
       testM("empty content") {
-        val actual          = Http.empty
-        val responseContent = actual.requestBody()
+        val app             = Http.empty
+        val responseContent = app.requestBody()
         assertM(responseContent)(isEmpty)
       } +
       testM("text content") {
-        val actual = Http.response(Response(status = Status.OK, data = HttpData.fromText("zio user does not exist")))
-        val responseContent = actual.requestBodyAsString()
+        val app             = Http.text("zio user does not exist")
+        val responseContent = app.requestBodyAsString()
         assertM(responseContent)(containsString("user"))
       }
 
