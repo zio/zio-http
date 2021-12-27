84db25d9d5282f1c054e92462afee7fa7d5d87a2
Tushar Mathur <tusharmath@gmail.com>
2021-12-27 17:44:09 +0530

    doc: update documentation for templating

--- example/src/main/scala/example/HtmlTemplating.scala
+++ example/src/main/scala/example/HtmlTemplating.scala
@@ -1,38 +1,50 @@
 package example
 
-import zhttp.html._
 import zhttp.http._
 import zhttp.service.Server
 import zio._
 
 object HtmlTemplating extends App {
+  // Importing everything from `zhttp.html`
+  import zhttp.html._
 
-  def app: HttpApp[Any, Nothing] = Http.html {
-    html(
-      head(
-        title("ZIO Http"),
-      ),
-      body(
-        div(
-          css := "container" :: Nil,
-          h1("Hello World"),
-          ul(
-            styles := Seq("list-style" -> "none"),
-            li(
-              a(href := "/hello/world", "Hello World"),
-            ),
-            li(
-              a(href := "/hello/world/again", "Hello World Again"),
-            ),
-            (2 to 10) map { i =>
+  def app: HttpApp[Any, Nothing] = {
+    // Html response takes in a `Html` instance.
+    Http.html {
+
+      // Support for default Html tags
+      html(
+        // Support for child nodes
+        head(
+          title("ZIO Http"),
+        ),
+        body(
+          div(
+            // Support for css class names
+            css := "container" :: "text-align-left" :: Nil,
+            h1("Hello World"),
+            ul(
+              // Support for inline css
+              styles := Seq("list-style" -> "none"),
               li(
-                a(href := s"/hello/world/i", s"Hello World $i"),
-              )
-            },
+                // Support for attributes
+                a(href := "/hello/world", "Hello World"),
+              ),
+              li(
+                a(href := "/hello/world/again", "Hello World Again"),
+              ),
+
+              // Support for Seq of Html elements
+              (2 to 10) map { i =>
+                li(
+                  a(href := s"/hello/world/i", s"Hello World $i"),
+                )
+              },
+            ),
           ),
         ),
-      ),
-    )
+      )
+    }
   }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
