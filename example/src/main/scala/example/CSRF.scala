404506358f179ef8ce5386743701104ebd322ac9
Shrey Mehta <shrey@dream11.com>
2021-12-27 21:33:19 +0530

    doc(csrf): make example more readable

--- example/src/main/scala/example/CSRF.scala
+++ example/src/main/scala/example/CSRF.scala
@@ -7,7 +7,7 @@ import zio.{App, ExitCode, URIO}
 
 object CSRF extends App {
   // Will give FORBIDDEN when CSRF token in header doesn't match CSRF token set in cookies
-  val app: HttpApp[Any, Nothing] = Http.ok @@ csrf("x-token", "token")
+  val app: HttpApp[Any, Nothing] = Http.ok @@ csrf(headerName = "x-token", cookieName = "token")
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     Server.start(8090, app).exitCode
