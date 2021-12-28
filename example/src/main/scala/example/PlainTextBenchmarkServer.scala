7a6f36ab7476f6f58b15dab5005f8d5eada1e7c2
Tushar Mathur <tusharmath@gmail.com>
2021-12-29 02:14:51 +0530

    chore: update benchmarking code to use freeze operator

--- example/src/main/scala/example/PlainTextBenchmarkServer.scala
+++ example/src/main/scala/example/PlainTextBenchmarkServer.scala
@@ -6,24 +6,34 @@ import zhttp.service.server.ServerChannelFactory
 import zhttp.service.{EventLoopGroup, Server}
 import zio.{App, ExitCode, URIO}
 
-case class Message(message: String)
-
 /**
  * This server is used to run plaintext benchmarks on CI.
  */
 object Main extends App {
-  val message: String            = "Hello, World!"
+
+  private val message: String = "Hello, World!"
+
   private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
 
-  val app = Http.response(Response.text(message).withServerTime.withServer(STATIC_SERVER_NAME))
-
-  val server = Server.app(app) ++
-    Server.port(8080) ++
-    Server.keepAlive ++
-    Server.disableLeakDetection ++ Server.consolidateFlush
+  private val frozenResponse = Response
+    .text(message)
+    .withServerTime
+    .withServer(STATIC_SERVER_NAME)
+    .freeze
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    server.make.useForever.provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8)).exitCode
+    frozenResponse
+      .flatMap(server(_).make.useForever)
+      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
+      .exitCode
   }
+
+  private def app(response: Response[Any, Nothing]) = Http.response(response)
+
+  private def server(response: Response[Any, Nothing]) =
+    Server.app(app(response)) ++
+      Server.port(8080) ++
+      Server.keepAlive ++
+      Server.paranoidLeakDetection ++ Server.consolidateFlush
 
 }
