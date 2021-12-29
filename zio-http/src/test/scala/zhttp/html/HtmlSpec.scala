c9926dc3a9079fe36d86773596260082730b6bde
Tushar Mathur <tusharmath@gmail.com>
2021-12-29 22:05:42 +0530

    refactor: remove assertion on doctype tag from HtmlSpec

--- zio-http/src/test/scala/zhttp/html/HtmlSpec.scala
+++ zio-http/src/test/scala/zhttp/html/HtmlSpec.scala
@@ -8,36 +8,36 @@ case object HtmlSpec extends DefaultRunnableSpec {
     suite("HtmlSpec")(
       test("tags") {
         val view     = html(head(), body(div()))
-        val expected = """<!DOCTYPE html><html><head></head><body><div></div></body></html>"""
+        val expected = """<html><head></head><body><div></div></body></html>"""
         assert(view.encode)(equalTo(expected.stripMargin))
       } +
         test("tags with attributes") {
           val view     = html(body(div(css := "container" :: Nil, "Hello!")))
-          val expected = """<!DOCTYPE html><html><body><div class="container">Hello!</div></body></html>"""
+          val expected = """<html><body><div class="container">Hello!</div></body></html>"""
           assert(view.encode)(equalTo(expected.stripMargin))
         } +
         test("tags with children") {
           val view     = html(body(div(css := "container" :: Nil, "Hello!", span("World!"))))
           val expected =
-            """<!DOCTYPE html><html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
+            """<html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
           assert(view.encode)(equalTo(expected.stripMargin))
         } +
         test("tags with attributes and children") {
           val view     = html(body(div(css := "container" :: Nil, "Hello!", span("World!"))))
           val expected =
-            """<!DOCTYPE html><html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
+            """<html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
           assert(view.encode)(equalTo(expected.stripMargin))
         } +
         test("tags with attributes and children") {
           val view     = html(body(div(css := "container" :: Nil, "Hello!", span("World!"))))
           val expected =
-            """<!DOCTYPE html><html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
+            """<html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
           assert(view.encode)(equalTo(expected.stripMargin))
         } +
         test("tags with attributes and children") {
           val view     = html(body(div(css := "container" :: Nil, "Hello!", span("World!"))))
           val expected =
-            """<!DOCTYPE html><html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
+            """<html><body><div class="container">Hello!<span>World!</span></div></body></html>"""
           assert(view.encode)(equalTo(expected.stripMargin))
         } +
         test("tags with attributes and children") {
