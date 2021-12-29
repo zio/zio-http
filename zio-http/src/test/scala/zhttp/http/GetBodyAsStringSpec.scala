6d926ac29ec4b63e8d0710a466913aabdfc9b099
Brendan McKee <brendan@stedi.com>
2021-12-29 03:40:01 -0800

    Remove Endpoint from Client.request (#714)
    Breaking Change in Client API

--- zio-http/src/test/scala/zhttp/http/GetBodyAsStringSpec.scala
+++ zio-http/src/test/scala/zhttp/http/GetBodyAsStringSpec.scala
@@ -19,7 +19,8 @@ object GetBodyAsStringSpec extends DefaultRunnableSpec {
       check(charsetGen) { charset =>
         val encoded = Client
           .ClientParams(
-            endpoint = Method.GET -> URL(Path("/")),
+            Method.GET,
+            URL(Path("/")),
             getHeaders = Headers(HttpHeaderNames.CONTENT_TYPE.toString, s"text/html; charset=$charset"),
             data = HttpData.BinaryChunk(Chunk.fromArray("abc".getBytes())),
           )
@@ -32,9 +33,9 @@ object GetBodyAsStringSpec extends DefaultRunnableSpec {
       test("should map bytes to default utf-8 if no charset given") {
         val data                            = Chunk.fromArray("abc".getBytes())
         val content: HttpData[Any, Nothing] = HttpData.BinaryChunk(data)
-        val request = Client.ClientParams(endpoint = Method.GET -> URL(Path("/")), data = content)
-        val encoded = request.getBodyAsString
-        val actual  = Option(new String(data.toArray, HTTP_CHARSET))
+        val request                         = Client.ClientParams(Method.GET, URL(Path("/")), data = content)
+        val encoded                         = request.getBodyAsString
+        val actual                          = Option(new String(data.toArray, HTTP_CHARSET))
         assert(actual)(equalTo(encoded))
       },
   )
