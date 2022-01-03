fc812d99a69b9d3f085254a8be3e4a6bca114a4f
amitsingh <amitksingh1490@gmail.com>
2022-01-03 20:08:01 +0530

    Response Json headers

--- zio-http/src/test/scala/zhttp/http/ResponseHelpersSpec.scala
+++ zio-http/src/test/scala/zhttp/http/ResponseHelpersSpec.scala
@@ -1,29 +1,31 @@
 package zhttp.http
 
-import zio.test.Assertion._
-import zio.test.{assert, _}
+import zio.test._
 
-object ResponseHelpersSpec {
+object ResponseHelpersSpec extends DefaultRunnableSpec {
   val redirectSpec = {
     val location = "www.google.com"
     suite("redirectSpec")(
       test("Temporary redirect should produce a response with a TEMPORARY_REDIRECT") {
         val x = Response.redirect(location)
-        assert(x.status)(equalTo(Status.TEMPORARY_REDIRECT)) && assert(x.getHeadersAsList)(
-          contains(Headers.location(location)),
-        )
+        assertTrue(x.status == Status.TEMPORARY_REDIRECT && x.getHeaderValue(HeaderNames.location).contains(location))
       } +
         test("Temporary redirect should produce a response with a location") {
           val x = Response.redirect(location)
-          assert(x.getHeadersAsList)(contains(Headers.location(location)))
+          assertTrue(x.getHeaderValue(HeaderNames.location).contains(location))
         } +
         test("Permanent redirect should produce a response with a PERMANENT_REDIRECT") {
-          val x = Response.redirect(location)
-          assert(x.status)(equalTo(Status.PERMANENT_REDIRECT))
+          val x = Response.redirect(location, true)
+          assertTrue(x.status == Status.PERMANENT_REDIRECT)
         } +
         test("Permanent redirect should produce a response with a location") {
-          val x = Response.redirect(location)
-          assert(x.getHeadersAsList)(contains(Headers.location(location)))
+          val x = Response.redirect(location, true)
+          assertTrue(x.getHeaderValue(HeaderNames.location).contains(location))
+        } +
+        test("Json should set content type to ApplicationJson") {
+          val x = Response.json("""{"message": "Hello"}""")
+          println(x.getHeadersAsList)
+          assertTrue(x.getHeaderValue(HeaderNames.contentType).contains(HeaderValues.applicationJson.toString))
         },
     )
   }
