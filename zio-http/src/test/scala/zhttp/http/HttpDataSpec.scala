1c1ef52f875cd361b62b817b20b38d6933f424c5
AshPrakasan <prakasanaiswarya@gmail.com>
2022-01-03 17:41:11 +0530

    Update zio-http/src/test/scala/zhttp/http/HttpDataSpec.scala
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

--- zio-http/src/test/scala/zhttp/http/HttpDataSpec.scala
+++ zio-http/src/test/scala/zhttp/http/HttpDataSpec.scala
@@ -7,7 +7,7 @@ import java.io.File
 
 object HttpDataSpec extends DefaultRunnableSpec {
   // TODO : Add tests for othe HttpData types
-  override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] =
+  override def spec =
     suite("HttpDataSpec")(suite("Test toByteBuf")(testM("HttpData.fromFile") {
       val file = new File(getClass.getResource("/TestFile.txt").getPath)
       val res  = HttpData.fromFile(file).toByteBuf.map(_.toString(HTTP_CHARSET))
