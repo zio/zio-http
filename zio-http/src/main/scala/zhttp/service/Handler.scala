3e31d659931b939e102e36d9e60782d8576b3998
amitsingh <amitksingh1490@gmail.com>
2022-01-03 18:33:51 +0530

    revert

--- zio-http/src/main/scala/zhttp/service/Handler.scala
+++ zio-http/src/main/scala/zhttp/service/Handler.scala
@@ -37,8 +37,7 @@ private[zhttp] final case class Handler[R](
     msg match {
       case jReq: HttpRequest    =>
         ctx.channel().config().setAutoRead(false)
-        self.request = null.asInstanceOf[Request]
-        /*new Request {
+        self.request = new Request {
           override def method: Method                                 = Method.fromHttpMethod(jReq.method())
           override def url: URL                                       = URL.fromString(jReq.uri()).getOrElse(null)
           override def getHeaders: Headers                            = Headers.make(jReq.headers())
@@ -77,7 +76,7 @@ private[zhttp] final case class Handler[R](
               case _                    => None
             }
           }
-        }*/
+        }
         unsafeRun(
           jReq,
           app,
