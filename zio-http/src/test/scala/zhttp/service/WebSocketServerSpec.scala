fc2f01b8824df6e45138cb4bfc67143e5ce4a50f
amitsingh <amitksingh1490@gmail.com>
2022-01-05 12:53:26 +0530

    failing test

--- zio-http/src/test/scala/zhttp/service/WebSocketServerSpec.scala
+++ zio-http/src/test/scala/zhttp/service/WebSocketServerSpec.scala
@@ -7,9 +7,12 @@ import zhttp.service.server._
 import zhttp.socket.{Socket, SocketApp, WebSocketFrame}
 import zio._
 import zio.duration._
+import zio.stream.ZStream
 import zio.test.Assertion.equalTo
 import zio.test.TestAspect.timeout
 import zio.test._
+import zio.test.environment.TestClock
+import zio.test.environment.TestClock.save.delay
 
 object WebSocketServerSpec extends HttpRunnableSpec(8011) {
 
@@ -23,6 +26,18 @@ object WebSocketServerSpec extends HttpRunnableSpec(8011) {
         val socketApp = SocketApp(Socket.succeed(WebSocketFrame.text("BAR")))
         val app       = Http.fromEffect(ZIO(Response.socket(socketApp)))
         assertM(app.webSocketStatusCode(!! / "subscriptions").repeatN(1024))(equalTo(101))
+      }
+      testM("should not close connection on errors") {
+        val open = Socket.fromStream(ZStream.fail(new Error))
+        for {
+          r <- Ref.make(true)
+          socketApp = SocketApp().onOpen(open).onClose(_ => r.set(false))
+          _   <- Http.fromEffect(ZIO(Response.socket(socketApp))).webSocketStatusCode(!! / "subscriptions")
+          f   <- delay(10 seconds).fork
+          _   <- TestClock.adjust(10 seconds)
+          _   <- f.join
+          res <- r.get
+        } yield assertTrue(res)
       }
     }
   }
