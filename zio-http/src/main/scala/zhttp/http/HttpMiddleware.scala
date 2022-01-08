54d5682b0b007cd386dfa80732ea59d53d5d6184
amitsingh <amitksingh1490@gmail.com>
2022-01-08 17:53:07 +0530

    Fix doc generation

--- zio-http/src/main/scala/zhttp/http/HttpMiddleware.scala
+++ zio-http/src/main/scala/zhttp/http/HttpMiddleware.scala
@@ -77,9 +77,9 @@ object HttpMiddleware {
    * CSRF middlewares : To prevent Cross-site request forgery attacks. This middleware is modeled after the double
    * submit cookie pattern.
    * @see
-   *   [[Middleware#csrfGenerate]] - Sets cookie with CSRF token
+   *   [[HttpMiddleware#csrfGenerate]] - Sets cookie with CSRF token
    * @see
-   *   [[Middleware#csrfValidate]] - Validate token value in request headers against value in cookies
+   *   [[HttpMiddleware#csrfValidate]] - Validate token value in request headers against value in cookies
    * @see
    *   https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#double-submit-cookie
    */
