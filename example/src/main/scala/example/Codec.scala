81ed5e2d541f3c8f3ec38e59c739c3c376fa5bc0
amitsingh <amitksingh1490@gmail.com>
2022-01-11 17:40:10 +0530

    refactor example

--- example/src/main/scala/example/Codec.scala
+++ example/src/main/scala/example/Codec.scala
@@ -8,28 +8,20 @@ import zio.json._
 
 object Codec extends App {
 
-  // Input
-  case class CreateUser(name: String)
+  case class CreateUser(name: String)            // Input
+  case class UserCreated(id: Long, name: String) // Output
+  implicit val userCodec: JsonCodec[UserCreated] = DeriveJsonCodec.gen[UserCreated]
 
-  // Output
-  case class UserCreated(id: Long, name: String)
-  object UserCreated {
-    implicit val decoder: JsonDecoder[UserCreated] = DeriveJsonDecoder.gen[UserCreated]
-    implicit val encoder: JsonEncoder[UserCreated] = DeriveJsonEncoder.gen[UserCreated]
-  }
+  val user = Http.collect[CreateUser] { case CreateUser(name) => UserCreated(2, name) }
 
-  val user: Http[Any, Nothing, CreateUser, UserCreated]                         =
-    Http.collect[CreateUser] { case CreateUser(name) =>
-      UserCreated(2, name)
-    }
   // Middleware to transform above http to HttpApp
-  val mid: Middleware[Any, Nothing, CreateUser, UserCreated, Request, Response] =
-    codec[Request, UserCreated](
-      decoder = req => CreateUser(req.path.asString.filterNot(_ == '/')),
-      encoder = userCreated => Response.json(userCreated.toJson),
-    )
+
+  val mid                        = codec[Request, UserCreated](
+    _ => CreateUser("John"),
+    user => Response.json(user.toJson),
+  )
   // Create HTTP route
-  val app: HttpApp[Any, Nothing]                                                = user @@ mid
+  val app: HttpApp[Any, Nothing] = user @@ mid
 
   // Run it like any simple app
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
