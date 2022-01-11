e2a304f6328a60de63d38c24e22b1185c8d5470e
Amit Kumar Singh <amitksingh1490@gmail.com>
2022-01-11 09:07:36 +0000

    Regenerate workflow with sbt-github-actions

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -7,7 +7,7 @@ object BuildHelper extends ScalaSettings {
   val Scala212         = "2.12.15"
   val Scala213         = "2.13.8"
   val ScalaDotty       = "3.1.0"
-  val ScoverageVersion = "1.9.2"
+  val ScoverageVersion = "1.9.3"
 
   private val stdOptions = Seq(
     "-deprecation",
