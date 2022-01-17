c08e919b23d7c990908e5df8050917142b364fde
kaushik143 <kaushik@dream11.com>
2022-01-17 15:10:04 +0530

    feat(Client): Add host in client from absolute URL

--- zio-http/src/main/scala/zhttp/service/EncodeClientParams.scala
+++ zio-http/src/main/scala/zhttp/service/EncodeClientParams.scala
@@ -9,13 +9,24 @@ trait EncodeClientParams {
    * Converts client params to JFullHttpRequest
    */
   def encodeClientParams(jVersion: HttpVersion, req: Client.ClientParams): FullHttpRequest = {
-    val method      = req.method.asHttpMethod
-    val uri         = req.url.asString
-    val content     = req.getBodyAsString match {
+    val method          = req.method.asHttpMethod
+    val url             = req.url
+    val incomingHeaders = req.getHeaders
+    val uri             = url.asString
+    val content         = req.getBodyAsString match {
       case Some(text) => Unpooled.copiedBuffer(text, HTTP_CHARSET)
       case None       => Unpooled.EMPTY_BUFFER
     }
-    val headers     = req.getHeaders.encode
+
+    val headers = incomingHeaders.encode
+
+    if (!incomingHeaders.hasHeader(HttpHeaderNames.HOST)) {
+      url.host match {
+        case Some(value) => headers.set(HttpHeaderNames.HOST, value)
+        case None        => headers.set(HttpHeaderNames.HOST, None)
+      }
+    }
+
     val writerIndex = content.writerIndex()
     if (writerIndex != 0) {
       headers.set(HttpHeaderNames.CONTENT_LENGTH, writerIndex.toString())
