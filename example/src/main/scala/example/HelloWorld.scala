89d1a2fc0024b359f95c76bd057bc90a17a25cba
Gabriel Ciuloaica <con_gabriel.ciuloaica@dream11.com>
2022-01-19 10:06:31 +0200

    removed unused code

--- example/src/main/scala/example/HelloWorld.scala
+++ example/src/main/scala/example/HelloWorld.scala
@@ -10,8 +10,6 @@ object HelloWorld extends App {
   // Create HTTP route
   val app = Http.collectZIO[Request] { case req =>
     req.decodeContent(ContentDecoder.backPressure).map { content =>
-      // Response(data = HttpData.fromStream(ZStream.fromChunkQueue(content)))
-      // Response(data = HttpData.fromByteBuf(content))
       Response(data = HttpData.fromStreamByteBuf(ZStream.fromQueue(content)))
     }
   }
