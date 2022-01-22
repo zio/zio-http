d156cc666e8ce570508a2d120a8ca60d379801c1
Scala Steward <me@scala-steward.org>
2022-01-22 12:14:20 +0100

    Update client3:async-http-client-backend-zio, ... to 3.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val NettyIncubatorVersion         = "0.0.11.Final"
   val ScalaCompactCollectionVersion = "2.6.0"
   val ZioVersion                    = "1.0.13"
-  val SttpVersion                   = "3.3.18"
+  val SttpVersion                   = "3.4.0"
 
   val `jwt-core`                 = "com.github.jwt-scala"   %% "jwt-core"                % JwtCoreVersion
   val `scala-compact-collection` = "org.scala-lang.modules" %% "scala-collection-compat" % ScalaCompactCollectionVersion
