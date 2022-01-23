c2b6a6cb9f9d17fccd508b82f21feb3cc902d1a7
kaushik143 <kaushik@dream11.com>
2022-01-23 11:40:03 +0530

    chore(ci): make changes in benchmarkWorkflow script

--- project/BenchmarkWorkFlow.scala
+++ project/BenchmarkWorkFlow.scala
@@ -9,7 +9,7 @@ object BenchmarkWorkFlow {
       name = "Benchmarks",
       oses = List("centos"),
       cond = Some(
-        "${{ github.event_name == 'pull_request'}} && contains(github.event.pull_request.labels.*.name, 'documentation')",
+        "${{ github.event_name == 'pull_request'}} && !contains(github.event.pull_request.labels.*.name, 'documentation')",
       ),
       scalas = List(Scala213),
       steps = List(
