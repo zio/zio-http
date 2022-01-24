4bf78b6e87024367e92f4aa26dfdb1fc10d87760
shrutiverma97 <shruti@dream11.com>
2022-01-24 09:48:31 +0530

    test(client): added test for req url string

--- zio-http/src/test/scala/zhttp/service/ClientSpec.scala
+++ zio-http/src/test/scala/zhttp/service/ClientSpec.scala
@@ -37,6 +37,13 @@ object ClientSpec extends HttpRunnableSpec {
         val app             = Http.text("zio user does not exist")
         val responseContent = app.requestBodyAsString()
         assertM(responseContent)(containsString("user"))
+      } +
+      testM("client req url") {
+        val app      = Http.collect[Request] { case req @ Method.GET -> !! / "foo" =>
+          Response.text(s"${req.url.asString}")
+        }
+        val expected = app.request(!! / "foo").flatMap(_.getBodyAsString)
+        assertM(expected)(equalTo("/foo"))
       }
   }
 
