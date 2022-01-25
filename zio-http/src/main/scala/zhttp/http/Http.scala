e988c1001414211f8570d9529f6093da1ebf4fa3
AshPrakasan <prakasanaiswarya@gmail.com>
2022-01-25 22:56:07 +0530

    Update zio-http/src/main/scala/zhttp/http/Http.scala
    Co-authored-by: Tushar Mathur <tusharmath@gmail.com>

--- zio-http/src/main/scala/zhttp/http/Http.scala
+++ zio-http/src/main/scala/zhttp/http/Http.scala
@@ -547,7 +547,7 @@ object Http {
           responseHttp(file)
       }.catchAll {
         case a: SecurityException     =>
-          Http.error(HttpError.Forbidden(a.getMessage))
+          Http.forbidden(a.getMessage)
         case _: FileNotFoundException =>
           Http.error(HttpError.NotFound(Path(request.path.asString)))
         case e                        =>
