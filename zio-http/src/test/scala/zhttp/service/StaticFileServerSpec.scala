d1258bd3144e8640662467f6f7bad863bac34877
ashprakasan <prakasanaiswarya@gmail.com>
2022-01-25 18:36:15 +0530

    Fix PR comments

--- zio-http/src/test/scala/zhttp/service/StaticFileServerSpec.scala
+++ zio-http/src/test/scala/zhttp/service/StaticFileServerSpec.scala
@@ -8,47 +8,54 @@ import zhttp.service.server._
 import zio.duration.durationInt
 import zio.test.Assertion.{containsString, equalTo, isSome}
 import zio.test.TestAspect.timeout
-import zio.test.{ZSpec, assertM}
+import zio.test.assertM
 
 import java.nio.file.Paths
 
 object StaticFileServerSpec extends HttpRunnableSpec {
 
-  private val staticApp = Http.fromPath(Paths.get(getClass.getResource("/TestStatic/Folder2").getPath))
-  private val staticApp2 = Http.fromPath(Paths.get(getClass.getResource("").getPath + "/NonExistentDirectory"))
-  val app         = serve { staticApp ++ staticApp2 }
-
   private val env =
     EventLoopGroup.nio() ++ ChannelFactory.nio ++ ServerChannelFactory.nio ++ DynamicServer.live
 
-  def staticSpec = suite("Static File Server")(
+  def staticSpec1 = suite("Static File Server")(
     testM("should list contents of root directory") {
-      val res: HttpIO[Any, String] = staticApp.requestBodyAsString()
+      val app                      = Http.fromPath(Paths.get(getClass.getResource("/TestStatic/Folder2").getPath))
+      val res: HttpIO[Any, String] = app.requestBodyAsString()
       assertM(res)(containsString("<li><a href=\"TestFile2.txt\">TestFile2.txt</a></li>"))
     },
     testM("should show file contents") {
-      val res = staticApp.requestBodyAsString(path = Path("/TestFile2.txt"))
+      val app = Http.fromPath(Paths.get(getClass.getResource("/TestStatic/Folder2").getPath))
+      val res = app.requestBodyAsString(path = Path("/TestFile2.txt"))
       assertM(res)(equalTo("This is a test file for testing Static File Server."))
     },
     testM("should have content-type header \"text/plain\"") {
-      val res = staticApp.request(path = Path("/TestFile2.txt")).map(_.getHeaderValue(HttpHeaderNames.CONTENT_TYPE))
+      val app = Http.fromPath(Paths.get(getClass.getResource("/TestStatic/Folder2").getPath))
+      val res = app.request(path = Path("/TestFile2.txt")).map(_.getHeaderValue(HttpHeaderNames.CONTENT_TYPE))
       assertM(res)(isSome(equalTo("text/plain")))
     },
     testM("should respond with \"Not Found\"") {
-      val res = staticApp.requestBodyAsString(path = Path("/NonExistentFile.txt"))
+      val app = Http.fromPath(Paths.get(getClass.getResource("/TestStatic/Folder2").getPath))
+      val res = app.requestBodyAsString(path = Path("/NonExistentFile.txt"))
       assertM(res)(equalTo("The requested URI \"/NonExistentFile.txt\" was not found on this server\n"))
     },
     testM("should respond with status 405") {
-      val res = staticApp.requestStatus(method = Method.POST, path = Path("/TextFile2.txt"))
+      val app = Http.fromPath(Paths.get(getClass.getResource("/TestStatic/Folder2").getPath))
+      val res = app.requestStatus(method = Method.POST, path = Path("/TextFile2.txt"))
       assertM(res)(equalTo(Status.METHOD_NOT_ALLOWED))
     },
-    testM("should respond with \"Not Found\" is directory path does not exist") {
-      val res = staticApp2.requestBodyAsString()
+    testM("should respond with \"Not Found\" if directory path does not exist") {
+      val app = Http.fromPath(Paths.get(getClass.getResource("").getPath + "/NonExistentDir"))
+      val res = app.requestBodyAsString()
       assertM(res)(equalTo("The requested URI \"/\" was not found on this server\n"))
-    }
+    },
+    testM("should respond with file contents if root path is a file") {
+      val app = Http.fromPath(Paths.get(getClass.getResource("/TestFile.txt").getPath))
+      val res = app.requestBodyAsString()
+      assertM(res)(equalTo("abc\nfoo"))
+    },
   )
 
-  override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] = suiteM("StaticFileServer") {
-    app.as(List(staticSpec)).useNow
-  }.provideCustomLayerShared(env) @@ timeout(30 seconds)
+  override def spec = suiteM("StaticFileServer") {
+    serve(DynamicServer.app).as(List(staticSpec1)).useNow
+  }.provideCustomLayerShared(env) @@ timeout(5 seconds)
 }
