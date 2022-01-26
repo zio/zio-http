3d960966848842f46ae3329978f0541f33e56926
Tushar Mathur <tusharmath@gmail.com>
2022-01-26 17:53:40 +0530

    refactor: use `url` as a string instead of URL type

--- zio-http/src/main/scala/zhttp/service/client/SocketClient.scala
+++ zio-http/src/main/scala/zhttp/service/client/SocketClient.scala
@@ -3,49 +3,43 @@ package zhttp.service.client
 import io.netty.bootstrap.Bootstrap
 import io.netty.channel.{Channel, ChannelFactory => JChannelFactory, EventLoopGroup => JEventLoopGroup}
 import io.netty.handler.codec.http.websocketx.WebSocketClientProtocolHandler
-import zhttp.http.URL._
 import zhttp.http._
 import zhttp.service._
 import zhttp.service.client.ClientSSLHandler.ClientSSLOptions
 import zhttp.service.client.content.handlers.ClientSocketUpgradeHandler
 import zhttp.socket.SocketApp
-import zhttp.socket.SocketProtocol.Config.ClientConfig
 import zio._
 
-import java.net.InetSocketAddress
+import java.net.{InetSocketAddress, URI}
 
 class SocketClient[R](rtm: HttpRuntime[R], cf: JChannelFactory[Channel], el: JEventLoopGroup) {
   def unsafeSocket(
+    uri: String,
     headers: Headers,
     ss: SocketApp[R],
     pr: Promise[Throwable, Client.ClientResponse],
     clientSSLOptions: ClientSSLOptions,
   ): Unit = {
-    val config   = ss.protocol.narrow[ClientConfig].clientConfig(headers)
+    val url      = new URI(uri)
+    val config   = ss.protocol.toClientConfig(headers).webSocketUri(uri)
     val handlers = List(
       ClientSocketUpgradeHandler(rtm, pr),
-      new WebSocketClientProtocolHandler(config),
+      new WebSocketClientProtocolHandler(config.build()),
       ClientSocketHandler(rtm, ss),
     )
 
-    val url = URL.fromString(config.webSocketUri().toString).toOption
+    val host = url.getHost
+    assert(host != null)
 
-    val host   = url.flatMap(_.host)
-    val port   = url.flatMap(_.port).fold(80) {
-      case -1   => 80
-      case port => port
-    }
-    val scheme = url
-      .map(_.kind match {
-        case Location.Relative               => ""
-        case Location.Absolute(scheme, _, _) => scheme.asString
-      })
-      .get
+    val scheme = url.getScheme
+    assert(scheme != null)
+
+    val port = if (url.getPort == -1) 80 else url.getPort
 
     val init = ClientChannelInitializer(handlers, scheme, clientSSLOptions)
 
     val jboo = new Bootstrap().channelFactory(cf).group(el).handler(init)
-    if (host.isDefined) jboo.remoteAddress(new InetSocketAddress(host.get, port))
+    jboo.remoteAddress(new InetSocketAddress(host, port))
 
     jboo.connect(): Unit
   }
