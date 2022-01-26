dcad0dd6bb3eef4821b692e99f89216c7e70146f
Tushar Mathur <tusharmath@gmail.com>
2022-01-26 17:41:35 +0530

    refactor: update client example

--- example/src/main/scala/example/WebSocketSimpleClient.scala
+++ example/src/main/scala/example/WebSocketSimpleClient.scala
@@ -1,29 +1,26 @@
 package example
 
-import zhttp.service.{ChannelFactory, Client, EventLoopGroup}
-import zhttp.socket.{Socket, SocketProtocol, WebSocketFrame}
+import zhttp.service.{ChannelFactory, EventLoopGroup}
+import zhttp.socket.{Socket, WebSocketFrame}
 import zio._
 import zio.stream.ZStream
 
 object WebSocketSimpleClient extends zio.App {
-  private val env = EventLoopGroup.auto() ++ ChannelFactory.auto
-  val url         = "ws://localhost:8090/subscriptions"
 
-  private val sa = Socket
+  // Setup client envs
+  val env = EventLoopGroup.auto() ++ ChannelFactory.auto
+
+  val url = "ws://localhost:8090/subscriptions"
+
+  val app = Socket
     .collect[WebSocketFrame] {
       case WebSocketFrame.Text("BAZ") => ZStream.succeed(WebSocketFrame.close(1000))
       case frame                      => ZStream.succeed(frame)
     }
     .toSocketApp
-    .onOpen(Socket.succeed(WebSocketFrame.text("FOO")))
-    .onClose(_ => ZIO.unit)
-    .onError(thr => ZIO.die(thr))
-    .withProtocol(SocketProtocol.uri(url))
+    .connect(url)
 
-  override def run(args: List[String]): URIO[ZEnv, ExitCode] =
-    Client
-      .socket(sa)
-      .flatMap(response => console.putStr(s"${response.status.asJava}"))
-      .exitCode
-      .provideCustomLayer(env)
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
+    app.exitCode.provideCustomLayer(env)
+  }
 }
