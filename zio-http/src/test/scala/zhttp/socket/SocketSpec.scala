0b60d79205863bdf0078ecceadce7b11b30e0aa9
Shubham Girdhar <girdharshubham@hotmail.com>
2022-01-27 15:19:13 +0530

    test: add unit test for Socket#echo

--- zio-http/src/test/scala/zhttp/socket/SocketSpec.scala
+++ zio-http/src/test/scala/zhttp/socket/SocketSpec.scala
@@ -1,17 +1,19 @@
 package zhttp.socket
 
 import zio._
+import zio.duration.durationInt
 import zio.stream.ZStream
 import zio.test.Assertion._
+import zio.test.TestAspect.timeout
 import zio.test._
 
 object SocketSpec extends DefaultRunnableSpec {
 
   def spec = suite("SocketSpec") {
-    OperationsSpec
-  }
+    operationsSpec
+  } @@ timeout(5 seconds)
 
-  def OperationsSpec = suite("Operations Spec") {
+  def operationsSpec = suite("Operations Spec") {
     testM("fromStream provide") {
       val text        = "Cat ipsum dolor sit amet"
       val environment = ZStream.environment[String]
@@ -23,38 +25,46 @@ object SocketSpec extends DefaultRunnableSpec {
       assertM(socket.runCollect) {
         equalTo(Chunk(text))
       }
-    } + testM("fromFunction provide") {
-      val environmentFunction = (_: Any) => ZStream.environment[WebSocketFrame]
-      val socket              = Socket
-        .fromFunction(environmentFunction)
-        .provide(WebSocketFrame.text("Foo"))
-        .execute(WebSocketFrame.text("Bar"))
+    } +
+      testM("fromFunction provide") {
+        val environmentFunction = (_: Any) => ZStream.environment[WebSocketFrame]
+        val socket              = Socket
+          .fromFunction(environmentFunction)
+          .provide(WebSocketFrame.text("Foo"))
+          .execute(WebSocketFrame.text("Bar"))
 
-      assertM(socket.runCollect) {
-        equalTo(Chunk(WebSocketFrame.text("Foo")))
-      }
-    } + testM("collect provide") {
-      val environment = ZStream.environment[WebSocketFrame]
-      val socket      = Socket
-        .collect[WebSocketFrame] { case WebSocketFrame.Pong =>
-          environment
+        assertM(socket.runCollect) {
+          equalTo(Chunk(WebSocketFrame.text("Foo")))
         }
-        .provide(WebSocketFrame.ping)
-        .execute(WebSocketFrame.pong)
+      } +
+      testM("collect provide") {
+        val environment = ZStream.environment[WebSocketFrame]
+        val socket      = Socket
+          .collect[WebSocketFrame] { case WebSocketFrame.Pong =>
+            environment
+          }
+          .provide(WebSocketFrame.ping)
+          .execute(WebSocketFrame.pong)
 
-      assertM(socket.runCollect) {
-        equalTo(Chunk(WebSocketFrame.ping))
+        assertM(socket.runCollect) {
+          equalTo(Chunk(WebSocketFrame.ping))
+        }
+      } +
+      testM("ordered provide") {
+        val socket = Socket.collect[Int] { case _ =>
+          ZStream.environment[Int]
+        }
+
+        val socketA: Socket[Int, Nothing, Int, Int] = socket.provide(12)
+        val socketB: Socket[Int, Nothing, Int, Int] = socketA.provide(1)
+        val socketC: Socket[Any, Nothing, Int, Int] = socketB.provide(42)
+
+        assertM(socketC.execute(1000).runCollect)(equalTo(Chunk(12)))
+      } +
+      testM("echo") {
+        val stream = Socket.echo(WebSocketFrame.text("FOO"))
+
+        assertM(stream.runCollect)(equalTo(Chunk(WebSocketFrame.text("FOO"))))
       }
-    } + testM("ordered provide") {
-      val socket = Socket.collect[Int] { case _ =>
-        ZStream.environment[Int]
-      }
-
-      val socketA: Socket[Int, Nothing, Int, Int] = socket.provide(12)
-      val socketB: Socket[Int, Nothing, Int, Int] = socketA.provide(1)
-      val socketC: Socket[Any, Nothing, Int, Int] = socketB.provide(42)
-
-      assertM(socketC.execute(1000).runCollect)(equalTo(Chunk(12)))
-    }
   }
 }
