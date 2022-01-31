071e5ee2c18a308798016521231acf8c15061ad8
Gabriel Ciuloaica <con_gabriel.ciuloaica@dream11.com>
2022-01-31 16:30:33 +0200

    refactored the Request entity too large implementation

--- zio-http/src/main/scala/zhttp/service/RequestBodyHandler.scala
+++ zio-http/src/main/scala/zhttp/service/RequestBodyHandler.scala
@@ -2,7 +2,6 @@ package zhttp.service
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
 import io.netty.handler.codec.http.HttpContent
-import zhttp.http.{Response, Status}
 import zhttp.service.server.content.handlers.UnsafeRequestHandler.{UnsafeChannel, UnsafeContent}
 
 @Sharable
@@ -13,14 +12,9 @@ final case class RequestBodyHandler[R](
 
   override def channelRead0(ctx: ChannelHandlerContext, msg: Any): Unit = {
 
-    println("RBODY: " + msg)
-    if (msg.isInstanceOf[HttpContent]) {
-      val httpContent = msg.asInstanceOf[HttpContent]
-      if (httpContent.content().readableBytes() > config.maxRequestSize)
-        ctx.fireChannelRead(Response.status(Status.REQUEST_ENTITY_TOO_LARGE)): Unit
-      else
-        msgCallback(UnsafeChannel(ctx), UnsafeContent(msg.asInstanceOf[HttpContent]))
-    } else
+    if (msg.isInstanceOf[HttpContent])
+      msgCallback(UnsafeChannel(ctx), UnsafeContent(msg.asInstanceOf[HttpContent], config.maxRequestSize))
+    else
       ctx.fireChannelRead(msg): Unit
   }
 
