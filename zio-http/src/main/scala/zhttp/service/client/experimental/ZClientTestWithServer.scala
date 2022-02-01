cbd5c1ab7ad82a8860798a004e61704f69c56699
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2022-02-01 11:42:57 +0530

    a cleaned up test client

--- zio-http/src/main/scala/zhttp/service/client/experimental/ZClientTestWithServer.scala
+++ zio-http/src/main/scala/zhttp/service/client/experimental/ZClientTestWithServer.scala
@@ -19,39 +19,31 @@ object ZClientTestWithServer extends App {
 
   private val server =
     Server.port(PORT) ++                     // Setup port
-      Server.paranoidLeakDetection ++        // Paranoid leak detection (affects performance)
       Server.app(fooBar) ++ Server.keepAlive // Setup the Http app
 
+  implicit val e = new Exception("")
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    server.make.use { start =>
-      println(s"Server started on port ${start.port}")
-      clientTest(start.port) //*> ZIO.never
-    }
+    server
+      .make
+      .use (_ => clientTest)
       .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(2))
       .exitCode
   }
 
-  def clientTest(port: Int) = {
-    val client                                   = ZClient.port(port) ++ ZClient.threads(2)
+  def clientTest = {
     for {
-      cl <- client.make
-      _  <- triggerClientMultipleTimes(cl)
+      client <- (
+        ZClient.threads(2) ++
+          ZClient.maxConnectionsPerRequestKey(10) ++
+          ZClient.maxTotalConnections(20)
+        ).make
+      _  <- triggerClientSequential(client)
     } yield ()
 
   }
 
-  // multiple client shared resources
-  // different conn pool for diff host port
-  // pool should correspond to host / port combo
-  // have one client and
-  // optimizations in the background
-  // just one client serves all and sundry
-
-  // use cases like pipelining ... httpclient document
-
-  import zhttp.http._
-
-  def triggerClientMultipleTimes(cl: DefaultZClient) = for {
+  // sequential execution test
+  def triggerClientSequential(cl: DefaultZClient) = for {
     req1 <- ZIO.effect("http://localhost:8081/foo/1")
     resp <- cl.run(req1)
     rval <- resp.getBodyAsString
@@ -59,7 +51,7 @@ object ZClientTestWithServer extends App {
 
     req3 <- ZIO.effect("http://www.google.com")
     resp3 <- cl.run(req3)
-//    resp3 <- cl.run("http://sports.api.decathlon.com/groups/water-aerobics")
+    //    resp3 <- cl.run("http://sports.api.decathlon.com/groups/water-aerobics")
     r3    <- resp3.getBodyAsString
     _     <- ZIO.effect(println(s"Response Content from $req3  ${r3.length}"))
 
@@ -75,5 +67,16 @@ object ZClientTestWithServer extends App {
     activeCon <- cl.connectionManager.getActiveConnections
     _ <- ZIO.effect(println(s"Number of active connections for four requests: $activeCon \n\n connections map ${cl.connectionManager.connRef}"))
   } yield ()
+
+  // multiple client shared resources
+  // different conn pool for diff host port
+  // pool should correspond to host / port combo
+  // have one client and
+  // optimizations in the background
+  // just one client serves all and sundry
+
+  // use cases like pipelining ... httpclient document
+
+  import zhttp.http._
 
 }
