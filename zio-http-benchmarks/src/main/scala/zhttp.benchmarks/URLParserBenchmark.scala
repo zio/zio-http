485a45d0bd4fccf7266307b9a687a8c409384917
shrutiverma97 <shruti@dream11.com>
2022-02-07 19:40:12 +0530

    updated benchmarks

--- zio-http-benchmarks/src/main/scala/zhttp.benchmarks/URLParserBenchmark.scala
+++ zio-http-benchmarks/src/main/scala/zhttp.benchmarks/URLParserBenchmark.scala
@@ -10,22 +10,19 @@ import java.util.concurrent.TimeUnit
 @OutputTimeUnit(TimeUnit.SECONDS)
 class URLParserBenchmark {
   private val MAX = 1000
+  private val str = "http://yourdomain.com/list/users/cusers/dusers/eusers/fusers/gusers?q=1&q=2&q=26&q=227828&q=217718&t=26728&y=1672882&j=28828&k=3767387"
 
   @Benchmark
   def benchmarkURLParser(): Unit  = {
-    (0 to MAX).foreach(_ => URL5("http://yourdomain.com/list/users"))
+    (0 to MAX).foreach(_ => URL(str).getHost)
     ()
   }
   @Benchmark
   def benchmarkURLParser2(): Unit = {
-    (0 to MAX).foreach(_ => URLOld.fromString("http://yourdomain.com/list/users"))
-    ()
-  }
-
-  @Benchmark
-  def benchmarkURLParser3(): Unit = {
-    (0 to MAX).foreach(_ => URLOld.fromString("http://yourdomain.com/list/users"))
-
+    (0 to MAX).foreach(_ => URLOld.fromString(str) match {
+      case a @ Left(_) => a
+      case Right(value) => value.host
+    })
     ()
   }
 }
