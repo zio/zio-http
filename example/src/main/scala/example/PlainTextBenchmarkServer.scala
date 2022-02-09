0349e6fc9c493f6b393db372c8fa4550527eee85
shrutiverma97 <shruti@dream11.com>
2022-02-09 15:24:47 +0530

    changes in benchmark

--- example/src/main/scala/example/PlainTextBenchmarkServer.scala
+++ example/src/main/scala/example/PlainTextBenchmarkServer.scala
@@ -1,6 +1,5 @@
 package example
 
-import io.netty.util.AsciiString
 import zhttp.http._
 import zhttp.service.server.ServerChannelFactory
 import zhttp.service.{EventLoopGroup, Server}
@@ -11,31 +10,23 @@ import zio.{App, ExitCode, UIO, URIO}
  */
 object Main extends App {
 
-  private val message: String = "Hello, World!"
-
-  private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
-
-  private val frozenResponse = Response
-    .text(message)
-    .withServerTime
-    .withServer(STATIC_SERVER_NAME)
-    .freeze
-
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    frozenResponse
-      .flatMap(server(_).make.useForever)
-      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
-      .exitCode
+  // Create HTTP route
+  val app: HttpApp[Any, Nothing] = Http.collect[Request] {
+    case req @ Method.GET -> !! / "text" => Response.text(s"${req.url.host.orNull}")
   }
 
-  private def app(response: Response) = Http.response(response)
-
-  private def server(response: Response) =
-    Server.app(app(response)) ++
+  private def server =
+    Server.app(app) ++
       Server.port(8080) ++
       Server.error(_ => UIO.unit) ++
       Server.disableLeakDetection ++
       Server.consolidateFlush ++
       Server.disableFlowControl
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    server.make.useForever
+      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
+      .exitCode
+  }
 
 }
