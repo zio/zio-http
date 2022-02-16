4faa972d1ab52c4f8f6911c691613602c1c86260
shrutiverma97 <shruti@dream11.com>
2022-02-16 18:26:09 +0530

    fix: build

--- zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpRouteTextPerf.scala
+++ zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpRouteTextPerf.scala
@@ -1,19 +1,19 @@
 package zhttp.benchmarks
 
+import io.netty.util.AsciiString
 import org.openjdk.jmh.annotations._
 import zhttp.http._
 import zio._
-import java.util.concurrent.TimeUnit
 
-import io.netty.util.AsciiString
+import java.util.concurrent.TimeUnit
 
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
 class HttpRouteTextPerf {
 
-  private val runtime = Runtime.default
-  private val msg = "HELLO WORLD"
+  private val runtime            = Runtime.default
+  private val msg                = "HELLO WORLD"
   private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
 
   private val frozenResponse = Response
@@ -25,7 +25,7 @@ class HttpRouteTextPerf {
   private val app          = Http.succeed(frozenResponse)
   private val req: Request = Request(Method.GET, URL(!!))
   private val httpProgram  = app.execute(req).toZIO
-  private val UIOProgram   =  UIO(frozenResponse)
+  private val UIOProgram   = UIO(frozenResponse)
 
   @Benchmark
   def benchmarkHttpProgram(): Unit = {
