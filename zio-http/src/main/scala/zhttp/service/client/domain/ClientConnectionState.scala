210c171f936d40a1aaaecbdcbf60fd76b069cefb
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2022-02-16 16:05:15 +0530

    removed unused types

--- zio-http/src/main/scala/zhttp/service/client/domain/ClientConnectionState.scala
+++ zio-http/src/main/scala/zhttp/service/client/domain/ClientConnectionState.scala
@@ -1,13 +1,11 @@
 package zhttp.service.client.domain
 
 import io.netty.channel.Channel
-import io.netty.handler.codec.http.FullHttpRequest
 import zhttp.service.client.domain.ConnectionData.ReqKey
 import zio.Ref
 import zio.duration.Duration
 
 import java.net.InetSocketAddress
-import java.time.Instant
 import scala.collection.immutable
 
 /**
@@ -29,8 +27,6 @@ case class Timeouts(
   idleTimeout: Duration = Duration.Infinity,
   requestTimeout: Duration = Duration.Infinity,
 )
-
-case class PendingRequest(req: FullHttpRequest, requestedTime: Instant)
 
 case class ConnectionState(
   currentAllocatedChannels: Map[Channel, ReqKey],
@@ -62,21 +58,7 @@ case class ConnectionData(connectionData: Ref[(Option[Connection], ConnectionSta
     idleConnections  = connectionData._2.idleConnectionsMap.valuesIterator
       .foldLeft(0) { case (acc, queue) => acc + queue.size }
   } yield (allocConnections + idleConnections)
-
-  //    def incrementConnection: Unit = ???
-  //    def decrementConnection = ???
-  //    def isConnectionExpired = ???
-  //    def isConnectionWithinLimits = ???
-  //    def addConnectionToIdleQ = ???
-  //    def addConnectionToWaitQ = ???
-  //
-  //    def releaseConnection = ???
-  //    def shutdownConnectionManager = ???
-  //
-  //    def getActiveConnectionsForReqKey(reqKey: ReqKey): Task[Int] = ???
-  //    def getIdleConnections: Task[Int] = ???
-  //    def getIdleConnectionsForReqKey(reqKey: ReqKey): Task[Int] = ???
-
+  
   // TBD thready safety and appropriate namespace
   var currMaxTotalConnections: Int     = 0
   var currMaxConnectionPerRequest: Int = 0
