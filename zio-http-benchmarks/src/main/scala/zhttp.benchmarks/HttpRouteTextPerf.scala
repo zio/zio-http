ae538b887783bed034925d2a826114e0ce4423ce
shrutiverma97 <shruti@dream11.com>
2022-02-16 18:11:16 +0530

    No optimization

--- zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpRouteTextPerf.scala
+++ zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpRouteTextPerf.scala
@@ -3,8 +3,9 @@ package zhttp.benchmarks
 import org.openjdk.jmh.annotations._
 import zhttp.http._
 import zio._
-
 import java.util.concurrent.TimeUnit
+
+import io.netty.util.AsciiString
 
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
@@ -12,12 +13,19 @@ import java.util.concurrent.TimeUnit
 class HttpRouteTextPerf {
 
   private val runtime = Runtime.default
+  private val msg = "HELLO WORLD"
+  private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
 
-  private val res          = Response.text("HELLO WORLD")
-  private val app          = Http.succeed(res)
+  private val frozenResponse = Response
+    .text(msg)
+    .withServerTime
+    .withServer(STATIC_SERVER_NAME)
+    .freeze
+
+  private val app          = Http.succeed(frozenResponse)
   private val req: Request = Request(Method.GET, URL(!!))
   private val httpProgram  = ZIO.foreach_(0 to 1000) { _ => app.execute(req).toZIO }
-  private val UIOProgram   = ZIO.foreach_(0 to 1000) { _ => UIO(res) }
+  private val UIOProgram   = ZIO.foreach_(0 to 1000) { _ => UIO(frozenResponse) }
 
   @Benchmark
   def benchmarkHttpProgram(): Unit = {
