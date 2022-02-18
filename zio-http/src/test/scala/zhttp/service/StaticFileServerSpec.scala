ad11c1b00f89b694a916466ec2a4dc667db2ea40
Tushar Mathur <tusharmath@gmail.com>
2022-02-18 18:57:36 +0530

    test: StaticFileServerSpec add tests for directory

--- zio-http/src/test/scala/zhttp/service/StaticFileServerSpec.scala
+++ zio-http/src/test/scala/zhttp/service/StaticFileServerSpec.scala
@@ -17,35 +17,48 @@ object StaticFileServerSpec extends HttpRunnableSpec {
     EventLoopGroup.nio() ++ ChannelFactory.nio ++ ServerChannelFactory.nio ++ DynamicServer.live
 
   private def staticSpec = suite("Static RandomAccessFile Server") {
-
     suite("fromResource") {
-      val testFileTxt = Http.fromResource("/TestFile.txt").deploy
-      val noFile      = Http.fromResource("/Nothing").deploy
-      val directory   = Http.fromResource("/TestStatic").deploy
-
-      testM("should have 200 status code") {
-        val res = testFileTxt.run().map(_.status)
-        assertM(res)(equalTo(Status.OK))
+      suite("file") {
+        val fileOk       = Http.fromResource("/TestFile.txt").deploy
+        val fileNotFound = Http.fromResource("/Nothing").deploy
+        testM("should have 200 status code") {
+          val res = fileOk.run().map(_.status)
+          assertM(res)(equalTo(Status.OK))
+        } +
+          testM("should have content-length") {
+            val res = fileOk.run().map(_.contentLength)
+            assertM(res)(isSome(equalTo(7L)))
+          } +
+          testM("should have content") {
+            val res = fileOk.run().flatMap(_.bodyAsString)
+            assertM(res)(equalTo("abc\nfoo"))
+          } +
+          testM("should have content-type") {
+            val res = fileOk.run().map(_.mediaType)
+            assertM(res)(isSome(equalTo(MediaType.text.plain)))
+          } +
+          testM("should respond with empty") {
+            val res = fileNotFound.run().map(_.status)
+            assertM(res)(equalTo(Status.NOT_FOUND))
+          }
       } +
-        testM("should have content-length") {
-          val res = testFileTxt.run().map(_.contentLength)
-          assertM(res)(isSome(equalTo(7L)))
-        } +
-        testM("should have content") {
-          val res = testFileTxt.run().flatMap(_.bodyAsString)
-          assertM(res)(equalTo("abc\nfoo"))
-        } +
-        testM("should have content-type") {
-          val res = testFileTxt.run().map(_.mediaType)
-          assertM(res)(isSome(equalTo(MediaType.text.plain)))
-        } +
-        testM("should respond with empty") {
-          val res = noFile.run().map(_.status)
-          assertM(res)(equalTo(Status.NOT_FOUND))
-        } +
-        testM("should respond with error") {
-          val res = directory.run().map(_.status)
-          assertM(res)(equalTo(Status.INTERNAL_SERVER_ERROR))
+        suite("directory") {
+          val simpleDirectory = Http.fromResource("/TestStatic").deploy
+          val fileInDirectory = Http.fromResource("/TestStatic/Folder2/TestFile2.txt").deploy
+          val fileNotFound    = Http.fromResource("/TestStatic/Folder2/Nothing").deploy
+
+          testM("should respond with error") {
+            val res = simpleDirectory.run().map(_.status)
+            assertM(res)(equalTo(Status.INTERNAL_SERVER_ERROR))
+          } +
+            testM("should have 200 status code") {
+              val res = fileInDirectory.run().map(_.status)
+              assertM(res)(equalTo(Status.OK))
+            } +
+            testM("should respond not found") {
+              val res = fileNotFound.run().map(_.status)
+              assertM(res)(equalTo(Status.NOT_FOUND))
+            }
         }
     } +
       suite("fromPath") {
