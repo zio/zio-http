e8af52183c59bfbfb32220a18be3921df0204a6b
Tushar Mathur <tusharmath@gmail.com>
2022-02-19 18:46:35 +0530

    refactor: improve style of StyledContainerHtml

--- zio-http/src/main/scala/zhttp/html/StyledContainerHtml.scala
+++ zio-http/src/main/scala/zhttp/html/StyledContainerHtml.scala
@@ -4,12 +4,23 @@ package zhttp.html
  * A ZIO Http styled container for HTML templating.
  */
 object StyledContainerHtml {
-  def apply(title: String)(element: Html): Html = {
+  def apply(heading: String)(element: Html): Html = {
     html(
-      head(),
+      head(
+        title(s"ZIO Http - ${heading}"),
+        style("""
+                | body {
+                |   font-family: monospace;
+                |   font-size: 16px;
+                | }
+                |""".stripMargin),
+      ),
       body(
-        h1(title),
-        element,
+        div(
+          styles := Seq("margin" -> "auto", "padding" -> "2em 4em", "max-width" -> "1024px"),
+          h1(heading),
+          element,
+        ),
       ),
     )
   }
