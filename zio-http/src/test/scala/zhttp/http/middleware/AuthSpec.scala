001429833252c3525f621e36cbd538e1f7e40834
dinojohn <dino.john@dream11.com>
2022-02-21 12:15:33 +0530

    added tests

--- zio-http/src/test/scala/zhttp/http/middleware/AuthSpec.scala
+++ zio-http/src/test/scala/zhttp/http/middleware/AuthSpec.scala
@@ -2,13 +2,17 @@ package zhttp.http.middleware
 
 import zhttp.http._
 import zhttp.internal.HttpAppTestExtensions
+import zio.UIO
 import zio.test.Assertion._
 import zio.test._
 
 object AuthSpec extends DefaultRunnableSpec with HttpAppTestExtensions {
-  private val basicHS    = Headers.basicAuthorizationHeader("user", "resu")
-  private val basicHF    = Headers.basicAuthorizationHeader("user", "user")
-  private val basicAuthM = Middleware.basicAuth { case (u, p) => p.toString.reverse == u }
+  private val basicHS: Headers                         = Headers.basicAuthorizationHeader("user", "resu")
+  private val basicHF: Headers                         = Headers.basicAuthorizationHeader("user", "user")
+  private val basicAuthM: HttpMiddleware[Any, Nothing] = Middleware.basicAuth { case (u, p) => p.toString.reverse == u }
+  private val basicAuthZIOM: HttpMiddleware[Any, Nothing] = Middleware.basicAuthZIO { case (u, p) =>
+    UIO(p.toString.reverse == u)
+  }
 
   def spec = suite("AuthSpec") {
     suite("basicAuth") {
@@ -24,6 +28,20 @@ object AuthSpec extends DefaultRunnableSpec with HttpAppTestExtensions {
           val app = Http.ok @@ basicAuthM header "WWW-AUTHENTICATE"
           assertM(app(Request().addHeaders(basicHF)))(isSome)
         }
-    }
+    } +
+      suite("basicAuthZIO") {
+        testM("HttpApp is accepted if the basic authentication succeeds") {
+          val app = (Http.ok @@ basicAuthZIOM).status
+          assertM(app(Request().addHeaders(basicHS)))(equalTo(Status.OK))
+        } +
+          testM("Uses forbidden app if the basic authentication fails") {
+            val app = (Http.ok @@ basicAuthZIOM).status
+            assertM(app(Request().addHeaders(basicHF)))(equalTo(Status.FORBIDDEN))
+          } +
+          testM("Responses should have WWW-Authentication header if Basic Auth failed") {
+            val app = Http.ok @@ basicAuthZIOM header "WWW-AUTHENTICATE"
+            assertM(app(Request().addHeaders(basicHF)))(isSome)
+          }
+      }
   }
 }
