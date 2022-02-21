0d9b87aa9e4c85ce2ad48668b38addf4041bc752
dinojohn <dino.john@dream11.com>
2022-02-21 11:47:54 +0530

    added effectful athu middlewares

--- zio-http/src/main/scala/zhttp/http/middleware/Auth.scala
+++ zio-http/src/main/scala/zhttp/http/middleware/Auth.scala
@@ -3,6 +3,7 @@ package zhttp.http.middleware
 import io.netty.handler.codec.http.HttpHeaderNames
 import zhttp.http.Headers.BasicSchemeName
 import zhttp.http._
+import zio.{UIO, ZIO}
 
 private[zhttp] trait Auth {
 
@@ -14,6 +15,19 @@ private[zhttp] trait Auth {
       _.basicAuthorizationCredentials match {
         case Some(header) => f(header)
         case None         => false
+      },
+      Headers(HttpHeaderNames.WWW_AUTHENTICATE, BasicSchemeName),
+    )
+
+  /**
+   * Creates a middleware for basic authentication using an effectful
+   * verification function
+   */
+  final def basicAuthZIO[R, E](f: Header => ZIO[R, E, Boolean]): HttpMiddleware[R, E] =
+    customAuthZIO(
+      _.basicAuthorizationCredentials match {
+        case Some(header) => f(header)
+        case None         => UIO(false)
       },
       Headers(HttpHeaderNames.WWW_AUTHENTICATE, BasicSchemeName),
     )
@@ -34,6 +48,20 @@ private[zhttp] trait Auth {
     responseHeaders: Headers = Headers.empty,
   ): HttpMiddleware[Any, Nothing] =
     Middleware.ifThenElse[Request](req => verify(req.headers))(
+      _ => Middleware.identity,
+      _ => Middleware.fromHttp(Http.status(Status.FORBIDDEN).addHeaders(responseHeaders)),
+    )
+
+  /**
+   * Creates an authentication middleware that only allows authenticated
+   * requests to be passed on to the app using an effectful verification
+   * function.
+   */
+  final def customAuthZIO[R, E](
+    verify: Headers => ZIO[R, E, Boolean],
+    responseHeaders: Headers = Headers.empty,
+  ): HttpMiddleware[R, E] =
+    Middleware.ifThenElseZIO[Request](req => verify(req.headers))(
       _ => Middleware.identity,
       _ => Middleware.fromHttp(Http.status(Status.FORBIDDEN).addHeaders(responseHeaders)),
     )
