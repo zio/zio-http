cf1f4c19e8c29bfdf341f5e70953ddc3569c692e
shrutiverma97 <shruti@dream11.com>
2022-02-25 11:50:35 +0530

    revert: HttpRouteTextPerf

--- zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpRouteTextPerf.scala
+++ zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpRouteTextPerf.scala
@@ -2,6 +2,7 @@ package zhttp.benchmarks
 
 import org.openjdk.jmh.annotations._
 import zhttp.http._
+import zio._
 
 import java.util.concurrent.TimeUnit
 
@@ -10,16 +11,23 @@ import java.util.concurrent.TimeUnit
 @OutputTimeUnit(TimeUnit.SECONDS)
 class HttpRouteTextPerf {
 
+  private val runtime = Runtime.default
+
   private val res          = Response.text("HELLO WORLD")
-  private val app          = Http.fromHExit(HExit.succeed(res)).whenPathEq("/text") composeHttp Http
-    .fromHExit(HExit.succeed(res))
-    .whenPathEq("/plain")
-  private val req: Request = Request(Method.GET, URL(!! / "text"))
+  private val app          = Http.succeed(res)
+  private val req: Request = Request(Method.GET, URL(!!))
+  private val httpProgram  = ZIO.foreach_(0 to 1000) { _ => app.execute(req).toZIO }
+  private val UIOProgram   = ZIO.foreach_(0 to 1000) { _ => UIO(res) }
 
   @Benchmark
   def benchmarkHttpProgram(): Unit = {
-    val _ = app.execute(req)
+    runtime.unsafeRun(httpProgram)
     ()
   }
 
+  @Benchmark
+  def benchmarkUIOProgram(): Unit = {
+    runtime.unsafeRun(UIOProgram)
+    ()
+  }
 }
