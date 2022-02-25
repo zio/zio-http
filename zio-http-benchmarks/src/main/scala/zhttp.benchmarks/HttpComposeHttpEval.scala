361f4913184f65ee9374b92946ea9f62564990c5
shrutiverma97 <shruti@dream11.com>
2022-02-25 19:10:16 +0530

    added benchmark code

---
+++ zio-http-benchmarks/src/main/scala/zhttp.benchmarks/HttpComposeHttpEval.scala
@@ -0,0 +1,27 @@
+package zhttp.benchmarks
+
+import org.openjdk.jmh.annotations._
+import zhttp.http._
+
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class HttpComposeHttpEval {
+  private val MAX  = 1000
+  private val app  = Http.collect[Int] { case 0 => 1 }
+  private val spec = (0 to MAX).foldLeft(app)((a, _) => a composeHttp app)
+
+  @Benchmark
+  def benchmarkNotFound(): Unit = {
+    spec.execute(-1)
+    ()
+  }
+
+  @Benchmark
+  def benchmarkOk(): Unit = {
+    spec.execute(0)
+    ()
+  }
+}
