5f522b1bbfbd9adeef9513afd1bbb7f642d2e63d
shrutiverma97 <shruti@dream11.com>
2022-03-01 12:33:44 +0530

    refactor: plaintextBenchmarkServer

--- example/src/main/scala/example/PlainTextBenchmarkServer.scala
+++ example/src/main/scala/example/PlainTextBenchmarkServer.scala
@@ -1,7 +1,7 @@
 package example
 
 import io.netty.util.AsciiString
-import zhttp.http._
+import zhttp.http.{Http, _}
 import zhttp.service.server.ServerChannelFactory
 import zhttp.service.{EventLoopGroup, Server}
 import zio.{App, ExitCode, UIO, URIO}
@@ -11,41 +11,43 @@ import zio.{App, ExitCode, UIO, URIO}
  */
 object Main extends App {
 
-  private val message: String = "Hello, World!"
-  private val json: String    = """{"greetings": "Hello World!"}"""
+  private val plainTextMessage: String = "Hello, World!"
+  private val jsonMessage: String      = """{"greetings": "Hello World!"}"""
 
-  val path1 = "/plaintext"
-  val path2 = "/json"
+  private val plaintextPath = "/plaintext"
+  private val jsonPath      = "/json"
 
   private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
 
   private val frozenJsonResponse = Response
-    .json(json)
+    .json(jsonMessage)
     .withServerTime
     .withServer(STATIC_SERVER_NAME)
     .freeze
 
-  private val frozenResponse = Response
-    .text(message)
+  private val frozenPlainTextResponse = Response
+    .text(plainTextMessage)
     .withServerTime
     .withServer(STATIC_SERVER_NAME)
     .freeze
 
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    val s = for {
-      res1 <- frozenResponse
-      res2 <- frozenJsonResponse
-    } yield server(res1, res2)
-    s.flatMap(_.make.useForever.provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))).exitCode
-  }
+  private def plainTextApp(response: Response) = Http.fromHExit(HExit.succeed(response)).whenPathEq(plaintextPath)
 
-  private def app(response: Response, json: Response) =
-    Http.fromHExit(HExit.succeed(response)).whenPathEq(path1) ++ Http
-      .fromHExit(HExit.succeed(json))
-      .whenPathEq(path2)
+  private def jsonApp(json: Response) = Http.fromHExit(HExit.succeed(json)).whenPathEq(jsonPath)
 
-  private def server(response: Response, json: Response) =
-    Server.app(app(response, json)) ++
+  private def app = for {
+    plainTextResponse <- frozenPlainTextResponse
+    jsonResponse      <- frozenJsonResponse
+  } yield plainTextApp(plainTextResponse) ++ jsonApp(jsonResponse)
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    app
+      .flatMap(server(_).make.useForever)
+      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
+      .exitCode
+
+  private def server(app: HttpApp[Any, Nothing]) =
+    Server.app(app) ++
       Server.port(8080) ++
       Server.error(_ => UIO.unit) ++
       Server.disableLeakDetection ++
