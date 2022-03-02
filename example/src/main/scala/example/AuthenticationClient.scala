576e29daf89b615643f8f0be82dc829fb687df56
Shubham Girdhar <girdharshubham@hotmail.com>
2022-03-02 18:17:26 +0530

    refactor: example AuthenticationClient

--- example/src/main/scala/example/AuthenticationClient.scala
+++ example/src/main/scala/example/AuthenticationClient.scala
@@ -6,18 +6,14 @@ import zio.{App, ExitCode, URIO}
 
 object AuthenticationClient extends App {
   // start AuthenticationServer before running this client
-
+  val url = "http://localhost:8090"
   val env = ChannelFactory.auto ++ EventLoopGroup.auto()
 
   val program = for {
-    res1  <- Client.request("http://localhost:8090/login/username/emanresu")
-    data1 <- res1.bodyAsString
-    res2  <- Client.request(
-      "http://localhost:8090/user/userName/greet",
-      headers = Headers.bearerAuthorizationHeader(data1),
-    )
-    data2 <- res2.bodyAsString
-    _ = println(data2)
+    token    <- Client.request(s"${url}/login/username/emanresu").flatMap(_.bodyAsString)
+    response <- Client.request(s"${url}/user/userName/greet", headers = Headers.bearerAuthorizationHeader(token))
+    body     <- response.bodyAsString
+    _        <- zio.console.putStrLn(body)
   } yield ()
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = program.exitCode.provideCustomLayer(env)
