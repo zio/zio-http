0d7f6d9eede5810d5d110bf2f5fe99d130d6b3bd
ex0ns <g.tournigand@gmail.com>
2022-03-04 18:15:16 +0100

    Fix toByteBuf for streamed HttpData

--- zio-http/src/test/scala/zhttp/http/HttpDataSpec.scala
+++ zio-http/src/test/scala/zhttp/http/HttpDataSpec.scala
@@ -1,16 +1,31 @@
 package zhttp.http
 
 import zio.test.Assertion.equalTo
-import zio.test.{DefaultRunnableSpec, assertM}
+import zio.test.{DefaultRunnableSpec, assertM, checkAllM}
 
 import java.io.File
+import zio.stream.ZStream
+import zio.test.Gen
 
 object HttpDataSpec extends DefaultRunnableSpec {
   // TODO : Add tests for othe HttpData types
   override def spec =
-    suite("HttpDataSpec")(suite("Test toByteBuf")(testM("HttpData.fromFile") {
-      val file = new File(getClass.getResource("/TestFile.txt").getPath)
-      val res  = HttpData.fromFile(file).toByteBuf.map(_.toString(HTTP_CHARSET))
-      assertM(res)(equalTo("abc\nfoo"))
-    }))
+    suite("HttpDataSpec")(
+      suite("Test toByteBuf")(
+        testM("HttpData.fromFile") {
+          val file = new File(getClass.getResource("/TestFile.txt").getPath)
+          val res  = HttpData.fromFile(file).toByteBuf.map(_.toString(HTTP_CHARSET))
+          assertM(res)(equalTo("abc\nfoo"))
+        },
+        testM("HttpData.fromStream") {
+          checkAllM(Gen.anyString) { payload =>
+            println(payload)
+            val stringBuffer    = payload.toString.getBytes(HTTP_CHARSET)
+            val responseContent = ZStream.fromIterable(stringBuffer)
+            val res             = HttpData.fromStream(responseContent).toByteBuf.map(_.toString(HTTP_CHARSET))
+            assertM(res)(equalTo(payload))
+          }
+        },
+      ),
+    )
 }
