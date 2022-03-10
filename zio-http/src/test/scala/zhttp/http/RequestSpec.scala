8b0033486ba98b69e7fb9bdfd047399ecf105ea8
Nikolay Artamonov <nartamonov@gmail.com>
2022-03-10 14:16:11 +0300

    Improved tests for Request.toString

--- zio-http/src/test/scala/zhttp/http/RequestSpec.scala
+++ zio-http/src/test/scala/zhttp/http/RequestSpec.scala
@@ -1,18 +1,25 @@
 package zhttp.http
 
+import zhttp.internal.HttpGen
 import zio.test.Assertion._
 import zio.test._
 
 object RequestSpec extends DefaultRunnableSpec {
   def spec = suite("Request")(
     suite("toString") {
-      test("should produce string representation of a request") {
-        val r = Request()
-        assert(r.toString)(matchesRegex("""Request\(.*\)"""))
+      testM("should produce string representation of a request") {
+        check(HttpGen.request) { req =>
+          assert(req.toString)(equalTo(s"Request(${req.method}, ${req.url}, ${req.headers}, ${req.remoteAddress})"))
+        }
       } +
-        test("should produce string representation of a parameterized request") {
-          val r = Request.ParameterizedRequest(Request(), "TestParam")
-          assert(r.toString)(matchesRegex("""ParameterizedRequest\(.*\)"""))
+        testM("should produce string representation of a parameterized request") {
+          check(HttpGen.parameterizedRequest(Gen.alphaNumericString)) { req =>
+            assert(req.toString)(
+              equalTo(
+                s"ParameterizedRequest(Request(${req.method}, ${req.url}, ${req.headers}, ${req.remoteAddress}), ${req.params})",
+              ),
+            )
+          }
         }
     },
   )
