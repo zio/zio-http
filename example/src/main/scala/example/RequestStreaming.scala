7d2a32000c874f101f0eab9bac22bda0db6f6378
Tushar Mathur <tusharmath@gmail.com>
2022-03-10 21:05:26 +0530

    doc: add documentation

--- example/src/main/scala/example/RequestStreaming.scala
+++ example/src/main/scala/example/RequestStreaming.scala
@@ -5,9 +5,17 @@ import zhttp.service.Server
 import zio._
 object RequestStreaming extends App {
 
-  // Create HTTP route which echos back the request body as Stream
-  val app: HttpApp[Any, Nothing] = Http.collect[Request] { case req @ Method.POST -> !! / "echo" =>
-    Response(data = HttpData.fromStream(req.bodyAsStream))
+  // Create HTTP route which echos back the request body
+  val app = Http.collect[Request] { case req @ Method.POST -> !! / "echo" =>
+    // Returns a stream of bytes from the request
+    // The stream supports back-pressure
+    val stream = req.bodyAsStream
+
+    // Creating HttpData from the stream
+    // This works for file of any size
+    val data = HttpData.fromStream(stream)
+
+    Response(data = data)
   }
 
   // Run it like any simple app
