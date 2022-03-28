71a5b0afd0ced9d10e2ffcd91a0120f9b3022303
Tushar Mathur <tusharmath@gmail.com>
2022-03-28 09:32:52 +0530

    refactor: DynamicServer is not bound to HttpEnv only (#1159)

--- zio-http/src/test/scala/zhttp/internal/package.scala
+++ zio-http/src/test/scala/zhttp/internal/package.scala
@@ -1,7 +1,9 @@
 package zhttp
 
+import zhttp.service.{ChannelFactory, EventLoopGroup, ServerChannelFactory}
 import zio.Has
 
 package object internal {
   type DynamicServer = Has[DynamicServer.Service]
+  type HttpEnv       = EventLoopGroup with ChannelFactory with DynamicServer with ServerChannelFactory
 }
