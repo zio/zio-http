6a33bbaa542eb9653cb6b0c3e3505cf047e26ca4
Tushar Mathur <tusharmath@gmail.com>
2022-04-09 16:32:06 +0530

    refactor: reorder methods

--- zio-http-logging/src/main/scala/zhttp/logging/LogLevel.scala
+++ zio-http-logging/src/main/scala/zhttp/logging/LogLevel.scala
@@ -1,39 +1,43 @@
 package zhttp.logging
 
 sealed trait LogLevel { self =>
-  def name: String
-  def level: Int
   private[zhttp] def methodName    = name.toLowerCase
   def >=(right: LogLevel): Boolean = right.level >= self.level
+  def level: Int
+  def name: String
 }
 
 /**
  * Defines standard log levels.
  */
 object LogLevel {
-  case object OFF   extends LogLevel {
-    override def name: String = ""
+  case object OFF extends LogLevel {
     override def level: Int   = 999
-  }
-  case object TRACE extends LogLevel {
-    override def name: String = "TRACE"
-    override def level: Int   = 1
-  }
-  case object DEBUG extends LogLevel {
-    override def name: String = "DEBUG"
-    override def level: Int   = 2
-  }
-  case object INFO  extends LogLevel {
-    override def name: String = "INFO"
-    override def level: Int   = 3
-  }
-  case object WARN  extends LogLevel {
-    override def name: String = "WARN"
-    override def level: Int   = 4
-  }
-  case object ERROR extends LogLevel {
-    override def name: String = "ERROR"
-    override def level: Int   = 5
+    override def name: String = ""
   }
 
+  case object TRACE extends LogLevel {
+    override def level: Int   = 1
+    override def name: String = "TRACE"
+  }
+
+  case object DEBUG extends LogLevel {
+    override def level: Int   = 2
+    override def name: String = "DEBUG"
+  }
+
+  case object INFO extends LogLevel {
+    override def level: Int   = 3
+    override def name: String = "INFO"
+  }
+
+  case object WARN extends LogLevel {
+    override def level: Int   = 4
+    override def name: String = "WARN"
+  }
+
+  case object ERROR extends LogLevel {
+    override def level: Int   = 5
+    override def name: String = "ERROR"
+  }
 }
