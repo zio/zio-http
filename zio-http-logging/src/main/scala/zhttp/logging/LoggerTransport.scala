35ebba4dba5ada98130b4840e2ff861d80221a76
Tushar Mathur <tusharmath@gmail.com>
2022-04-10 14:45:31 +0530

    refactor: add LogTransport

---
+++ zio-http-logging/src/main/scala/zhttp/logging/LoggerTransport.scala
@@ -0,0 +1,35 @@
+package zhttp.logging
+
+import zhttp.logging.LogFrontend.LogLine
+import zhttp.logging.LoggerTransport.Transport
+
+/**
+ * Provides a way to build and configure transports for logging. Transports are
+ * used to, format and serialize LogLines and them to a backend.
+ */
+final case class LoggerTransport(level: LogLevel, format: _ => String, transport: Transport) { self =>
+  def withFormat(format: LogLine => String): LoggerTransport = self.copy(format = format)
+  def withFormat(format: LogFormat): LoggerTransport         = self.copy(format = format(_))
+  def withLevel(level: LogLevel): LoggerTransport            = self.copy(level = level)
+}
+
+object LoggerTransport {
+  def console: LoggerTransport = LoggerTransport(
+    level = LogLevel.OFF,
+    format = LogFormat.default(_),
+    transport = Transport.UnsafeSync(println),
+  )
+
+  sealed trait Transport { self =>
+    def run(line: String): Unit =
+      self match {
+        case Transport.UnsafeSync(log) => log(line)
+        case Transport.Empty           => ()
+      }
+  }
+
+  object Transport {
+    final case class UnsafeSync(log: String => Unit) extends Transport
+    case object Empty                                extends Transport
+  }
+}
