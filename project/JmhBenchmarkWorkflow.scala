6412ec07223e6863de426e3dd6083e771c5c5bf4
shrutiverma97 <shruti@dream11.com>
2022-04-13 14:37:33 +0530

    change in workflow

--- project/JmhBenchmarkWorkflow.scala
+++ project/JmhBenchmarkWorkflow.scala
@@ -81,6 +81,7 @@ object JmhBenchmarkWorkflow {
          |          echo "" >> finalMain.txt;
          |          fi
          |           done < c.txt
+         |echo "$$(<Current.txt)"
          |echo "$$(<finalCurrent.txt)"
          |paste -d '|' c.txt finalCurrent.txt finalMain.txt > FinalOutput.txt
          | sed -i -e 's/^/|/' FinalOutput.txt
