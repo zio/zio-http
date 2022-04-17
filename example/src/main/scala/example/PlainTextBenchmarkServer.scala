d1c0dae0555fd93c69736a47fe1e61dd8ccdaf89
Shubham Girdhar <girdharshubham@hotmail.com>
2022-04-17 23:51:04 +0530

    workflow dispatch configurable server

--- example/src/main/scala/example/PlainTextBenchmarkServer.scala
+++ example/src/main/scala/example/PlainTextBenchmarkServer.scala
@@ -3,7 +3,7 @@ package example
 import io.netty.util.AsciiString
 import zhttp.http.{Http, _}
 import zhttp.service.server.ServerChannelFactory
-import zhttp.service.{EventLoopGroup, Server}
+import zhttp.service.{EventLoopGroup, Server, UServer}
 import zio.{App, ExitCode, UIO, URIO}
 
 /**
@@ -42,16 +42,32 @@ object Main extends App {
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     app
-      .flatMap(server(_).make.useForever)
+      .flatMap(app => server(app))
+      .flatMap(_.make.useForever)
       .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
       .exitCode
 
-  private def server(app: HttpApp[Any, Nothing]) =
-    Server.app(app) ++
-      Server.port(8080) ++
-      Server.error(_ => UIO.unit) ++
-      Server.disableLeakDetection ++
-      Server.consolidateFlush ++
-      Server.disableFlowControl
+  private def server(app: HttpApp[Any, Nothing]) = {
+    def leakDetectionLevel(level: String): UServer = level match {
+      case "disabled" => Server.disableLeakDetection
+      case "simple"   => Server.simpleLeakDetection
+      case "advanced" => Server.advancedLeakDetection
+      case "paranoid" => Server.paranoidLeakDetection
+    }
+
+    def from[E <: Throwable](condition: Boolean, server: Server[Any, E], setting: UServer) =
+      if (condition) server ++ setting else server
+
+    zio.system.envs.map { envs =>
+      val server = Server.port(8080) ++
+        Server.app(app) ++
+        Server.error(_ => UIO.unit) ++
+        leakDetectionLevel(envs.getOrElse("LEAK_DETECTION_LEVEL", "disabled"))
+
+      server ++
+        from(envs.getOrElse("CONSOLIDATE_FLUSH", "true").toBoolean, server, Server.consolidateFlush) ++
+        from(envs.getOrElse("DISABLE_FLOW_CONTROL", "true").toBoolean, server, Server.disableFlowControl)
+    }
+  }
 
 }
