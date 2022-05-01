7c6c023bf60b2332468e0aca4f67ae79b3f9414a
Tushar Mathur <tusharmath@gmail.com>
2022-05-01 17:15:14 +0530

    refactor: simplify LogTransport

--- zio-http-logging/src/main/scala/zhttp/logging/LoggerTransport.scala
+++ zio-http-logging/src/main/scala/zhttp/logging/LoggerTransport.scala
@@ -12,16 +12,18 @@ import java.util
  * used to, format and serialize LogLines and them to a backend.
  */
 private[logging] final case class LoggerTransport(
-  level: LogLevel,
   format: Setup.LogFormat,
-  filter: String => Boolean,
-  transport: Transport,
+  level: LogLevel = LogLevel.Disable,
+  filter: String => Boolean = _ => true,
+  transport: Transport = Transport.empty,
+  tags: List[String] = Nil,
 ) { self =>
   private[zhttp] val isDebugEnabled: Boolean = self.level >= LogLevel.Debug
   private[zhttp] val isErrorEnabled: Boolean = self.level >= LogLevel.Error
   private[zhttp] val isInfoEnabled: Boolean  = self.level >= LogLevel.Info
   private[zhttp] val isTraceEnabled: Boolean = self.level >= LogLevel.Trace
   private[zhttp] val isWarnEnabled: Boolean  = self.level >= LogLevel.Warn
+  private[zhttp] val isEnabled: Boolean      = self.level != LogLevel.Disable
 
   private def buildLines(
     msg: String,
@@ -50,20 +52,6 @@ private[logging] final case class LoggerTransport(
     }
   }
 
-  private def logMayBe(
-    msg: String,
-    throwable: Option[Throwable],
-    logLevel: LogLevel,
-    tags: List[String],
-    enclosingClass: String,
-    lineNumber: Int,
-  ): Unit =
-    if (filter(tags.mkString)) {
-      buildLines(msg, throwable, logLevel, tags, enclosingClass, lineNumber).foreach { line =>
-        self.log(format(line))
-      }
-    }
-
   private def stackTraceAsString(throwable: Throwable): String = {
     val sw = new StringWriter
     throwable.printStackTrace(new PrintWriter(sw))
@@ -72,54 +60,40 @@ private[logging] final case class LoggerTransport(
 
   private def thread = Thread.currentThread()
 
-  def debug(msg: String, tags: List[String], enclosingClass: String, lineNumber: Int): Unit =
-    logMayBe(msg, None, LogLevel.Debug, tags, enclosingClass, lineNumber)
+  def addTags(tags: List[String]): LoggerTransport = self.copy(tags = self.tags ++ tags)
 
-  def error(msg: String, tags: List[String], enclosingClass: String, lineNumber: Int): Unit =
-    logMayBe(msg, None, LogLevel.Error, tags, enclosingClass, lineNumber)
-
-  def error(
-    msg: String,
-    throwable: Throwable,
-    tags: List[String],
-    enclosingClass: String,
-    lineNumber: Int,
-  ): Unit =
-    logMayBe(msg, Some(throwable), LogLevel.Error, tags, enclosingClass, lineNumber)
-
-  def info(msg: String, tags: List[String], enclosingClass: String, lineNumber: Int): Unit =
-    logMayBe(msg, None, LogLevel.Info, tags, enclosingClass, lineNumber)
-
-  def log(msg: CharSequence): Unit = if (filter(msg.toString)) transport.run(msg)
-
-  def trace(msg: String, tags: List[String], enclosingClass: String, lineNumber: Int): Unit =
-    logMayBe(msg, None, LogLevel.Trace, tags, enclosingClass, lineNumber)
-
-  def warn(msg: String, tags: List[String], enclosingClass: String, lineNumber: Int): Unit =
-    logMayBe(msg, None, LogLevel.Warn, tags, enclosingClass, lineNumber)
+  def log(msg: String, cause: Option[Throwable], level: LogLevel, tags: List[String], cname: String, lno: Int): Unit =
+    if (filter(tags.mkString)) {
+      buildLines(msg, cause, level, tags ++ self.tags, cname, lno).foreach { line =>
+        if (filter(format(line).toString)) transport.run(format(line))
+      }
+    }
 
   def withFilter(filter: String => Boolean): LoggerTransport = self.copy(filter = filter)
 
-  def withFormat(format: LogLine => CharSequence): LoggerTransport = self.copy(format = format)
-
   def withFormat(format: LogFormat): LoggerTransport = self.copy(format = LogFormat.run(format))
 
+  def withFormat(format: LogLine => CharSequence): LoggerTransport = self.copy(format = format)
+
   def withLevel(level: LogLevel): LoggerTransport = self.copy(level = level)
+
+  def withTags(tags: List[String]): LoggerTransport = self.copy(tags = tags)
+
 }
 
 object LoggerTransport {
   def console: LoggerTransport = LoggerTransport(
-    level = LogLevel.Disable,
     format = LogFormat.default,
+    level = LogLevel.Disable,
     filter = _ => true,
-    transport = Transport.UnsafeSync(println),
+    transport = Transport.unsafeSync(println),
   )
 
   def file(filePath: Path): LoggerTransport = LoggerTransport(
-    level = LogLevel.Disable,
     format = LogFormat.simple,
+    level = LogLevel.Disable,
     filter = _ => true,
-    transport = Transport.UnsafeFileSync(filePath),
+    transport = Transport.unsafeFileSync(filePath),
   )
 
   trait Transport { self =>
@@ -127,24 +101,21 @@ object LoggerTransport {
   }
 
   object Transport {
-    final case class UnsafeSync(log: CharSequence => Unit) extends Transport {
-      override def run(line: CharSequence): Unit = log(line)
-
-    }
-
-    final case class UnsafeFileSync(path: Path) extends Transport {
-      override def run(line: CharSequence): Unit = {
-        Files.write(
-          path,
-          util.Arrays.asList(line),
-          StandardOpenOption.APPEND,
-          StandardOpenOption.CREATE,
-        ): Unit
-      }
-    }
-
-    case object Empty extends Transport {
+    val empty: Transport = new Transport {
       override def run(line: CharSequence): Unit = ()
+    }
+
+    def unsafeFileSync(path: Path): Transport = unsafeSync { line =>
+      Files.write(
+        path,
+        util.Arrays.asList(line),
+        StandardOpenOption.APPEND,
+        StandardOpenOption.CREATE,
+      ): Unit
+    }
+
+    def unsafeSync(log: CharSequence => Unit): Transport = new Transport {
+      override def run(line: CharSequence): Unit = log(line)
     }
   }
 }
