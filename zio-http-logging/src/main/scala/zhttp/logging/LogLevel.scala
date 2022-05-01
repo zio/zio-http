a0728fc57b15328d7372fb9a6608ecdc5f6983fd
Tushar Mathur <tusharmath@gmail.com>
2022-05-01 12:15:33 +0530

    feat: add `detectFromEnv` method to LogLevel

--- zio-http-logging/src/main/scala/zhttp/logging/LogLevel.scala
+++ zio-http-logging/src/main/scala/zhttp/logging/LogLevel.scala
@@ -11,6 +11,21 @@ sealed trait LogLevel { self =>
  * Defines standard log levels.
  */
 object LogLevel {
+
+  /**
+   * Automatically detects the level from the environment variable - "ZHTTP_LOG"
+   */
+  def detectFromEnv(name: String): LogLevel = {
+    sys.env.getOrElse(name, Debug.name).toUpperCase match {
+      case "TRACE" => Trace
+      case "DEBUG" => Debug
+      case "INFO"  => Info
+      case "WARN"  => Warn
+      case "ERROR" => Error
+      case _       => Disable
+    }
+  }
+
   case object Disable extends LogLevel {
     override def level: Int   = 999
     override def name: String = ""
