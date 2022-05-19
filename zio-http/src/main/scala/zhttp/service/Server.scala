192e1e03e61852f63f4cab399b4c56aa0aa41fac
Amit Kumar Singh <amitksingh1490@gmail.com>
2022-05-19 09:07:36 +0000

    Reformat with scalafmt 3.5.4

--- zio-http/src/main/scala/zhttp/service/Server.scala
+++ zio-http/src/main/scala/zhttp/service/Server.scala
@@ -212,8 +212,8 @@ object Server {
     port: Int,
     http: HttpApp[R, Throwable],
   ): ZIO[R, Throwable, Nothing] = {
-    (Server(http)
-      .withPort(port))
+    Server(http)
+      .withPort(port)
       .make
       .flatMap(start => ZManaged.succeed(println(s"Server started on port: ${start.port}")))
       .useForever
@@ -225,8 +225,8 @@ object Server {
     port: Int,
     http: HttpApp[R, Throwable],
   ): ZIO[R, Throwable, Nothing] =
-    (Server(http)
-      .withBinding(address, port))
+    Server(http)
+      .withBinding(address, port)
       .make
       .useForever
       .provideSomeLayer[R](EventLoopGroup.auto(0) ++ ServerChannelFactory.auto)
@@ -235,8 +235,8 @@ object Server {
     socketAddress: InetSocketAddress,
     http: HttpApp[R, Throwable],
   ): ZIO[R, Throwable, Nothing] =
-    (Server(http)
-      .withBinding(socketAddress))
+    Server(http)
+      .withBinding(socketAddress)
       .make
       .useForever
       .provideSomeLayer[R](EventLoopGroup.auto(0) ++ ServerChannelFactory.auto)
