19c646d02accf7b313c6112df09000246d520fb3
Gabriel Ciuloaica <con_gabriel.ciuloaica@dream11.com>
2022-06-23 13:34:57 +0300

    removed the PING/PONG example as it creates confusions. The PING/PONG protocol is implemented in Netty layer and it is not reachig zhttp layer

--- example/src/main/scala/example/WebSocketEcho.scala
+++ example/src/main/scala/example/WebSocketEcho.scala
@@ -12,8 +12,6 @@ object WebSocketEcho extends App {
     Socket.collect[WebSocketFrame] {
       case WebSocketFrame.Text("FOO")  => ZStream.succeed(WebSocketFrame.text("BAR"))
       case WebSocketFrame.Text("BAR")  => ZStream.succeed(WebSocketFrame.text("FOO"))
-      case WebSocketFrame.Ping         => ZStream.succeed(WebSocketFrame.pong)
-      case WebSocketFrame.Pong         => ZStream.succeed(WebSocketFrame.ping)
       case fr @ WebSocketFrame.Text(_) => ZStream.repeat(fr).schedule(Schedule.spaced(1 second)).take(10)
     }
 
