2d31c65dd23129c471b154b3d7044274603a805e
Tushar Mathur <tusharmath@gmail.com>
2022-06-30 14:54:35 +0530

    refactor: WebSocketAppHandler to dispatch ChannelEvent.

--- zio-http/src/main/scala/zhttp/service/WebSocketAppHandler.scala
+++ zio-http/src/main/scala/zhttp/service/WebSocketAppHandler.scala
@@ -3,10 +3,9 @@ package zhttp.service
 import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
 import io.netty.handler.codec.http.websocketx.WebSocketClientProtocolHandler.ClientHandshakeStateEvent
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler.ServerHandshakeStateEvent
-import io.netty.handler.codec.http.websocketx.{WebSocketFrame => JWebSocketFrame, WebSocketServerProtocolHandler}
-import zhttp.socket.SocketApp.Handle
+import io.netty.handler.codec.http.websocketx.{WebSocketServerProtocolHandler, WebSocketFrame => JWebSocketFrame}
+import zhttp.service.ChannelEvent.UserEvent
 import zhttp.socket.{SocketApp, WebSocketFrame}
-import zio.stream.ZStream
 
 /**
  * A generic SocketApp handler that can be used on both - the client and the
@@ -17,60 +16,34 @@ final class WebSocketAppHandler[R](
   app: SocketApp[R],
 ) extends SimpleChannelInboundHandler[JWebSocketFrame] {
 
-  override def channelRead0(ctx: ChannelHandlerContext, msg: JWebSocketFrame): Unit = {
+  private def dispatch(ctx: ChannelHandlerContext)(event: ChannelEvent[WebSocketFrame, WebSocketFrame]): Unit =
     app.message match {
-      case Some(v) =>
-        WebSocketFrame.fromJFrame(msg) match {
-          case Some(frame) => writeAndFlush(ctx, v(frame))
-          case _           => ()
-        }
+      case Some(f) => zExec.unsafeRun(ctx)(f(event))
       case None    => ()
     }
-  }
 
-  override def channelUnregistered(ctx: ChannelHandlerContext): Unit = {
-    app.close match {
-      case Some(v) => zExec.unsafeRunUninterruptible(ctx)(v(ctx.channel().remoteAddress()))
-      case None    => ctx.fireChannelUnregistered()
+  override def channelRead0(ctx: ChannelHandlerContext, msg: JWebSocketFrame): Unit =
+    dispatch(ctx) {
+      ChannelEvent.channelRead(ctx, WebSocketFrame.unsafeFromJFrame(msg))
     }
-    ()
-  }
 
-  override def exceptionCaught(ctx: ChannelHandlerContext, x: Throwable): Unit = {
-    app.error match {
-      case Some(v) => zExec.unsafeRunUninterruptible(ctx)(v(x))
-      case None    => ctx.fireExceptionCaught(x)
+  override def channelUnregistered(ctx: ChannelHandlerContext): Unit =
+    dispatch(ctx) {
+      ChannelEvent.channelUnregistered(ctx)
     }
-    ()
-  }
 
-  override def userEventTriggered(ctx: ChannelHandlerContext, event: AnyRef): Unit = {
-    event match {
-      case _: WebSocketServerProtocolHandler.HandshakeComplete | ClientHandshakeStateEvent.HANDSHAKE_COMPLETE =>
-        app.open match {
-          case Some(v) =>
-            v match {
-              case Handle.WithEffect(f) => zExec.unsafeRun(ctx)(f(ctx.channel().remoteAddress()))
-              case Handle.WithSocket(s) => writeAndFlush(ctx, s(ctx.channel().remoteAddress()))
-            }
-          case None    => ctx.fireUserEventTriggered(event)
-        }
-      case ServerHandshakeStateEvent.HANDSHAKE_TIMEOUT | ClientHandshakeStateEvent.HANDSHAKE_TIMEOUT          =>
-        app.timeout match {
-          case Some(v) => zExec.unsafeRun(ctx)(v)
-          case None    => ctx.fireUserEventTriggered(event)
-        }
-      case event => ctx.fireUserEventTriggered(event)
+  override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit =
+    dispatch(ctx) {
+      ChannelEvent.exceptionCaught(ctx, cause)
     }
-    ()
-  }
 
-  /**
-   * Unsafe channel reader for WSFrame
-   */
-  private def writeAndFlush(ctx: ChannelHandlerContext, stream: ZStream[R, Throwable, WebSocketFrame]): Unit = {
-    zExec.unsafeRun(ctx) {
-      stream.foreach(frame => ChannelFuture.unit(ctx.writeAndFlush(frame.toWebSocketFrame)))
+  override def userEventTriggered(ctx: ChannelHandlerContext, event: AnyRef): Unit =
+    dispatch(ctx) {
+      event match {
+        case _: WebSocketServerProtocolHandler.HandshakeComplete | ClientHandshakeStateEvent.HANDSHAKE_COMPLETE =>
+          ChannelEvent.userEventTriggered(ctx, UserEvent.HandshakeComplete)
+        case ServerHandshakeStateEvent.HANDSHAKE_TIMEOUT | ClientHandshakeStateEvent.HANDSHAKE_TIMEOUT          =>
+          ChannelEvent.userEventTriggered(ctx, UserEvent.HandshakeTimeout)
+      }
     }
-  }
 }
