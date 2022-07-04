e30e872f33f8b06da6a6b7e8dd33d477137fa3ff
Tushar Mathur <tusharmath@gmail.com>
2022-07-04 17:34:25 +0530

    refactor: update WebSocketServerSpec

--- zio-http/src/test/scala/zhttp/service/WebSocketServerSpec.scala
+++ zio-http/src/test/scala/zhttp/service/WebSocketServerSpec.scala
@@ -20,77 +20,76 @@ object WebSocketServerSpec extends HttpRunnableSpec {
     EventLoopGroup.nio() ++ ServerChannelFactory.nio ++ DynamicServer.live ++ ChannelFactory.nio
   private val app = serve { DynamicServer.app }
 
-  override def spec = suiteM("Server") {
-    app.as(List(websocketServerSpec, websocketFrameSpec, websocketOnCloseSpec)).useNow
-  }.provideCustomLayerShared(env) @@ timeout(30 seconds)
+  override def spec = suiteM("Server") { app.as(List(websocketSpec)).useNow }
+    .provideCustomLayerShared(env) @@ timeout(30 seconds)
 
-  def websocketFrameSpec = suite("WebSocketFrameSpec") {
-    testM("binary") {
-      val socket = Http.collectZIO[WebSocketChannelEvent] {
-        case ChannelEvent(ch, ChannelRead(WebSocketFrame.Binary(buffer))) =>
-          ch.writeAndFlush(WebSocketFrame.Binary(buffer))
+  val websocketSpec = suite("WebsocketSpec")(
+    suite("WebSocketFrameSpec") {
+      testM("binary") {
+        val socket = Http.collectZIO[WebSocketChannelEvent] {
+          case ChannelEvent(ch, ChannelRead(WebSocketFrame.Binary(buffer))) =>
+            ch.writeAndFlush(WebSocketFrame.Binary(buffer))
 
-        case ChannelEvent(ch, UserEventTriggered(HandshakeComplete)) =>
-          ch.writeAndFlush(WebSocketFrame.binary(Chunk.fromArray("Hello, World".getBytes)))
+          case ChannelEvent(ch, UserEventTriggered(HandshakeComplete)) =>
+            ch.writeAndFlush(WebSocketFrame.binary(Chunk.fromArray("Hello, World".getBytes)))
+        }
+
+        val app = socket.toSocketApp.toHttp.deployWS
+
+        assertM(app(socket.toSocketApp).map(_.status))(equalTo(Status.SwitchingProtocols))
       }
+    },
+    suite("WebSocketOnCloseSpec") {
+      testM("success") {
+        for {
+          clockEnv <- ZIO.environment[Clock]
 
-      val app = socket.toSocketApp.toHttp.deployWS
+          // Maintain a flag to check if the close handler was completed
+          isSet     <- Promise.make[Nothing, Unit]
+          isStarted <- Promise.make[Nothing, Unit]
 
-      assertM(app(socket.toSocketApp).map(_.status))(equalTo(Status.SwitchingProtocols))
-    }
-  }
+          // Setup websocket server
 
-  def websocketOnCloseSpec = suite("WebSocketOnCloseSpec") {
-    testM("success") {
-      for {
-        clockEnv <- ZIO.environment[Clock]
+          serverHttp   = Http
+            .collectZIO[WebSocketChannelEvent] { case ChannelEvent(_, ChannelUnregistered) =>
+              isStarted.succeed(()) <&> isSet.succeed(()).delay(5 seconds)
+            }
+            .toSocketApp
+            .toHttp
+            .deployWS
 
-        // Maintain a flag to check if the close handler was completed
-        isSet     <- Promise.make[Nothing, Unit]
-        isStarted <- Promise.make[Nothing, Unit]
+          // Setup Client
+          // Client closes the connection after 1 secon
+          clientSocket = Http
+            .collectZIO[WebSocketChannelEvent] { case ChannelEvent(ch, UserEventTriggered(HandshakeComplete)) =>
+              ch.writeAndFlush(WebSocketFrame.close(1000)).delay(1 second)
+            }
+            .toSocketApp
 
-        // Setup websocket server
+          // Deploy the server and send it a socket request
+          _ <- serverHttp(clientSocket.provideEnvironment(clockEnv))
 
-        serverHttp   = Http
-          .collectZIO[WebSocketChannelEvent] { case ChannelEvent(_, ChannelUnregistered) =>
-            isStarted.succeed(()) <&> isSet.succeed(()).delay(5 seconds)
-          }
-          .toSocketApp
-          .toHttp
-          .deployWS
+          // Wait for the close handler to complete
+          _ <- TestClock.adjust(2 seconds)
+          _ <- isStarted.await
+          _ <- TestClock.adjust(5 seconds)
+          _ <- isSet.await
 
-        // Setup Client
-        // Client closes the connection after 1 secon
-        clientSocket = Http
-          .collectZIO[WebSocketChannelEvent] { case ChannelEvent(ch, UserEventTriggered(HandshakeComplete)) =>
-            ch.writeAndFlush(WebSocketFrame.close(1000)).delay(1 second)
-          }
-          .toSocketApp
+          // Check if the close handler was completed
+        } yield assertCompletes
+      } @@ nonFlaky
+    },
+    suite("WebSocketServer") {
+      suite("connections") {
+        testM("Multiple websocket upgrades") {
+          val app   = Http.succeed(WebSocketFrame.text("BAR")).toSocketApp.toHttp.deployWS
+          val codes = ZIO
+            .foreach(1 to 1024)(_ => app(Http.empty.toSocketApp).map(_.status))
+            .map(_.count(_ == Status.SwitchingProtocols))
 
-        // Deploy the server and send it a socket request
-        _ <- serverHttp(clientSocket.provideEnvironment(clockEnv))
-
-        // Wait for the close handler to complete
-        _ <- TestClock.adjust(2 seconds)
-        _ <- isStarted.await
-        _ <- TestClock.adjust(5 seconds)
-        _ <- isSet.await
-
-        // Check if the close handler was completed
-      } yield assertCompletes
-    } @@ nonFlaky
-  }
-
-  def websocketServerSpec = suite("WebSocketServer") {
-    suite("connections") {
-      testM("Multiple websocket upgrades") {
-        val app   = Http.succeed(WebSocketFrame.text("BAR")).toSocketApp.toHttp.deployWS
-        val codes = ZIO
-          .foreach(1 to 1024)(_ => app(Http.empty.toSocketApp).map(_.status))
-          .map(_.count(_ == Status.SwitchingProtocols))
-
-        assertM(codes)(equalTo(1024))
+          assertM(codes)(equalTo(1024))
+        }
       }
-    }
-  }
+    },
+  )
 }
