8c9986f528b90ad8f4fc39f68e462c144915c12c
Tushar Mathur <tusharmath@gmail.com>
2022-07-06 18:13:54 +0530

    doc: update scala doc

--- zio-http/src/main/scala/zhttp/socket/package.scala
+++ zio-http/src/main/scala/zhttp/socket/package.scala
@@ -3,7 +3,14 @@ package zhttp
 import zhttp.service.{Channel, ChannelEvent}
 
 package object socket {
-  type WebSocketChannel      = Channel[WebSocketFrame]
+
+  /**
+   * A channel that allows websocket frames to be written to it.
+   */
+  type WebSocketChannel = Channel[WebSocketFrame]
+
+  /**
+   * A channel that allows websocket frames to be read and write to it.
+   */
   type WebSocketChannelEvent = ChannelEvent[WebSocketFrame, WebSocketFrame]
-  type WebSocketEvent        = ChannelEvent.Event[WebSocketFrame]
 }
