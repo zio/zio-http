eda93497e2ed2749b4a8c55bd895077d7f5b4d9e
Scala Steward <scala_steward@virtuslab.com>
2022-07-11 16:01:52 +0000

    Revert commit(s) 7e9fd73

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val JwtCoreVersion                = "9.0.5"
   val NettyVersion                  = "4.1.78.Final"
   val NettyIncubatorVersion         = "0.0.14.Final"
-  val ScalaCompactCollectionVersion = "2.8.0"
+  val ScalaCompactCollectionVersion = "2.7.0"
   val ZioVersion                    = "1.0.15"
   val SttpVersion                   = "3.3.18"
 
