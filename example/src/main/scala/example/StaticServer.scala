64d48a91f0c742587c79c08b25d5eba6831cc073
Gabriel Ciuloaica <95849448+gciuloaica@users.noreply.github.com>
2022-07-14 14:03:26 +0300

    Doc: Update `StaticServer` example (#1347)
    * updated StaticServer example to use the new Path syntax and added a minor correction to the implementation.
    
    * Update StaticServer.scala
    
    rolled back change of the port.
    
    * formatting

--- example/src/main/scala/example/StaticServer.scala
+++ example/src/main/scala/example/StaticServer.scala
@@ -8,16 +8,15 @@ import zio.ZIOAppDefault
 object StaticServer extends ZIOAppDefault {
 
   // A simple app to serve static resource files from a local directory.
-  val app = Http.collectHttp[Request] { case Method.GET -> "static" /: path =>
+  val app = Http.collectHttp[Request] { case Method.GET -> "" /: "static" /: path =>
     for {
-      file <- Http.getResourceAsFile(path.encode.tail)
+      file <- Http.getResourceAsFile(path.encode)
       http <-
         // Rendering a custom UI to list all the files in the directory
         if (file.isDirectory) {
-
           // Accessing the files in the directory
           val files = file.listFiles.toList.sortBy(_.getName)
-          val base  = "/static"
+          val base  = "/static/"
           val rest  = path.dropLast(1)
 
           // Custom UI to list all the files in the directory
