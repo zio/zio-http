4e05f12c0fef18b1fdecfbe764ede6dc80450ffc
Gabriel Ciuloaica <con_gabriel.ciuloaica@dream11.com>
2022-07-26 19:04:41 +0300

    refactored  to sealed trait

--- zio-http/src/main/scala/zhttp/http/Headers.scala
+++ zio-http/src/main/scala/zhttp/http/Headers.scala
@@ -15,8 +15,10 @@ import scala.jdk.CollectionConverters._
  * defined here. A better place would be one of the traits extended by
  * `HeaderExtension`.
  */
-final case class Headers(toChunk: Chunk[Header]) extends HeaderExtension[Headers] {
+sealed trait Headers extends HeaderExtension[Headers] {
   self =>
+
+  def toChunk: Chunk[Header]
 
   def ++(other: Headers): Headers = self.combine(other)
 
@@ -51,11 +53,16 @@ object Headers extends HeaderConstructors {
   val BasicSchemeName  = "Basic"
   val BearerSchemeName = "Bearer"
 
-  def apply(name: CharSequence, value: CharSequence): Headers = Headers(Chunk((name, value)))
+  def apply(name: CharSequence, value: CharSequence): Headers = new Headers() {
+    override def toChunk: Chunk[(CharSequence, CharSequence)] = Chunk((name, value))
+  }
 
   def apply(tuples: Header*): Headers = Headers(Chunk.fromIterable(tuples))
 
-  def apply(iter: Iterable[Header]): Headers = Headers(Chunk.fromIterable(iter))
+  def apply(iter: Iterable[Header]): Headers =
+    new Headers() {
+      override def toChunk: Chunk[(CharSequence, CharSequence)] = Chunk.fromIterable(iter)
+    }
 
   def ifThenElse(cond: Boolean)(onTrue: => Headers, onFalse: => Headers): Headers = if (cond) onTrue else onFalse
 
@@ -71,4 +78,5 @@ object Headers extends HeaderConstructors {
 
   private[zhttp] def decode(headers: HttpHeaders): Headers =
     Headers(headers.entries().asScala.toList.map(entry => (entry.getKey, entry.getValue)))
+
 }
