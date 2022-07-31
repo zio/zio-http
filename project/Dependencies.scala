ae214c4cdb8c00dccb17a3cf27dd671a707fffbf
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-31 14:42:09 +0200

    Update scala-collection-compat to 2.8.1 (#1376)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val JwtCoreVersion                = "9.0.5"
   val NettyVersion                  = "4.1.79.Final"
   val NettyIncubatorVersion         = "0.0.14.Final"
-  val ScalaCompactCollectionVersion = "2.8.0"
+  val ScalaCompactCollectionVersion = "2.8.1"
   val ZioVersion                    = "2.0.0"
   val SttpVersion                   = "3.3.18"
 
