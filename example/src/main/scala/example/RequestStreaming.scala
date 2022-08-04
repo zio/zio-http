eaf26330d52ad8da3790ac83a6cd732f64d336e5
Tushar Mathur <tusharmath@gmail.com>
2022-08-04 13:06:10 +0530

    revert: RequestStreaming

--- example/src/main/scala/example/RequestStreaming.scala
+++ example/src/main/scala/example/RequestStreaming.scala
@@ -1,34 +1,24 @@
 package example
 
 import zhttp.http._
-import zhttp.service.server.ServerChannelFactory
-import zhttp.service.{EventLoopGroup, Server}
+import zhttp.service.Server
 import zio._
 object RequestStreaming extends ZIOAppDefault {
 
   // Create HTTP route which echos back the request body
-  val app = Http.collectZIO[Request] { case req @ Method.POST -> !! / "echo" =>
+  val app = Http.collect[Request] { case req @ Method.POST -> !! / "echo" =>
     // Returns a stream of bytes from the request
     // The stream supports back-pressure
     val stream = req.body.asStream
 
-    // Creating Body from the stream
+    // Creating HttpData from the stream
     // This works for file of any size
     val data = Body.fromStream(stream)
 
-    // ZIO.succeed(Response(body = req.body))
-
-    ZIO.succeed(Response(body = data))
-
-//    for {
-//      _ <- req.body.asString
-//      _ <- req.body.asString
-//    } yield Response.ok
+    Response(body = data)
   }
 
   // Run it like any simple app
-  val run: UIO[ExitCode] = {
-    val server = Server.app(app).withPort(8090)
-    server.start.exitCode.provideLayer(EventLoopGroup.auto() ++ ServerChannelFactory.auto)
-  }
+  val run: UIO[ExitCode] =
+    Server.start(8090, app).exitCode
 }
