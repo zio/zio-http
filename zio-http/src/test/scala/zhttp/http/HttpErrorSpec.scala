af331c240a9bb949d605a060c4bfb93e5d90deef
Tushar Mathur <tusharmath@gmail.com>
2022-08-07 18:22:55 +0530

    refactor: change param type for NotFound error

--- zio-http/src/test/scala/zhttp/http/HttpErrorSpec.scala
+++ zio-http/src/test/scala/zhttp/http/HttpErrorSpec.scala
@@ -1,7 +1,7 @@
 package zhttp.http
 
 import zio.test.Assertion.equalTo
-import zio.test.{ZIOSpecDefault, assert}
+import zio.test.{assert, ZIOSpecDefault}
 
 object HttpErrorSpec extends ZIOSpecDefault {
   def spec = suite("HttpError")(
@@ -12,7 +12,7 @@ object HttpErrorSpec extends ZIOSpecDefault {
         assert(result)(equalTo("Internal server error"))
       },
       test("should fold with no cause") {
-        val error  = HttpError.NotFound(!!)
+        val error  = HttpError.NotFound(!!.encode)
         val result = error.foldCause("Page not found")(cause => cause.getMessage)
         assert(result)(equalTo("Page not found"))
       },
