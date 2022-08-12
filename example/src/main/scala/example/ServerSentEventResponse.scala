3ee5a296a3e11034d003140efce8f510019342dd
claaslohmann <claas.lohmann@riskident.com>
2022-08-12 08:42:44 +0200

    Small clean-up

--- example/src/main/scala/example/ServerSentEventResponse.scala
+++ example/src/main/scala/example/ServerSentEventResponse.scala
@@ -13,16 +13,11 @@ object ServerSentEventResponse extends ZIOAppDefault {
   // Starting the server (for more advanced startup configuration checkout `HelloWorldAdvanced`)
   def run = Server.start(8090, app)
 
-  // Create a message as a Chunk[Byte]
-  def message                    = Chunk.fromArray("Hello world !\r\n".getBytes(HTTP_CHARSET))
-
   // Create a stream of Server-Sent-Events
   def stream = ZStream.repeat(Event("my-data", Some("message"), Some("my-id"), None)).schedule(Schedule.spaced(1.seconds) && Schedule.recurs(10))
 
   // Use `Http.collect` to match on route
   def app: HttpApp[Any, Nothing] = Http.collect[Request] {
-
-
 
     // Simple (non-stream) based route
     case Method.GET -> !! / "health" => Response.ok
