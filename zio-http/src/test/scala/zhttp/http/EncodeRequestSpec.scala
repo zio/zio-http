d71277e3cbd6e8631773a9c1ab552ca2824c7158
Paul Lysak <paul.lysak@gmail.com>
2022-08-26 10:44:01 +0300

    Fix for #1372 - Scala 2.12 compatibility for the tests

--- zio-http/src/test/scala/zhttp/http/EncodeRequestSpec.scala
+++ zio-http/src/test/scala/zhttp/http/EncodeRequestSpec.scala
@@ -67,8 +67,9 @@ object EncodeRequestSpec extends ZIOSpecDefault with EncodeRequest {
       check(anyClientParam) { params =>
         val req =
           encode(params).map(i => Option(i.headers().get(HttpHeaderNames.HOST)))
-        assertZIO(req)(equalTo(params.url.host.zip(params.url.port).fold(params.url.host) { case (host, port) =>
-          Some(s"$host:$port")
+        assertZIO(req)(equalTo((params.url.host, params.url.port) match {
+          case (Some(host), Some(port)) => Some(s"$host:$port")
+          case _                        => params.url.host
         }))
       }
     },
@@ -76,8 +77,9 @@ object EncodeRequestSpec extends ZIOSpecDefault with EncodeRequest {
       check(clientParamWithAbsoluteUrl) { params =>
         val req = encode(params)
           .map(i => Option(i.headers().get(HttpHeaderNames.HOST)))
-        assertZIO(req)(equalTo(params.url.host.zip(params.url.port).fold(params.url.host) { case (host, port) =>
-          Some(s"$host:$port")
+        assertZIO(req)(equalTo((params.url.host, params.url.port) match {
+          case (Some(host), Some(port)) => Some(s"$host:$port")
+          case _                        => params.url.host
         }))
       }
     },
