15df73714547aed7aade74a9f568142c0b944206
Scott T Weaver <scott.t.weaver@gmail.com>
2022-09-14 11:38:11 -0500

    Encoding was breaking VS Code Metals

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -2,8 +2,8 @@ trait ScalaSettings {
   private val baseSettings = Seq(
     "-language:postfixOps",      // Added by @tusharmath
     "-deprecation",              // Emit warning and location for usages of deprecated APIs.
-    "-encoding",
-    "utf-8",                     // Specify character encoding used by source files.
+    // "-encoding",
+    // "utf-8",                     // Specify character encoding used by source files.
     "-explaintypes",             // Explain type errors in more detail.
     "-feature",                  // Emit warning and location for usages of features that should be imported explicitly.
     "-language:existentials",    // Existential types (besides wildcard types) can be written and inferred
