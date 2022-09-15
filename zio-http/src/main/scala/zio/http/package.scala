14c5df63967c3e2311aa7db7e01a9cd4eb1cfe83
tobialoschiavo <tobia.loschiavo@gmail.com>
2022-09-15 17:42:34 +0200

    fixed method visibility

--- zio-http/src/main/scala/zio/http/package.scala
+++ zio-http/src/main/scala/zio/http/package.scala
@@ -21,5 +21,5 @@ package object http extends PathSyntax with RequestSyntax with RouteDecoderModul
   object HeaderNames  extends headers.HeaderNames
   object HeaderValues extends headers.HeaderValues
 
-  private val failNoStacktrace: ZIO[Any, None.type, Nothing] = ZIO.refailCause(Cause.fail(None))
+  private[http] val failNoStacktrace: ZIO[Any, None.type, Nothing] = ZIO.refailCause(Cause.fail(None))
 }
