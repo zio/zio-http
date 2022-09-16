d9bf349dbf437dec4e857f9ebdd70e71698b0544
Scott T Weaver <scott.t.weaver@gmail.com>
2022-09-16 11:05:36 -0500

    Make 'Names' 'private[zio]' and ... ...remove scope from 'val's.

--- zio-http/src/main/scala/zio/http/netty/package.scala
+++ zio-http/src/main/scala/zio/http/netty/package.scala
@@ -13,26 +13,26 @@ package object netty {
 
   private[zio] val AutoReleaseRequest = false
 
-  object Names {
-    private[zio] val ServerCodecHandler             = "SERVER_CODEC"
-    private[zio] val HttpObjectAggregator           = "HTTP_OBJECT_AGGREGATOR"
-    private[zio] val HttpRequestHandler             = "HTTP_REQUEST"
-    private[zio] val HttpResponseHandler            = "HTTP_RESPONSE"
-    private[zio] val HttpKeepAliveHandler           = "HTTP_KEEPALIVE"
-    private[zio] val FlowControlHandler             = "FLOW_CONTROL_HANDLER"
-    private[zio] val WebSocketHandler               = "WEB_SOCKET_HANDLER"
-    private[zio] val SSLHandler                     = "SSL_HANDLER"
-    private[zio] val HttpOnHttpsHandler             = "HTTP_ON_HTTPS_HANDLER"
-    private[zio] val HttpServerCodec                = "HTTP_SERVER_CODEC"
-    private[zio] val HttpClientCodec                = "HTTP_CLIENT_CODEC"
-    private[zio] val HttpServerExpectContinue       = "HTTP_SERVER_EXPECT_CONTINUE"
-    private[zio] val HttpServerFlushConsolidation   = "HTTP_SERVER_FLUSH_CONSOLIDATION"
-    private[zio] val ClientInboundHandler           = "CLIENT_INBOUND_HANDLER"
-    private[zio] val WebSocketClientProtocolHandler = "WEB_SOCKET_CLIENT_PROTOCOL_HANDLER"
-    private[zio] val HttpRequestDecompression       = "HTTP_REQUEST_DECOMPRESSION"
-    private[zio] val LowLevelLogging                = "LOW_LEVEL_LOGGING"
-    private[zio] val ProxyHandler                   = "PROXY_HANDLER"
-    private[zio] val HttpContentHandler             = "HTTP_CONTENT_HANDLER"
+  private[zio] object Names {
+    val ServerCodecHandler             = "SERVER_CODEC"
+    val HttpObjectAggregator           = "HTTP_OBJECT_AGGREGATOR"
+    val HttpRequestHandler             = "HTTP_REQUEST"
+    val HttpResponseHandler            = "HTTP_RESPONSE"
+    val HttpKeepAliveHandler           = "HTTP_KEEPALIVE"
+    val FlowControlHandler             = "FLOW_CONTROL_HANDLER"
+    val WebSocketHandler               = "WEB_SOCKET_HANDLER"
+    val SSLHandler                     = "SSL_HANDLER"
+    val HttpOnHttpsHandler             = "HTTP_ON_HTTPS_HANDLER"
+    val HttpServerCodec                = "HTTP_SERVER_CODEC"
+    val HttpClientCodec                = "HTTP_CLIENT_CODEC"
+    val HttpServerExpectContinue       = "HTTP_SERVER_EXPECT_CONTINUE"
+    val HttpServerFlushConsolidation   = "HTTP_SERVER_FLUSH_CONSOLIDATION"
+    val ClientInboundHandler           = "CLIENT_INBOUND_HANDLER"
+    val WebSocketClientProtocolHandler = "WEB_SOCKET_CLIENT_PROTOCOL_HANDLER"
+    val HttpRequestDecompression       = "HTTP_REQUEST_DECOMPRESSION"
+    val LowLevelLogging                = "LOW_LEVEL_LOGGING"
+    val ProxyHandler                   = "PROXY_HANDLER"
+    val HttpContentHandler             = "HTTP_CONTENT_HANDLER"
   }
 
   private val isReadKey = AttributeKey.newInstance[Boolean]("IS_READ_KEY")
