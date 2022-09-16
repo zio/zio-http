79cfd690379796664428e38f5ac5a34fc4c94ca9
frekw <fredrik@warnsberg.se>
2022-09-16 02:03:29 +0200

    Cleanup implementation

--- zio-http/src/main/scala/zio/http/middleware/Metrics.scala
+++ zio-http/src/main/scala/zio/http/middleware/Metrics.scala
@@ -5,98 +5,72 @@ import zio._
 import zio.metrics.{Metric, MetricKeyType, MetricLabel}
 
 private[zio] trait Metrics {
-  def metrics[R, E](
-    mapper: Metrics.Mapper = Map.empty,
-    boundaries: MetricKeyType.Histogram.Boundaries = Metrics.defaultBoundaries,
-  ): HttpMiddleware[R with Metrics.InFlightRequests, E] =
-    new Middleware[R with Metrics.InFlightRequests, E, Request, Response, Request, Response] {
-      private def metricLabelsForRequest(req: Request): Set[MetricLabel] =
+  def metrics(
+    pathLabelMapper: PartialFunction[Request, String] = Map.empty,
+    concurrentRequestsName: String = "http_concurrent_requests_total",
+    totalRequestsName: String = "http_requests_total",
+    requestDurationName: String = "http_request_duration_seconds",
+    requestDurationBoundaries: MetricKeyType.Histogram.Boundaries = Metrics.defaultBoundaries,
+  ): HttpMiddleware[Any, Nothing] = {
+    new Middleware[Any, Nothing, Request, Response, Request, Response] {
+      val requestsTotal      = Metric.counterInt(totalRequestsName)
+      val concurrentRequests = Metric.gauge(concurrentRequestsName)
+      val requestDuration    = Metric.histogram(requestDurationName, requestDurationBoundaries)
+      val status404          = Set(MetricLabel("status", "404"))
+      val status500          = Set(MetricLabel("status", "500"))
+      val nanosToSeconds     = 1e9d
+
+      def labelsForRequest(req: Request): Set[MetricLabel] =
         Set(
           MetricLabel("method", req.method.toString),
-          MetricLabel("path", mapper.lift(req).getOrElse(req.path.toString())),
+          MetricLabel("path", pathLabelMapper.lift(req).getOrElse(req.path.toString())),
         )
 
-      private def withRequestMeta[Type, In, Out](metric: Metric[Type, In, Out])(req: Request) =
-        metric.tagged(metricLabelsForRequest(req))
+      def labelsForResponse(res: Response): Set[MetricLabel] =
+        Set(
+          MetricLabel("status", res.status.code.toString),
+        )
 
-      private def requestsTotal(req: Request) =
-        withRequestMeta(Metric.counterInt("http_requests_total"))(req)
+      def observeRequest(start: Long, labels: Set[MetricLabel]) =
+        for {
+          _   <- requestsTotal.tagged(labels).increment
+          end <- Clock.nanoTime
+          took = end - start
+          _ <- requestDuration.tagged(labels).update(took / nanosToSeconds)
+        } yield ()
 
-      private val errorCount = Metric.counterInt("http_errors_total")
-
-      private def errorCountWithRequest(req: Request) =
-        withRequestMeta(errorCount)(req)
-
-      private def errorCountWithResponse(req: Request, res: Response) =
-        errorCountWithStatus(req, res.status.code.toString)
-
-      private def errorCountWithStatus(req: Request, status: String) =
-        errorCountWithRequest(req).tagged("status_code", status)
-
-      private def requestDuration(req: Request) =
-        withRequestMeta(
-          Metric.histogram(
-            "http_request_duration_seconds",
-            boundaries,
-          ),
-        )(req).trackDurationWith(toSeconds)
-
-      private val NANOS                  = 1e9d
-      private def toSeconds(d: Duration) =
-        (d.getSeconds() * NANOS + d.getNano()) / NANOS
-
-      def apply[R1 <: R with Metrics.InFlightRequests, E1 >: E](
+      def apply[R1 <: Any, E1 >: Nothing](
         http: Http[R1, E1, Request, Response],
       ): Http[R1, E1, Request, Response] =
         Http.fromOptionFunction[Request] { req =>
-          val labels = metricLabelsForRequest(req)
-          Metrics.InFlightRequests.increment(labels) *>
-            (http(req)
-              .ensuring(requestsTotal(req).increment *> Metrics.InFlightRequests.decrement(labels))
+          val requestLabels = labelsForRequest(req)
+
+          (for {
+            start <- Clock.nanoTime
+            _     <- concurrentRequests.tagged(requestLabels).increment
+            res   <- http(req)
               .tapBoth(
-                err =>
-                  err.fold(errorCountWithStatus(req, "404").increment)(_ => errorCountWithStatus(req, "500").increment),
-                res => ZIO.when(res.status.code > 399)(errorCountWithResponse(req, res).increment),
+                err => {
+                  observeRequest(start, requestLabels ++ err.fold(status404)(_ => status500))
+                },
+                response => {
+                  observeRequest(start, requestLabels ++ labelsForResponse(response))
+                },
               )
-              .tapDefect(_ => errorCountWithStatus(req, "500").increment)) @@ requestDuration(req)
+              .tapDefect(_ => {
+                observeRequest(start, requestLabels ++ status500)
+              })
+          } yield res).ensuring(concurrentRequests.tagged(requestLabels).decrement)
         }
     }
+  }
 }
 
 object Metrics {
-  type Mapper = PartialFunction[Request, String]
-
   // Prometheus defaults
   private[zio] val defaultBoundaries = MetricKeyType.Histogram.Boundaries.fromChunk(
     Chunk(
       .005, .01, .025, .05, .075, .1, .25, .5, .75, 1, 2.5, 5, 7.5, 10,
     ),
   )
-
-  trait InFlightRequests {
-    def increment(labels: Set[MetricLabel]): UIO[Int]
-    def decrement(labels: Set[MetricLabel]): UIO[Int]
-    def get: UIO[Int]
-  }
-
-  object InFlightRequests {
-    def increment(labels: Set[MetricLabel]): URIO[InFlightRequests, Any] = ZIO.serviceWithZIO(_.increment(labels))
-    def decrement(labels: Set[MetricLabel]): URIO[InFlightRequests, Any] = ZIO.serviceWithZIO(_.decrement(labels))
-    val get: URIO[InFlightRequests, Int]                                 = ZIO.serviceWithZIO(_.get)
-
-    val live = ZLayer.fromZIO {
-      for {
-        ref <- Ref.make(0)
-        gauge = Metric.gauge("http_inflight_requests_total").contramap[Int](_.toDouble)
-      } yield new InFlightRequests {
-        def increment(labels: Set[MetricLabel]): UIO[Int] = ref.updateAndGet(_ + 1) @@ gauge.tagged(labels.toSet)
-
-        def decrement(labels: Set[MetricLabel]): UIO[Int] = ref.updateAndGet(_ - 1) @@ gauge.tagged(labels.toSet)
-
-        def get: UIO[Int] = ref.get
-      }
-    }
-  }
-
-  val live = Metrics.InFlightRequests.live
 }
--- zio-http/src/test/scala/zio/http/middleware/MetricsSpec.scala
+++ zio-http/src/test/scala/zio/http/middleware/MetricsSpec.scala
@@ -1,11 +1,11 @@
-package zhttp.http.middleware
+package zio.http.middleware
 
-import zhttp.http.Middleware.metrics
-import zhttp.http._
-import zhttp.internal.HttpAppTestExtensions
+import zio.http.Middleware.metrics
+import zio.http._
+import zio.http.internal.HttpAppTestExtensions
 import zio._
 import zio.metrics.{Metric, MetricState}
-import zio.test.TestAspect.{sequential, withLiveClock}
+import zio.test.TestAspect.{sequential}
 import zio.test._
 
 object MetricsSpec extends ZIOSpecDefault with HttpAppTestExtensions {
@@ -19,33 +19,25 @@ object MetricsSpec extends ZIOSpecDefault with HttpAppTestExtensions {
         }
         .flatten @@ metrics()
 
-      val total        = Metric.counterInt("http_requests_total")
-      val totalOk      = total.tagged("path", "/ok").tagged("method", "GET")
-      val totalErrors  = total.tagged("path", "/error").tagged("method", "GET")
-      val totalDefects = total.tagged("path", "/defect").tagged("method", "GET")
-
-      val errors         = Metric.counterInt("http_errors_total")
-      val errorsErrors   = errors.tagged("path", "/error").tagged("method", "GET").tagged("status_code", "500")
-      val errorsDefects  = errors.tagged("path", "/defect").tagged("method", "GET").tagged("status_code", "500")
-      val errorsNotFound = errors.tagged("path", "/not-found").tagged("method", "GET").tagged("status_code", "404")
+      val total         = Metric.counterInt("http_requests_total")
+      val totalOk       = total.tagged("path", "/ok").tagged("method", "GET").tagged("status", "200")
+      val totalErrors   = total.tagged("path", "/error").tagged("method", "GET").tagged("status", "500")
+      val totalDefects  = total.tagged("path", "/defect").tagged("method", "GET").tagged("status", "500")
+      val totalNotFound = total.tagged("path", "/not-found").tagged("method", "GET").tagged("status", "404")
 
       for {
         _ <- app(Request(method = Method.GET, url = URL(!! / "ok")))
         _ <- app(Request(method = Method.GET, url = URL(!! / "error")))
         _ <- app(Request(method = Method.GET, url = URL(!! / "defect"))).catchAllDefect(_ => ZIO.unit)
         _ <- app(Request(method = Method.GET, url = URL(!! / "not-found"))).ignore.catchAllDefect(_ => ZIO.unit)
-        totalOkCount        <- totalOk.value
-        totalErrorsCount    <- totalErrors.value
-        totalDefectsCount   <- totalDefects.value
-        errorsErrorsCount   <- errorsErrors.value
-        errorsDefectsCount  <- errorsDefects.value
-        errorsNotFoundCount <- errorsNotFound.value
+        totalOkCount       <- totalOk.value
+        totalErrorsCount   <- totalErrors.value
+        totalDefectsCount  <- totalDefects.value
+        totalNotFoundCount <- totalNotFound.value
       } yield assertTrue(totalOkCount == MetricState.Counter(1)) &&
         assertTrue(totalErrorsCount == MetricState.Counter(1)) &&
         assertTrue(totalDefectsCount == MetricState.Counter(1)) &&
-        assertTrue(errorsErrorsCount == MetricState.Counter(1)) &&
-        assertTrue(errorsDefectsCount == MetricState.Counter(1)) &&
-        assertTrue(errorsNotFoundCount == MetricState.Counter(1))
+        assertTrue(totalNotFoundCount == MetricState.Counter(1))
     },
     test("http_request_duration_seconds") {
       val histogram = Metric
@@ -55,13 +47,14 @@ object MetricsSpec extends ZIOSpecDefault with HttpAppTestExtensions {
         )
         .tagged("path", "/ok")
         .tagged("method", "GET")
+        .tagged("status", "200")
 
       for {
         observed <- histogram.value.map(_.buckets.exists { case (_, count) => count > 0 })
       } yield assertTrue(observed)
     },
-    test("http_inflight_requests_total") {
-      val gauge = Metric.gauge("http_inflight_requests_total").tagged("path", "/slow").tagged("method", "GET")
+    test("http_concurrent_requests_total") {
+      val gauge = Metric.gauge("http_concurrent_requests_total").tagged("path", "/slow").tagged("method", "GET")
 
       for {
         promise <- Promise.make[Nothing, Unit]
@@ -74,10 +67,10 @@ object MetricsSpec extends ZIOSpecDefault with HttpAppTestExtensions {
         fiber  <- app(Request(method = Method.GET, url = URL(!! / "slow"))).fork
         _      <- promise.await
         during <- gauge.value
-        _      <- fiber.interrupt
+        _      <- TestClock.adjust(11.seconds)
         after  <- gauge.value
       } yield assertTrue(before == MetricState.Gauge(0)) &&
         assertTrue(before == after) && assertTrue(during == MetricState.Gauge(1))
-    } @@ withLiveClock,
-  ).provideSomeLayerShared[TestEnvironment](Metrics.live) @@ sequential
+    },
+  ) @@ sequential
 }
