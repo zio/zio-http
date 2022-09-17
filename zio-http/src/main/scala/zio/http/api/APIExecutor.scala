1f7e49c60e15c0d5c1cfc58086d8e0a7b24f2ed9
John A. De Goes <john@degoes.net>
2022-09-18 00:59:26 +0100

    re-org

--- zio-http/src/main/scala/zio/http/api/APIExecutor.scala
+++ zio-http/src/main/scala/zio/http/api/APIExecutor.scala
@@ -31,134 +31,112 @@ object APIExecutor      {
     UntypedServiceExecutor(client, locator)
 
   private final case class UntypedServiceExecutor(client: Client, locator: APILocator) extends APIExecutor[Nothing] {
+    val metadata = zio.http.api.internal.APIMetadata[CompiledExecutor[Any, Any]] { (api: API[_, _]) =>
+      CompiledExecutor(
+        locator.locate(api).getOrElse(throw NotFound(s"Could not locate API", api)),
+        api.asInstanceOf[API[Any, Any]],
+      )
+    }
+
+    def apply[Id, A, B](invocation: Invocation[Id, A, B])(implicit ev: Nothing <:< Id): ZIO[Any, Throwable, B] = {
+      val executor = metadata.get(invocation.api).asInstanceOf[CompiledExecutor[A, B]]
+
+      executor.execute(client, invocation.input).asInstanceOf[ZIO[Any, Throwable, B]]
+    }
+  }
+
+  private[api] final case class CompiledExecutor[I, O](apiRoot: URL, api: API[I, O]) {
     import zio.schema._
     import zio.schema.codec._
     import zio.http.api.internal.Mechanic
 
-    sealed case class Cached(
-      url: URL,
-      encoder: Encoder,
-      decoder: Chunk[Byte] => Either[String, Any],
-      deconstructor: Mechanic.Deconstructor[Any],
-    )
+    private val optionSchema: Option[Schema[Any]]    = api.input.bodySchema.map(_.asInstanceOf[Schema[Any]])
+    private val inputJsonEncoder: Any => Chunk[Byte] =
+      JsonCodec.encode(optionSchema.getOrElse(Schema[Unit].asInstanceOf[Schema[Any]]))
+    private val outputJsonDecoder: Chunk[Byte] => Either[String, Any] =
+      JsonCodec.decode(api.output.bodySchema.asInstanceOf[Schema[Any]])
+    private val deconstructor = Mechanic.makeDeconstructor(api.input).asInstanceOf[Mechanic.Deconstructor[Any]]
+    private val flattened     = Mechanic.flatten(api.input)
 
-    trait Encoder {
-      def encodeRoute(inputs: Array[Any]): Path
+    private def encodeRoute(inputs: Array[Any]): Path = {
+      var path = Path.empty
 
-      def encodeQuery(inputs: Array[Any]): Map[String, List[String]]
+      var i = 0
+      while (i < inputs.length) {
+        val route = flattened.routes(i).asInstanceOf[In.Route[Any]]
+        val input = inputs(i)
 
-      def encodeHeaders(inputs: Array[Any]): Headers
+        val segment = route.textCodec.encode(input)
 
-      def encodeBody(inputs: Array[Any]): Body
-    }
-
-    val metadata = zio.http.api.internal.APIMetadata[Cached] { (api: API[_, _]) =>
-      val optionSchema: Option[Schema[Any]]                 = api.input.bodySchema.map(_.asInstanceOf[Schema[Any]])
-      val inputEncoder: Any => Chunk[Byte]                  =
-        JsonCodec.encode(optionSchema.getOrElse(Schema[Unit].asInstanceOf[Schema[Any]]))
-      val outputDecoder: Chunk[Byte] => Either[String, Any] =
-        JsonCodec.decode(api.output.bodySchema.asInstanceOf[Schema[Any]])
-      val deconstructor = Mechanic.makeDeconstructor(api.input).asInstanceOf[Mechanic.Deconstructor[Any]]
-      val flattened     = Mechanic.flatten(api.input)
-
-      val encoder =
-        new Encoder {
-          def encodeRoute(inputs: Array[Any]): Path = {
-            var path = Path.empty
-
-            var i = 0
-            while (i < inputs.length) {
-              val route = flattened.routes(i).asInstanceOf[In.Route[Any]]
-              val input = inputs(i)
-
-              val segment = route.textCodec.encode(input)
-
-              path = path / segment
-              i = i + 1
-            }
-
-            path
-          }
-
-          def encodeQuery(inputs: Array[Any]): Map[String, List[String]] = {
-            var queryParams = Map.empty[String, List[String]]
-
-            var i = 0
-            while (i < inputs.length) {
-              val query = flattened.queries(i).asInstanceOf[In.Query[Any]]
-              val input = inputs(i)
-
-              val value = query.textCodec.encode(input)
-
-              queryParams =
-                if (queryParams.contains(query.name)) queryParams.updated(query.name, value :: queryParams(query.name))
-                else queryParams.updated(query.name, value :: Nil)
-
-              i = i + 1
-            }
-
-            queryParams
-          }
-
-          def encodeHeaders(inputs: Array[Any]): Headers = {
-            var headers = Headers.contentType("application/json")
-
-            var i = 0
-            while (i < inputs.length) {
-              val header = flattened.headers(i).asInstanceOf[In.Header[Any]]
-              val input  = inputs(i)
-
-              val value = header.textCodec.encode(input)
-
-              headers = headers ++ Headers(header.name, value)
-
-              i = i + 1
-            }
-
-            headers
-          }
-
-          def encodeBody(inputs: Array[Any]): Body = Body.fromChunk(inputEncoder(inputs(0)))
-        }
-
-      val url = locator.locate(api).getOrElse(throw NotFound(s"Could not locate API", api))
-
-      Cached(url, encoder, outputDecoder, deconstructor)
-    }
-
-    def apply[Id, A, B](invocation: Invocation[Id, A, B])(implicit ev: Nothing <:< Id): ZIO[Any, Throwable, B] = {
-      val location = locator.locate(invocation.api)
-
-      location match {
-        case Some(loc) => execute(loc, invocation)
-        case None => ZIO.die(NotFound("An API could not be located during execution of an invocation", invocation.api))
+        path = path / segment
+        i = i + 1
       }
+
+      path
     }
 
-    def execute[Id, A, B](loc: URL, invocation: Invocation[Id, A, B]): ZIO[Any, Throwable, B] = {
-      val cached = metadata.get(invocation.api)
+    private def encodeQuery(inputs: Array[Any]): Map[String, List[String]] = {
+      var queryParams = Map.empty[String, List[String]]
 
-      val inputs = cached.deconstructor(invocation.input)
+      var i = 0
+      while (i < inputs.length) {
+        val query = flattened.queries(i).asInstanceOf[In.Query[Any]]
+        val input = inputs(i)
 
-      val encoder = cached.encoder
+        val value = query.textCodec.encode(input)
 
-      val route   = encoder.encodeRoute(inputs.routes)
-      val query   = encoder.encodeQuery(inputs.queries)
-      val headers = encoder.encodeHeaders(inputs.headers)
-      val body    = encoder.encodeBody(inputs.inputBodies)
+        queryParams =
+          if (queryParams.contains(query.name)) queryParams.updated(query.name, value :: queryParams(query.name))
+          else queryParams.updated(query.name, value :: Nil)
+
+        i = i + 1
+      }
+
+      queryParams
+    }
+
+    private def encodeHeaders(inputs: Array[Any]): Headers = {
+      var headers = Headers.contentType("application/json")
+
+      var i = 0
+      while (i < inputs.length) {
+        val header = flattened.headers(i).asInstanceOf[In.Header[Any]]
+        val input  = inputs(i)
+
+        val value = header.textCodec.encode(input)
+
+        headers = headers ++ Headers(header.name, value)
+
+        i = i + 1
+      }
+
+      headers
+    }
+
+    private def encodeBody(inputs: Array[Any]): Body =
+      if (inputs.length == 0) Body.empty
+      else Body.fromChunk(inputJsonEncoder(inputs(0)))
+
+    def execute(client: Client, input: I): ZIO[Any, Throwable, O] = {
+      val inputs = deconstructor(input)
+
+      val route   = encodeRoute(inputs.routes)
+      val query   = encodeQuery(inputs.queries)
+      val headers = encodeHeaders(inputs.headers)
+      val body    = encodeBody(inputs.inputBodies)
 
       val request = Request(
-        method = invocation.api.method,
-        url = cached.url ++ URL(route, URL.Location.Relative, query),
+        method = api.method,
+        url = apiRoot ++ URL(route, URL.Location.Relative, query),
         headers = headers,
         body = body,
       )
 
       client.request(request).flatMap { response =>
         response.body.asChunk.flatMap { response =>
-          cached.decoder(response) match {
-            case Left(error)  => ZIO.die(DecodeError(s"Could not decode response: $error", invocation.api))
-            case Right(value) => ZIO.succeed(value.asInstanceOf[B])
+          outputJsonDecoder(response) match {
+            case Left(error)  => ZIO.die(DecodeError(s"Could not decode response: $error", api))
+            case Right(value) => ZIO.succeed(value.asInstanceOf[O])
           }
         }
       }
