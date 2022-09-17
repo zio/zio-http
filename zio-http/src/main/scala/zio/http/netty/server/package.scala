795947449993248823608811962af11cc4ea21bc
Scott T Weaver <scott.t.weaver@gmail.com>
2022-09-17 10:33:05 -0500

    Formatting

--- zio-http/src/main/scala/zio/http/netty/server/package.scala
+++ zio-http/src/main/scala/zio/http/netty/server/package.scala
@@ -1,10 +1,11 @@
 package zio.http.netty
 
-import java.util.concurrent.atomic.AtomicReference
 import zio.http._
 
+import java.util.concurrent.atomic.AtomicReference
+
 package object server {
-  
+
   private[server] type ErrorCallbackRef = AtomicReference[Option[Server.ErrorCallback]]
-  private[server] type AppRef  = AtomicReference[HttpApp[Any, Throwable]]
+  private[server] type AppRef           = AtomicReference[HttpApp[Any, Throwable]]
 }
