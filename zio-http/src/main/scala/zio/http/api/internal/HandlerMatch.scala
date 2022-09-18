0fb9383aa5189d49bcff0d6003be1d62b83f1f1c
John A. De Goes <john@degoes.net>
2022-09-18 01:10:07 +0100

    mirror structure of client for server in prep

--- zio-http/src/main/scala/zio/http/api/internal/HandlerMatch.scala
+++ zio-http/src/main/scala/zio/http/api/internal/HandlerMatch.scala
@@ -34,3 +34,62 @@ final case class HandlerMatch[-R, +E, Input, Output](
     }
   }
 }
+
+private[api] final case class CompiledExecutor[I, O](api: API[I, O]) {
+  import zio.http._
+  import zio.schema._
+  import zio.schema.codec._
+  import zio.http.api.internal.Mechanic
+
+  private val optionSchema: Option[Schema[Any]]            = api.input.bodySchema.map(_.asInstanceOf[Schema[Any]])
+  val inputJsonDecoder: Chunk[Byte] => Either[String, Any] =
+    JsonCodec.decode(optionSchema.getOrElse(Schema[Unit].asInstanceOf[Schema[Any]]))
+  val outputJsonEncoder: Any => Chunk[Byte]                =
+    JsonCodec.encode(api.output.bodySchema.asInstanceOf[Schema[Any]])
+  val constructor       = Mechanic.makeConstructor(api.input).asInstanceOf[Mechanic.Constructor[Any]]
+  private val flattened = Mechanic.flatten(api.input)
+
+  def decodeRoute(path: Path, inputs: Array[Any]): Unit = {
+    var i = 0
+    while (i < flattened.routes.length) {
+      val route = flattened.routes(i).asInstanceOf[In.Route[Any]]
+
+      val _ = route
+
+      i = i + 1
+    }
+  }
+
+  def decodeQuery(queryParams: Map[String, List[String]], inputs: Array[Any]): Unit = {
+    var i = 0
+    while (i < flattened.queries.length) {
+      val query = flattened.queries(i).asInstanceOf[In.Query[Any]]
+      val input = inputs(i)
+
+      val _ = query
+      val _ = input
+
+      i = i + 1
+    }
+  }
+
+  def decodeHeaders(headers: Headers, inputs: Array[Any]): Unit = {
+    var i = 0
+    while (i < flattened.headers.length) {
+      val header = flattened.headers(i).asInstanceOf[In.Header[Any]]
+      val input  = inputs(i)
+
+      val _ = header
+      val _ = input
+
+      i = i + 1
+    }
+  }
+
+  def decodeBody(body: Body, inputs: Array[Any]): Unit =
+    ???
+
+  def execute(request: Request): ZIO[Any, Throwable, Response] = {
+    ???
+  }
+}
