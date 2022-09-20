67171ebc62524b385464f4d7d14c07670e66671c
Yonas Ghidei <ghidei@kth.se>
2022-09-20 17:54:44 +0200

    Refactor SSLConfig data type for server (#1513)

--- zio-http/src/main/scala/zio/http/netty/server/ServerHttpsHandler.scala
+++ zio-http/src/main/scala/zio/http/netty/server/ServerHttpsHandler.scala
@@ -2,16 +2,17 @@ package zio.http.netty.server
 
 import io.netty.channel.{ChannelFutureListener, ChannelHandlerContext, SimpleChannelInboundHandler}
 import io.netty.handler.codec.http.{DefaultHttpResponse, HttpMessage, HttpResponseStatus, HttpVersion}
-import zio.http.netty.server.ServerSSLHandler.SSLHttpBehaviour
+
 import zio.stacktracer.TracingImplicits.disableAutoTrace // scalafix:ok;
 
-private[zio] class ServerHttpsHandler(httpBehaviour: SSLHttpBehaviour)
-    extends SimpleChannelInboundHandler[HttpMessage] {
+import zio.http.SSLConfig.HttpBehaviour
+
+private[zio] class ServerHttpsHandler(httpBehaviour: HttpBehaviour) extends SimpleChannelInboundHandler[HttpMessage] {
   override def channelRead0(ctx: ChannelHandlerContext, msg: HttpMessage): Unit = {
 
     // TODO: PatMat maybe???
     if (msg.isInstanceOf[HttpMessage]) {
-      if (httpBehaviour == SSLHttpBehaviour.Redirect) {
+      if (httpBehaviour == HttpBehaviour.Redirect) {
         val message  = msg.asInstanceOf[HttpMessage]
         val address  = message.headers.get("Host")
         val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.PERMANENT_REDIRECT)
