b7c3fb41242144ef23da2acb6fba26df65fd3ca5
Scott T Weaver <scott.t.weaver@gmail.com>
2022-09-21 17:18:37 -0500

    Per iteration 10. Fixed issue with JSON benchmark output. * The field in the benchmark must be named "message" not "greetings".

--- project/BenchmarkWorkFlow.scala
+++ project/BenchmarkWorkFlow.scala
@@ -36,7 +36,6 @@ object BenchmarkWorkFlow {
           env = Map("GITHUB_TOKEN" -> "${{secrets.ACTIONS_PAT}}"),
           id = Some("result"),
           commands = List(
-            "cat ./FrameworkBenchMarks/frameworks/Scala/zio-http/src/main/scala/Main.scala",
             "cp ./zio-http/zio-http-example/src/main/scala/example/PlainTextBenchmarkServer.scala ./FrameworkBenchMarks/frameworks/Scala/zio-http/src/main/scala/Main.scala",
             "cd ./FrameworkBenchMarks",
             """sed -i "s/---COMMIT_SHA---/${{github.event.pull_request.head.repo.owner.login}}\/zio-http.git#${{github.event.pull_request.head.sha}}/g" frameworks/Scala/zio-http/build.sbt""",
