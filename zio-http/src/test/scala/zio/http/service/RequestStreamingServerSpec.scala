9dd391c41c6c8706902102d78ec7b7d161ab9bcb
Scott T Weaver <scott.t.weaver@gmail.com>
2022-09-24 13:53:53 -0500

    Formatting

--- zio-http/src/test/scala/zio/http/service/RequestStreamingServerSpec.scala
+++ zio-http/src/test/scala/zio/http/service/RequestStreamingServerSpec.scala
@@ -2,13 +2,12 @@ package zio.http.service
 
 import zio.http._
 import zio.http.internal.{DynamicServer, HttpRunnableSpec}
-import zio.http.model.Status
+import zio.http.model.{Method, Status}
 import zio.http.service.ServerSpec.requestBodySpec
 import zio.test.Assertion.equalTo
 import zio.test.TestAspect.{sequential, shrinks, timeout}
 import zio.test.assertZIO
 import zio.{Scope, ZIO, durationInt}
-import zio.http.model.Method
 
 object RequestStreamingServerSpec extends HttpRunnableSpec {
 
