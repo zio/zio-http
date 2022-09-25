5ee43cfca4dc47682509c94d8903fb34ce0a0c5b
jakubjanecek <jakub.janecek@productboard.com>
2022-09-25 14:20:58 +0200

    refactor: Reimplement CharSequenceExtensions from scratch
    Closes #1510

--- zio-http/src/main/scala/zio/http/internal/CharSequenceExtensions.scala
+++ zio-http/src/main/scala/zio/http/internal/CharSequenceExtensions.scala
@@ -5,75 +5,122 @@ private[http] object CharSequenceExtensions {
   def equals(left: CharSequence, right: CharSequence, caseMode: CaseMode = CaseMode.Sensitive): Boolean =
     if (left eq right) true else compare(left, right, caseMode) == 0
 
+  /**
+   * Lexicographically compares two `CharSequence`s.
+   *
+   * @return
+   *   Zero if the arguments are equal. Negative value if left `CharSequence` is
+   *   less than the right `CharSequence`. Positive value if left `CharSequence`
+   *   is greater than the right `CharSequence`.
+   */
   def compare(left: CharSequence, right: CharSequence, caseMode: CaseMode = CaseMode.Sensitive): Int = {
-    val modify = modifier(caseMode)
-
     if (left eq right) {
       0
     } else {
-      var i      = 0
-      val length = Math.min(left.length, right.length)
-      while (i < length) {
-        val a = modify(left.charAt(i))
-        val b = modify(right.charAt(i))
-        if (a != b) return a - b
-
-        i += 1
+      val leftLength  = left.length
+      val rightLength = right.length
+      var result: Int = 0
+      caseMode match {
+        case CaseMode.Sensitive   =>
+          var i = 0
+          while (i < leftLength && i < leftLength && i < rightLength) {
+            val leftChar  = left.charAt(i)
+            val rightChar = right.charAt(i)
+            if (leftChar != rightChar) {
+              result = leftChar - rightChar
+              i = leftLength
+            } else {
+              i += 1
+            }
+          }
+        case CaseMode.Insensitive =>
+          var i = 0
+          while (i < leftLength && i < leftLength && i < rightLength) {
+            val leftChar  = left.charAt(i).toLower
+            val rightChar = right.charAt(i).toLower
+            if (leftChar != rightChar) {
+              result = leftChar - rightChar
+              i = leftLength
+            } else {
+              i += 1
+            }
+          }
       }
 
-      left.length - right.length
+      if (result != 0) result else leftLength.compare(rightLength)
     }
+
   }
 
-  def hashCode(cs: CharSequence): Int = {
-    val length = cs.length()
+  def hashCode(value: CharSequence): Int = {
+    val length = value.length()
     var hash   = 0
     var i      = 0
     while (i < length) {
-      val ch = cs.charAt(i)
-      hash = 31 * hash + (ch & 0xff)
+      val character = value.charAt(i)
+      hash = 31 * hash + (character & 0xff)
       i += 1
     }
     hash
   }
 
   def contains(sequence: CharSequence, subsequence: CharSequence, caseMode: CaseMode = CaseMode.Sensitive): Boolean = {
-    val subsequenceLength = subsequence.length
-    if (subsequenceLength == 0) return true
     val sequenceLength    = sequence.length
-    if (sequenceLength == 0) return false
+    val subsequenceLength = subsequence.length
+    if (sequenceLength >= subsequenceLength && subsequenceLength != 0) {
+      val maxPossibleStartIndex = sequenceLength - subsequenceLength
+      var result                = false
+      var i                     = 0
+      caseMode match {
+        case CaseMode.Sensitive =>
+          val firstCharacter = subsequence.charAt(0)
+          while (i <= maxPossibleStartIndex && !result) {
+            if (sequence.charAt(i) != firstCharacter) {
+              i += 1
+            } else {
+              var sequenceIndex    = i + 1
+              var subsequenceIndex = 1
+              while (
+                subsequenceIndex < subsequenceLength &&
+                sequence.charAt(sequenceIndex) == subsequence.charAt(subsequenceIndex)
+              ) {
+                sequenceIndex += 1
+                subsequenceIndex += 1
+              }
+              if (sequenceIndex - i == subsequenceLength) {
+                result = true
+              } else {
+                i += 1
+              }
+            }
+          }
 
-    val modify = modifier(caseMode)
-    val first  = modify(subsequence.charAt(0))
-    val max    = sequenceLength - subsequenceLength
-    var i      = 0
-    while (i <= max) { // Look for first character.
-      if (sequence.charAt(i) != first) {
-        while (i <= max && modify(sequence.charAt(i)) != first) {
-          i += 1
-        }
+        case CaseMode.Insensitive =>
+          val firstCharacter = subsequence.charAt(0).toLower
+          while (i <= maxPossibleStartIndex && !result) {
+            if (sequence.charAt(i).toLower != firstCharacter) {
+              i += 1
+            } else {
+              var sequenceIndex    = i + 1
+              var subsequenceIndex = 1
+              while (
+                subsequenceIndex < subsequenceLength &&
+                sequence.charAt(sequenceIndex).toLower == subsequence.charAt(subsequenceIndex).toLower
+              ) {
+                sequenceIndex += 1
+                subsequenceIndex += 1
+              }
+              if (sequenceIndex - i == subsequenceLength) {
+                result = true
+              } else {
+                i += 1
+              }
+            }
+          }
       }
-      // Found first character, now look at the rest of value
-      if (i <= max) {
-        var j   = i + 1
-        val end = j + subsequenceLength - 1
-        var k   = 1
-        while (j < end && modify(sequence.charAt(j)) == modify(subsequence.charAt(k))) {
-          j += 1
-          k += 1
-        }
-        if (j == end) { // Found whole string.
-          return true
-        }
-      }
-
-      i += 1
+      result
+    } else {
+      subsequenceLength == 0
     }
-    false
-  }
-
-  private def modifier(caseMode: CaseMode): Char => Char = caseMode match {
-    case CaseMode.Sensitive   => identity
-    case CaseMode.Insensitive => original => Character.toLowerCase(original)
   }
 }
