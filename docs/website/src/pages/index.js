da7f486aea311dc9fedc936ae428afc3ed231f7e
Benjamin Cavy <ptitFicus@users.noreply.github.com>
2022-09-28 15:51:06 +0200

    fix: get started link
    The link currently redirect to a "Page Not Found" error page

--- docs/website/src/pages/index.js
+++ docs/website/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/v1.x/index">
+            to="/docs/v1.x/">
             Get Started
           </Link>
         </div>
