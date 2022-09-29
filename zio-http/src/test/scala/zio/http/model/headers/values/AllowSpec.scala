870a2f92a83c4bbfbbec19c6e4a6127c43f24641
ccerbusca <cristian.cerbusca@gmail.com>
2022-09-29 16:41:59 +0300

    Refactor Allow to be an ADT

--- zio-http/src/test/scala/zio/http/model/headers/values/AllowSpec.scala
+++ zio-http/src/test/scala/zio/http/model/headers/values/AllowSpec.scala
@@ -2,18 +2,25 @@ package zio.http.model.headers.values
 
 import zio.{Chunk, Scope}
 import zio.http.internal.HttpGen
+import zio.http.model.headers.values.Allow._
 import zio.test._
 
 object AllowSpec extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment with Scope, Any] = suite("Allow header suite")(
     test("allow header transformation must be symmetrical") {
-      check(Gen.chunkOfBounded(1, 6)(HttpGen.method).map(Allow.apply)) { allowHeader =>
+      check(HttpGen.allowHeader) { allowHeader =>
         assertTrue(Allow.toAllow(Allow.fromAllow(allowHeader)) == allowHeader)
       }
     },
 
     test("empty header value should be parsed to an empty chunk") {
-      assertTrue(Allow.toAllow("") == Allow(Chunk.empty))
+      assertTrue(Allow.toAllow("") == AllowMethods(Chunk.empty))
+    },
+
+    test("invalid values parsing") {
+      check(Gen.stringBounded(10, 15)(Gen.char)) { value =>
+        assertTrue(Allow.toAllow(value) == AllowMethods(Chunk(InvalidAllowMethod)))
+      }
     }
   )
 }
