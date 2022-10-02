461845d48ea77ec5d9aecd0c5bb1edcae43b5462
Wosin <blind.van@gmail.com>
2022-10-02 20:27:47 +0200

    Fix import.

--- zio-http/src/main/scala/zio/http/model/headers/HeaderValue.scala
+++ zio-http/src/main/scala/zio/http/model/headers/HeaderValue.scala
@@ -1,7 +1,5 @@
 package zio.http.model.headers
 
-import zio.prelude.Hash
-
 object HeaderValue {
   sealed trait ContentMD5
   object ContentMD5 {
@@ -10,7 +8,7 @@ object HeaderValue {
     case class ContentMD5Value(digest: MD5) extends ContentMD5
 
     def toContentMD5(hexString: String): ContentMD5 = {
-      if (hexString.size == 32) ContentMD5Value(MD5(hexString)) else InvalidContentMD5Value
+      if (hexString.length == 32) ContentMD5Value(MD5(hexString)) else InvalidContentMD5Value
     }
 
     def fromContentMD5(contentMD5: ContentMD5): String =
