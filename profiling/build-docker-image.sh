aa5b055096548ae3a95ca039308512721758881a
Scott T Weaver <scott.t.weaver@gmail.com>
2022-10-17 14:43:37 -0500

    Add profiling setup.

---
+++ profiling/build-docker-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COMMIT_SHA=$(git rev-parse --short HEAD)
+TAG="zio-http:$COMMIT_SHA"
+
+if [[ $(uname -m) == 'arm64' ]]; then
+    export ARCH="linux-arm-64"
+else
+    export ARCH="linux-x86-64"
+fi
+
+if [ ! -e "/var/run/docker.sock" ]; then
+    echo "'/var/run/docker.sock' does not exist.  Are you sure Docker is running?"
+    exit 1
+fi
+
+mkdir -p src/main/scala
+rm ./src/main/scala/Main.scala
+cp ../zio-http-example/src/main/scala/example/PlainTextBenchmarkServer.scala ./src/main/scala/Main.scala
+
+docker build --build-arg ARCH=$ARCH -t $TAG .
+docker tag $TAG zio-http:latest
