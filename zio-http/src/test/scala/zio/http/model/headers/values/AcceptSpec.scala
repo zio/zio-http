19ebaadaeb1faaa7cd1e33d9e3d57fb818cfa4f7
Cristian Cerbusca <cristian.cerbusca@gmail.com>
2022-10-24 16:00:15 +0300

    Accept-Patch header + MediaType optional parameters (#1675)
    * Add MediaType optional parameters + refactor Accept header to use them
    
    * Add Accept-Patch header
    
    * Add Scaladoc
    
    * fmt
    
    * fix compilation issue for 2.12

--- zio-http/src/test/scala/zio/http/model/headers/values/AcceptSpec.scala
+++ zio-http/src/test/scala/zio/http/model/headers/values/AcceptSpec.scala
@@ -1,5 +1,6 @@
 package zio.http.model.headers.values
 
+import zio.Chunk
 import zio.http.model.headers.values.Accept.{AcceptValue, InvalidAcceptValue, MediaTypeWithQFactor}
 import zio.http.model.{MediaType, MimeDB}
 import zio.test._
@@ -12,23 +13,31 @@ object AcceptSpec extends ZIOSpecDefault with MimeDB {
       assertTrue(Accept.toAccept("text/html;q=0.8, bla=q") == InvalidAcceptValue)
     },
     test("parsing of valid Accept values") {
-      assertTrue(Accept.toAccept("text/html") == AcceptValue(List(MediaTypeWithQFactor(text.`html`, None)))) &&
       assertTrue(
-        Accept.toAccept("text/html;q=0.8") == AcceptValue(List(MediaTypeWithQFactor(text.`html`, Some(0.8)))),
+        Accept.toAccept("text/html") == AcceptValue(Chunk(MediaTypeWithQFactor(text.`html`, None))),
       ) &&
-      assertTrue(Accept.toAccept("text/*") == AcceptValue(List(MediaTypeWithQFactor(MediaType("text", "*"), None)))) &&
-      assertTrue(Accept.toAccept("*/*") == AcceptValue(List(MediaTypeWithQFactor(MediaType("*", "*"), None)))) &&
       assertTrue(
-        Accept.toAccept("*/*;q=0.1") == AcceptValue(List(MediaTypeWithQFactor(MediaType("*", "*"), Some(0.1)))),
+        Accept.toAccept("text/html;q=0.8") ==
+          AcceptValue(Chunk(MediaTypeWithQFactor(text.`html`.withQFactor(0.8), Some(0.8)))),
+      ) &&
+      assertTrue(
+        Accept.toAccept("text/*") == AcceptValue(Chunk(MediaTypeWithQFactor(MediaType("text", "*"), None))),
+      ) &&
+      assertTrue(
+        Accept.toAccept("*/*") == AcceptValue(Chunk(MediaTypeWithQFactor(MediaType("*", "*"), None))),
+      ) &&
+      assertTrue(
+        Accept.toAccept("*/*;q=0.1") ==
+          AcceptValue(Chunk(MediaTypeWithQFactor(MediaType("*", "*").withQFactor(0.1), Some(0.1)))),
       ) &&
       assertTrue(
         Accept.toAccept("text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8") ==
           AcceptValue(
-            List(
+            Chunk(
               MediaTypeWithQFactor(text.`html`, None),
               MediaTypeWithQFactor(application.`xhtml+xml`, None),
-              MediaTypeWithQFactor(application.`xml`, Some(0.9)),
-              MediaTypeWithQFactor(MediaType("*", "*"), Some(0.8)),
+              MediaTypeWithQFactor(application.`xml`.withQFactor(0.9), Some(0.9)),
+              MediaTypeWithQFactor(MediaType("*", "*").withQFactor(0.8), Some(0.8)),
             ),
           ),
       )
@@ -38,4 +47,11 @@ object AcceptSpec extends ZIOSpecDefault with MimeDB {
       assertTrue(allMediaTypes.map(_.fullType) == results)
     },
   )
+
+  implicit class MediaTypeTestOps(mediaType: MediaType) {
+    def withQFactor(double: Double): MediaType = {
+      mediaType
+        .copy(parameters = Map("q" -> double.toString))
+    }
+  }
 }
