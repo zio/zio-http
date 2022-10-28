fa072b2a1d83c3fbf1841cfcae789ddee9428f9a
sf17490 <sflynn@duck.com>
2022-10-28 10:05:54 -0400

    Halfway through adding date validation

--- zio-http/src/main/scala/zio/http/model/headers/values/Warning.scala
+++ zio-http/src/main/scala/zio/http/model/headers/values/Warning.scala
@@ -1,3 +1,80 @@
-package zio.http.model.headers.values trait Warning {
+package zio.http.model.headers.values
 
+import java.time.{ZoneId, ZonedDateTime}
+import java.time.format.DateTimeFormatter
+import scala.util.Try
+
+sealed trait Warning {}
+
+object Warning {
+
+  final case class WarningValue(code: Int, agent: String, text: String, date: Option[ZonedDateTime]) extends Warning
+
+  case object InvalidWarning extends Warning
+
+  def toWarning(warningText: String): Warning = {
+    // Make the warning
+    val warningParts  = warningText.split(" ")
+    val code: Int     = Try { Integer.parseInt(warningParts(0)) }.getOrElse(-1)
+    val agent: String = warningParts(1)
+
+    val messageStartIndex = warningText.indexOf('\"')
+    val messageEndIndex   = warningText.indexOf("\"", warningText.indexOf("\"") + 1)
+    val message           =
+      Try { warningText.substring(messageStartIndex, messageEndIndex + 1) }.getOrElse("Error: Message is missing")
+
+    // check if two substrings exist.
+    // if yes, validate first as message and second as date.
+    // If only one substring exists, validate that it is NOT a date. If it is a date, then message is missing
+    // If only one substring exists and is not a date, then do normal validation.
+    val dateStartIndex = warningText.indexOf("\"", messageEndIndex)
+
+    def makeDate(dateString: String): ZonedDateTime = {
+      val formatter   = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz")
+      val messageDate = ZonedDateTime.parse(dateString, formatter)
+      messageDate
+    }
+
+    val defaultDate = ZonedDateTime.of(0, 0, 0, 0, 0, 0, 0, ZoneId.systemDefault())
+    val chosenDate = Try {
+      val dateStartIndex = warningText.indexOf("\"", messageEndIndex)
+      val dateEndIndex   = warningText.indexOf("\"", dateStartIndex)
+      val dateSubstring  = warningText.substring(dateStartIndex, dateEndIndex)
+      makeDate(dateSubstring)
+    }.getOrElse(defaultDate)
+
+    val warningDate = chosenDate match{
+      case defaultDate => None
+      case _ => Some(chosenDate)
+    }
+
+    val result = WarningValue(code, agent, message, warningDate)
+
+    // Validate the warning
+    val validCodesAndMessages = Map(
+      110 -> "\"response is stale\"",
+      111 -> "\"revalidation failed\"",
+      112 -> "\"disconnected operation\"",
+      113 -> "\"heuristic expiration\"",
+      199 -> "\"miscellaneous warning\"",
+      214 -> "\"transformation applied\"",
+      299 -> "\"miscellaneous persistent warning\"",
+    )
+
+    if (validCodesAndMessages.getOrElse(result.code, "") == result.text.toLowerCase) {
+      result
+    } else {
+      InvalidWarning
+    }
+
+  }
+
+  def fromWarning(warning: Warning): String = {
+    println(s"Inputting $warning")
+    val result = warning match {
+      case WarningValue(code, agent, text, date) => s"$code $agent $text"
+      case InvalidWarning                        => ""
+    }
+    result
+  }
 }
--- zio-http/src/test/scala/zio/http/model/headers/values/WarningSpec.scala
+++ zio-http/src/test/scala/zio/http/model/headers/values/WarningSpec.scala
@@ -1,3 +1,43 @@
-package zio.http.model.headers.values object warningSpec {
+package zio.http.model.headers.values
 
+import zio.http.model.headers.values.Warning.{InvalidWarning, WarningValue}
+import zio.test.{ZIOSpecDefault, assertTrue}
+
+object WarningSpec extends ZIOSpecDefault {
+  override def spec = suite("Warning header suite")(
+    test("Invalid warning headers") {
+      assertTrue(1 + 1 == 2)
+      val invalidCode    = "1 anderson/1.3.37 \"Response is stale\""
+      val invalidMessage = "110 anderson/1.3.37 \"Invalid Message\""
+      assertTrue(Warning.toWarning(invalidCode) == InvalidWarning)
+      assertTrue(Warning.toWarning(invalidMessage) == InvalidWarning)
+    },
+    test("Missing necessary parts of warning header") {
+      assertTrue(1 + 1 == 2)
+      val missingCode = "anderson/1.3.37 \"Response is stale\""
+      val missingAgent = "110 \"Response is stale\""
+      val missingMessage = "110 anderson/1.3.37 "
+      assertTrue(Warning.toWarning(missingCode) == InvalidWarning)
+      assertTrue(Warning.toWarning(missingAgent) == InvalidWarning) //Dodgy when date is introduced
+      assertTrue(Warning.toWarning(missingMessage) == InvalidWarning)
+    },
+    test("Valid Warnings") {
+      assertTrue(1 + 1 == 2)
+      val validWarning  = "110 anderson/1.3.37 \"Response is stale\""
+      val validWarning2 = "299 anderson/1.3.37 \"Miscellaneous Persistent Warning\""
+      assertTrue(Warning.toWarning(validWarning) == WarningValue(110, "anderson/1.3.37", "\"Response is stale\""))
+      assertTrue(
+        Warning.toWarning(validWarning2) == WarningValue(299, "anderson/1.3.37", "\"Miscellaneous Persistent Warning\""),
+      )
+      // map through codes etc. from this page: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Warning
+
+    },
+    test("parsing and encoding is symmetrical") {
+      assertTrue(1 + 1 == 2)
+      val validWarning  = "110 anderson/1.3.37 \"Response is stale\""
+      val encodedWarning = Warning.fromWarning(Warning.toWarning(validWarning))
+      assertTrue(encodedWarning == validWarning)
+    }
+  )
+  //Might be the case that any warning text is acceptable - see example on Mozilla docs.
 }
