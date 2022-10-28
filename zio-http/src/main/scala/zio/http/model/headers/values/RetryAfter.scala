ec58fca106f6a473a1c5e45269735d992a58f2f2
Boguslav, Louis <louis.boguslav312@gmail.com>
2022-10-28 15:21:04 -0400

    rename

--- zio-http/src/main/scala/zio/http/model/headers/values/RetryAfter.scala
+++ zio-http/src/main/scala/zio/http/model/headers/values/RetryAfter.scala
@@ -8,7 +8,7 @@ sealed trait RetryAfter
 
 object RetryAfter {
   // Wed, 21 Oct 2015 07:28:00 GMT
-  private val webDateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz")
+  private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz")
 
   final case class WebDate(date: ZonedDateTime) extends RetryAfter
 
@@ -20,11 +20,11 @@ object RetryAfter {
     case maybePositiveSeconds if value.nonEmpty && maybePositiveSeconds.forall(Character.isDigit) =>
       Try(DelaySeconds(value.toLong))
     case date                                                                                     =>
-      Try(WebDate(ZonedDateTime.from(webDateFormatter.parse(date))))
+      Try(WebDate(ZonedDateTime.from(formatter.parse(date))))
   }) getOrElse InvalidRetryAfter
 
   def fromRetryAfter(retryAfter: RetryAfter): String = retryAfter match {
-    case WebDate(date)         => webDateFormatter.format(date)
+    case WebDate(date)         => formatter.format(date)
     case DelaySeconds(seconds) => seconds.toString
     case InvalidRetryAfter     => ""
   }
