5489b74a248d87d5fb9e9f0c7a7e9b230fad4c76
cyb3rn3t1c1an <2145095+cyb3rn3t1c1an@users.noreply.github.com>
2022-10-27 22:49:38 -0400

    Add test for the fromExpect

--- zio-http/src/test/scala/zio/http/model/headers/values/ExpectSpec.scala
+++ zio-http/src/test/scala/zio/http/model/headers/values/ExpectSpec.scala
@@ -7,11 +7,13 @@ object ExpectSpec extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment with Scope, Nothing] =
     suite("Expect header suite")(
       test("parse valid value") {
-        assertTrue(Expect.toExpect("100-continue") == Expect.ExpectValue)
+        assertTrue(Expect.toExpect("100-continue") == Expect.ExpectValue) &&
+        assertTrue(Expect.fromExpect(Expect.ExpectValue) == "100-continue")
       },
       test("parse invalid value") {
         assertTrue(Expect.toExpect("") == Expect.InvalidExpectValue) &&
-        assertTrue(Expect.toExpect("200-ok") == Expect.InvalidExpectValue)
+        assertTrue(Expect.toExpect("200-ok") == Expect.InvalidExpectValue) &&
+        assertTrue(Expect.fromExpect(Expect.InvalidExpectValue).isEmpty)
       },
     )
 }
