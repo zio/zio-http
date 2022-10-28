f1a2e53f045e0aa64bc24f76108b415f20794bdb
John A. De Goes <john@degoes.net>
2022-10-28 09:22:03 -0400

    Add basic test suite for HttpCodec (#1719)
    * Add basic test suite for HttpCodec
    
    * fix scala-version specific issues

---
+++ zio-http/src/test/scala/zio/http/api/HttpCodecSpec.scala
@@ -0,0 +1,51 @@
+package zio.http.api
+
+import zio._
+import zio.http._
+import zio.http.api._
+import zio.http.model._
+import zio.test._
+
+object HttpCodecSpec extends ZIOSpecDefault {
+  val googleUrl = URL.fromString("http://google.com").toOption.get
+  val usersUrl  = URL.fromString("http://mywebservice.com/users").toOption.get
+
+  val headerExample =
+    Headers.contentType("application/json") ++ Headers("X-Trace-ID", "1234")
+
+  val emptyJson = Body.fromString("{}")
+
+  def spec = suite("HttpCodecSpec")(
+    suite("RouteCodec") {
+      test("decode route with one path segment") {
+        val codec = RouteCodec.literal("users")
+
+        for {
+          result <- codec.decodeRequest(Request.get(url = usersUrl))
+        } yield assertTrue(result == (()))
+      } +
+        test("encode route with one path segment") {
+          val codec = RouteCodec.literal("users")
+
+          val request = codec.encodeRequest(())
+
+          assertTrue(request.path.toString() == "users")
+        }
+    } +
+      suite("HeaderCodec") {
+        test("dummy test") {
+          assertTrue(true)
+        }
+      } +
+      suite("BodyCodec") {
+        test("dummy test") {
+          assertTrue(true)
+        }
+      } +
+      suite("QueryCodec") {
+        test("dummy test") {
+          assertTrue(true)
+        }
+      },
+  )
+}
