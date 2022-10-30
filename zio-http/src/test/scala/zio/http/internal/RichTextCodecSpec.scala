cc00ee5538c46d59c6fa936c83772d046fc91193
John A. De Goes <john@degoes.net>
2022-10-30 11:33:27 -0400

    Implement RichTextCodec#describe & add unit tests; general cleanup of… (#1739)
    * Implement RichTextCodec#describe & add unit tests; general cleanup of RTC
    
    * fix scala variant
    
    * fix scala variant

--- zio-http/src/test/scala/zio/http/internal/RichTextCodecSpec.scala
+++ zio-http/src/test/scala/zio/http/internal/RichTextCodecSpec.scala
@@ -1,8 +1,8 @@
-package zio.http.internal
+package zio.http.api.internal
 
 import zio.Scope
-import zio.http.api.Combiner
-import zio.http.api.internal.RichTextCodec
+import zio.http._
+import zio.http.api._
 import zio.test.Assertion.equalTo
 import zio.test._
 
@@ -10,59 +10,89 @@ object RichTextCodecSpec extends ZIOSpecDefault {
 
   def success[A](a: A): Either[String, A] = Right(a)
 
+  def textOf(doc: Doc): Option[String] =
+    doc match {
+      case Doc.Paragraph(Doc.Span.Code(text)) => Some(text)
+      case _                                  => None
+    }
+
   override def spec = suite("Rich Text Codec Spec")(
-    test("encode empty spec") {
-      assert(RichTextCodec.empty.encode(()))(equalTo(Right("")))
-    },
-    test("encode char spec") {
-      check(Gen.char) { c =>
-        val charCodec = RichTextCodec.char(c)
-        assertTrue(charCodec.encode(c) == Right(c.toString))
-      }
-    },
-    test("encode transform or fail") {
-      check(Gen.int(0, 9)) { int =>
-        val transformCodec = RichTextCodec.digit
-        assertTrue(transformCodec.encode(int) == Right(int.toString()))
-      }
-    },
-    test("encode alt Right spec") {
-      check(Gen.char) { c =>
-        val altCodec = RichTextCodec.empty | RichTextCodec.char(c)
-        val result   = altCodec.encode(Right(c))
-        assertTrue(result == Right(c.toString))
-      }
-    },
-    test("encode alt left spec") {
-      check(Gen.char('a', 'z')) { s =>
-        val altCodec = RichTextCodec.letter | RichTextCodec.digit
-        val result   = altCodec.encode(Left(s))
-        assertTrue(result == Right(s.toString))
-      }
-    },
-    test("encode lazy whitespace codec") {
-      check(Gen.whitespaceChars) { s =>
-        val whitespaceCodec = RichTextCodec.whitespaceChar
-        val result          = whitespaceCodec.encode(())
-        assertTrue(result == Right(" "))
-      }
-    },
-    test("encode lazy spec") {
-      check(Gen.char) { c =>
-        val altCodec = RichTextCodec.defer(RichTextCodec.empty | RichTextCodec.char(c))
-        val result   = altCodec.encode(Right(c))
-        assertTrue(result == Right(c.toString))
-      }
-    },
-    test("encode combined codec") {
-      check(Gen.char('a', 'z'), Gen.int(0, 9)) { (c, i) =>
-//        implicit val combiner                         = Combiner.combine
-        val combinedCodec: RichTextCodec[(Char, Int)] =
-          RichTextCodec.letter ~ RichTextCodec.digit
-        val result                                    = combinedCodec.encode((c, i))
-        assertTrue(result == Right(c.toString + i.toString))
-      }
-    },
+    suite("describe spec")(
+      test("describe of specific char") {
+        val codec = RichTextCodec.char('x')
+
+        assertTrue(textOf(codec.describe).get == "x")
+      },
+      test("describe of char range") {
+        val codec = RichTextCodec.filter(c => c >= 'A' && c <= 'Z')
+
+        assertTrue(textOf(codec.describe).get == "[A-Z]")
+      },
+      test("describe of literal") {
+        val codec = RichTextCodec.literal("hello")
+
+        assertTrue(textOf(codec.describe).get == "hello")
+      },
+      test("describe of CI literal") {
+        val codec = RichTextCodec.literalCI("hello")
+
+        assertTrue(textOf(codec.describe).get == "[Hh][Ee][Ll][Ll][Oo]")
+      },
+    ),
+    suite("encode spec")(
+      test("encode empty spec") {
+        assert(RichTextCodec.empty.encode(()))(equalTo(Right("")))
+      },
+      test("encode char spec") {
+        check(Gen.char) { c =>
+          val charCodec = RichTextCodec.char(c)
+          assertTrue(charCodec.encode(c) == Right(c.toString))
+        }
+      },
+      test("encode transform or fail") {
+        check(Gen.int(0, 9)) { int =>
+          val transformCodec = RichTextCodec.digit
+          assertTrue(transformCodec.encode(int) == Right(int.toString()))
+        }
+      },
+      test("encode alt Right spec") {
+        check(Gen.char) { c =>
+          val altCodec = RichTextCodec.empty | RichTextCodec.char(c)
+          val result   = altCodec.encode(Right(c))
+          assertTrue(result == Right(c.toString))
+        }
+      },
+      test("encode alt left spec") {
+        check(Gen.char('a', 'z')) { s =>
+          val altCodec = RichTextCodec.letter | RichTextCodec.digit
+          val result   = altCodec.encode(Left(s))
+          assertTrue(result == Right(s.toString))
+        }
+      },
+      test("encode lazy whitespace codec") {
+        check(Gen.whitespaceChars) { s =>
+          val whitespaceCodec = RichTextCodec.whitespaceChar
+          val result          = whitespaceCodec.encode(())
+          assertTrue(result == Right(" "))
+        }
+      },
+      test("encode lazy spec") {
+        check(Gen.char) { c =>
+          val altCodec = RichTextCodec.defer(RichTextCodec.empty | RichTextCodec.char(c))
+          val result   = altCodec.encode(Right(c))
+          assertTrue(result == Right(c.toString))
+        }
+      },
+      test("encode combined codec") {
+        check(Gen.char('a', 'z'), Gen.int(0, 9)) { (c, i) =>
+          //        implicit val combiner                         = Combiner.combine
+          val combinedCodec: RichTextCodec[(Char, Int)] =
+            RichTextCodec.letter ~ RichTextCodec.digit
+          val result                                    = combinedCodec.encode((c, i))
+          assertTrue(result == Right(c.toString + i.toString))
+        }
+      },
+    ),
     suite("decode spec")(
       test("empty decoder") {
         assertTrue(success(()) == RichTextCodec.empty.decode("abc")) &&
