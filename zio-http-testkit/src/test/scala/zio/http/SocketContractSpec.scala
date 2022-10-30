534f0cd64da7d774bee185a6dc55b8b390b8a7b9
Bill Frasure <bill.frasure@gmail.com>
2022-10-30 11:58:03 -0400

    Test client socket implementation (#1727)
    * Rename Channel->ChannelNetty
    
    Create ChannelT trait
    
    Rename ChannelT -> Channel.
    Have to wait for my current TestClient PR to get merged to rebase/submit this
    
    Fix comments
    
    Rename file
    
    * Cleanup
    
    * Resurrect socket experiments. +test TODOs
    
    Pair with Adam
    
    Cleanup imports
    
    * Work on Channels
    
    * Work on SocketApps with Dimitry
    
    * Simpler type alias usage in TestChannel
    
    * Wire TestChannels together
    
    * 1st rough run with some Server/Client output w/ Dimitri
    
    * Race Apps and close when Closed message is received w/ Dimitri
    
    * Discuss future work with Adam w/ Dimitri
    
    * Simplify some unit tests w/ Dimitri
    
    * Work on Contract test w/ Dimitri
    
    * Fighting with ChannelUnregistered w/ Dimitri
    
    * Use the right freaking protocol w/ Dimitri
    
    * Get Contract test running on Live client w/ Dimitri
    
    * Simplify EventLoop w/ Dimitri
    
    * Close both Channels w/ Dimitri
    
    * Satisfy contract with both Live and Test Client! w/ Dimitri
    
    * Cleanup a few files w/ Dimitri
    
    * Cleanup files and noisy output w/ Dimitri
    
    * Cleanup files and noisy output w/ Dimitri
    
    * Pre-PR cleanup
    
    * fmt
    
    * Fix tests

---
+++ zio-http-testkit/src/test/scala/zio/http/SocketContractSpec.scala
@@ -0,0 +1,114 @@
+package zio.http
+
+import zio._
+import zio.http.ChannelEvent.{ChannelRead, ChannelUnregistered, UserEvent, UserEventTriggered}
+import zio.http.ServerConfig.LeakDetectionLevel
+import zio.http.model.Status
+import zio.http.netty.server.NettyDriver
+import zio.http.socket._
+import zio.test._
+
+object SocketContractSpec extends ZIOSpecDefault {
+  val testServerConfig: ZLayer[Any, Nothing, ServerConfig] =
+    ZLayer.succeed(ServerConfig.default.port(0).leakDetection(LeakDetectionLevel.PARANOID))
+
+  val severTestLayer = testServerConfig >+> Server.live
+
+  val messageFilter: Http[Any, Nothing, WebSocketChannelEvent, (Channel[WebSocketFrame], String)] =
+    Http.collect[WebSocketChannelEvent] { case ChannelEvent(channel, ChannelRead(WebSocketFrame.Text(message))) =>
+      (channel, message)
+    }
+
+  val messageSocketServer: Http[Any, Throwable, WebSocketChannelEvent, Unit] = messageFilter >>>
+    Http.collectZIO[(WebSocketChannel, String)] {
+      case (ch, text) if text.contains("Hi Server") =>
+        ZIO.debug("Server got message: " + text) *> ch.close()
+      case (_, text)                                => // TODO remove?
+        ZIO.debug("Unrecognized message sent to server: " + text)
+    }
+
+  def channelSocketServer(p: Promise[Throwable, Unit]): Http[Any, Throwable, WebSocketChannelEvent, Unit] =
+    Http.collectZIO[WebSocketChannelEvent] {
+      case ChannelEvent(ch, UserEventTriggered(UserEvent.HandshakeComplete)) =>
+        ch.writeAndFlush(WebSocketFrame.text("Hi Client"))
+
+      case ChannelEvent(_, ChannelRead(WebSocketFrame.Close(status, reason))) =>
+        p.succeed(()) *>
+          Console.printLine("Closing channel with status: " + status + " and reason: " + reason)
+      case ChannelEvent(_, ChannelUnregistered)                               =>
+        p.succeed(()) *>
+          Console.printLine("Server Channel unregistered")
+      case ChannelEvent(ch, ChannelRead(WebSocketFrame.Text("Hi Server")))    =>
+        ch.write(WebSocketFrame.text("Hi Client"))
+
+      case ChannelEvent(_, other) =>
+        Console.printLine("Server Other: " + other)
+    }
+
+  val protocol = SocketProtocol.default.withSubProtocol(Some("json"))
+
+  val decoder = SocketDecoder.default.withExtensions(allowed = true)
+
+  def socketAppServer(p: Promise[Throwable, Unit]): SocketApp[Any] =
+    (messageSocketServer ++ channelSocketServer(p)).toSocketApp
+      .withDecoder(decoder)
+      .withProtocol(protocol)
+
+  sys.props.put("ZIOHttpLogLevel", "DEBUG")
+  def spec =
+    suite("SocketOps")(
+      contract(
+        "Live",
+        ZIO.serviceWithZIO[Server](server =>
+          for {
+            p <- Promise.make[Throwable, Unit]
+            _ <- server.install(socketAppServer(p).toHttp)
+
+          } yield (server.port, p),
+        ),
+      ).provide(Client.default, Scope.default, TestServer.layer, NettyDriver.default, ServerConfig.liveOnOpenPort),
+      contract(
+        "Test", {
+          for {
+            p <- Promise.make[Throwable, Unit]
+            _ <- TestClient.addSocketApp(socketAppServer(p))
+
+          } yield (0, p)
+        },
+      )
+        .provide(TestClient.layer, Scope.default),
+    )
+
+  def contract[R](name: String, serverSetup: ZIO[R, Nothing, (Int, Promise[Throwable, Unit])]) =
+    test(name) {
+      val messageSocketClient: Http[Any, Throwable, WebSocketChannelEvent, Unit] = messageFilter >>>
+        Http.collectZIO[(WebSocketChannel, String)] {
+          case (ch, text) if text.contains("Hi Client") =>
+            ch.writeAndFlush(WebSocketFrame.text("Hi Server"), await = true).debug("Client got message: " + text)
+        }
+
+      val channelSocketClient: Http[Any, Throwable, WebSocketChannelEvent, Unit] =
+        Http.collectZIO[WebSocketChannelEvent] {
+          case ChannelEvent(_, ChannelUnregistered) =>
+            Console.printLine("Client Channel unregistered")
+
+          case ChannelEvent(_, other) =>
+            Console.printLine("Client received other event: " + other)
+        }
+
+      val httpSocketClient: Http[Any, Throwable, WebSocketChannelEvent, Unit] =
+        messageSocketClient ++ channelSocketClient
+
+      val socketAppClient: SocketApp[Any] =
+        httpSocketClient.toSocketApp
+          .withDecoder(decoder)
+          .withProtocol(protocol)
+
+      for {
+        portAndPromise <- serverSetup
+        response       <- ZIO.serviceWithZIO[Client](_.socket(s"ws://localhost:${portAndPromise._1}/", socketAppClient))
+        _              <- portAndPromise._2.await
+      } yield assertTrue(response.status == Status.SwitchingProtocols)
+    }
+
+}
