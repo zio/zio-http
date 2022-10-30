389c27f277f1435aed91f12c2532e0b5e8fead95
John A. De Goes <john@degoes.net>
2022-10-30 11:06:33 -0400

    fix scala variant

--- zio-http/src/test/scala/zio/http/internal/RichTextCodecSpec.scala
+++ zio-http/src/test/scala/zio/http/internal/RichTextCodecSpec.scala
@@ -3,7 +3,6 @@ package zio.http.api.internal
 import zio.Scope
 import zio.http._
 import zio.http.api._
-import zio.http.api.internal.RichTextCodec
 import zio.test.Assertion.equalTo
 import zio.test._
 
