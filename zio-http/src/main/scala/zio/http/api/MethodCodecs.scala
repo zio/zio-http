dfc18d1b4b1a02de3104c6251b5be565b80756a7
Andrey Lebedev <5165852+dev-andrey@users.noreply.github.com>
2022-10-30 11:14:51 -0400

    feat: add auth and cors middleware (#1736)
    converting existing middleware to new encoding

--- zio-http/src/main/scala/zio/http/api/MethodCodecs.scala
+++ zio-http/src/main/scala/zio/http/api/MethodCodecs.scala
@@ -7,6 +7,14 @@ private[api] trait MethodCodecs {
   def method(method: zio.http.model.Method): HttpCodec[CodecType.Method, Unit] =
     HttpCodec.Method(TextCodec.constant(method.toString()))
 
+  def method: HttpCodec[CodecType.Method, zio.http.model.Method] =
+    HttpCodec
+      .Method(TextCodec.string)
+      .transform(
+        methodStr => zio.http.model.Method.fromString(methodStr),
+        method => method.text,
+      )
+
   def get: HttpCodec[Method, Unit]    = method(zio.http.model.Method.GET)
   def put: HttpCodec[Method, Unit]    = method(zio.http.model.Method.PUT)
   def post: HttpCodec[Method, Unit]   = method(zio.http.model.Method.POST)
