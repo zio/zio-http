5453efb8c19783603444ceaffc81bfd4afd643eb
Watt Poosanguansit <wpoosanguansit@yahoo.com>
2022-11-19 08:10:13 +0700

    Commit code for expires middleware.

--- zio-http/src/main/scala/zio/http/api/Middleware.scala
+++ zio-http/src/main/scala/zio/http/api/Middleware.scala
@@ -316,6 +316,14 @@ object Middleware {
     )(identity)
   }
 
+  def withExpires(value: CharSequence): Middleware[Any, Unit, Unit] = {
+    fromFunction(
+      MiddlewareSpec.withExpires.mapOut(
+        _.unit(Expires.toExpires(value.toString)),
+      ),
+    )(identity)
+  }
+
   val none: Middleware[Any, Unit, Unit] =
     fromFunction(MiddlewareSpec.none)(_ => ())
 
