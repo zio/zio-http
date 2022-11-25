0d46756a9005583a80748278da1595d9756d4c2c
Gabriel Ciuloaica <gciuloaica@gmail.com>
2022-11-25 10:02:08 +0200

    implemented LastModified header (#1826)
    * implemented LastModified header
    
    * fixed conflict

--- zio-http/src/main/scala/zio/http/api/HeaderCodecs.scala
+++ zio-http/src/main/scala/zio/http/api/HeaderCodecs.scala
@@ -137,9 +137,13 @@ trait HeaderCodecs {
       .transform(IfRange.toIfRange, IfRange.fromIfRange)
   final val ifUnmodifiedSince: HeaderCodec[IfUnmodifiedSince] =
     header(HeaderNames.ifUnmodifiedSince.toString(), TextCodec.string)
-      .transform(IfUnmodifiedSince.toIfUnmodifiedSince, IfUnmodifiedSince.fromIfUnmodifiedSince)
-  final val lastModified: HeaderCodec[String]                 =
+      .transform(
+        IfUnmodifiedSince.toIfUnmodifiedSince,
+        IfUnmodifiedSince.fromIfUnmodifiedSince,
+      )
+  final val lastModified: HeaderCodec[LastModified]           =
     header(HeaderNames.lastModified.toString(), TextCodec.string)
+      .transform(LastModified.toLastModified, LastModified.fromLastModified)
   final val location: HeaderCodec[Location]                   =
     header(HeaderNames.location.toString(), TextCodec.string).transform(Location.toLocation, Location.fromLocation)
   final val maxForwards: HeaderCodec[MaxForwards]             =
