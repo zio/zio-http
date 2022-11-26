f3cfe146d69322411a65745f1b3c9168d243419a
Gabriel Ciuloaica <gciuloaica@gmail.com>
2022-11-26 18:20:35 +0200

    imlemented Via header

--- zio-http/src/main/scala/zio/http/model/headers/values/Via.scala
+++ zio-http/src/main/scala/zio/http/model/headers/values/Via.scala
@@ -1,6 +1,7 @@
 package zio.http.model.headers.values
 
 import zio.Chunk
+import zio.http.model.headers.values.Via.ReceivedProtocol.InvalidProtocol
 
 sealed trait Via
 
@@ -11,59 +12,57 @@ sealed trait Via
  * of senders along the request/response chain
  */
 object Via {
-  // Via: 1.1 vegur
-  // Via: HTTP/1.1 GWA
-  // Via: 1.0 fred, 1.1 p.example.net
+  sealed trait ReceivedProtocol
+  object ReceivedProtocol {
+    final case class Version(version: String)                           extends ReceivedProtocol
+    final case class ProtocolVersion(protocol: String, version: String) extends ReceivedProtocol
+
+    case object InvalidProtocol extends ReceivedProtocol
+  }
+
   final case class ViaValues(values: Chunk[Via]) extends Via
-  final case class ViaValue(protocol: String, host: String, port: Option[Int] = None, comment: Option[String] = None)
+  final case class DetailedValue(receivedProtocol: ReceivedProtocol, receivedBy: String, comment: Option[String])
       extends Via
-  final case class InvalidVia(value: String)     extends Via
+  case object InvalidVia                         extends Via
 
-  def toVia(values: String): Via = if (values.isEmpty) InvalidVia(values)
+  def toVia(values: String): Via = if (values.isEmpty) InvalidVia
   else {
-    val parsedValues: Array[Via] = values
-      .split(",")
-      .map(_.trim)
-      .map { value =>
-        val parts = value.split(" ")
-        if (parts.length == 2) {
-          val protocol = parts(0)
-          val host     = parts(1)
-          ViaValue(protocol, host, None, None)
-        } else if (parts.length == 3) {
-          val protocol = parts(0)
-          val host     = parts(1)
-          val port     = parts(2).toInt
-          ViaValue(protocol, host, Some(port), None)
-        } else if (parts.length == 4) {
-          val protocol = parts(0)
-          val host     = parts(1)
-          val port     = parts(2).toInt
-          val comment  = parts(3)
-          ViaValue(protocol, host, Some(port), Some(comment))
-        } else {
-          InvalidVia(value)
-        }
+    val viaValues = values.split(",").map(_.trim).map { value =>
+      value.split(" ").toList match {
+        case receivedProtocol :: receivedBy :: Nil            =>
+          val rp = toReceivedProtocol(receivedProtocol)
+          if (rp == InvalidProtocol) InvalidVia
+          else DetailedValue(rp, receivedBy, None)
+        case receivedProtocol :: receivedBy :: comment :: Nil =>
+          val rp = toReceivedProtocol(receivedProtocol)
+          if (rp == InvalidProtocol) InvalidVia
+          else DetailedValue(rp, receivedBy, Some(comment))
+        case _                                                => InvalidVia
       }
-
-    ViaValues(Chunk.fromArray(parsedValues))
+    }
+    ViaValues(Chunk.fromArray(viaValues))
   }
 
   def fromVia(via: Via): String = via match {
-    case ViaValues(values)                       =>
-      values.map {
-        case ViaValue(protocol, host, port, comment) =>
-          formatViaValue(protocol, host, port, comment)
-        case InvalidVia(value)                       => value
-      }.mkString(", ")
-    case ViaValue(protocol, host, port, comment) =>
-      formatViaValue(protocol, host, port, comment)
-    case InvalidVia(value)                       => value
+    case ViaValues(values)                                    =>
+      values.map(fromVia).mkString(", ")
+    case DetailedValue(receivedProtocol, receivedBy, comment) =>
+      s"${fromReceivedProtocol(receivedProtocol)} $receivedBy ${comment.getOrElse("")}"
+    case InvalidVia                                           => ""
   }
 
-  private def formatViaValue(protocol: String, host: String, port: Option[Int], comment: Option[String]): String = {
-    val portString    = port.fold("")(p => s" $p")
-    val commentString = comment.fold("")(c => s" $c")
-    s"$protocol $host$portString$commentString"
+  private def fromReceivedProtocol(receivedProtocol: ReceivedProtocol): String = receivedProtocol match {
+    case ReceivedProtocol.Version(version)                   => version
+    case ReceivedProtocol.ProtocolVersion(protocol, version) => s"$protocol/$version"
+    case ReceivedProtocol.InvalidProtocol                    => ""
   }
+
+  private def toReceivedProtocol(value: String): ReceivedProtocol = {
+    value.split("/").toList match {
+      case version :: Nil             => ReceivedProtocol.Version(version)
+      case protocol :: version :: Nil => ReceivedProtocol.ProtocolVersion(protocol, version)
+      case _                          => InvalidProtocol
+    }
+  }
+
 }
