e4b410583be8c7857ea9c1f09cba641598816bf2
Gabriel Ciuloaica <gciuloaica@gmail.com>
2022-11-28 11:04:12 +0200

    Via header (#1851)
    * started implementation for VIA header
    
    * imlemented Via header

---
+++ zio-http/src/test/scala/zio/http/model/headers/values/ViaSpec.scala
@@ -0,0 +1,39 @@
+package zio.http.model.headers.values
+
+import zio.http.model.headers.values.Via.ReceivedProtocol
+import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertTrue}
+import zio.{Chunk, Scope}
+
+object ViaSpec extends ZIOSpecDefault {
+  override def spec: Spec[TestEnvironment with Scope, Any] = suite("Via suite")(
+    test("parsing of valid values") {
+      assertTrue(
+        Via.toVia("1.1 vegur") == Via.ViaValues(
+          Chunk(Via.DetailedValue(ReceivedProtocol.Version("1.1"), "vegur", None)),
+        ),
+      ) &&
+      assertTrue(
+        Via.toVia("HTTP/1.1 GWA") == Via.ViaValues(
+          Chunk(Via.DetailedValue(ReceivedProtocol.ProtocolVersion("HTTP", "1.1"), "GWA", None)),
+        ),
+      ) &&
+      assertTrue(
+        Via.toVia("1.0 fred, 1.1 p.example.net") == Via.ViaValues(
+          Chunk(
+            Via.DetailedValue(ReceivedProtocol.Version("1.0"), "fred", None),
+            Via.DetailedValue(ReceivedProtocol.Version("1.1"), "p.example.net", None),
+          ),
+        ),
+      )
+      assertTrue(
+        Via.toVia("1.0 fred, 1.1 p.example.net, 1.1 nowhere.com (Apache/1.1)") == Via.ViaValues(
+          Chunk(
+            Via.DetailedValue(ReceivedProtocol.Version("1.0"), "fred", None),
+            Via.DetailedValue(ReceivedProtocol.Version("1.1"), "p.example.net", None),
+            Via.DetailedValue(ReceivedProtocol.Version("1.1"), "nowhere.com", Some("(Apache/1.1)")),
+          ),
+        ),
+      )
+    },
+  )
+}
