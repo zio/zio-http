79413b9199876515113c4205b0e1274669ab2731
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2023-02-08 10:46:30 +0100

    Call error callback for ZIO die failures (#1979)
    * Call error callback for ZIO die failures
    
    * Change callback parameter type

--- zio-http/src/main/scala/zio/http/netty/server/ServerInboundHandler.scala
+++ zio-http/src/main/scala/zio/http/netty/server/ServerInboundHandler.scala
@@ -83,7 +83,7 @@ private[zio] final case class ServerInboundHandler(
 
   override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
     if (errCallback != null) {
-      runtime.run(ctx, NettyRuntime.noopEnsuring)(errCallback(cause))
+      runtime.run(ctx, NettyRuntime.noopEnsuring)(errCallback(Cause.die(cause)))
     } else {
       cause match {
         case ioe: IOException if ioe.getMessage.contentEquals("Connection reset by peer") =>
@@ -264,13 +264,14 @@ private[zio] final case class ServerInboundHandler(
     runtime.run(ctx, ensured) {
       val pgm = for {
         response <- exit.sandbox.catchAll { error =>
-          ZIO.succeedNow {
-            error.failureOrCause
-              .fold[Response](
-                identity,
-                cause => HttpError.InternalServerError(cause = Some(FiberFailure(cause))).toResponse,
-              )
-          }
+          error.failureOrCause
+            .fold[UIO[Response]](
+              response => ZIO.succeedNow(response),
+              cause =>
+                (if (errCallback ne null) errCallback(cause) else ZIO.unit).as(
+                  HttpError.InternalServerError(cause = Some(FiberFailure(cause))).toResponse,
+                ),
+            )
         }
         _        <-
           if (response ne null) {
