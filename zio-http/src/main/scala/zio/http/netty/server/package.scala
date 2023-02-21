8c66e6922f5f7fb943052456159770241e1362e1
frekw <fredrik@warnsberg.se>
2023-02-21 22:42:15 +0100

    Re-export NettyDriver's ZLayers

--- zio-http/src/main/scala/zio/http/netty/server/package.scala
+++ zio-http/src/main/scala/zio/http/netty/server/package.scala
@@ -1,11 +1,30 @@
 package zio.http.netty
 
-import zio.http._
+import io.netty.channel.{Channel => NettyChannel, _}
 import zio._
-import java.util.concurrent.atomic.AtomicReference
+import zio.http._
+import zio.http.netty._
 import zio.stacktracer.TracingImplicits.disableAutoTrace // scalafix:ok;
 
+import java.util.concurrent.atomic.AtomicReference
+
 package object server {
+  // Re-export this to work around Scala 2.12 bug where a private
+  // trait's companion object cannot be accessed.
+  val nettyDriverDefault: ZLayer[ServerConfig, Throwable, Driver] = NettyDriver.default
+  val nettyDriverManual: ZLayer[EventLoopGroup & ChannelFactory[ServerChannel] & ServerConfig, Nothing, Driver] =
+    NettyDriver.manual
+  val nettyDriverMake: ZIO[
+    AppRef
+      & ChannelFactory[ServerChannel]
+      & ChannelInitializer[NettyChannel]
+      & EventLoopGroup
+      & ErrorCallbackRef
+      & ServerConfig
+      & ServerInboundHandler,
+    Nothing,
+    Driver,
+  ] = NettyDriver.make
 
   private[server] type ErrorCallbackRef = AtomicReference[Option[Server.ErrorCallback]]
   private[server] type AppRef           = AtomicReference[(App[Any], ZEnvironment[Any])]
--- zio-http/src/test/scala/otherorg/NettyDriverVisibilitySpec.scala
+++ zio-http/src/test/scala/otherorg/NettyDriverVisibilitySpec.scala
@@ -1,17 +1,18 @@
 package otherorg
 
-import zio.http.netty.server.NettyDriver
+import zio.test.Assertion._
 import zio.test._
 
-import Assertion._
+import zio.http.netty.server.NettyDriver
 
 object NettyDriverVisibilitySpec extends ZIOSpecDefault {
   def spec = suite("NettyDriverVisibilitySpec")(
     test("NettyDriver is visible outside of zio") {
       val result = typeCheck {
         """
-        val default = NettyDriver.default
-        val make    = NettyDriver.make
+        val default = zio.http.netty.server.nettyDriverDefault
+        val make    = zio.http.netty.server.nettyDriverMake
+        val manual    = zio.http.netty.server.nettyDriverManual
         """
       }
       assertZIO(result)(isRight(anything))
