e7057ef423e5db7fa8e6316dec744139923c7def
Jorge Vasquez <jorge.vasquez0123@proton.me>
2023-02-27 15:55:45 -0400

    Rebase to include latest changes in main branch

--- zio-http-cli/src/main/scala/zio/http/cli/HttpCli.scala
+++ zio-http-cli/src/main/scala/zio/http/cli/HttpCli.scala
@@ -2,8 +2,11 @@ package zio.http.endpoint.cli
 
 import zio._
 import zio.cli._
-import zio.http.endpoint.internal.TextCodec
-import zio.http.endpoint.{CodecType, Endpoint, EndpointMiddleware, HttpCodec}
+
+import zio.http.codec.HttpCodec._
+import zio.http.codec.SimpleCodec.{Specified, Unspecified}
+import zio.http.codec.{HttpCodec, HttpCodecType, SimpleCodec}
+import zio.http.endpoint.{Endpoint, EndpointMiddleware}
 import zio.http.{Middleware, Request, Response, model}
 
 final case class CliEndpoint[A](embed: (A, Request) => Request, options: Options[A]) { self =>
@@ -19,30 +22,27 @@ object CliEndpoint                                                              
   def fromEndpoint[In, Err, Out, M <: EndpointMiddleware](endpoint: Endpoint[In, Err, Out, M]): CliEndpoint[_] =
     fromInput(endpoint.input)
 
-  private def fromInput[Input](input: HttpCodec[CodecType.RequestType, Input]): CliEndpoint[_] =
+  private def fromInput[Input](input: HttpCodec[HttpCodecType.RequestType, Input]): CliEndpoint[_] =
     input.asInstanceOf[HttpCodec[_, _]] match {
-      case HttpCodec.Halt                                => ???
-      case HttpCodec.TransformOrFail(api, f, g)          => ???
-      case HttpCodec.Combine(left, right, inputCombiner) => ???
-      case HttpCodec.WithDoc(in, doc)                    => ???
-      case HttpCodec.Query(name, textCodec, index)       => ???
-      case HttpCodec.Status(textCodec, index)            => ???
-      case HttpCodec.Method(methodCodec, index)          =>
-        methodCodec.asInstanceOf[TextCodec[_]] match {
-          case TextCodec.StringCodec      => ???
-          case TextCodec.BooleanCodec     => ???
-          case TextCodec.IntCodec         => ???
-          case TextCodec.UUIDCodec        => ???
-          case TextCodec.Constant(string) =>
-            val method = model.Method.fromString(string)
-            CliEndpoint[Unit]((_, request) => request.copy(method = method), Options.none)
+      case Combine(left, right, inputCombiner) => ???
+      case Content(schema, index)              => ???
+      case ContentStream(schema, index)        => ???
+      case Empty                               => ???
+      case Fallback(left, right)               => ???
+      case Halt                                => ???
+      case Header(name, textCodec, index)      => ???
+      case Method(codec, index)                =>
+        codec.asInstanceOf[SimpleCodec[_, _]] match {
+          case SimpleCodec.Specified(method) =>
+            CliEndpoint[Unit]((_, request) => request.copy(method = method.asInstanceOf[model.Method]), Options.none)
+          case SimpleCodec.Unspecified()     =>
+            CliEndpoint[Unit]((_, request) => request, Options.none)
         }
-      case HttpCodec.Header(name, textCodec, index)      => ???
-      case HttpCodec.Body(schema, index)                 => ???
-      case HttpCodec.Path(textCodec, name, index)        => ???
-      case HttpCodec.BodyStream(schema, index)           => ???
-      case HttpCodec.Fallback(left, right)               => ???
-      case HttpCodec.Empty                               => ???
+      case Path(textCodec, name, index)        => ???
+      case Query(name, textCodec, index)       => ???
+      case Status(codec, index)                => ???
+      case TransformOrFail(api, f, g)          => ???
+      case WithDoc(in, doc)                    => ???
     }
 }
 
