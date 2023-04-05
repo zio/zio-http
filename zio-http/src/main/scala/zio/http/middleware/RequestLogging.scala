867869756651491241dc2f63aeffea364226fb1d
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2023-04-05 09:48:09 +0200

    Log failures in debug and request logging middlewares (#2082)

--- zio-http/src/main/scala/zio/http/middleware/RequestLogging.scala
+++ zio-http/src/main/scala/zio/http/middleware/RequestLogging.scala
@@ -18,7 +18,7 @@ package zio.http.middleware
 
 import java.nio.charset.{Charset, StandardCharsets}
 
-import zio.{Clock, LogAnnotation, LogLevel, Trace, ZIO}
+import zio.{Exit, LogAnnotation, LogLevel, Trace, ZIO}
 
 import zio.http._
 import zio.http.model.{Header, Status}
@@ -27,6 +27,7 @@ private[zio] trait RequestLogging { self: RequestHandlerMiddlewares =>
 
   final def requestLogging(
     level: Status => LogLevel = (_: Status) => LogLevel.Info,
+    failureLevel: LogLevel = LogLevel.Warning,
     loggedRequestHeaders: Set[String] = Set.empty,
     loggedResponseHeader: Set[String] = Set.empty,
     logRequestBody: Boolean = false,
@@ -34,64 +35,104 @@ private[zio] trait RequestLogging { self: RequestHandlerMiddlewares =>
     requestCharset: Charset = StandardCharsets.UTF_8,
     responseCharset: Charset = StandardCharsets.UTF_8,
   )(implicit trace: Trace): RequestHandlerMiddleware[Nothing, Any, Nothing, Any] =
-    interceptPatchZIO { request =>
-      Clock.nanoTime.map(start => (request, start))
-    } { case (response, (request, start)) =>
-      for {
-        end <- Clock.nanoTime
-        durationMs = (end - start) / 1000000
-        patch <- ZIO
-          .logLevel(level(response.status)) {
-            val requestHeaders  =
-              request.headers.collect {
-                case header: Header if loggedRequestHeaders.contains(header.headerName) =>
-                  LogAnnotation(header.headerName, header.renderedValue)
-              }.toSet
-            val responseHeaders =
-              response.headers.collect {
-                case header: Header if loggedResponseHeader.contains(header.headerName) =>
-                  LogAnnotation(header.headerName, header.renderedValue)
-              }.toSet
+    new RequestHandlerMiddleware.Simple[Any, Nothing] {
+      override def apply[R1 <: Any, Err1 >: Nothing](
+        handler: Handler[R1, Err1, Request, Response],
+      )(implicit trace: Trace): Handler[R1, Err1, Request, Response] =
+        Handler.fromFunctionZIO { (request: Request) =>
+          handler
+            .runZIO(request)
+            .sandbox
+            .exit
+            .timed
+            .tap {
+              case (duration, Exit.Success(response)) =>
+                ZIO
+                  .logLevel(level(response.status)) {
+                    val requestHeaders  =
+                      request.headers.collect {
+                        case header: Header if loggedRequestHeaders.contains(header.headerName) =>
+                          LogAnnotation(header.headerName, header.renderedValue)
+                      }.toSet
+                    val responseHeaders =
+                      response.headers.collect {
+                        case header: Header if loggedResponseHeader.contains(header.headerName) =>
+                          LogAnnotation(header.headerName, header.renderedValue)
+                      }.toSet
 
-            val requestBody  = if (request.body.isComplete) request.body.asChunk.option else ZIO.none
-            val responseBody = if (response.body.isComplete) response.body.asChunk.option else ZIO.none
+                    val requestBody  = if (request.body.isComplete) request.body.asChunk.option else ZIO.none
+                    val responseBody = if (response.body.isComplete) response.body.asChunk.option else ZIO.none
 
-            requestBody.flatMap { requestBodyChunk =>
-              responseBody.flatMap { responseBodyChunk =>
-                val bodyAnnotations = Set(
-                  requestBodyChunk.map(chunk => LogAnnotation("request_size", chunk.size.toString)),
-                  requestBodyChunk.flatMap(chunk =>
-                    if (logRequestBody)
-                      Some(LogAnnotation("request", new String(chunk.toArray, requestCharset)))
-                    else None,
-                  ),
-                  responseBodyChunk.map(chunk => LogAnnotation("response_size", chunk.size.toString)),
-                  responseBodyChunk.flatMap(chunk =>
-                    if (logResponseBody)
-                      Some(LogAnnotation("response", new String(chunk.toArray, responseCharset)))
-                    else None,
-                  ),
-                ).flatten
+                    requestBody.flatMap { requestBodyChunk =>
+                      responseBody.flatMap { responseBodyChunk =>
+                        val bodyAnnotations = Set(
+                          requestBodyChunk.map(chunk => LogAnnotation("request_size", chunk.size.toString)),
+                          requestBodyChunk.flatMap(chunk =>
+                            if (logRequestBody)
+                              Some(LogAnnotation("request", new String(chunk.toArray, requestCharset)))
+                            else None,
+                          ),
+                          responseBodyChunk.map(chunk => LogAnnotation("response_size", chunk.size.toString)),
+                          responseBodyChunk.flatMap(chunk =>
+                            if (logResponseBody)
+                              Some(LogAnnotation("response", new String(chunk.toArray, responseCharset)))
+                            else None,
+                          ),
+                        ).flatten
 
-                ZIO.logAnnotate(
-                  Set(
-                    LogAnnotation("status_code", response.status.text),
-                    LogAnnotation("method", request.method.toString()),
-                    LogAnnotation("url", request.url.encode),
-                    LogAnnotation("duration_ms", durationMs.toString),
-                  ) union
-                    requestHeaders union
-                    responseHeaders union
-                    bodyAnnotations,
-                ) {
-                  ZIO
-                    .log("Http request served")
-                    .as(Response.Patch.empty)
-                }
-              }
+                        ZIO.logAnnotate(
+                          Set(
+                            LogAnnotation("status_code", response.status.text),
+                            LogAnnotation("method", request.method.toString()),
+                            LogAnnotation("url", request.url.encode),
+                            LogAnnotation("duration_ms", duration.toMillis.toString),
+                          ) union
+                            requestHeaders union
+                            responseHeaders union
+                            bodyAnnotations,
+                        ) {
+                          ZIO.log("Http request served")
+                        }
+                      }
+                    }
+                  }
+              case (duration, Exit.Failure(cause))    =>
+                ZIO
+                  .logLevel(failureLevel) {
+                    val requestHeaders =
+                      request.headers.collect {
+                        case header: Header if loggedRequestHeaders.contains(header.headerName) =>
+                          LogAnnotation(header.headerName, header.renderedValue)
+                      }.toSet
+
+                    val requestBody = if (request.body.isComplete) request.body.asChunk.option else ZIO.none
+
+                    requestBody.flatMap { requestBodyChunk =>
+                      val bodyAnnotations = Set(
+                        requestBodyChunk.map(chunk => LogAnnotation("request_size", chunk.size.toString)),
+                        requestBodyChunk.flatMap(chunk =>
+                          if (logRequestBody)
+                            Some(LogAnnotation("request", new String(chunk.toArray, requestCharset)))
+                          else None,
+                        ),
+                      ).flatten
+
+                      ZIO.logAnnotate(
+                        Set(
+                          LogAnnotation("method", request.method.toString()),
+                          LogAnnotation("url", request.url.encode),
+                          LogAnnotation("duration_ms", duration.toMillis.toString),
+                        ) union
+                          requestHeaders union
+                          bodyAnnotations,
+                      ) {
+                        ZIO.logCause("Http request failed", cause)
+                      }
+                    }
+                  }
             }
-          }
-      } yield patch
+            .flatMap(_._2)
+            .unsandbox
+        }
     }
-
 }
