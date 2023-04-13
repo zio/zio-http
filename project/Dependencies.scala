4523966f8f4f52feb1fa07403a444616a91a8e03
Scala Steward <scala_steward@virtuslab.com>
2023-04-13 13:55:04 +0000

    Update zio, zio-streams, zio-test, ... to 2.0.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val NettyVersion                  = "4.1.91.Final"
   val NettyIncubatorVersion         = "0.0.19.Final"
   val ScalaCompactCollectionVersion = "2.8.1"
-  val ZioVersion                    = "2.0.11"
+  val ZioVersion                    = "2.0.12"
   val ZioSchemaVersion              = "0.4.8"
   val SttpVersion                   = "3.3.18"
 
