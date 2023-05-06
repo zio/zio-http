345b205348b208c15349a8d2066c7fe2578c83b1
Adam Fraser <adam.fraser@gmail.com>
2023-05-06 19:55:29 +0100

    cleanup

--- zio-http-example/src/main/scala/example/WebSocketSimpleClient.scala
+++ zio-http-example/src/main/scala/example/WebSocketSimpleClient.scala
@@ -3,7 +3,7 @@ package example
 import zio._
 
 import zio.http.ChannelEvent.{ChannelRead, UserEvent, UserEventTriggered}
-import zio.http.socket.{WebSocketChannel, WebSocketChannelEvent, WebSocketFrame}
+import zio.http.socket.{WebSocketChannel, WebSocketFrame}
 import zio.http.{ChannelEvent, Client, Http, Response}
 
 object WebSocketSimpleClient extends ZIOAppDefault {
