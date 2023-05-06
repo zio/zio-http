5591fe076138bc5637dddc5a51f3d61d4d4ba33d
Adam Fraser <adam.fraser@gmail.com>
2023-05-06 20:38:14 +0100

    channel operators

--- zio-http/src/main/scala/zio/http/Channel.scala
+++ zio-http/src/main/scala/zio/http/Channel.scala
@@ -18,12 +18,62 @@ package zio.http
 
 import zio._
 
-trait Channel[-In, +Out] {
+/**
+  * A `Channel` is an asynchronous communication channel that supports
+  * receiving messages of type `In` and sending messages of type `Out`.
+  */
+trait Channel[-In, +Out] { self =>
+
+  /**
+    * Await shutdown of the channel.
+    */
   def awaitShutdown: UIO[Unit]
 
+  /**
+    * Read a message from the channel, suspending until the next message is
+    * available.
+    */
   def receive: Task[Out]
 
+  /**
+    * Send a message to the channel.
+    */
   def send(in: In): Task[Unit]
 
+  /**
+    * Shut down the channel.
+    */
   def shutdown: UIO[Unit]
+
+  /**
+    * Constructs a new channel that automatically transforms messages sent to
+    * this channel using the specified function.
+    */
+  final def contramap[In2](f: In2 => In): Channel[In2, Out] =
+    new Channel[In2, Out] {
+      def awaitShutdown: UIO[Unit] =
+        self.awaitShutdown
+      def receive: Task[Out] =
+        self.receive
+      def send(in: In2): Task[Unit] =
+        self.send(f(in))
+      def shutdown: UIO[Unit] =
+        self.shutdown
+    }
+
+  /**
+   * Constructs a new channel that automatically transforms messages received
+   * from this channel using the specified function.
+   */
+  final def map[Out2](f: Out => Out2): Channel[In, Out2] =
+    new Channel[In, Out2] {
+      def awaitShutdown: UIO[Unit] =
+        self.awaitShutdown
+      def receive: Task[Out2] =
+        self.receive.map(f)
+      def send(in: In): Task[Unit] =
+        self.send(in)
+      def shutdown: UIO[Unit] =
+        self.shutdown
+    }
 }
