733faa2446b576f2472318bdf5768a8397c0ccc3
Adam Fraser <adam.fraser@gmail.com>
2023-05-06 19:46:28 +0100

    cleanup

--- zio-http-example/src/main/scala/example/WebSocketAdvanced.scala
+++ zio-http-example/src/main/scala/example/WebSocketAdvanced.scala
@@ -1,67 +1,56 @@
-// package example
+package example
 
-// import zio._
+import zio._
 
-// import zio.http.ChannelEvent.{ChannelRead, ExceptionCaught, UserEvent, UserEventTriggered}
-// import zio.http._
-// import zio.http.socket._
+import zio.http.ChannelEvent.{ChannelRead, ExceptionCaught, UserEvent, UserEventTriggered}
+import zio.http._
+import zio.http.socket._
 
-// object WebSocketAdvanced extends ZIOAppDefault {
-//   val messageFilter: Http[Any, Nothing, WebSocketChannel, (WebSocketChannelEvent, String)] =
-//     Http.collect[WebSocketChannel] { case ChannelEvent(channel, ChannelRead(WebSocketFrame.Text(message))) =>
-//       (channel, message)
-//     }
+object WebSocketAdvanced extends ZIOAppDefault {
 
-//   val messageSocket: Http[Any, Throwable, WebSocketChannelEvent, Unit] =
-//     messageFilter >>> Handler.fromFunctionZIO[(WebSocketChannel, String)] {
-//       case (ch, "end") => ch.close()
+  val httpSocket: Http[Any, Throwable, WebSocketChannelEvent, Unit] =
+    Http.collectZIO[WebSocketChannel] { case channel =>
+      channel.receive.flatMap {
+        case ChannelRead(WebSocketFrame.Text("end"))           =>
+          channel.shutdown
 
-//       // Send a "bar" if the server sends a "foo"
-//       case (ch, "foo") => ch.writeAndFlush(WebSocketFrame.text("bar"))
+        // Send a "bar" if the server sends a "foo"
+        case ChannelRead(WebSocketFrame.Text("foo"))           =>
+          channel.send(ChannelRead(WebSocketFrame.text("bar")))
 
-//       // Send a "foo" if the server sends a "bar"
-//       case (ch, "bar") => ch.writeAndFlush(WebSocketFrame.text("foo"))
+        // Send a "foo" if the server sends a "bar"
+        case ChannelRead(WebSocketFrame.Text("bar"))           =>
+          channel.send(ChannelRead(WebSocketFrame.text("foo")))
 
-//       // Echo the same message 10 times if it's not "foo" or "bar"
-//       // Improve performance by writing multiple frames at once
-//       // And flushing it on the channel only once.
-//       case (ch, text) =>
-//         ch.write(WebSocketFrame.text(text)).repeatN(10) *> ch.flush
-//     }
+        // Echo the same message 10 times if it's not "foo" or "bar"
+        case ChannelRead(WebSocketFrame.Text(text))            =>
+          channel.send(ChannelRead(WebSocketFrame.text(text))).repeatN(10)
 
-//   val channelSocket: Http[Any, Throwable, WebSocketChannelEvent, Unit] =
-//     Http.collectZIO[WebSocketChannelEvent] {
+        // Send a "greeting" message to the server once the connection is established
+        case UserEventTriggered(UserEvent.HandshakeComplete)   =>
+          channel.send(ChannelRead(WebSocketFrame.text("Greetings!")))
 
-//       // Send a "greeting" message to the server once the connection is established
-//       case ChannelEvent(ch, UserEventTriggered(UserEvent.HandshakeComplete))  =>
-//         ch.writeAndFlush(WebSocketFrame.text("Greetings!"))
+        // Log when the channel is getting closed
+        case ChannelRead(WebSocketFrame.Close(status, reason)) =>
+          Console.printLine("Closing channel with status: " + status + " and reason: " + reason)
 
-//       // Log when the channel is getting closed
-//       case ChannelEvent(_, ChannelRead(WebSocketFrame.Close(status, reason))) =>
-//         Console.printLine("Closing channel with status: " + status + " and reason: " + reason)
+        // Print the exception if it's not a normal close
+        case ExceptionCaught(cause)                            =>
+          Console.printLine(s"Channel error!: ${cause.getMessage}")
 
-//       // Print the exception if it's not a normal close
-//       case ChannelEvent(_, ExceptionCaught(cause))                            =>
-//         Console.printLine(s"Channel error!: ${cause.getMessage}")
-//     }
+        case _ =>
+          ZIO.unit
+      }.forever
+    }
 
-//   val httpSocket: Http[Any, Throwable, WebSocketChannelEvent, Unit] =
-//     messageSocket ++ channelSocket
+  val socketApp: SocketApp[Any] =
+    httpSocket.toSocketApp
 
-//   val protocol = SocketProtocol.default.withSubProtocol(Some("json")) // Setup protocol settings
+  val app: Http[Any, Nothing, Request, Response] =
+    Http.collectZIO[Request] {
+      case Method.GET -> !! / "greet" / name  => ZIO.succeed(Response.text(s"Greetings ${name}!"))
+      case Method.GET -> !! / "subscriptions" => socketApp.toResponse
+    }
 
-//   val decoder = SocketDecoder.default.withExtensions(allowed = true) // Setup decoder settings
-
-//   val socketApp: SocketApp[Any] = // Combine all channel handlers together
-//     httpSocket.toSocketApp
-//       .withDecoder(decoder)   // Setup websocket decoder config
-//       .withProtocol(protocol) // Setup websocket protocol config
-
-//   val app: Http[Any, Nothing, Request, Response] =
-//     Http.collectZIO[Request] {
-//       case Method.GET -> !! / "greet" / name  => ZIO.succeed(Response.text(s"Greetings ${name}!"))
-//       case Method.GET -> !! / "subscriptions" => socketApp.toResponse
-//     }
-
-//   override val run = Server.serve(app).provide(Server.default)
-// }
+  override val run = Server.serve(app).provide(Server.default)
+}
