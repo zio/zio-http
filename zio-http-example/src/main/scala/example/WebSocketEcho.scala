59b025c03a21012864d66e5d6d73acbd6f2de237
Adam Fraser <adam.fraser@gmail.com>
2023-05-06 19:48:05 +0100

    cleanup

--- zio-http-example/src/main/scala/example/WebSocketEcho.scala
+++ zio-http-example/src/main/scala/example/WebSocketEcho.scala
@@ -4,7 +4,7 @@ import zio._
 
 import zio.http.ChannelEvent.ChannelRead
 import zio.http._
-import zio.http.socket.{WebSocketChannel, WebSocketChannelEvent, WebSocketFrame}
+import zio.http.socket.{WebSocketChannel, WebSocketFrame}
 
 object WebSocketEcho extends ZIOAppDefault {
   private val socket: Http[Any, Throwable, WebSocketChannel, Unit] =
