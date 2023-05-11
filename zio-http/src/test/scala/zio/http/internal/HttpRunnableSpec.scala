42c7396df57a7688e660dcfd6a5e510e00dd096c
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2023-05-11 12:51:54 +0200

    Improve dx for ZClient (#2139)
    - don't append trailing slash for empty pathSuffixes
    - add no params method for get/head/delete

--- zio-http/src/test/scala/zio/http/internal/HttpRunnableSpec.scala
+++ zio-http/src/test/scala/zio/http/internal/HttpRunnableSpec.scala
@@ -92,6 +92,23 @@ abstract class HttpRunnableSpec extends ZIOSpecDefault { self =>
         } yield response
       }
 
+    def deployAndRequest(
+      call: Client => ZIO[Any, Throwable, Response],
+    ): Handler[Client with DynamicServer with R, Throwable, Any, Response] = {
+      for {
+        port     <- Handler.fromZIO(DynamicServer.port)
+        id       <- Handler.fromZIO(DynamicServer.deploy[R](app))
+        client   <- Handler.fromZIO(ZIO.service[Client])
+        response <- Handler.fromZIO(
+          call(
+            client
+              .addHeader(DynamicServer.APP_ID, id)
+              .url(URL.decode(s"http://localhost:$port").toOption.get),
+          ),
+        )
+      } yield response
+    }
+
     def deployChunked: Http[R with Client with DynamicServer, Throwable, Request, Response] =
       Http.fromHandler {
         for {
