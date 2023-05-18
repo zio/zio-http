82199a41e7cf5d7a683bec8de839953a423aa0b7
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2023-05-18 17:02:08 +0200

    Formatting

--- zio-http/src/main/scala/zio/http/codec/internal/BodyCodec.scala
+++ zio-http/src/main/scala/zio/http/codec/internal/BodyCodec.scala
@@ -22,13 +22,17 @@ import zio.stacktracer.TracingImplicits.disableAutoTrace
 import zio.stream.{ZPipeline, ZStream}
 
 import zio.http.{Body, MediaType}
+import java.nio.charset.Charset
+
+import zio._
+
+import zio.stream.{ZPipeline, ZStream}
+
 import zio.schema._
 import zio.schema.codec.{BinaryCodec, Codec}
-import zio.stream.{ZPipeline, ZStream}
-import zio.{ZIO, _}
 
 import zio.http.codec.HttpCodecError
-import java.nio.charset.Charset
+import zio.http.{Body, MediaType}
 
 /**
  * A BodyCodec encapsulates the logic necessary to both encode and decode bodies
