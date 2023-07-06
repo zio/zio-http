a854df95bd2601966b2e1e272d6df08a4b34157d
John A. De Goes <john@degoes.net>
2023-07-06 08:50:06 +0100

    end-to-end sketch of new middleware

--- zio-http/src/main/scala/zio/http/Middleware.scala
+++ zio-http/src/main/scala/zio/http/Middleware.scala
@@ -2,93 +2,34 @@ package zio.http
 
 import zio._
 
-sealed trait Middleware[-Env, +Ctx] { self =>
-  import Middleware._
-
-  final def @@[Env1 <: Env, Ctx2](that: Middleware[Env1, Ctx2]): Middleware[Env1, Ctx with Ctx2] =
-    Sequence[Env1, Ctx, Ctx2](self, that)
-
-  private[http] lazy val flattened: Chunk[Leaf[Env, _]] = flatten(self).materialize
+trait Middleware[-Env, +CtxOut2] {
+  def apply[Env1 <: Env, CtxIn, CtxOut](
+    stack: MiddlewareStack[Env1, CtxIn, CtxOut],
+  ): MiddlewareStack[Env1, CtxIn, CtxOut with CtxOut2]
 }
-object Middleware                   {
-  private[http] case object Identity                        extends Middleware[Any, Any]
-  private[http] final case class Sequence[-Env, Ctx1, Ctx2](
-    first: Middleware[Env, Ctx1],
-    second: Middleware[Env, Ctx2],
-  ) extends Middleware[Env, Ctx1 with Ctx2]
-  private[http] abstract class Leaf[-Env, +Ctx]             extends Middleware[Env, Ctx]
-  private[http] sealed trait InterceptContextZIO[-Env, Ctx] extends Leaf[Env, Ctx] {
-    type Session
+object Middleware extends zio.http.internal.HeaderModifier[Middleware[Any, Any]] {
 
-    def incoming(request: Request): ZIO[Env, Response, (Request, Session, ZEnvironment[Ctx])]
+  override def updateHeaders(update: Headers => Headers): Middleware[Any, Any] =
+    updateResponse(_.updateHeaders(update))
 
-    def outgoing(response: Response, session: Session): ZIO[Env, Nothing, Response]
-  }
-  private[http] sealed trait InterceptContext[Ctx]          extends Leaf[Any, Ctx] {
-    type Session
-
-    def incoming(request: Request): Either[Response, (Request, Session, ZEnvironment[Ctx])]
-
-    def outgoing(response: Response, session: Session): Response
-  }
-  private[http] sealed trait InterceptZIO[-Env, Ctx]        extends Leaf[Env, Any] {
-    type Session
-
-    def incoming(request: Request): ZIO[Env, Response, (Request, Session)]
-
-    def outgoing(response: Response, session: Session): ZIO[Env, Nothing, Response]
-  }
-  private[http] sealed trait Intercept                      extends Leaf[Any, Any] {
-    type Session
-
-    def incoming(request: Request): Either[Response, (Request, Session)]
-
-    def outgoing(response: Response, session: Session): Response
-  }
-  private[http] sealed trait InterceptFast                  extends Leaf[Any, Any] {
-    type Session
-
-    def incoming(request: Request): Request
-
-    def outgoing(response: Response, session: Session): Response
-  }
-
-  private def flatten[Env, Ctx](middleware: Middleware[Env, Ctx]): Chunk[Leaf[Env, _]] =
-    middleware match {
-      case Identity                => Chunk.empty
-      case Sequence(first, second) => flatten(first) ++ flatten(second)
-      case leaf: Leaf[Env, ctx]    => Chunk(leaf)
+  def updateResponse(update: Response => Response): Middleware[Any, Any] =
+    new Middleware[Any, Any] {
+      def apply[Env1 <: Any, CtxIn, CtxOut](
+        stack: MiddlewareStack[Env1, CtxIn, CtxOut],
+      ): MiddlewareStack[Env1, CtxIn, CtxOut] =
+        stack.++[Env1, Any](
+          MiddlewareStack.outgoing[Env1](
+            Handler.fromFunction[Either[Response, Response]] { either =>
+              either.map(update)
+            },
+          ),
+        )
     }
 
-  private[http] def executeIncoming[Env, Ctx](
-    middleware: Middleware[Env, Ctx],
-    request: Request,
-  ): ZIO[Env, Response, (Request, ZEnvironment[Ctx])] = {
-    val flattened = middleware.flattened
-
-    val len = flattened.length
-    var i   = 0
-
-    var result: ZIO[Env, Response, (Request, ZEnvironment[Any])] = Exit.succeed((request, ZEnvironment.empty))
-
-    while (i < len) {
-      val leaf = flattened(i)
-
-      leaf match {
-        case leaf: InterceptContextZIO[_, _] => ???
-
-        case leaf: InterceptContext[_] => ???
-
-        case leaf: InterceptZIO[_, _] => ???
-
-        case leaf: Intercept => ???
-
-        case leaf: InterceptFast => ???
-      }
-
-      i = i + 1
-    }
-
-    result.asInstanceOf[ZIO[Env, Response, (Request, ZEnvironment[Ctx])]]
+  val identity: Middleware[Any, Any] = new Middleware[Any, Any] {
+    def apply[Env1 <: Any, CtxIn, CtxOut](
+      stack: MiddlewareStack[Env1, CtxIn, CtxOut],
+    ): MiddlewareStack[Env1, CtxIn, CtxOut] =
+      stack
   }
 }
---
+++ zio-http/src/main/scala/zio/http/ProtocolStack.scala
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2023 the ZIO HTTP contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.http
+
+import zio._
+
+/**
+ * A [[zio.http.ProtocolStack]] represents a linear stack of protocol layers,
+ * each of which can statefully transform incoming and outgoing values of a
+ * handler.
+ *
+ * Protocol stacks can be thought of as a formal model of the process of
+ * transforming one handler into another handler.
+ *
+ * Protocol stacks are designed to support precise semantics around error
+ * handling. In particular, if a layer successfully processes an incoming value
+ * of a handler, then that same layer will also have a chance to process the
+ * outgoing value of the handler. This requirement constrains the ways in which
+ * layers can fail, and the types of handlers they may be applied to.
+ *
+ * In particular, protocol stacks can be applied to handlers that fail with the
+ * same type as their output type. This guarantees that should a handler fail,
+ * it will still produce an outgoing value that can be processed by all the
+ * layers that successfully transformed the incoming value.
+ *
+ * Further, a layer may only fail while it processes an incoming value, and it
+ * may only value with the same type as its outgoing value. This guarantees that
+ * should a layer fail, it will still produce an outgoing value that can be
+ * processed by all the earlier layers that successfully transformed the
+ * incoming value.
+ *
+ * In a way, the entire design of protocol stacks is geared at ensuring layers
+ * that successfully process incoming values will also have a chance to process
+ * outgoing values.
+ *
+ * The only composition operator on protocol stacks is `++`, which simply chains
+ * two stacks together. This operator is associative and has an identity (which
+ * is the protocol stack that neither transforms incoming nor outgoing values,
+ * and which acts as an identity when used to transform handlers).
+ */
+sealed trait ProtocolStack[-Env, -IncomingIn, +IncomingOut, -OutgoingIn, +OutgoingOut] { self =>
+  import ProtocolStack._
+
+  type State
+
+  final def apply[Env1 <: Env, Err >: OutgoingOut, IncomingOut1 >: IncomingOut, OutgoingIn1 <: OutgoingIn](
+    handler: Handler[Env1, Err, IncomingOut1, OutgoingIn1],
+  ): Handler[Env1, Err, IncomingIn, OutgoingOut] =
+    Handler.fromFunctionZIO[IncomingIn] { incomingIn =>
+      incoming(incomingIn).flatMap { case (state, incomingOut) =>
+        handler(incomingOut).flatMap { outgoingIn =>
+          outgoing(state, outgoingIn)
+        }
+      }
+    }
+
+  def incoming(in: IncomingIn): ZIO[Env, OutgoingOut, (State, IncomingOut)]
+
+  def outgoing(state: State, in: OutgoingIn): ZIO[Env, Nothing, OutgoingOut]
+
+  final def ++[Env1 <: Env, MiddleIncoming, MiddleOutgoing](
+    that: ProtocolStack[Env1, IncomingOut, MiddleIncoming, MiddleOutgoing, OutgoingIn],
+  ): ProtocolStack[Env1, IncomingIn, MiddleIncoming, MiddleOutgoing, OutgoingOut] =
+    Concat(self, that)
+}
+object ProtocolStack                                                                   {
+  def cond[IncomingIn]: CondBuilder[IncomingIn] = new CondBuilder(())
+
+  def identity[I, O]: ProtocolStack[Any, I, I, O, O] = incoming(Handler.identity)
+
+  def incoming[Env, IncomingIn, IncomingOut, Outgoing](
+    handler: Handler[Env, Outgoing, IncomingIn, IncomingOut],
+  ): ProtocolStack[Env, IncomingIn, IncomingOut, Outgoing, Outgoing] =
+    Incoming(handler)
+
+  def interceptStateful[Env, State0, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut](
+    incoming0: Handler[Env, OutgoingOut, IncomingIn, (State0, IncomingOut)],
+  )(
+    outgoing0: Handler[Env, Nothing, (State0, OutgoingIn), OutgoingOut],
+  ): ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut] =
+    IncomingOutgoing(incoming0, outgoing0)
+
+  def intercept[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut](
+    incoming0: Handler[Env, OutgoingOut, IncomingIn, IncomingOut],
+  )(
+    outgoing0: Handler[Env, Nothing, OutgoingIn, OutgoingOut],
+  ): ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut] =
+    interceptStateful(incoming0.map(((), _)))(outgoing0.contramap[(Unit, OutgoingIn)](_._2))
+
+  def outgoing[Env, Incoming, OutgoingIn, OutgoingOut](
+    handler: Handler[Env, Nothing, OutgoingIn, OutgoingOut],
+  ): ProtocolStack[Env, Incoming, Incoming, OutgoingIn, OutgoingOut] =
+    Outgoing(handler)
+
+  private[http] final case class Incoming[Env, IncomingIn, IncomingOut, Outgoing](
+    handler: Handler[Env, Outgoing, IncomingIn, IncomingOut],
+  ) extends ProtocolStack[Env, IncomingIn, IncomingOut, Outgoing, Outgoing] {
+    type State = Unit
+
+    def incoming(in: IncomingIn): ZIO[Env, Outgoing, (State, IncomingOut)] =
+      handler(in).map(() -> _)
+
+    def outgoing(state: State, in: Outgoing): ZIO[Env, Nothing, Outgoing] =
+      Exit.succeed(in)
+  }
+  private[http] final case class Outgoing[Env, Incoming, OutgoingIn, OutgoingOut](
+    handler: Handler[Env, Nothing, OutgoingIn, OutgoingOut],
+  ) extends ProtocolStack[Env, Incoming, Incoming, OutgoingIn, OutgoingOut] {
+    type State = Unit
+
+    def incoming(in: Incoming): ZIO[Env, OutgoingOut, (State, Incoming)] = Exit.succeed(() -> in)
+
+    def outgoing(state: State, in: OutgoingIn): ZIO[Env, Nothing, OutgoingOut] =
+      handler(in)
+  }
+  private[http] final case class Concat[
+    Env,
+    IncomingIn,
+    IncomingOut,
+    OutgoingIn,
+    OutgoingOut,
+    MiddleIncoming,
+    MiddleOutgoing,
+  ](
+    left: ProtocolStack[Env, IncomingIn, MiddleIncoming, MiddleOutgoing, OutgoingOut],
+    right: ProtocolStack[Env, MiddleIncoming, IncomingOut, OutgoingIn, MiddleOutgoing],
+  ) extends ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut] {
+    type State = (left.State, right.State)
+
+    def incoming(in: IncomingIn): ZIO[Env, OutgoingOut, (State, IncomingOut)] =
+      left.incoming(in).flatMap { case (leftState, middleIn) =>
+        right.incoming(middleIn).catchAll(out => left.outgoing(leftState, out).flip).map {
+          case (rightState, incomingOut) => (leftState -> rightState) -> incomingOut
+        }
+      }
+
+    def outgoing(state: State, in: OutgoingIn): ZIO[Env, Nothing, OutgoingOut] =
+      right.outgoing(state._2, in).flatMap { middleOut =>
+        left.outgoing(state._1, middleOut)
+      }
+  }
+  private[http] final case class IncomingOutgoing[Env, State0, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut](
+    incoming0: Handler[Env, OutgoingOut, IncomingIn, (State0, IncomingOut)],
+    outgoing0: Handler[Env, Nothing, (State0, OutgoingIn), OutgoingOut],
+  ) extends ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut] {
+    type State = State0
+
+    def incoming(in: IncomingIn): ZIO[Env, OutgoingOut, (State, IncomingOut)] = incoming0(in)
+
+    def outgoing(state: State, in: OutgoingIn): ZIO[Env, Nothing, OutgoingOut] = outgoing0(state, in)
+  }
+  private[http] final case class Cond[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut](
+    predicate: IncomingIn => Boolean,
+    ifTrue: ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut],
+    ifFalse: ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut],
+  ) extends ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut] {
+    type State = Either[ifTrue.State, ifFalse.State]
+
+    def incoming(in: IncomingIn): ZIO[Env, OutgoingOut, (State, IncomingOut)] =
+      if (predicate(in)) ifTrue.incoming(in).map { case (state, out) => (Left(state), out) }
+      else ifFalse.incoming(in).map { case (state, out) => (Right(state), out) }
+
+    def outgoing(state: State, in: OutgoingIn): ZIO[Env, Nothing, OutgoingOut] =
+      state match {
+        case Left(state)  => ifTrue.outgoing(state, in)
+        case Right(state) => ifFalse.outgoing(state, in)
+      }
+  }
+
+  class CondBuilder[IncomingIn](dummy: Unit) {
+    val _ = dummy
+
+    def apply[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut](
+      predicate: IncomingIn => Boolean,
+    )(ifTrue: ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut])(
+      ifFalse: ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut],
+    ): ProtocolStack[Env, IncomingIn, IncomingOut, OutgoingIn, OutgoingOut] =
+      Cond(predicate, ifTrue, ifFalse)
+  }
+}
