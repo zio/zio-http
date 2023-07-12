e27b581c64e8fcb45e2e7651c666fd14a3099f77
sallyjellyy <min27604@gmail.com>
2023-07-12 21:21:39 +0900

    Change indent

--- zio-http/src/main/scala/zio/http/Scheme.scala
+++ zio-http/src/main/scala/zio/http/Scheme.scala
@@ -18,38 +18,36 @@ package zio.http
 
 import zio.Unsafe
 
-sealed trait Scheme {
-  self =>
+sealed trait Scheme { self =>
   def encode: String = self match {
-    case Scheme.HTTP => "http"
+    case Scheme.HTTP  => "http"
     case Scheme.HTTPS => "https"
-    case Scheme.WS => "ws"
-    case Scheme.WSS => "wss"
+    case Scheme.WS    => "ws"
+    case Scheme.WSS   => "wss"
   }
 
   def isHttp: Boolean = !isWebSocket
 
   def isWebSocket: Boolean = self match {
-    case Scheme.WS => true
+    case Scheme.WS  => true
     case Scheme.WSS => true
-    case _ => false
+    case _          => false
   }
 
   def isSecure: Boolean = self match {
     case Scheme.HTTPS => true
-    case Scheme.WSS => true
-    case _ => false
+    case Scheme.WSS   => true
+    case _            => false
   }
 
   def defaultPort: Int = self match {
-    case Scheme.HTTP => 80
+    case Scheme.HTTP  => 80
     case Scheme.HTTPS => 443
-    case Scheme.WS => 80
-    case Scheme.WSS => 443
+    case Scheme.WS    => 80
+    case Scheme.WSS   => 443
   }
 }
-
-object Scheme {
+object Scheme       {
 
   /**
    * Decodes a string to an Option of Scheme. Returns None in case of
