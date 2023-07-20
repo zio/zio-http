7d635dd2a812a109643deb21e19bec43952e313e
Scala Steward <scala_steward@virtuslab.com>
2023-07-20 14:32:10 +0000

    Update netty-codec-http, ... to 4.1.95.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Dependencies {
   val JwtCoreVersion                = "9.1.1"
-  val NettyVersion                  = "4.1.94.Final"
+  val NettyVersion                  = "4.1.95.Final"
   val NettyIncubatorVersion         = "0.0.20.Final"
   val ScalaCompactCollectionVersion = "2.11.0"
   val ZioVersion                    = "2.0.13"
