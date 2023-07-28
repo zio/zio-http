3e0a39b1a9ee0552f5d25fa16415bea3e232fdd4
Jasper van Zandbeek <jvz@net-v.com>
2023-07-28 11:23:54 +0200

    benchmark changes

--- zio-http-benchmarks/src/main/scala/zhttp.benchmarks/ServerInboundHandlerBenchmark.scala
+++ zio-http-benchmarks/src/main/scala/zhttp.benchmarks/ServerInboundHandlerBenchmark.scala
@@ -8,6 +8,9 @@ import zio.http._
 
 import org.openjdk.jmh.annotations._
 
+@Warmup(iterations = 2)
+@Measurement(iterations = 3)
+@Fork(value = 1)
 @State(org.openjdk.jmh.annotations.Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
@@ -15,7 +18,7 @@ class ServerInboundHandlerBenchmark {
 
   private val MAX      = 10000
   private val PAR      = 10
-  private val res      = ZIO.succeed(Response.ok)
+  private val res      = ZIO.succeed(Response.text("Hello World!"))
   private val endPoint = "test"
   private val http     = Routes(Route.route(Method.GET / endPoint)(handler(res))).toHttpApp
 
@@ -24,7 +27,8 @@ class ServerInboundHandlerBenchmark {
       port   <- Server.install(http)
       client <- ZIO.service[Client]
       url    <- ZIO.fromEither(URL.decode(s"http://localhost:$port/$endPoint"))
-      _      <- ZIO.foreachParDiscard((0 until PAR).toList)(_ => client.request(Request(url = url)).repeatN(MAX / PAR))
+      call = client.request(Request(url = url))
+      _ <- ZIO.collectAllParDiscard(List.fill(MAX)(call)).withParallelism(PAR)
     } yield ()).provide(Server.default, ZClient.default, zio.Scope.default)
 
   @Benchmark
