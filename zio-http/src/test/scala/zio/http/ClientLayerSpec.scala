3fe2a23d36a79e48a673612b249ef73ae99077a5
John Hungerford <jiveshungerford@gmail.com>
2023-08-28 15:23:59 -0400

    Fixed/formatted

--- zio-http/src/test/scala/zio/http/ClientLayerSpec.scala
+++ zio-http/src/test/scala/zio/http/ClientLayerSpec.scala
@@ -1,9 +1,11 @@
 package zio.http
 
 import java.util.concurrent.TimeUnit
+
 import zio.test.Assertion.{isGreaterThan, isLessThan, isWithin}
 import zio.test.{TestAspect, assertZIO}
 import zio.{Clock, ZIO, ZLayer, durationInt}
+
 import zio.http.Client
 import zio.http.netty.NettyConfig
 
@@ -23,7 +25,7 @@ object ClientLayerSpec extends ZIOHttpSpec {
     test("netty client should allow customizing quiet period for client shutdown") {
       val customNettyConfig =
         NettyConfig.default
-                   .copy(shutdownQuietPeriodDuration = 2900.millis, shutdownTimeoutDuration = 3100.millis)
+          .copy(shutdownQuietPeriodDuration = 2900.millis, shutdownTimeoutDuration = 3100.millis)
       val customClientLayer =
         (ZLayer.succeed(Client.Config.default) ++ ZLayer.succeed(customNettyConfig) ++
           DnsResolver.default) >>> Client.live
@@ -32,10 +34,10 @@ object ClientLayerSpec extends ZIOHttpSpec {
         startTime <- ZIO.scoped {
           customClientLayer.build *> Clock.currentTime(TimeUnit.MILLISECONDS)
         }
-        endTime <- Clock.currentTime(TimeUnit.MILLISECONDS)
+        endTime   <- Clock.currentTime(TimeUnit.MILLISECONDS)
       } yield endTime - startTime
       assertZIO[Any, Throwable, Long](timeDifference)(
-        isWithin(2900L, 3100L)
+        isWithin(2900L, 3100L),
       )
     } @@ TestAspect.withLiveClock,
   )
