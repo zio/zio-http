f6c41d8aff26a90ca319b3410f752e836c141e87
Eshu <tjano.xibalba@gmail.com>
2023-10-14 11:38:13 +0900

    Cardinality descriptor

--- zio-http/src/main/scala/zio/http/codec/TextChunkCodec.scala
+++ zio-http/src/main/scala/zio/http/codec/TextChunkCodec.scala
@@ -5,6 +5,7 @@ import scala.annotation.tailrec
 import zio.{Chunk, ChunkBuilder, NonEmptyChunk}
 
 sealed trait TextChunkCodec[A, I] {
+  def cardinality: String
   def parent: TextCodec[I]
   def decode(chunk: Chunk[String]): TextChunkCodec.DecodeResult[A]
   def encode(value: A): Chunk[String]
@@ -14,11 +15,13 @@ object TextChunkCodec {
   def unapply[I](codec: TextChunkCodec[_, I]): Option[TextCodec[I]] = Some(codec.parent)
 
   def any[I](codec: TextCodec[I]): TextChunkCodec[Chunk[I], I]               = new TextChunkCodec[Chunk[I], I] {
+    def cardinality: String                                  = "any"
     def parent: TextCodec[I]                                 = codec
     def decode(chunk: Chunk[String]): DecodeResult[Chunk[I]] = _decode(codec, chunk)
     def encode(value: Chunk[I]): Chunk[String]               = value map codec.encode
   }
   def oneOrMore[I](codec: TextCodec[I]): TextChunkCodec[NonEmptyChunk[I], I] = new TextChunkCodec[NonEmptyChunk[I], I] {
+    def cardinality: String                                          = "one or more"
     def parent: TextCodec[I]                                         = codec
     def decode(chunk: Chunk[String]): DecodeResult[NonEmptyChunk[I]] = {
       _decode(codec, chunk) flatMap (_.nonEmptyOrElse[DecodeResult[NonEmptyChunk[I]]](MissedData)(DecodeSuccess(_)))
@@ -27,11 +30,12 @@ object TextChunkCodec {
     def encode(value: NonEmptyChunk[I]): Chunk[String] = value map codec.encode
   }
   def optional[I](codec: TextCodec[I]): TextChunkCodec[Option[I], I]         = new TextChunkCodec[Option[I], I] {
+    def cardinality: String                                            = "one or none"
     def parent: TextCodec[I]                                           = codec
     override def decode(chunk: Chunk[String]): DecodeResult[Option[I]] = chunk match {
       case Chunk(value) => if (codec.isDefinedAt(value)) DecodeSuccess(Some(codec(value))) else MalformedData(codec)
       case chunk if chunk.isEmpty => DecodeSuccess(None)
-      case _                      => InvalidCardinality(chunk.length, "one or none")
+      case _                      => InvalidCardinality(chunk.length, cardinality)
     }
     override def encode(value: Option[I]): Chunk[String]               = value match {
       case Some(item) => Chunk(codec.encode(item))
@@ -39,11 +43,12 @@ object TextChunkCodec {
     }
   }
   def one[I](codec: TextCodec[I]): TextChunkCodec[I, I]                      = new TextChunkCodec[I, I] {
+    def cardinality: String                                    = "exactly one"
     def parent: TextCodec[I]                                   = codec
     override def decode(chunk: Chunk[String]): DecodeResult[I] = chunk match {
       case Chunk(value)           => if (codec.isDefinedAt(value)) DecodeSuccess(codec(value)) else MalformedData(codec)
       case chunk if chunk.isEmpty => MissedData
-      case _                      => InvalidCardinality(chunk.length, "exactly one")
+      case _                      => InvalidCardinality(chunk.length, cardinality)
     }
     override def encode(value: I): Chunk[String]               = Chunk(codec.encode(value))
   }
