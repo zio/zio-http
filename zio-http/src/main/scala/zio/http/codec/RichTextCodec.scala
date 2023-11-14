cc451fc174e839883737a9a8388fd1afbafa8a22
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2023-11-14 07:47:15 +0100

    Add withError to RichTextCodec; Improve ContentHeader parsing

--- zio-http/src/main/scala/zio/http/codec/RichTextCodec.scala
+++ zio-http/src/main/scala/zio/http/codec/RichTextCodec.scala
@@ -21,7 +21,6 @@ import java.lang.Integer.parseInt
 import scala.annotation.tailrec
 import scala.collection.immutable.BitSet
 
-import zio.stacktracer.TracingImplicits.disableAutoTrace
 import zio.{Chunk, NonEmptyChunk}
 
 /**
@@ -108,6 +107,14 @@ sealed trait RichTextCodec[A] { self =>
    */
   final def encode(value: A): Either[String, String] = RichTextCodec.encode(value, self)
 
+  /**
+   * This method is Right biased merge
+   */
+  final def merge[B](implicit ev: A <:< Either[B, B]): RichTextCodec[B] = {
+    val codec = self.asInstanceOf[RichTextCodec[Either[B, B]]]
+    codec.transform[B](_.merge)(Right(_))
+  }
+
   final def optional(default: A): RichTextCodec[Option[A]] =
     self.transform[Option[A]](a => Some(a))(_.fold(default)(identity))
 
@@ -115,10 +122,10 @@ sealed trait RichTextCodec[A] { self =>
     ((self ~ repeat).transform[NonEmptyChunk[A]](t => NonEmptyChunk(t._1, t._2: _*))(c =>
       (c.head, c.tail),
     ) | RichTextCodec.empty.as(Chunk.empty[A]))
-      .transform[Chunk[A]](_ match {
+      .transform[Chunk[A]] {
         case Left(nonEmpty)    => nonEmpty
         case Right(maybeEmpty) => maybeEmpty
-      })(c => c.nonEmptyOrElse[Either[NonEmptyChunk[A], Chunk[A]]](Right(c))(Left(_)))
+      }(c => c.nonEmptyOrElse[Either[NonEmptyChunk[A], Chunk[A]]](Right(c))(Left(_)))
 
   final def singleton: RichTextCodec[NonEmptyChunk[A]] =
     self.transform(a => NonEmptyChunk(a))(_.head)
@@ -151,11 +158,15 @@ sealed trait RichTextCodec[A] { self =>
       case x if p(x) => x
     }
 
+  final def withError(errorMessage: String): RichTextCodec[A] =
+    (self | RichTextCodec.fail[A](errorMessage)).merge
+
 }
 object RichTextCodec {
   private[codec] case object Empty                                        extends RichTextCodec[Unit]
   private[codec] final case class CharIn(set: BitSet)                     extends RichTextCodec[Char] {
-    lazy val errorMessage = s"Not found: ${set.toArray.map(_.toChar).mkString}"
+    val errorMessage: Left[String, Nothing] =
+      Left(s"Expected, but did not find: ${this.describe}")
   }
   private[codec] final case class TransformOrFail[A, B](
     codec: RichTextCodec[A],
@@ -190,6 +201,12 @@ object RichTextCodec {
    */
   def char(c: Char): RichTextCodec[Char] = CharIn(BitSet(c.toInt))
 
+  def chars(cs: Char*): RichTextCodec[Char] =
+    CharIn(BitSet(cs.map(_.toInt): _*))
+
+  def charsNot(cs: Char*): RichTextCodec[Char] =
+    filter(c => !cs.contains(c))
+
   /**
    * A codec that describes a digit character.
    */
@@ -202,12 +219,18 @@ object RichTextCodec {
    */
   val empty: RichTextCodec[Unit] = Empty
 
+  def fail[A](message: String): RichTextCodec[A] =
+    empty.transformOrFail(_ => Left(message))(_ => Left(message))
+
   /**
    * Defines a new codec for a single character based on the specified
    * predicate.
    */
   def filter(pred: Char => Boolean): RichTextCodec[Char] =
     CharIn(BitSet((Char.MinValue to Char.MaxValue).filter(pred).map(_.toInt): _*))
+
+  def filterOrFail(pred: Char => Boolean)(failure: String): RichTextCodec[Char] =
+    filter(pred).collectOrFail(failure) { case c => c }
 
   /**
    * A codec that describes a letter character.
@@ -532,7 +555,7 @@ object RichTextCodec {
 
       case self @ CharIn(bitset) =>
         if (value.length == 0 || !bitset.contains(value.charAt(0).toInt))
-          Left(self.errorMessage)
+          self.errorMessage
         else
           Right((value.subSequence(1, value.length), value.charAt(0)))
 
