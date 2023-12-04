9c79d5e30a227b74f45bd421e5d6a2bf5ac35d93
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-04 21:08:13 +0530

    format

--- zio-http-react-example/src/main/scala/react_example/ReactHelloWorld.scala
+++ zio-http-react-example/src/main/scala/react_example/ReactHelloWorld.scala
@@ -8,31 +8,30 @@ import java.io.File
 import java.nio.file.Paths
 
 object ReactHelloWorld extends ZIOAppDefault {
-    // Create the build relative directory path
-    private val buildDirectory = "src/main/scala/react_example/react/build"
+  // Create the build relative directory path
+  private val buildDirectory = "src/main/scala/react_example/react/build"
 
-    val app: HttpApp[Any] = Routes.singleton {
-        handler { (_: Path, req: Request) => {
-            if (req.url.path.toString == "/api/hello") {
-                Response.text("Hello World")
-            }
-            else {
-                if (req.url.path.toString == "/") {
-                    val file = new File(s"$buildDirectory/index.html")
-                    val length = Headers(Header.ContentLength(file.length()))
-                    Response(headers = length, body = Body.fromFile(file))
-                }
-                else {
-                    val tmp = req.url.path.toString
-                    val file = new File(s"$buildDirectory/$tmp")
-                    val length = Headers(Header.ContentLength(file.length()))
-                    Response(headers = length, body = Body.fromFile(file))
-                }
-            }
+  val app: HttpApp[Any] = Routes.singleton {
+    handler { (_: Path, req: Request) =>
+      {
+        if (req.url.path.toString == "/api/hello") {
+          Response.text("Hello World")
+        } else {
+          if (req.url.path.toString == "/") {
+            val file   = new File(s"$buildDirectory/index.html")
+            val length = Headers(Header.ContentLength(file.length()))
+            Response(headers = length, body = Body.fromFile(file))
+          } else {
+            val tmp    = req.url.path.toString
+            val file   = new File(s"$buildDirectory/$tmp")
+            val length = Headers(Header.ContentLength(file.length()))
+            Response(headers = length, body = Body.fromFile(file))
           }
         }
-      }.toHttpApp
+      }
+    }
+  }.toHttpApp
 
-    // Run it like any simple app
-    val run = Server.serve(app).provide(Server.default) 
+  // Run it like any simple app
+  val run = Server.serve(app).provide(Server.default)
 }
