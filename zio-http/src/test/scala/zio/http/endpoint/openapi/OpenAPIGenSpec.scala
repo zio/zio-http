28bddd56f9bd5f612950b21b9c208f1bdfd8b4a6
Eshu <tjano.xibalba@gmail.com>
2023-12-09 11:21:08 +0900

    Tests fixed

--- zio-http/src/test/scala/zio/http/endpoint/openapi/OpenAPIGenSpec.scala
+++ zio-http/src/test/scala/zio/http/endpoint/openapi/OpenAPIGenSpec.scala
@@ -121,7 +121,7 @@ object OpenAPIGenSpec extends ZIOSpecDefault {
   private val queryParamEndpoint =
     Endpoint(GET / "withQuery")
       .in[SimpleInputBody]
-      .query(QueryCodec.paramStr("query"))
+      .query(QueryCodec.query("query"))
       .out[SimpleOutputBody]
       .outError[NotFoundError](Status.NotFound)
 
@@ -887,7 +887,7 @@ object OpenAPIGenSpec extends ZIOSpecDefault {
                 HttpCodec
                   .content[SimpleInputBody] ?? Doc.p("simple input"),
             )
-            .query(QueryCodec.paramStr("query"))
+            .query(QueryCodec.query("query"))
             .outCodec(
               HttpCodec
                 .content[SimpleOutputBody] ?? Doc.p("simple output") |
