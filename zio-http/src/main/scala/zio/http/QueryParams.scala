7433c9fcb4de5ea1dc65c9db26bf27b31b6a3478
Erik LaBianca <elabianca@carahsoft.com>
2024-01-08 11:13:53 -0500

    Add documentation to map method

--- zio-http/src/main/scala/zio/http/QueryParams.scala
+++ zio-http/src/main/scala/zio/http/QueryParams.scala
@@ -38,7 +38,9 @@ trait QueryParams {
   private[http] def seq: Seq[util.Map.Entry[String, util.List[String]]]
 
   /**
-   * All query parameters as a map
+   * All query parameters as a map. Note that by default this method constructs the map on
+   * access from the underlying storage implementation, so should be used with care. Prefer
+   * to use `getAll` and friends if all you need is to access the values.
    */
   def map: Map[String, Chunk[String]] = ListMap.from(seq.map { entry =>
     (entry.getKey, Chunk.fromIterable(entry.getValue.asScala))
