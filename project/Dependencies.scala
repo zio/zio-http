36ad0aa428318f10a425de3f54715ab37ddc0882
jkcs <1778768609@qq.com>
2024-01-23 18:36:25 +0800

    fix: ZioSchemaVersion version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val ScalaCompactCollectionVersion = "2.11.0"
   val ZioVersion                    = "2.0.21"
   val ZioCliVersion                 = "0.5.0"
-  val ZioSchemaVersion              = "0.4.17+4-0acf5af7-SNAPSHOT"
+  val ZioSchemaVersion              = "0.4.17"
   val SttpVersion                   = "3.3.18"
 
   val `jwt-core`                 = "com.github.jwt-scala"   %% "jwt-core"                % JwtCoreVersion
