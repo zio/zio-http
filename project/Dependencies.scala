911b5ff7f3977b2eb81ef64f87cc7a675a065fce
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2024-02-08 22:47:45 +0100

    Add brotli compression (#2646)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,6 +27,7 @@ object Dependencies {
       "io.netty" % "netty-transport-native-kqueue" % NettyVersion,
       "io.netty" % "netty-transport-native-kqueue" % NettyVersion % Runtime classifier "osx-x86_64",
       "io.netty" % "netty-transport-native-kqueue" % NettyVersion % Runtime classifier "osx-aarch_64",
+      "com.aayushatharva.brotli4j" % "brotli4j" % "1.16.0" % "provided",
     )
 
   val `netty-incubator` =
