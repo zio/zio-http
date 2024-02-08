f5860c1532db7834c239cc377c549f6f5905f515
guptadeepak8 <vectord7738@gmail.com>
2024-02-08 16:43:09 +0530

    updated

--- zio-http/shared/src/main/scala/zio/http/template/Html.scala
+++ zio-http/shared/src/main/scala/zio/http/template/Html.scala
@@ -48,7 +48,9 @@ sealed trait Html { self =>
 }
 
 object Html {
-  implicit def fromString(string: CharSequence): Html = Html.Single(Dom.raw(string))
+  implicit def raw(string: CharSequence): Html = Html.Single(Dom.raw(string))
+
+  implicit def fromString(string: CharSequence): Html = Html.Single(Dom.text(string))
 
   implicit def fromSeq(elements: Seq[Dom]): Html = Html.Multiple(elements)
 
