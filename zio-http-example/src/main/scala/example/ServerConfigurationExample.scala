09cf56aba1187e9492e4b1412a2245bc1ed56bd1
Milad Khajavi <khajavi@gmail.com>
2024-03-15 20:00:10 +0330

    add server configuration example.

---
+++ zio-http-example/src/main/scala/example/ServerConfigurationExample.scala
@@ -0,0 +1,16 @@
+package example
+
+import zio._
+import zio.config.typesafe._
+import zio.http._
+
+object ServerConfigurationExample extends ZIOAppDefault {
+  val httpApp = Routes(
+    Method.GET / "hello" -> handler(Response.text("Hello, World!")),
+  ).sandbox.toHttpApp
+
+  override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
+    Runtime.setConfigProvider(ConfigProvider.fromResourcePath())
+
+  def run = Server.serve(httpApp).provide(Server.configured())
+}
