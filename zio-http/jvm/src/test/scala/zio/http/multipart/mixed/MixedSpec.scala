1250271f01ff6bc360e4a3486002a40670dfe2a7
Eyal Farago <efarago@salesforce.com>
2024-04-09 15:46:05 +0300

    multipart_mixed: add tests

--- zio-http/jvm/src/test/scala/zio/http/multipart/mixed/MixedSpec.scala
+++ zio-http/jvm/src/test/scala/zio/http/multipart/mixed/MixedSpec.scala
@@ -1,7 +1,7 @@
 package zio.http.multipart.mixed
 
-import zio.Scope
-import zio.http.{Boundary, ZIOHttpSpec}
+import zio.{Chunk, Scope, ZIO}
+import zio.http.{Body, Boundary, MediaType, ZIOHttpSpec}
 import zio.stream.ZStream
 import zio.test.{Assertion, Spec, TestEnvironment}
 
@@ -16,7 +16,6 @@ object MixedSpec  extends ZIOHttpSpec {
     suiteAll("empty") {
       val empty = Mixed.fromParts(ZStream.empty, Boundary(defaultSep))
 
-
       test("has no parts") {
         empty
           .parts
@@ -25,9 +24,76 @@ object MixedSpec  extends ZIOHttpSpec {
             zio.test.assert(collected)(Assertion.isEmpty)
           }
       }
+
+      test("emoty no preamble, no epilogue") {
+        val body = Body.fromString(s"--${defaultSep}--").contentType(MediaType.multipart.`mixed`, Boundary(defaultSep))
+        val mpm = Mixed.fromBody(body)
+
+        ZIO
+          .fromOption(mpm)
+          .flatMap(_.parts.runCollect)
+          .map{collected =>
+            zio.test.assert(collected)(Assertion.isEmpty)
+          }
+      }
+
+      test("empty no preamble, with epilogue") {
+        val body = Body.fromString(s"--${defaultSep}--\r\nsome nasty epilogue").contentType(MediaType.multipart.`mixed`, Boundary(defaultSep))
+        val mpm = Mixed.fromBody(body)
+
+        ZIO
+          .fromOption(mpm)
+          .flatMap(_.parts.runCollect)
+          .map{collected =>
+            zio.test.assert(collected)(Assertion.isEmpty)
+          }
+      }
     }
 
+    test("rfc-1341 sample") {
+      val msg =
+        """
+          |
+          |This is the preamble.  It is to be ignored, though it
+          |is a handy place for mail composers to include an
+          |explanatory note to non-MIME compliant readers.
+          |--simple boundary
+          |
+          |This is implicitly typed plain ASCII text.
+          |It does NOT end with a linebreak.
+          |--simple boundary
+          |Content-type: text/plain; charset=us-ascii
+          |
+          |This is explicitly typed plain ASCII text.
+          |It DOES end with a linebreak.
+          |
+          |--simple boundary--
+          |This is the epilogue.  It is also to be ignored.
+          |""".stripMargin.replaceAll("\n", "\r\n")
 
+      val body = Body.fromString(msg).contentType(MediaType.multipart.`mixed`, Boundary(defaultSep))
+      val mpm = Mixed.fromBody(body)
+      val expectedStrs = Chunk(
+        """|This is implicitly typed plain ASCII text.
+           |It does NOT end with a linebreak.
+           |""".stripMargin.replaceAll("\n", "\r\n"),
+        """|This is explicitly typed plain ASCII text.
+           |It DOES end with a linebreak.
+           |
+           |""".stripMargin.replaceAll("\n", "\r\n")
+      )
+
+      ZIO
+        .fromOption(mpm)
+        .flatMap{
+          _.parts.mapZIO(_.toBody.asString)
+            .runCollect
+        }
+        .map{collected =>
+          zio.test.assert(collected)(Assertion.equalTo(expectedStrs))
+        }
+
+    }
   }
 
 }
