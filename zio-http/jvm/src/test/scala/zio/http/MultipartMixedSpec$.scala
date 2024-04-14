e4473a58b4042a8d399e40d4fb702ac6eeb1baf7
Eyal Farago <efarago@salesforce.com>
2024-04-15 00:10:08 +0300

    multipart_mixed: fmt

--- zio-http/jvm/src/test/scala/zio/http/MultipartMixedSpec$.scala
+++ zio-http/jvm/src/test/scala/zio/http/MultipartMixedSpec$.scala
@@ -1,9 +1,11 @@
 package zio.http
 
-import zio.http.Headers.fromIterable
-import zio.test.{Assertion, Gen, Spec, TestAspect, TestEnvironment, check, checkAll}
+import zio.test._
 import zio.{Chunk, Scope, ZIO, ZNothing}
+
 import zio.stream.{ZChannel, ZPipeline, ZStream}
+
+import zio.http.Headers.fromIterable
 import zio.http.multipart.mixed.MultipartMixed
 import zio.http.multipart.mixed.MultipartMixed.Part
 
@@ -126,83 +128,94 @@ object MultipartMixedSpec$ extends ZIOHttpSpec {
                  |explanatory note to non-MIME compliant readers.""".stripMargin),
           boundary,
           Chunk(
-            new TestPart( Headers(),
+            new TestPart(
+              Headers(),
               utf8("""This is implicitly typed plain ASCII text.
                      |It does NOT end with a linebreak.""".stripMargin),
-              boundary
+              boundary,
             ),
             new TestPart(
               Headers("Content-type", "text/plain; charset=us-ascii"),
               utf8("""This is explicitly typed plain ASCII text.
                      |It DOES end with a linebreak.
                      |""".stripMargin),
-              boundary
-            )
+              boundary,
+            ),
           ),
           utf8("This is the epilogue.  It is also to be ignored."),
           15,
-          gens.breaker.fixed(32)
+          gens.breaker.fixed(32),
         )
         testCase.runTests
       }
     }
 
     test("empty test case") {
-      val tc = TestCase(Chunk(),
-        Boundary("(((b010903d-0d3d-4d22-b5c4-f3f0bf574468)))",Charsets.Utf8),
+      val tc = TestCase(
+        Chunk(),
+        Boundary("(((b010903d-0d3d-4d22-b5c4-f3f0bf574468)))", Charsets.Utf8),
         Chunk(),
         Chunk(),
         7412,
-        gens.breaker.alternating(Chunk(10,10)),
-        Chunk(45,45,40,40,40,98,48,49,48,57,48,51,100,45,48,100,51,100,45,52,100,50,50,45,98,53,99,52,45,102,51,102,48,98,102,53,55,52,52,54,56,41,41,41,45,45))
+        gens.breaker.alternating(Chunk(10, 10)),
+        Chunk(45, 45, 40, 40, 40, 98, 48, 49, 48, 57, 48, 51, 100, 45, 48, 100, 51, 100, 45, 52, 100, 50, 50, 45, 98,
+          53, 99, 52, 45, 102, 51, 102, 48, 98, 102, 53, 55, 52, 52, 54, 56, 41, 41, 41, 45, 45),
+      )
 
       tc.testFromRepr
     }
 
     test("closing boundary trailing crlf is split between two reads") {
-      val tc = TestCase(Chunk(44),
-        Boundary("(((ba4584f8-ca16-457a-921e-adf1a01996a7)))",Charsets.Utf8),
+      val tc = TestCase(
+        Chunk(44),
+        Boundary("(((ba4584f8-ca16-457a-921e-adf1a01996a7)))", Charsets.Utf8),
         Chunk(
-          TestPart(Headers(),
+          TestPart(
+            Headers(),
             Chunk(),
-            Chunk(45,45,40,40,40,98,97,52,53,56,52,102,56,45,99,97,49,54,45,52,53,55,97,45,57,50,49,101,45,97,100,102,49,97,48,49,57,57,54,97,55,41,41,41,13,10,13,10,13,10)
-          )
+            Chunk(45, 45, 40, 40, 40, 98, 97, 52, 53, 56, 52, 102, 56, 45, 99, 97, 49, 54, 45, 52, 53, 55, 97, 45, 57,
+              50, 49, 101, 45, 97, 100, 102, 49, 97, 48, 49, 57, 57, 54, 97, 55, 41, 41, 41, 13, 10, 13, 10, 13, 10),
+          ),
         ),
-        Chunk(-59,119,33,117,-6,15,101),
+        Chunk(-59, 119, 33, 117, -6, 15, 101),
         5711,
         gens.breaker.alternating(Chunk(10)),
-        Chunk(44,13,10,45,45,40,40,40,98,97,52,53,56,52,102,56,45,99,97,49,54,45,52,53,55,97,45,57,50,49,101,45,97,100,102,49,97,48,49,57,57,54,97,55,41,41,41,13,10,13,10,13,10,45,45,40,40,40,98,97,52,53,56,52,102,56,45,99,97,49,54,45,52,53,55,97,45,57,50,49,101,45,97,100,102,49,97,48,49,57,57,54,97,55,41,41,41,45,45,13,10,-59,119,33,117,-6,15,101)
+        Chunk(44, 13, 10, 45, 45, 40, 40, 40, 98, 97, 52, 53, 56, 52, 102, 56, 45, 99, 97, 49, 54, 45, 52, 53, 55, 97,
+          45, 57, 50, 49, 101, 45, 97, 100, 102, 49, 97, 48, 49, 57, 57, 54, 97, 55, 41, 41, 41, 13, 10, 13, 10, 13, 10,
+          45, 45, 40, 40, 40, 98, 97, 52, 53, 56, 52, 102, 56, 45, 99, 97, 49, 54, 45, 52, 53, 55, 97, 45, 57, 50, 49,
+          101, 45, 97, 100, 102, 49, 97, 48, 49, 57, 57, 54, 97, 55, 41, 41, 41, 45, 45, 13, 10, -59, 119, 33, 117, -6,
+          15, 101),
       )
       tc.testFromRepr
     }
 
     test("breaker.alternating(10,10)") {
-      val inp = ZStream.range(0, 40, 9).map(_.toByte)
+      val inp     = ZStream.range(0, 40, 9).map(_.toByte)
       val breaker = gens.breaker.alternating(Chunk(10, 10))
 
-      inp.via(breaker.breakerPl).chunks.runCollect.map{ collected =>
-        zio.test.assert(collected){
-          Assertion.equalTo{
+      inp.via(breaker.breakerPl).chunks.runCollect.map { collected =>
+        zio.test.assert(collected) {
+          Assertion.equalTo {
             Chunk(
               Chunk.range(0, 10),
               Chunk.range(10, 20),
               Chunk.range(20, 30),
-              Chunk.range(30, 40)
+              Chunk.range(30, 40),
             )
-            .map(_.map(_.toByte))
+              .map(_.map(_.toByte))
           }
         }
       }
     }
 
     test("property") {
-      check(gens.genTestCase){ testCase =>
+      check(gens.genTestCase) { testCase =>
         zio.Console.printLine(testCase) *> testCase.runTests
       }
     } @@ TestAspect.shrinks(0)
   }
 
-  def utf8(str : String) : Chunk[Byte] =
+  def utf8(str: String): Chunk[Byte] =
     Chunk.fromArray(str.getBytes(Charsets.Utf8))
 
   def rechunk(chunkSz: Int)(body: Body): Body = {
@@ -245,57 +258,70 @@ object MultipartMixedSpec$ extends ZIOHttpSpec {
     }
   }
 
-  case class TestPart(headers : Headers, contents : Chunk[Byte], binaryRepr : Chunk[Byte]) {
+  case class TestPart(headers: Headers, contents: Chunk[Byte], binaryRepr: Chunk[Byte]) {
     def this(headers: Headers, contents: Chunk[Byte], boundary: Boundary) =
-      this(headers, contents, {
-        val headersStr = headers
-          .map(h => s"${h.headerName}: ${h.renderedValue}\r\n")
-          .mkString
-        val headerBytes = Chunk.fromArray(headersStr.getBytes(Charsets.Utf8))
+      this(
+        headers,
+        contents, {
+          val headersStr  = headers
+            .map(h => s"${h.headerName}: ${h.renderedValue}\r\n")
+            .mkString
+          val headerBytes = Chunk.fromArray(headersStr.getBytes(Charsets.Utf8))
 
-        boundary.encapsulationBoundaryBytes ++
-          MultipartMixed.crlf ++
-          headerBytes ++
-          MultipartMixed.crlf ++
-          contents  ++
-          MultipartMixed.crlf
-      })
+          boundary.encapsulationBoundaryBytes ++
+            MultipartMixed.crlf ++
+            headerBytes ++
+            MultipartMixed.crlf ++
+            contents ++
+            MultipartMixed.crlf
+        },
+      )
 
     def toPart = MultipartMixed.Part(headers, ZStream.fromChunk(contents))
   }
-  case class TestCase(preamble : Chunk[Byte],
-                      boundary : Boundary,
-                      parts : Chunk[TestPart],
-                      epilogue : Chunk[Byte],
-                      bufferSize : Int,
-                      breaker : Breaker,
-                      binaryRepr : Chunk[Byte]) {
-    def this(preamble : Chunk[Byte],
-             boundary : Boundary,
-             parts : Chunk[TestPart],
-             epilogue : Chunk[Byte],
-             bufferSize : Int,
-             breaker : Breaker) =
-      this(preamble, boundary, parts, epilogue, bufferSize, breaker, {
-        {
-          preamble match {
-            case pb if pb.nonEmpty =>
-              pb ++ MultipartMixed.crlf
-            case _ =>
-              Chunk.empty
-          }
-        } ++
-        parts.flatMap(_.binaryRepr) ++
-        boundary.closingBoundaryBytes ++
-        {
-          epilogue match {
-            case epBytes if epBytes.nonEmpty =>
-              MultipartMixed.crlf ++ epBytes
-            case _ =>
-              Chunk.empty
-          }
-        }
-      })
+  case class TestCase(
+    preamble: Chunk[Byte],
+    boundary: Boundary,
+    parts: Chunk[TestPart],
+    epilogue: Chunk[Byte],
+    bufferSize: Int,
+    breaker: Breaker,
+    binaryRepr: Chunk[Byte],
+  ) {
+    def this(
+      preamble: Chunk[Byte],
+      boundary: Boundary,
+      parts: Chunk[TestPart],
+      epilogue: Chunk[Byte],
+      bufferSize: Int,
+      breaker: Breaker,
+    ) =
+      this(
+        preamble,
+        boundary,
+        parts,
+        epilogue,
+        bufferSize,
+        breaker, {
+          {
+            preamble match {
+              case pb if pb.nonEmpty =>
+                pb ++ MultipartMixed.crlf
+              case _                 =>
+                Chunk.empty
+            }
+          } ++
+            parts.flatMap(_.binaryRepr) ++
+            boundary.closingBoundaryBytes ++ {
+              epilogue match {
+                case epBytes if epBytes.nonEmpty =>
+                  MultipartMixed.crlf ++ epBytes
+                case _                           =>
+                  Chunk.empty
+              }
+            }
+        },
+      )
 
     def strip = new TestCase(Chunk.empty, boundary, parts, Chunk.empty, bufferSize, breaker)
 
@@ -303,25 +329,18 @@ object MultipartMixedSpec$ extends ZIOHttpSpec {
       val res0 = MultipartMixed.fromParts(ZStream.fromChunk(parts).map(_.toPart), boundary, bufferSize)
       res0.copy(source = res0.source >>> breaker.breakerPl)
     }
-    def multipartFromRepr = MultipartMixed(ZStream.fromChunk(binaryRepr) >>> breaker.breakerPl, boundary, bufferSize)
+    def multipartFromRepr  = MultipartMixed(ZStream.fromChunk(binaryRepr) >>> breaker.breakerPl, boundary, bufferSize)
 
     def testFromParts = {
-      multipartFromParts
-        .source
-        .runCollect
+      multipartFromParts.source.runCollect
         .map(zio.test.assert(_)(Assertion.equalTo(this.strip.binaryRepr)))
     }
 
     def testFromRepr = {
-      multipartFromRepr
-        .parts
-        .mapZIO { p =>
-          p
-            .bytes
-            .runCollect
-            .map(new TestPart(fromIterable(p.headers.toVector), _, boundary))
-        }
-        .runCollect
+      multipartFromRepr.parts.mapZIO { p =>
+        p.bytes.runCollect
+          .map(new TestPart(fromIterable(p.headers.toVector), _, boundary))
+      }.runCollect
         .map(zio.test.assert(_)(Assertion.equalTo(parts)))
     }
 
@@ -329,26 +348,25 @@ object MultipartMixedSpec$ extends ZIOHttpSpec {
       testFromRepr.zipWith(testFromParts)(_ && _)
   }
 
-  //thin wrapper over pipeline, gives a usable string repr
-  class Breaker(val breakerPl :  ZPipeline[Any, Nothing, Byte, Byte],
-                override val toString : String)
+  // thin wrapper over pipeline, gives a usable string repr
+  class Breaker(val breakerPl: ZPipeline[Any, Nothing, Byte, Byte], override val toString: String)
 
   object gens {
     val genBytes = Gen.chunkOf(Gen.byte)
 
-    val genHeaderName = Gen.alphaNumericString.filter(_.nonEmpty)
+    val genHeaderName  = Gen.alphaNumericString.filter(_.nonEmpty)
     val genHeaderValue = Gen.alphaNumericString.map(_.filterNot(Set('\r', '\n'))).filter(_.nonEmpty)
 
-    val genHeader = for{
+    val genHeader = for {
       name <- genHeaderName
-      v <- genHeaderValue
+      v    <- genHeaderValue
     } yield {
       Header.Custom(name, v)
     }
 
     val genBoundary = Gen.fromZIO(Boundary.randomUUID)
 
-    def genTestPart(boundary : Boundary) = for{
+    def genTestPart(boundary: Boundary) = for {
       headers <- Gen.chunkOf(genHeader)
       content <- genBytes
     } yield {
@@ -356,30 +374,30 @@ object MultipartMixedSpec$ extends ZIOHttpSpec {
     }
 
     object breaker {
-      def fixed(n : Int): Breaker = new Breaker(ZPipeline.rechunk[Byte](n), s"fixed($n)")
+      def fixed(n: Int): Breaker      = new Breaker(ZPipeline.rechunk[Byte](n), s"fixed($n)")
       val genFixed: Gen[Any, Breaker] = Gen.fromIterable(Seq(10, 15, 30, 100, 256)).map(fixed)
 
-      def alternatingPl(sizes : Chunk[Int]): ZPipeline[Any, Nothing, Byte, Byte] = {
+      def alternatingPl(sizes: Chunk[Int]): ZPipeline[Any, Nothing, Byte, Byte] = {
 
-        def ch(buffered : Chunk[Byte],
-               leftovers : Chunk[Byte],
-               rem : Int,
-               nextSz : ZIO[Any, Nothing, Int]) : ZChannel[Any, ZNothing, Chunk[Byte], Any, Nothing, Chunk[Byte], Any] = {
-          if(0 == rem)
-            ZChannel.write(buffered)  *>
+        def ch(
+          buffered: Chunk[Byte],
+          leftovers: Chunk[Byte],
+          rem: Int,
+          nextSz: ZIO[Any, Nothing, Int],
+        ): ZChannel[Any, ZNothing, Chunk[Byte], Any, Nothing, Chunk[Byte], Any] = {
+          if (0 == rem)
+            ZChannel.write(buffered) *>
               ZChannel
                 .fromZIO(nextSz)
                 .flatMap(ch(Chunk.empty, leftovers, _, nextSz))
-          else if(leftovers.nonEmpty)
+          else if (leftovers.nonEmpty)
             ch(buffered ++ leftovers.take(rem), leftovers.drop(rem), (rem - leftovers.size) max 0, nextSz)
           else
             ZChannel
               .readWithCause(
-                in =>
-                  ch(buffered, in, rem, nextSz),
+                in => ch(buffered, in, rem, nextSz),
                 ZChannel.refailCause(_),
-                _ =>
-                  if(buffered.nonEmpty) ZChannel.write(buffered) else ZChannel.unit
+                _ => if (buffered.nonEmpty) ZChannel.write(buffered) else ZChannel.unit,
               )
         }
 
@@ -387,43 +405,44 @@ object MultipartMixedSpec$ extends ZIOHttpSpec {
         val s0Pull: ZIO[Any with Scope, Nothing, ZIO[Any, Nothing, Int]] = s0
           .rechunk(1)
           .toPull
-          .map{ pull =>
+          .map { pull =>
             val z: ZIO[Any, Nothing, Int] = pull.map(_.headOption).orElse(ZIO.none).repeatUntil(_.nonEmpty).map(_.get)
             z
           }
 
         ZPipeline
-          .unwrapScoped[Any]{
+          .unwrapScoped[Any] {
             s0Pull
               .flatMap(p => ZIO.succeed(p) <*> p)
-              .map{
-                case (nextSz, firstSz) =>
-                  val channel: ZChannel[Any, ZNothing, Chunk[Byte], Any, Nothing, Chunk[Byte], Any] = ch(Chunk.empty, Chunk.empty, firstSz, nextSz)
-                  val pl: ZPipeline[Any, Nothing, Byte, Byte] = channel.toPipeline
-                  pl
+              .map { case (nextSz, firstSz) =>
+                val channel: ZChannel[Any, ZNothing, Chunk[Byte], Any, Nothing, Chunk[Byte], Any] =
+                  ch(Chunk.empty, Chunk.empty, firstSz, nextSz)
+                val pl: ZPipeline[Any, Nothing, Byte, Byte]                                       = channel.toPipeline
+                pl
               }
           }
       }
-      def alternating(sizes : Chunk[Int]): Breaker = new Breaker(alternatingPl(sizes), s"alternating(${sizes.mkString("Chunk(", ",", ")")})")
-      def genAlternating: Gen[Any, Breaker] = Gen
+      def alternating(sizes: Chunk[Int]): Breaker =
+        new Breaker(alternatingPl(sizes), s"alternating(${sizes.mkString("Chunk(", ",", ")")})")
+      def genAlternating: Gen[Any, Breaker]       = Gen
         .chunkOf(Gen.fromIterable(Seq(10, 15, 30, 100, 256)))
         .filter(_.nonEmpty)
         .map(alternating(_))
 
-      val asIs = new Breaker(ZPipeline.identity[Byte], "asIs")
+      val asIs                       = new Breaker(ZPipeline.identity[Byte], "asIs")
       val genAsIs: Gen[Any, Breaker] = Gen.const(asIs)
 
       val genBreaker: Gen[Any, Breaker] = Gen.oneOf(genFixed, genAlternating, genAsIs)
     }
 
     val genTestCase =
-      for{
-        boundary <- genBoundary
-        preamble <- Gen.oneOf(Gen.const(Chunk.empty), genBytes)
-        parts <- Gen.chunkOf(genTestPart(boundary))
-        epilogue <- Gen.oneOf(Gen.const(Chunk.empty), genBytes)
+      for {
+        boundary   <- genBoundary
+        preamble   <- Gen.oneOf(Gen.const(Chunk.empty), genBytes)
+        parts      <- Gen.chunkOf(genTestPart(boundary))
+        epilogue   <- Gen.oneOf(Gen.const(Chunk.empty), genBytes)
         bufferSize <- Gen.int(10, 8 * 1024)
-        breaker <- breaker.genBreaker
+        breaker    <- breaker.genBreaker
       } yield {
         new TestCase(preamble, boundary, parts, epilogue, bufferSize, breaker)
       }
