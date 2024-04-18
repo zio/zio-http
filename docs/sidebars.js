1c6075a6dc5c783f892cced411772ccd12db47c0
daveads <the.daveads@gmail.com>
2024-04-18 01:16:37 +0100

    changes to sidebar view

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -5,39 +5,40 @@ const sidebars = {
       label: "ZIO Http",
       collapsed: true,
       link: { type: "doc", id: "index" },
-
       items: [
-        "setup",
-        "quickstart",
-        "performance",
-        {
-          type: "category",
-          label: "Concepts",
-          link: { type: "doc", id: "index" },
-          items: [
-            "concepts/routing",
-            "concepts/request-handling",
-            "concepts/server",
-            "concepts/client",
-            "concepts/middleware",
-            "concepts/endpoint",
-          ],
-        },
-        {
+              "setup",
+              "getting-started",
+              "quickstart",
+      
+              
+    //concepts          
+    {
+      type: "category",
+      label: "Concepts", 
+      items: [
+        "concepts/client",
+        "concepts/endpoint",
+        "concepts/middleware",
+        "concepts/request-handling",
+        "concepts/routing",
+        "concepts/server",
+      ],
+    },
+    
+    // detailed tutorials
+    {
           type: "category",
           label: "Tutorials",
-          collapsed: false,
-          link: { type: "doc", id: "index" },
           items: [
             "tutorials/testing-your-zio-http-app",
             "tutorials/deeper-dive-into-middleware",
           ],
-        },
-        {
+    },
+    
+    // How to guide
+    {
           type: "category",
           label: "How-to-guides",
-          collapsed: false,
-          link: { type: "doc", id: "index" },
           items: [
             "how-to-guides/endpoint",
             "how-to-guides/cookie-authentication",
@@ -46,59 +47,55 @@ const sidebars = {
             "how-to-guides/how-to-utilize-signed-cookies",
             "how-to-guides/how-to-handle-WebSocket-exceptions-and-errors",
           ],
-        },
-        {
+    },
+    
+    //Reference
+    {
           type: "category",
           label: "Reference",
-          collapsed: false,
-          link: { type: "doc", id: "index" },
           items: [
             "reference/server-backend",
             "reference/websockets",
             "reference/json-handling",
             "reference/metrics",
             "reference/request-logging",
-            {
-              type: "category",
-              label: "DSL",
-              link: { type: "doc", id: "index" },
-              items: [
-                "dsl/server",
-                "dsl/routes",
-                "dsl/route_pattern",
-                "dsl/path_codec",
-                "dsl/request",
-                "dsl/response",
-                "dsl/handler",
-                "dsl/headers",
-                "dsl/body",
-                "dsl/form",
-                "dsl/cookies",
-                "dsl/flash",
-                "dsl/protocol-stack",
-                "dsl/middleware",
-                "dsl/handler_aspect",
-                "dsl/status",
-                {
-                  type: "category",
-                  label: "DSL",
-                  collapsed: false,
-                  items: [
-                    "dsl/socket/socket",
-                    "dsl/socket/websocketframe"
-                  ]
-                },
-                "dsl/template",
-                "dsl/client"
-              ]
-            }
-          ]
-        },
+        
+        //reference/dsl
         {
           type: "category",
+          label: "DSL",
+          items: [
+            "reference/dsl/server",
+            "reference/dsl/routes",
+            "reference/dsl/route_pattern",
+            "reference/dsl/path_codec",
+            "reference/dsl/request",
+            "reference/dsl/response",
+            "reference/dsl/handler",
+            "reference/dsl/headers",
+            "reference/dsl/body",
+            "reference/dsl/form",
+            "reference/dsl/cookies",
+            "reference/dsl/flash",
+            "reference/dsl/protocol-stack",
+            "reference/dsl/middleware",
+            "reference/dsl/handler_aspect",
+            "reference/dsl/status",
+            "reference/dsl/socket/socket",
+            "reference/dsl/socket/websocketframe",
+            "reference/dsl/template",
+            "reference/dsl/client",
+          ],
+        },
+          
+      ],    
+    },
+    
+    
+    // Examples #later 
+    {
+          type: "category",
           label: "Examples",
-          collapsed: false,
-          link: { type: "doc", id: "index" },
           items: [
             "examples/hello-world",
             "examples/http-client-server",
@@ -116,10 +113,12 @@ const sidebars = {
             "examples/multipart-form-data",
             "examples/server-sent-events-in-endpoints",
           ],
-        },
-      ],
+    },
+        
+            
+        ],
     },
   ],
 };
 
-
+module.exports = sidebars;
