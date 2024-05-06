ba103f959f9f8e97c20f49d5cb840b5c67ec0aca
Milad Khajavi <khajavi@gmail.com>
2024-05-06 15:31:14 +0330

    initial work.

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -8,7 +8,7 @@ const sidebars = {
       // main documentation index
       link: { type: "doc", id: "index" },
       items: [
-        "installation", 
+        "installation",
 
         // Reference section
         {
@@ -18,9 +18,9 @@ const sidebars = {
           label: "Reference",
           items: [
             "reference/overview",
-            "reference/server", 
-            "reference/client", 
-            "reference/endpoint", 
+            "reference/server",
+            "reference/client",
+            "reference/endpoint",
 
             // Routing subsection
             {
@@ -28,12 +28,12 @@ const sidebars = {
               label: "Routing",
               items: [
                 "reference/routing/routes",
-                "reference/routing/route_pattern", 
+                "reference/routing/route_pattern",
                 "reference/routing/path_codec",
               ],
             },
-            
-                "reference/handler",
+
+            "reference/handler",
 
             // HTTP Messages subsection
             {
@@ -49,9 +49,9 @@ const sidebars = {
                   type: "category",
                   label: "Headers",
                   items: [
-                    "reference/headers/headers", 
-                    "reference/headers/session/cookies", 
-                    "reference/headers/session/flash", 
+                    "reference/headers/headers",
+                    "reference/headers/session/cookies",
+                    "reference/headers/session/flash",
                   ],
                 },
 
@@ -60,25 +60,23 @@ const sidebars = {
                   type: "category",
                   label: "Message Body",
                   items: [
-                    "reference/body/body", 
+                    "reference/body/body",
                     "reference/body/form",
                     "reference/body/binary_codecs",
-                    "reference/body/template", 
+                    "reference/body/template",
                   ],
                 },
-                
               ],
             },
-
 
             // Aspects subsection
             {
               type: "category",
               label: "Aspects",
               items: [
-                "reference/aop/protocol-stack", 
-                "reference/aop/middleware", 
-                "reference/aop/handler_aspect", 
+                "reference/aop/protocol-stack",
+                "reference/aop/middleware",
+                "reference/aop/handler_aspect",
               ],
             },
 
@@ -87,46 +85,44 @@ const sidebars = {
               type: "category",
               label: "WebSocket",
               items: [
-                "reference/socket/socket", 
-                "reference/socket/websocketframe", 
+                "reference/socket/socket",
+                "reference/socket/websocketframe",
               ],
             },
+            "reference/http-codecs",
           ],
         },
 
         {
-          // Subcategory: Tutorials 
+          // Subcategory: Tutorials
           type: "category",
           label: "Tutorials",
-          items: [
-            "tutorials/testing-http-apps", 
-          ],
+          items: ["tutorials/testing-http-apps"],
         },
-        
+
         // Examples section
         {
           type: "category",
           label: "Examples",
           link: { type: "doc", id: "examples/index" },
           items: [
-            "examples/hello-world", 
-            "examples/http-client-server", 
-            "examples/https-client-server", 
+            "examples/hello-world",
+            "examples/http-client-server",
+            "examples/https-client-server",
             "examples/serving-static-files",
-            "examples/html-templating", 
-            "examples/websocket", 
-            "examples/streaming", 
-            "examples/endpoint", 
-            "examples/middleware-cors-handling", 
-            "examples/authentication", 
+            "examples/html-templating",
+            "examples/websocket",
+            "examples/streaming",
+            "examples/endpoint",
+            "examples/middleware-cors-handling",
+            "examples/authentication",
             "examples/graceful-shutdown",
-            "examples/cli", 
-            "examples/concrete-entity", 
-            "examples/multipart-form-data", 
+            "examples/cli",
+            "examples/concrete-entity",
+            "examples/multipart-form-data",
             "examples/server-sent-events-in-endpoints",
           ],
         },
-
         "faq",
       ],
     },
