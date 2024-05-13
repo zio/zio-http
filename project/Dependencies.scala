1822f1973e3fcd80efa0f87f7fa82eafbd94b4ca
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2024-05-13 17:10:37 +0200

    Fix zio-cli version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val NettyIncubatorVersion         = "0.0.24.Final"
   val ScalaCompactCollectionVersion = "2.11.0"
   val ZioVersion                    = "2.0.22"
-  val ZioCliVersion                 = "0.6.0"
+  val ZioCliVersion                 = "0.5.0"
   val ZioSchemaVersion              = "1.1.1"
   val SttpVersion                   = "3.3.18"
   val ZioConfigVersion              = "4.0.2"
