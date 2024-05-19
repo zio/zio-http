6ca3d7e73e7a2fe464ca14ce28f89bef2a28ec1b
Kyri Petrou <kyri.petrou@outlook.com>
2024-05-19 17:33:28 +0200

    Revert "Downgrade to 2.13.13"
    This reverts commit 8ac9d408230326e4a06392582e538c985f64250e.

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -7,7 +7,7 @@ import sbtcrossproject.CrossPlugin.autoImport.crossProjectPlatform
 
 object BuildHelper extends ScalaSettings {
   val Scala212         = "2.12.19"
-  val Scala213         = "2.13.13"
+  val Scala213         = "2.13.14"
   val Scala3           = "3.3.3"
   val ScoverageVersion = "2.0.12"
   val JmhVersion       = "0.4.7"
