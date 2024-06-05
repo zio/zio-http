4b28337ed02b3658b75b42a4154d69c5d3bb1b27
Kyri Petrou <kyri.petrou@outlook.com>
2024-06-05 19:10:09 +0200

    Don't share the Scope in `ContentTypeSpec`

--- zio-http/jvm/src/test/scala/zio/http/ContentTypeSpec.scala
+++ zio-http/jvm/src/test/scala/zio/http/ContentTypeSpec.scala
@@ -74,6 +74,8 @@ object ContentTypeSpec extends HttpRunnableSpec {
   override def spec = {
     suite("Content-type") {
       serve.as(List(contentSpec))
-    }.provideShared(DynamicServer.live, serverTestLayer, Client.default, Scope.default) @@ withLiveClock
+    }
+      .provideSome[DynamicServer & Server & Client](Scope.default)
+      .provideShared(DynamicServer.live, serverTestLayer, Client.default) @@ withLiveClock
   }
 }
