0afa589a7a7e44882c09aba2c34fcf4a69f1bdb3
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2024-06-21 13:26:37 +0200

    Revert "Ignore non-content codecs when building multipart OpenAPI spec (#2745)"
    This reverts commit c7f0598dc514d90ad0ed9cba42fde44330cf0bab.

--- zio-http/shared/src/main/scala/zio/http/endpoint/openapi/OpenAPIGen.scala
+++ zio-http/shared/src/main/scala/zio/http/endpoint/openapi/OpenAPIGen.scala
@@ -340,20 +340,6 @@ object OpenAPIGen {
                   .nullable(optional(metadata))
                   .description(description(metadata))
                   .annotate(annotations)
-              case (JsonSchema.Object(p, _, r), JsonSchema.Null)                =>
-                JsonSchema
-                  .Object(p, Left(false), r)
-                  .deprecated(deprecated(metadata))
-                  .nullable(optional(metadata))
-                  .description(description(metadata))
-                  .annotate(annotations)
-              case (JsonSchema.Null, JsonSchema.Object(p, _, r))                =>
-                JsonSchema
-                  .Object(p, Left(false), r)
-                  .deprecated(deprecated(metadata))
-                  .nullable(optional(metadata))
-                  .description(description(metadata))
-                  .annotate(annotations)
               case _ => throw new IllegalArgumentException("Multipart content without name.")
             }
 
@@ -812,7 +798,8 @@ object OpenAPIGen {
         (
           statusOrDefault,
           (
-            AtomizedMetaCodecs.flatten(codec),
+            AtomizedMetaCodecs
+              .flatten(codec),
             contentAsJsonSchema(codec, referenceType = referenceType) _,
           ),
         )
