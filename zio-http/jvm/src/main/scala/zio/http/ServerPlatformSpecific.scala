b14e34ff0504eb96224f43ec3cb6b3a7e22aa272
Eyal Farago <efarago@salesforce.com>
2024-06-26 22:18:20 +0300

    reorg_driver_layers: fix server's layer to handle uninitialized filed exception

--- zio-http/jvm/src/main/scala/zio/http/ServerPlatformSpecific.scala
+++ zio-http/jvm/src/main/scala/zio/http/ServerPlatformSpecific.scala
@@ -13,7 +13,7 @@ trait ServerPlatformSpecific {
   val customized: ZLayer[Config & NettyConfig, Throwable, Driver with Server] = {
     implicit val trace: Trace = Trace.empty
 
-    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = base >+> ZLayer.environment[Driver]
+    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = ZLayer.suspend(base) >+> ZLayer.environment[Driver]
     val nettyExtra: ZLayer[Config & NettyConfig, Throwable, Driver with Config] = NettyDriver.customized ++ ZLayer.environment[Config]
     val composed: ZLayer[Config & NettyConfig, Throwable, Server with Driver] = nettyExtra >>> baseExtra
     //NettyDriver.customized >>> base
@@ -22,7 +22,7 @@ trait ServerPlatformSpecific {
 
   val live: ZLayer[Config, Throwable, Server with Driver] = {
     implicit val trace: Trace = Trace.empty
-    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = base >+> ZLayer.environment[Driver]
+    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = ZLayer.suspend(base) >+> ZLayer.environment[Driver]
     val nettyExtra: ZLayer[Config, Throwable, Driver with Config] = NettyDriver.live ++ ZLayer.environment[Config]
     val res: ZLayer[Config, Throwable, Server with Driver] = nettyExtra >>> baseExtra
     res
