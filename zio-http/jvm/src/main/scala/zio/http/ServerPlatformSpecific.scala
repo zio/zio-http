43851677b6d29f1ef82fa83aea897d863b1ad577
Eyal Farago <efarago@salesforce.com>
2024-06-26 22:34:22 +0300

    reorg_driver_layers: fmt

--- zio-http/jvm/src/main/scala/zio/http/ServerPlatformSpecific.scala
+++ zio-http/jvm/src/main/scala/zio/http/ServerPlatformSpecific.scala
@@ -13,18 +13,21 @@ trait ServerPlatformSpecific {
   val customized: ZLayer[Config & NettyConfig, Throwable, Driver with Server] = {
     implicit val trace: Trace = Trace.empty
 
-    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = ZLayer.suspend(base) >+> ZLayer.environment[Driver]
-    val nettyExtra: ZLayer[Config & NettyConfig, Throwable, Driver with Config] = NettyDriver.customized ++ ZLayer.environment[Config]
-    val composed: ZLayer[Config & NettyConfig, Throwable, Server with Driver] = nettyExtra >>> baseExtra
-    //NettyDriver.customized >>> base
+    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver]       =
+      ZLayer.suspend(base) >+> ZLayer.environment[Driver]
+    val nettyExtra: ZLayer[Config & NettyConfig, Throwable, Driver with Config] =
+      NettyDriver.customized ++ ZLayer.environment[Config]
+    val composed: ZLayer[Config & NettyConfig, Throwable, Server with Driver]   = nettyExtra >>> baseExtra
+    // NettyDriver.customized >>> base
     composed
   }
 
   val live: ZLayer[Config, Throwable, Server with Driver] = {
-    implicit val trace: Trace = Trace.empty
-    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = ZLayer.suspend(base) >+> ZLayer.environment[Driver]
+    implicit val trace: Trace                                             = Trace.empty
+    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] =
+      ZLayer.suspend(base) >+> ZLayer.environment[Driver]
     val nettyExtra: ZLayer[Config, Throwable, Driver with Config] = NettyDriver.live ++ ZLayer.environment[Config]
-    val res: ZLayer[Config, Throwable, Server with Driver] = nettyExtra >>> baseExtra
+    val res: ZLayer[Config, Throwable, Server with Driver]        = nettyExtra >>> baseExtra
     res
   }
 
