7b92e346c9ded0ab2f2371dd19fb7825ecdaecaa
Eyal Farago <efarago@salesforce.com>
2024-06-26 21:53:16 +0300

    reorg_driver_layers: modify Server's layer such that they expose Driver as well

--- zio-http/jvm/src/main/scala/zio/http/ServerPlatformSpecific.scala
+++ zio-http/jvm/src/main/scala/zio/http/ServerPlatformSpecific.scala
@@ -10,14 +10,22 @@ trait ServerPlatformSpecific {
 
   private[http] val base: ZLayer[Driver & Config, Throwable, Server]
 
-  val customized: ZLayer[Config & NettyConfig, Throwable, Server] = {
+  val customized: ZLayer[Config & NettyConfig, Throwable, Driver with Server] = {
     implicit val trace: Trace = Trace.empty
-    NettyDriver.customized >>> base
+
+    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = base >+> ZLayer.environment[Driver]
+    val nettyExtra: ZLayer[Config & NettyConfig, Throwable, Driver with Config] = NettyDriver.customized ++ ZLayer.environment[Config]
+    val composed: ZLayer[Config & NettyConfig, Throwable, Server with Driver] = nettyExtra >>> baseExtra
+    //NettyDriver.customized >>> base
+    composed
   }
 
-  val live: ZLayer[Config, Throwable, Server] = {
+  val live: ZLayer[Config, Throwable, Server with Driver] = {
     implicit val trace: Trace = Trace.empty
-    NettyDriver.live >+> base
+    val baseExtra: ZLayer[Driver & Config, Throwable, Server with Driver] = base >+> ZLayer.environment[Driver]
+    val nettyExtra: ZLayer[Config, Throwable, Driver with Config] = NettyDriver.live ++ ZLayer.environment[Config]
+    val res: ZLayer[Config, Throwable, Server with Driver] = nettyExtra >>> baseExtra
+    res
   }
 
 }
