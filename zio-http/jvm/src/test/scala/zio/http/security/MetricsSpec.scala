c9a2f3b98a00690e0180b78fb75c3b37892c1d36
pablf <pablofeme@gmail.com>
2024-08-27 15:40:42 +0200

    change client methods

--- zio-http/jvm/src/test/scala/zio/http/security/MetricsSpec.scala
+++ zio-http/jvm/src/test/scala/zio/http/security/MetricsSpec.scala
@@ -56,7 +56,7 @@ object MetricsSpec extends ZIOHttpSpec {
         mkRequest = mkRequest0(port)
         _             <- ZIO.iterate((0, init))(_._1 < maxReq) { case (n, content) =>
           ZIO.scoped {
-            Client.request(mkRequest(content)).flatMap { req =>
+            Client.streaming(mkRequest(content)).flatMap { req =>
               if (req.status == Status.Ok) ZIO.unit @@ Metric.counter("received ok").tagged("test", name).fromConst(1)
               else ZIO.unit @@ Metric.counter("not received ok").tagged("test", name).fromConst(1)
             }
