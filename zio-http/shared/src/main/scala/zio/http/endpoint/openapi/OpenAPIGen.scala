ca3c32f246139864f7460fee03f35b3887a471db
ffprivacy <ffprivacy@netcourrier.com>
2025-01-21 18:08:20 +0100

    formatting

--- zio-http/shared/src/main/scala/zio/http/endpoint/openapi/OpenAPIGen.scala
+++ zio-http/shared/src/main/scala/zio/http/endpoint/openapi/OpenAPIGen.scala
@@ -19,10 +19,8 @@ import zio.http.codec.HttpCodec.Metadata
 import zio.http.codec._
 import zio.http.endpoint._
 import zio.http.endpoint.openapi.JsonSchema.SchemaStyle
-import zio.http.endpoint.openapi.OpenAPI.{Path, PathItem}
 import zio.http.endpoint.openapi.OpenAPI.SecurityScheme.SecurityRequirement
-import zio.http.endpoint.openapi.OpenAPI.SecurityScheme
-import zio.http.endpoint.openapi.OpenAPI.ReferenceOr
+import zio.http.endpoint.openapi.OpenAPI.{Path, PathItem, ReferenceOr, SecurityScheme}
 
 object OpenAPIGen {
   private val PathWildcard = "pathWildcard"
@@ -701,16 +699,16 @@ object OpenAPIGen {
     }
 
     def operation(endpoint: Endpoint[_, _, _, _, _]): OpenAPI.Operation = {
-      val maybeDoc = Some(pathDoc).filter(!_.isEmpty)
+      val maybeDoc                            = Some(pathDoc).filter(!_.isEmpty)
       val security: List[SecurityRequirement] = endpoint.authType match {
         case AuthType.None => {
           Nil
         }
-        case authType => {
+        case authType      => {
           val securitySchemes = Map(authType.toString() -> List.empty)
           List[SecurityRequirement](
-            SecurityRequirement(securitySchemes = securitySchemes)
-            )
+            SecurityRequirement(securitySchemes = securitySchemes),
+          )
         }
       }
       OpenAPI.Operation(
@@ -920,17 +918,18 @@ object OpenAPIGen {
       requestBodies = ListMap.empty,
       headers = ListMap.empty,
       securitySchemes = endpoint.authType match {
-        case AuthType.None => ListMap.empty
-        case AuthType.Basic | AuthType.Bearer | AuthType.Digest => ListMap(
-          OpenAPI.Key.fromString(endpoint.authType.toString()).get -> ReferenceOr.Or[SecurityScheme.Http](
-            SecurityScheme.Http(
-              scheme = endpoint.authType.toString(),
-              bearerFormat = None,
-              description = Some(Doc.empty)
-            )
+        case AuthType.None                                      => ListMap.empty
+        case AuthType.Basic | AuthType.Bearer | AuthType.Digest =>
+          ListMap(
+            OpenAPI.Key.fromString(endpoint.authType.toString()).get -> ReferenceOr.Or[SecurityScheme.Http](
+              SecurityScheme.Http(
+                scheme = endpoint.authType.toString(),
+                bearerFormat = None,
+                description = Some(Doc.empty),
+              ),
+            ),
           )
-        )
-        case _ => ???
+        case _                                                  => ???
       },
       links = ListMap.empty,
       callbacks = ListMap.empty,
