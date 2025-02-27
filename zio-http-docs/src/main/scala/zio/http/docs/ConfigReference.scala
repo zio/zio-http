5aa97009f8dfacb8cbcc9df91c5b682e145ecb02
Jakub Pavlik <severus@post.cz>
2025-02-27 23:12:15 +0100

    docs: Config reference

---
+++ zio-http-docs/src/main/scala/zio/http/docs/ConfigReference.scala
@@ -0,0 +1,35 @@
+package zio.http.docs
+
+import zio.config.generateDocs
+import zio._
+
+object ConfigReference {
+  private type ObjectWithConfig = Object{def config: Config[Any]}
+
+  private val configs =
+    Seq[ObjectWithConfig](
+      http.netty.NettyConfig,
+      http.netty.NettyConfig.LeakDetectionLevel,
+      http.ConnectionPoolConfig,
+      http.Decompression,
+      http.DnsResolver.Config,
+      http.DnsResolver.ExpireAction,
+      http.Proxy,
+      http.Server.Config,
+      http.URL,
+      // http.ZClient.Config, // TODO: causes stack overflow
+      // http.gen.openapi.Config, // TODO
+    ).map { obj => (
+      obj.getClass.getName.stripSuffix("$").replace("$", "."),
+      obj.config
+    ) }
+
+  def build(): String = {
+    configs.map(item => {
+      val (name, config) = item
+
+      s"# ${name}.config\n" +
+        generateDocs(config).toTable.toGithubFlavouredMarkdown
+    }).mkString("\n")
+  }
+}
