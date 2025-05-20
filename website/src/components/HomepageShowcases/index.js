431257ec5ee4f1fbf6eeda0c9bfbc1e0c98796e6
Milad Khajavi <khajavi@gmail.com>
2025-05-20 11:45:30 +0330

    improve showcases.

--- website/src/components/HomepageShowcases/index.js
+++ website/src/components/HomepageShowcases/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import CodeBlock from '@theme/CodeBlock';
-import styles from './styles.module.css';
+import React from "react";
+import CodeBlock from "@theme/CodeBlock";
+import styles from "./styles.module.css";
 
 export default function HomepageShowcases() {
   return (
     <section className={styles.showcases}>
       <div className="container">
+        <h2 className="text--center">Imperative API</h2>
         <div className="row">
           <div className="col col--6">
-            <h2>Server-side Example</h2>
+            <h3>Server-side Example</h3>
             <CodeBlock language="scala">
-{`import zio._
+              {`import zio._
 import zio.http._
 
 object GreetingServer extends ZIOAppDefault {
@@ -26,10 +27,13 @@ object GreetingServer extends ZIOAppDefault {
             </CodeBlock>
           </div>
           <div className="col col--6">
-            <h2>Client-side Example</h2>
+            <h3>Client-side Example</h3>
             <CodeBlock language="scala">
-{
-`object ClientServerExample extends ZIOAppDefault {
+              {`import zio._
+import zio.http._
+import zio.http.codec.TextBinaryCodec.fromSchema
+
+object ClientServerExample extends ZIOAppDefault {
   val clientApp: ZIO[Client, Throwable, Unit] =
     for {
       url <- ZIO.fromEither(URL.decode("http://localhost:8080/greet"))
@@ -44,10 +48,28 @@ object GreetingServer extends ZIOAppDefault {
       _ <- ZIO.debug("Received response: " +  body)
     }  yield ()
   val run = clientApp.provide(Client.default)
-}`
-}
+}`}
             </CodeBlock>
           </div>
+        </div>
+
+        <div className="col col--8 col--offset-2">
+          <h2 className="text--center">Declarative API</h2>
+          <CodeBlock language="scala">
+            {`
+// Endpoint Definition
+val endpoint =
+  Endpoint(GET / "greet" ?? Doc.p("Route for querying books"))
+    .query(
+      HttpCodec.query[String]("name") ?? Doc.p("Name of the person to greet"),
+    )
+    .out[String]
+
+// Endpoint Implementation
+val greetRoute: Route[Any, Nothing] =
+  endpoint.implementHandler(handler((name: String) => s"Hello, $name!"))
+`}
+          </CodeBlock>
         </div>
       </div>
     </section>
