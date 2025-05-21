e2b6da76d8889d3a18241d12d4d2271b1f9c80cf
Milad Khajavi <khajavi@gmail.com>
2025-05-21 21:59:25 +0330

    icons and better header rendering.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -60,7 +60,7 @@ const config = {
         title: 'ZIO HTTP',
         logo: {
           alt: 'ZIO HTTP Logo',
-          src: 'img/logo.svg',
+          src: 'img/ZIO.png',
         },
         items: [
           {
