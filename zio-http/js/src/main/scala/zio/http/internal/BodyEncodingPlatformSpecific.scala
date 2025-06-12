da32e6918e3551abecf4182aaf70fa68e382c7f6
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2025-06-12 14:37:59 +0200

    New default implementation for text-based bodies (#3529)

--- zio-http/js/src/main/scala/zio/http/internal/BodyEncodingPlatformSpecific.scala
+++
@@ -1,8 +0,0 @@
-package zio.http.internal
-
-import scala.annotation.nowarn
-
-@nowarn("msg=dead code")
-trait BodyEncodingPlatformSpecific {
-  val default: BodyEncoding = throw new NotImplementedError("No version implemented for Scala.js yet.")
-}
