f288460d58e6535bc24dd32f9b4810425a788aa1
Jisoo Park <guersam@moatai.io>
2025-07-17 23:24:23 +0900

    Revert "Add `JsonSchema.fromZSchemaInlineDeepOrFail` (#3168) (#3559)" (#3584)

--- zio-http/shared/src/test/scala/zio/http/endpoint/openapi/OpenAPISpec.scala
+++ zio-http/shared/src/test/scala/zio/http/endpoint/openapi/OpenAPISpec.scala
@@ -1,10 +1,16 @@
 package zio.http.endpoint.openapi
 
+import java.util.UUID
+
 import scala.collection.immutable.ListMap
 
 import zio.json.ast.Json
 import zio.test._
 
+import zio.schema.Schema
+
+import zio.http.endpoint.openapi.JsonSchema.SchemaStyle
+import zio.http.endpoint.openapi.OpenAPI.ReferenceOr
 import zio.http.endpoint.openapi.OpenAPI.SecurityScheme._
 
 object OpenAPISpec extends ZIOSpecDefault {
@@ -56,6 +62,59 @@ object OpenAPISpec extends ZIOSpecDefault {
                        |  ]
                        |}""".stripMargin
 
+      assertTrue(toJsonAst(json) == toJsonAst(expected))
+    },
+    test("JsonSchema.fromZSchemaMulti correctly handles Map schema with List as Value") {
+      val schema           = Schema.map[String, List[String]]
+      val sch: JsonSchemas = JsonSchema.fromZSchemaMulti(schema, SchemaStyle.Reference)
+
+      val isSchemaProperlyGenerated = if (sch.root.isCollection) sch.root match {
+        case JsonSchema.Object(_, additionalProperties, _) =>
+          additionalProperties match {
+            case Right(JsonSchema.ArrayType(items, _, _)) =>
+              items.exists(_.isInstanceOf[JsonSchema.String])
+            case _                                        => false
+          }
+        case _                                             => false
+      }
+      else false
+      assertTrue(isSchemaProperlyGenerated)
+    },
+    test("JsonSchema.fromZSchema correctly handles Map with non-simple string keys") {
+      val schema   = Schema.map[UUID, String]
+      val js       = JsonSchema.fromZSchema(schema)
+      val oapi     = OpenAPI.empty.copy(
+        components =
+          Some(OpenAPI.Components(schemas = ListMap(OpenAPI.Key.fromString("IdToName").get -> ReferenceOr.Or(js)))),
+      )
+      val json     = oapi.toJsonPretty
+      val expected = """{
+                       |  "openapi" : "3.1.0",
+                       |  "info" : {
+                       |    "title" : "",
+                       |    "version" : ""
+                       |  },
+                       |  "components" : {
+                       |    "schemas" : {
+                       |      "IdToName" :
+                       |        {
+                       |        "type" :
+                       |          "object",
+                       |        "properties" : {},
+                       |        "additionalProperties" :
+                       |          {
+                       |          "type" :
+                       |            "string",
+                       |          "x-string-key-schema" : {
+                       |            "type" :
+                       |              "string",
+                       |            "format" : "uuid"
+                       |          }
+                       |        }
+                       |      }
+                       |    }
+                       |  }
+                       |}""".stripMargin
       assertTrue(toJsonAst(json) == toJsonAst(expected))
     },
   )
