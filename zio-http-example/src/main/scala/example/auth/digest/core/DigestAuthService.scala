8fb175b83008ea89f73187096b21a365b9f3e64b
Milad Khajavi <khajavi@gmail.com>
2025-07-23 19:09:34 +0330

    wip.

--- zio-http-example/src/main/scala/example/auth/digest/core/DigestAuthService.scala
+++ zio-http-example/src/main/scala/example/auth/digest/core/DigestAuthService.scala
@@ -34,20 +34,20 @@ trait DigestAuthService {
 
 object DigestAuthService {
   val live: ZLayer[HashService & NonceService, Nothing, DigestAuthService] =
-    ZLayer.fromFunction((hashService: HashService, nonceService: NonceService) =>
-      new DigestAuthService {
-        def createChallenge(
-          realm: String,
-          qop: List[QualityOfProtection],
-        ): UIO[List[DigestChallenge]] =
-          for {
-            timestamp <- Clock.currentTime(TimeUnit.MILLISECONDS)
-            nonce     <- nonceService.generateNonce(timestamp)
-            bytes     <- Random.nextString(32).map(_.getBytes(Charsets.Utf8))
-            opaque    <- ZIO.succeed(Base64.getEncoder.encodeToString(bytes))
-          } yield {
-            List(MD5, MD5_SESS, SHA256, SHA256_SESS, SHA512, SHA512_SESS).map {
-              algorithm =>
+    ZLayer.fromFunction(in =
+      (hashService: HashService, nonceService: NonceService) =>
+        new DigestAuthService {
+          def createChallenge(
+            realm: String,
+            qop: List[QualityOfProtection],
+          ): UIO[List[DigestChallenge]] =
+            for {
+              timestamp <- Clock.currentTime(TimeUnit.MILLISECONDS)
+              nonce     <- nonceService.generateNonce(timestamp)
+              bytes     <- Random.nextString(32).map(_.getBytes(Charsets.Utf8))
+              opaque    <- ZIO.succeed(Base64.getEncoder.encodeToString(bytes))
+            } yield {
+              List(MD5, MD5_SESS, SHA256, SHA256_SESS, SHA512, SHA512_SESS).map { algorithm =>
                 DigestChallenge(
                   realm = realm,
                   nonce = nonce,
@@ -55,132 +55,128 @@ object DigestAuthService {
                   algorithm = algorithm,
                   qop = qop,
                 )
+              }
+
             }
 
+          private def calculateA1(
+            username: String,
+            realm: String,
+            password: Secret,
+            algorithm: HashAlgorithm,
+            nonce: String,
+            cnonce: String,
+          ): UIO[String] = {
+            val a1 = s"$username:$realm:${password.stringValue}"
+            algorithm match {
+              // if session algorithm
+              case MD5_SESS | SHA256_SESS | SHA512_SESS =>
+                hashService
+                  .hash(a1, algorithm)
+                  .map(ha1 => s"$ha1:$nonce:$cnonce")
+              // if regular algorithm
+              case _                                    =>
+                ZIO.succeed(a1)
+            }
           }
 
-        private def calculateA1(
-          username: String,
-          realm: String,
-          password: Secret,
-          algorithm: HashAlgorithm,
-          nonce: String,
-          cnonce: String,
-        ): UIO[String] = {
-          val a1 = s"$username:$realm:${password.stringValue}"
-          algorithm match {
-            // if session algorithm
-            case MD5_SESS | SHA256_SESS | SHA512_SESS =>
-              hashService
-                .hash(a1, algorithm)
-                .map(ha1 => s"$ha1:$nonce:$cnonce")
-            // if regular algorithm
-            case _                         =>
-              ZIO.succeed(a1)
-          }
-        }
+          private def calculateA2(
+            method: String,
+            uri: URI,
+            qop: Option[QualityOfProtection],
+            entityBody: Option[String],
+            algorithm: HashAlgorithm,
+          ): UIO[String] =
+            qop match {
+              case Some(QualityOfProtection.AuthInt) =>
+                entityBody match {
+                  case Some(body) =>
+                    for {
+                      bodyHash <- hashService.hash(body, algorithm)
+                    } yield s"$method:${uri.getPath}:$bodyHash"
+                  case None       =>
+                    ZIO.succeed(s"$method:${uri.getPath}:")
+                }
+              case _                                 =>
+                ZIO.succeed(s"$method:${uri.getPath}")
+            }
 
-        private def calculateA2(
-          method: String,
-          uri: URI,
-          qop: Option[QualityOfProtection],
-          entityBody: Option[String],
-          algorithm: HashAlgorithm,
-        ): UIO[String] =
-          qop match {
-            case Some(QualityOfProtection.AuthInt) =>
-              entityBody match {
-                case Some(body) =>
-                  for {
-                    bodyHash <- hashService.hash(body, algorithm)
-                  } yield s"$method:${uri.getPath}:$bodyHash"
-                case None       =>
-                  ZIO.succeed(s"$method:${uri.getPath}:")
-              }
-            case _                                 =>
-              ZIO.succeed(s"$method:${uri.getPath}")
+          private def calculateResponse(
+            ha1: String,
+            nonce: String,
+            nc: Option[String],
+            cnonce: Option[String],
+            qop: Option[QualityOfProtection],
+            ha2: String,
+            algorithm: HashAlgorithm,
+          ): UIO[String] = {
+            val responseData = qop match {
+              case Some(qop) =>
+                s"$nonce:${nc.get}:${cnonce.get}:${qop.name}:$ha2"
+              case None      =>
+                s"$nonce:$ha2"
+            }
+            hashService.keyedHash(responseData, algorithm, ha1)
           }
 
-        private def calculateResponse(
-          ha1: String,
-          nonce: String,
-          nc: Option[String],
-          cnonce: Option[String],
-          qop: Option[QualityOfProtection],
-          ha2: String,
-          algorithm: HashAlgorithm,
-        ): UIO[String] = {
-          val responseData = qop match {
-            case Some(_) =>
-              val ncValue     = nc.getOrElse("00000001")
-              val cnonceValue = cnonce.getOrElse("")
-              val qopValue    = qop.map(_.name).getOrElse("")
-              s"$nonce:$ncValue:$cnonceValue:$qopValue:$ha2"
-            case None    =>
-              s"$nonce:$ha2"
+          def validateCredentials(response: String)(
+            username: String,
+            password: Secret,
+            method: Method,
+            uri: URI,
+            realm: String,
+            nonce: String,
+            algorithm: HashAlgorithm = MD5,
+            cnonce: Option[String] = None,
+            opaque: Option[String] = None,
+            qop: Option[QualityOfProtection] = None,
+            nc: Option[String] = None,
+            userhash: Boolean = false,
+            body: Option[String] = None,
+          ): ZIO[Any, DigestAuthError, Boolean] = {
+
+            for {
+              // Validate nonce
+              nonceValid <- nonceService.validateNonce(nonce, Duration.fromSeconds(60))
+              _          <- ZIO.when(!nonceValid)(ZIO.fail(NonceExpired(nonce)))
+
+              // Check for replay attacks
+              isUsed <- nonceService.isNonceUsed(nonce, nc.get)
+              _      <- ZIO.when(isUsed)(ZIO.fail(ReplayAttack(nonce, nc.get)))
+
+              // Calculate expected response
+              a1  <- calculateA1(
+                username,
+                realm,
+                password,
+                algorithm,
+                nonce,
+                cnonce.getOrElse(""),
+              )
+              ha1 <- hashService.hash(a1, algorithm)
+
+              a2  <- calculateA2(method.name, uri, qop, body, algorithm)
+              ha2 <- hashService.hash(a2, algorithm)
+
+              expectedResponse <- calculateResponse(
+                ha1 = ha1,
+                nonce = nonce,
+                nc = nc,
+                cnonce = cnonce,
+                qop = qop,
+                ha2 = ha2,
+                algorithm = algorithm,
+              )
+
+              // Mark nonce as used
+              _ <- nonceService.markNonceUsed(nonce, nc.get)
+
+              // Compare responses
+              isValid = expectedResponse.equalsIgnoreCase(response)
+
+            } yield isValid
           }
-          hashService.keyedHash(responseData, algorithm, ha1)
-        }
-
-        def validateCredentials(response: String)(
-          username: String,
-          password: Secret,
-          method: Method,
-          uri: URI,
-          realm: String,
-          nonce: String,
-          algorithm: HashAlgorithm = MD5,
-          cnonce: Option[String] = None,
-          opaque: Option[String] = None,
-          qop: Option[QualityOfProtection] = None,
-          nc: Option[String] = None,
-          userhash: Boolean = false,
-          body: Option[String] = None,
-        ): ZIO[Any, DigestAuthError, Boolean] = {
-
-          for {
-            // Validate nonce
-            nonceValid <- nonceService.validateNonce(nonce, Duration.fromSeconds(60))
-            _          <- ZIO.when(!nonceValid)(ZIO.fail(NonceExpired(nonce)))
-
-            // Check for replay attacks
-            ncValue = nc.getOrElse("00000001")
-            isUsed <- nonceService.isNonceUsed(nonce, ncValue)
-            _      <- ZIO.when(isUsed)(ZIO.fail(ReplayAttack(nonce, ncValue)))
-
-            // Calculate expected response
-            a1  <- calculateA1(
-              username,
-              realm,
-              password,
-              algorithm,
-              nonce,
-              cnonce.getOrElse(""),
-            )
-            ha1 <- hashService.hash(a1, algorithm)
-
-            a2  <- calculateA2(method.name, uri, qop, body, algorithm)
-            ha2 <- hashService.hash(a2, algorithm)
-
-            expectedResponse <- calculateResponse(
-              ha1,
-              nonce,
-              nc,
-              cnonce,
-              qop,
-              ha2,
-              algorithm,
-            )
-
-            // Mark nonce as used
-            _ <- nonceService.markNonceUsed(nonce, ncValue)
-
-            // Compare responses
-            isValid = expectedResponse.equalsIgnoreCase(response)
-
-          } yield isValid
-        }
-      },
+        },
     )
 }
 
