9099f2ae8dcef702ef1e7dc0f6455e94d0404f66
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2025-09-01 16:30:30 +0200

    Add another version of forward headers middleware (#3668)

--- zio-http/jvm/src/test/scala/zio/http/ForwardHeaderSpec.scala
+++ zio-http/jvm/src/test/scala/zio/http/ForwardHeaderSpec.scala
@@ -1,12 +1,14 @@
 package zio.http
 
+import scala.util.chaining.scalaUtilChainingOps
+
 import zio._
 import zio.test._
 
 object ForwardHeaderSpec extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment with Scope, Any] =
     suite("ForwardHeaderSpec")(
-      test("should forward headers") {
+      test("forward headers by header type") {
         val routes = Routes(
           Method.GET / "get"   -> handler((_: Request) =>
             for {
@@ -25,6 +27,27 @@ object ForwardHeaderSpec extends ZIOSpecDefault {
           )
         } yield assertTrue(response.headers(Header.Accept).contains(Header.Accept(MediaType.application.json)))
       },
-    ).provide(Client.default, Server.default) @@ TestAspect.withLiveClock
+      test("forward by mapping headers") {
+        val routes = Routes(
+          Method.GET / "get2"   -> handler((_: Request) =>
+            for {
+              client   <- ZIO.service[Client]
+              response <- (client @@ ZClientAspect.forwardHeaders)
+                .batched(Request.post(url"http://localhost:8080/post", Body.empty))
+            } yield response,
+          ),
+          Method.POST / "post2" -> handler((req: Request) => Response.ok.addHeader(req.header(Header.Accept).get)),
+        ).sandbox @@ Middleware.forwardHeaders((headers: Headers) =>
+          Headers.fromIterable(headers.filter(_.headerName == Header.Accept.name)),
+        )
+
+        for {
+          _        <- Server.installRoutes(routes)
+          response <- Client.batched(
+            Request.get(url"http://localhost:8080/get2").addHeader(Header.Accept(MediaType.application.json)),
+          )
+        } yield assertTrue(response.headers(Header.Accept).contains(Header.Accept(MediaType.application.json)))
+      },
+    ).provideShared(Client.default, Server.default) @@ TestAspect.withLiveClock
 
 }
