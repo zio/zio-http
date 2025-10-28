05dcbe1ff8b5613484b7bce8060df7a01db082c1
Milad Khajavi <khajavi@gmail.com>
2025-10-28 13:35:38 +0330

    fix docusaurus edit url.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zio/zio-http/tree/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
