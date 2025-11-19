83b533ca98b8a9332d9ed8a90d15736cdccccd37
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2025-11-19 17:32:59 +0100

    CDN script tags and site template for datastar #3794

--- zio-http-datastar-sdk/src/main/scala/zio/http/datastar/package.scala
+++ zio-http-datastar-sdk/src/main/scala/zio/http/datastar/package.scala
@@ -6,7 +6,7 @@ import zio.stream._
 
 package object datastar extends DatastarPackageBase {
 
-  override private[datastar] val prefix: String = "data"
-
+  override private[datastar] val prefix: String     = "data"
+  override private[datastar] val scriptName: String = "datastar.js"
   // All definitions must be inside DatastarPackageBase
 }
