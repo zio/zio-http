489acca6ac3d82f77cef279605d6ee7c505ce87d
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-02-26 19:29:31 +0100

    feat: Add Route#handleErrorRequestZIO for symmetry (#3159)

--- zio-http/shared/src/main/scala/zio/http/Route.scala
+++ zio-http/shared/src/main/scala/zio/http/Route.scala
@@ -242,6 +242,25 @@ sealed trait Route[-Env, +Err] { self =>
     )
 
   /**
+   * Handles all typed errors in the route by converting them into a ZIO effect
+   * that produces a response, taking into account the request that caused the
+   * error. This method can be used to convert a route that does not handle its
+   * errors into one that does handle its errors.
+   *
+   * If the underlying handler uses the error channel to send responses, this
+   * method will not pass the responses to the provided function.
+   */
+  final def handleErrorRequestZIO[Env1 <: Env](
+    f: (Err, Request) => ZIO[Env1, Nothing, Response],
+  )(implicit trace: Trace): Route[Env1, Nothing] =
+    self.handleErrorRequestCauseZIO { (request, cause) =>
+      cause.failureOrCause match {
+        case Left(err)    => f(err, request)
+        case Right(cause) => ZIO.refailCause(cause)
+      }
+    }
+
+  /**
    * Handles all typed errors, as well as all non-recoverable errors, by
    * converting them into responses, taking into account the request that caused
    * the error. This method can be used to convert a route that does not handle
