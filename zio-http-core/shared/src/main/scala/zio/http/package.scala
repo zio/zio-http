f6d571fb542778eb836134cc58b1d44a79087faf
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-06-27 21:23:57 +0200

    refactor(core): replace v3 core APIs with v4 handler model

--- zio-http-core/shared/src/main/scala/zio/http/package.scala
+++ zio-http-core/shared/src/main/scala/zio/http/package.scala
@@ -16,38 +16,16 @@
 
 package zio
 
-import java.util.UUID
-
-import zio.http.codec.{PathCodec, SegmentCodec}
-
-package object http extends UrlInterpolator with MdInterpolator {
+package object http {
 
   /**
    * A smart constructor that attempts to construct a handler from the specified
    * value. If you have difficulty using this function, then please use the
    * constructors on [[zio.http.Handler]] directly.
    */
-  def handler[H](handler: => H)(implicit h: ToHandler[H]): Handler[h.Env, h.Err, h.In, h.Out] =
-    h.toHandler(handler)
+  def handler[H](handler: => H)(implicit h: ToHandler[H]): Handler[h.Ctx, h.Vars] =
+    HandlerMacros.handler(handler)
 
-  def handlerTODO(message: String): Handler[Any, Nothing, Any, Nothing] =
-    handler(ZIO.dieMessage(message))
-
-  def withContext[C](fn: => C)(implicit c: WithContext[C]): ZIO[c.Env, c.Err, c.Out] =
-    c.toZIO(fn)
-
-  def boolean(name: String): SegmentCodec[Boolean]         = SegmentCodec.bool(name)
-  def int(name: String): SegmentCodec[Int]                 = SegmentCodec.int(name)
-  def long(name: String): SegmentCodec[Long]               = SegmentCodec.long(name)
-  def string(name: String): SegmentCodec[String]           = SegmentCodec.string(name)
-  val trailing: SegmentCodec[Path]                         = SegmentCodec.trailing
-  def uuid(name: String): SegmentCodec[UUID]               = SegmentCodec.uuid(name)
-  def anyOf(name: String, names: String*): PathCodec[Unit] =
-    if (names.isEmpty) PathCodec.literal(name)
-    else names.foldLeft(PathCodec.literal(name))((acc, n) => acc.orElse(PathCodec.literal(n)))
-
-  val Root: PathCodec[Unit] = PathCodec.empty
-
-  type RequestHandler[-R, +Err] = Handler[R, Err, Request, Response]
+  type RequestHandler[-Ctx, -Vars] = Handler[Ctx, Vars]
 
 }
